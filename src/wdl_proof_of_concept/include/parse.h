e772ea841e073ddf74db64f2367f182fe3f1b4d2
Victor <jesusaa@uchicago.edu>
2019-05-03 15:57:42 -0500

    began drafting header file for parse module

---
+++ src/wdl_proof_of_concept/include/parse.h
@@ -0,0 +1,11 @@
+/* Header for parsing functions and other utilities for the proof of concept
+ */
+
+
+
+
+void print_room(obj_t *obj_t);
+
+void print_game(obj_t *obj_t);
+
+void print_item(obj_t *obj_t);
