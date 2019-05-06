ad543170762e647eb61604653994980ad01f7de2
Christian Bjerre-Fernandes <chrisfernandes18@cs.uchicago.edu>
2019-05-06 11:04:59 -0500

    renamded files and deleted sample.txt, load.c, and write.c

---
+++ sample-code/load.c
@@ -0,0 +1,103 @@
+#include <msgpack.h>
+#include <stdio.h>
+#include <string.h>
+#include "checkpointing.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2){
+    fprintf(stderr, "There was no file name given\n");
+    exit(1);
+  }
+  
+  int buffsize = 2048;
+
+  char *inbuffer = malloc(buffsize); // space for buffer size
+
+  FILE *reading = fopen(argv[1], "r"); //reading from given file in stdin
+  size_t off = 0;
+  size_t read = 0;
+  msgpack_unpacked unp;
+  msgpack_unpacked_init(&unp); // initializing the unpacking
+
+  FILE *write = fopen("loaded.txt", "w");
+  /* prints out the data from the file */
+  do {    
+    read = fread(inbuffer, sizeof(char), buffsize - off, reading);
+    off = 0;
+    while(msgpack_unpack_next(&unp, inbuffer, read, &off)) {
+      msgpack_object_print(write, unp.data); // writes to new write file
+      fputc('\n', write);
+    }
+    memcpy(inbuffer, &(inbuffer[off]), read-off);
+    off = read - off;
+  } while(read != 0);
+
+ 
+  free(inbuffer);
+    
+  fclose(reading); // closes the file that was read
+  msgpack_unpacked_destroy(&unp); // frees everything
+  fclose(write); // closes the human readable deserialized code
+
+  game_t *ex = malloc(sizeof(game_t));
+  ex->col = 0;
+  ex->row = 0;
+  ex->curr = 0;
+  int col = 0, row = 0;
+  FILE *load = fopen("loaded.txt", "r"); // opens human-readable deserialized code
+  fscanf(load,"[%d, %d,", &col, &row); // gets column and row
+
+  ex->col = col;
+  ex->row = row;
+
+  int **arr = malloc(sizeof(int*)*col);
+  int temp = 0;
+  for(int i=0; i<col; ++i){
+    arr[i] = malloc(sizeof(int)*row);
+    for (int j=0; j<row; ++j){
+      fscanf(load," %d,", &temp); // gets every value in array
+      arr[i][j] = temp;
+    }
+  }
+
+  ex->arr = arr;
+
+  int strlength = 0;
+  fscanf(load, " %d,", &strlength);
+  char *str = malloc(strlength*2);
+  char *pattern = " \"%[^\"]\","; 
+  fscanf(load, pattern, str); // gets the string
+
+  ex->desc = str;
+
+  int player = 0;
+  fscanf(load," %d]", &player); // gets the current player
+
+  ex->curr = player; 
+
+  /* prints out game struct and values */
+  printf("Game Struct Contains\n");
+  printf("column size: %d\n", ex->col);
+  printf("row size: %d\n", ex->row);
+  printf("array values: ");
+  for(int i=0; i<ex->col; ++i)
+    for(int j=0; j<ex->row; ++j){
+      temp = ex->arr[i][j];
+      printf("%d ", temp);
+    }
+  printf("\n");
+  printf("description: %s\n", ex->desc);
+  printf("current player: %s\n", ex->curr==0?"BLACK":"WHITE");
+
+  /* frees all the loaded structs */
+  fclose(load);
+  free(str);
+  for(int i=0; i<ex->col; ++i)
+    free(arr[i]);
+  free(arr);
+  free(ex);
+  remove("loaded.txt");
+  
+  return 0;
+}
--- sample-code/save.c
+++ sample-code/save.c
@@ -23,11 +23,13 @@ int main(int argc, char *argv[])
     }
   }
   
-  int size =  25; // size of 2D Array
+  int col =  5; // size of col array
+  int row =  5; // size of row array
 
   /* allocating room for game struct and filling out fields */
   game_t *ex = malloc(sizeof(game_t));
-  ex->size = size;
+  ex->col = col;
+  ex->row = row;
   ex->arr = arr;
   ex->desc = "Black Moved Somewhere";
   ex->curr = WHITE;
@@ -41,9 +43,10 @@ int main(int argc, char *argv[])
   msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
 
   
-  msgpack_pack_array(&pk, ex->size + 3); // determins size of pack array 
+  msgpack_pack_array(&pk, (ex->col*ex->row) + 5); // determines size of pack array 
 
-  msgpack_pack_int(&pk, ex->size); // packs the size of array first
+  msgpack_pack_int(&pk, ex->col); // packs the col size of array first
+  msgpack_pack_int(&pk, ex->row); // packs the row size of array first
 
   /* packs the 2D array into the pack array */
   for(int i=0; i<5; ++i)
@@ -52,10 +55,16 @@ int main(int argc, char *argv[])
 
   
   int strlength = strlen(ex->desc); // length of the string description
+  msgpack_pack_int(&pk, strlength); // packs length of string
   msgpack_pack_str(&pk, strlength); // packs room for string
   msgpack_pack_str_body(&pk, ex->desc, strlength); // packs the string in
-
-  msgpack_pack_int(&pk, ex->curr); // packs the current turn
+  int player = 0;
+  if (ex->curr == 1){
+    player = 1;
+  } else {
+    player = 0;
+  }
+  msgpack_pack_int(&pk, player); // packs the current turn
  
   fwrite(sbuf.data, sbuf.size, 1, save); // writes the packed array into the file
   
