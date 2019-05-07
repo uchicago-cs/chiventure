e176e4b845f3c12ea2efc73dad7bdbaceebdb8cf
Melinda Tang <49291150+MTUC08@users.noreply.github.com>
2019-05-06 20:05:28 -0500

    Implemented recommended changes by group members
    Changed the description of our recommended map to using a 2D array of ints. This would allow us to easily move in the four cardinal directions by assuming that either north or south and either east or west is +1/-1.

--- src/action_management/include/structs.h
+++ src/action_management/include/structs.h
@@ -6,7 +6,6 @@
 
 /* A NPC struct containing:
  * - dialogue: a line of dialogue NPC will say when spoken to 
- *
  */
 struct npc_struct {
     char *dialogue; 
@@ -16,21 +15,18 @@ struct npc_struct {
 /*  A room struct containing:
  * - NPCs: linked list of pointers to NPC_structs
  * - objects: linked list of pointers to object_structs
- * - overall_map: a map of the world as an array ? 
- * (not sure how this would be implemented)
- *
+ * - overall_map: a 2D array that would allow us to use x y coordinates
  */
 struct room_struct {
     list_t *npcs;
     list_t *objects;
-    int *overall_map;
+    int **overall_map;
 };
 
 
 /* An object struct containing:
  * - allowed_actions: an integer array that contains enumerated "actions" 
  * - num_allowed_actions: the number of elements in the allowed_actions array
- *
  */
 struct object_struct {
     int *allowed_actions;
