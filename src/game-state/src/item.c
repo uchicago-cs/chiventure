90aa9a79d41763ac14fa8d8b1591673647b27668
lhsi <lhsi@uchicago.edu>
2019-05-10 16:19:03 -0500

    wrote item funcs new, init, and various get functions #52

--- src/game-state/src/item.c
+++ src/game-state/src/item.c
@@ -2,59 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h.h>
 #include "item.h"
+#include <assert.h>
 
-item_t *item_new();
+item_t *item_new(char *item_id, char *short_desc, char *long_desc)
+{
+  item_t *new_item;
+  int check;
 
-int add_item_to_hash(all_items_t all_items, int item_id, item_t *item) {
-    item_t *s;
-    HASH_FIND_INT(all_items, &item_id, s);
-    if (s != NULL) {
-        /* WARNING */
-        /* SHOULD BE ABLE TO SUPPORT STACKING MULTIPLE OBJECTS */
-        printf("FATAL: item_id already used!\n");
-        exit(0);
-    }
-    HASH_ADD_INT(all_items, item_id, s);
-    return 1;
+  new_item = malloc(sizeof(item_t));
+  new_item->item_id = malloc(10*sizeof(char)); // tentative size allocation
+  new_item->short_desc = malloc(50*sizeof(char));
+  new_item->long_desc = malloc(300*sizeof(char));
+
+  if(new_item == NULL) {
+      error("Could not allocate memory!");
+      return NULL;
+  }
+
+  check = item_init(new_item, short_desc, long_desc);
+
+  if(check != SUCCESS) {
+      error("Could not initialize item!");
+      return NULL;
+  }
+
+  return new_item;
+
 }
 
-int item_init(char *item_id, char *short_desc, char *long_desc)
+int item_init(item_t *new_item, char *item_id, char *short_desc, char *long_desc)
 {
 
-    /* TO DO */
+  assert(new_item != NULL);
+  new_item->item_id = item_id;
+  new_item->short_desc = short_desc;
+  new_item->long_desc = long_desc;
 
-    return 0;
+  return SUCCESS;
 }
 
 /* the following functions retrieve specific information from desired object
 in anticipation of supporting player demands */
-/* will add function basics to ensure compilation */
 
-
-char *get_id(item_t obj)
+char *get_id(item_t *item)
 {
+  char *my_item_id = item->item_id;
 
-    /* TO DO */
-
-    return NULL;
+  return my_item_id;
 }
 
 
-char *get_short_desc(item_t obj)
+char *get_short_desc(item_t *item)
 {
+  char *item_shortd = item->short_desc;
 
-    /* TO DO */
-
-    return NULL;
+  return item_shortd;
 }
 
 
-char *get_long_desc(object_t obj)
+char *get_long_desc(item_t *item)
 {
+  char *item_longd = item->long_desc;
 
-    /* TO DO */
-
-    return NULL;
+  return item_longd;
 }
 
 
@@ -73,4 +83,33 @@ int take_object(object_t obj)
     /* TO DO */
 
     return 0;
+}
+
+
+/* adding item to room inventory hash */
+int add_item_to_room(all_items_t all_items, int item_id, item_t *item) {
+    item_t *s;
+    HASH_FIND_STR(all_items, &item_id, s);
+    if (s != NULL) {
+        /* WARNING */
+        /* SHOULD BE ABLE TO SUPPORT STACKING MULTIPLE OBJECTS */
+        printf("FATAL: item_id already used!\n");
+        exit(0);
+    }
+    HASH_ADD_INT(all_items, item_id, s);
+    return 1;
+}
+
+
+/* adding item attributes to item attr hash, in progress */
+int add_attr_to_item(item_t *item, char *attribute, attribute_value_t item_attr) {
+    item_t *s;
+    attribute_table_t all_attrs = item->attributes;
+    HASH_FIND_STR(all_attrs, attribute, s);
+    if (s != NULL) {
+        printf("This attribute is already present!\n");
+        exit(1);
+    }
+    HASH_ADD_INT(all_attrs, attribute, s);
+    return 1;
 }
