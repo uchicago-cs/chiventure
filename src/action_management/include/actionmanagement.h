1307699823023443f864f0ba3e3d8be228abd273
yoonsjeong <yoonsjeong@uchicago.edu>
2019-05-12 16:45:44 -0500

    fixed changes borja requested, including creating action KINDS as enums, making get functions, removing functions such as act_open and act_close

--- src/action_management/include/actionmanagement.h
+++ src/action_management/include/actionmanagement.h
@@ -6,13 +6,15 @@
 
 
 /* An enumeration of all supported actions.
- * TYPE 1 ACTIONS - ACTION <item>
- * TYPE 2 ACTIONS - ACTION <direction>
- * TYPE 3 ACTIONS - ACTION <npc>
- * TYPE 4 ACTIONS - ACTION <item> <npc> */
+ * KIND 1 ACTIONS - ACTION <item>
+ * KIND 2 ACTIONS - ACTION <direction>
+ * KIND 3 ACTIONS - ACTION <npc>
+ * KIND 4 ACTIONS - ACTION <item> <npc> 
+ * KIND 5 ACTIONS - ACTION <item> <item> 
+ */
 enum actions {
 
-/* TYPE 1 ACTIONS */
+/* KIND 1 ACTIONS */
 	OPEN,
 	CLOSE,
 	PUSH,
@@ -24,24 +26,28 @@ enum actions {
 	DROP,
 	CONSUME, // synonyms: "use" "drink" "eat"
 
-/* TYPE 2 ACTIONS */
-	GO, 	 // synonyms: "walk" "enter"
+/* KIND 2 ACTIONS */
+	GO, 	 // synonyms: "walk"
 
-/* TYPE 3 ACTIONS */
+/* KIND 3 ACTIONS */
 	TALK_TO,
 
-/* TYPE 4 ACTIONS */
+/* KIND 4 ACTIONS */
 	GIVE_TO,
-	USE_ON
+
+/* KIND 5 ACTIONS */
+    USE_ON
 };
 
-/* Outlines the three different objects that can be action'd upon */
-enum object_type {
-	ITEM_OBJ, 	   // e.g. door, potion
-	DIRECTION_OBJ, // e.g. north, east
-	NPC_OBJ 	   // e.g. Borja, wizard
+/* Each enum corresponds to a different "KIND" of action
+*/
+enum action_kind {
+    ITEM = 1, // ACTION <item>
+    DIRECTION = 2, // ACTION <direction
+    NPC = 3, // ACTION <npc>
+    ITEM_NPC = 4, // ACTION <item> <npc>
+    ITEM_ITEM = 5 // ACTION <item> <item>
 };
-
 
 /* An action struct that contains the following:
  * - act: the encoded enum name for this action
@@ -53,7 +59,7 @@ typedef struct {
 	enum actions act;   // e.g. CONSUME
 	char *c_name;  // e.g. "eat"
 	list_t *synonyms;   // e.g. "drink" -> "use"
-	list_t *parameters; // e.g. ITEM_OBJ -> NPC_OBJ
+	enum action_kind *kind; // e.g. KIND_1
 } action_t;
 
 // ===========================================================
@@ -68,7 +74,7 @@ typedef struct {
  * - a pointer to a new action struct
  */
 action_t *action_new(enum actions act, char *c_name,
-  list_t *synonyms, list_t *parameters);
+                     list_t *synonyms, enum action_kind *kind);
 
 
 /*
@@ -84,7 +90,7 @@ action_t *action_new(enum actions act, char *c_name,
  * - an error message in stderr if the action struct is not initialized
  */
 int action_init(action_t *a, enum actions act, char *c_name,
-  list_t *synonyms, list_t *parameters);
+                list_t *synonyms, enum action_kind *kind);
 
 
 /*
@@ -98,158 +104,61 @@ int action_init(action_t *a, enum actions act, char *c_name,
  */
 int action_free(action_t *a);
 
+// ===========================================================
 
 /* A function that returns all supported actions
+ * Parameters:
+ * - kind: The enumerated value of KIND, specifying which action KIND necessary
  * Returns:
- * a linked list of action_t structs */
-list_t *get_supported_actions();
+ * - a linked list of action_t structs 
+ */
+list_t *get_supported_actions(enum action_kind *kind);
+
+typedef struct {
+    enum actions act;   // e.g. CONSUME
+    char *c_name;  // e.g. "eat"
+    list_t *synonyms;   // e.g. "drink" -> "use"
+    enum action_kind *kind; // e.g. KIND_1
+} action_t;
+
+/* A function that returns the canonical name of an action as a string */
+char *get_action_cname(action_t *a);
+
+/* A function that returns the canonical name of an action as a string */
+list_t *get_action_synonyms(action_t *a);
+
+/* A function that returns the KIND of an action as an integer 
+ * Returns 1 for KIND 1, 2 for KIND 2, etc. 
+ */
+int *get_action_kind(action_t *a);
 
 // ===========================================================
 
-/* A function that executes TYPE 1 actions (ACTION <item>)
+/* A function that executes KIND 1 actions (ACTION <item>)
  * Returns 0 on success, 1 on failure. */
-int action_items(game_t *g, action_t *a, item_t *i);
+int action_item(game_t *g, action_t *a, item_t *i);
 
 
-/* A function that executes TYPE 2 actions (ACTION <direction>)
+/* A function that executes KIND 2 actions (ACTION <direction>)
  * Returns 0 on success, 1 on failure. */
-int action_directions(game_t *g, action_t *a, direction_t *d);
+int action_direction(game_t *g, action_t *a, direction_t *d);
 
 
-/* A function that executes TYPE 3 actions (ACTION <npc>)
+/* A function that executes KIND 3 actions (ACTION <npc>)
  * Returns 0 on success, 1 on failure. */
 int action_npc(game_t *g, action_t *a, npc_t *n);
 
 
-/* A function that executes TYPE 4 actions (ACTION <item> <npc>)
+/* A function that executes KIND 4 actions (ACTION <item> <npc>)
  * Returns 0 on success, 1 on failure. */
 int action_item_npc(game_t *g, action_t *a, item_t *i, npc_t *n);
 
-// ===========================================================
 
-/* TYPE 1 ACTIONS */
+/* A function that executes KIND 5 actions (ACTION <item> <item>)
+ * The first input is considered the DIRECT object, whereas the second is considered INDIRECT
+ * Returns 0 on success, 1 on failure. */
+int action_item_item(game_t *g, action_t *a, item_t *direct, item_t *indirect);
 
-/* Changes item state to opened
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_open(game_t *g, item_t *i);
-
-
-/* Changes item state to closed
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_close(game_t *g, item_t *i);
-
-
-/* Changes item state to pushed
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_push(game_t *g, item_t *i);
-
-/* Changes item state to pulled
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_pull(game_t *g, item_t *i);
-
-/* Prints a description of an item
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_examine(game_t *g, item_t *i);
-
-/* Changes item state to turned on
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_turn_on(game_t *g, item_t *i);
-
-/* Changes item state to turned off
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_turn_off(game_t *g, item_t *i);
-
-/* Picks up an item, removing it from world and adding to inventory
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_take(game_t *g, item_t *i);
-
-/* Drops an item, removing it from inventory
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_drop(game_t *g, item_t *i);
-
-/* Consumes an item, removing it from inventory
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * Output:
- * - 0 if success, 1 if error */
-int act_consume(game_t *g, item_t *i);
-
-/* TYPE 2 ACTIONS */
-
-/* Moves the player to the given direction
- * Paramters:
- * - g: the game struct
- * - d: the direction struct
- * Output:
- * - 0 if success, 1 if error */
-int act_go(game_t *g, direction_t *d);
-
-/* TYPE 3 ACTIONS */
-
-/* Prints dialogue from an npc
- * Paramters:
- * - g: the game struct
- * - n: the npc struct
- * Output:
- * - 0 if success, 1 if error */
-int act_talk_to(game_t *g, npc_t *n);
-
-/* TYPE 4 ACTIONS */
-
-/* Gives item to an npc, removing it from inventory and changing npc state
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * - n: the npc struct
- * Output:
- * - 0 if success, 1 if error */
-int act_give_to(game_t *g, item_t *i, npc_t *n);
-
-/* Uses item on npc, removing it from inventory and changing npc state
- * Paramters:
- * - g: the game struct
- * - i: the item struct
- * - n: the npc struct
- * Output:
- * - 0 if success, 1 if error */
-int act_use_on(game_t *g, item_t *i, npc_t *n);
 
 
 #endif
