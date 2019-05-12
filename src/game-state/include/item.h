a81139bffd2f9e72e2f0b866225daed33816cfb2
liuyifan <cnjsnjlyf@gmail.com>
2019-05-12 16:51:13 -0500

    pull new item.h/.c files from the objeto branch #60

---
+++ src/game-state/include/item.h
@@ -0,0 +1,60 @@
+#ifndef _OBJECT_H
+#define _OBJECT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "uthash.h"
+#include "player.h"
+#include "room.h"
+#include "game.h"
+
+enum attribute_tag {INTEGER, DOUBLE, CHARACTER, STRING}
+
+// values will be loaded from WDL/provided by action management
+typedef union attribute_value {
+    bool b;
+    char char_val;
+    char *str_val;
+    int int_val;
+} attribute_value_t;
+
+/* this object struct will include a door object to be used between rooms,
+i.e. included in the room struct in its list of exits */
+typedef struct object {
+    UT_hash_handle hh; //makes thuis struct hashable for the room struct (objects in rooms) and player struct (inventory)
+    char *object_id;
+    char *short_desc;
+    char *long_desc;
+    bool condition; /* reserved for future expansion */
+    attribute_table_t attributes;
+} object_t;
+
+typedef struct attribute_table{
+    UT_hash_handle hh; //makes this struct hashable for the object struct
+    char* attribute_name;
+    union attribute_value_t val;
+} attribute_table_t;
+
+object_t *object_new();
+
+// arguments are taken from WDL
+int object_init(char *object_id, char *short_desc, char *long_desc);
+//
+char *get_id(object_t obj);
+
+char *get_short_desc(object_t obj);
+
+char *get_long_desc(object_t obj);
+
+int *get_obj_type(enum object_type_t obj_t);
+
+int take_object(object_t obj);
+
+attribute_value_t* create_attribute(void* value, int type);
+
+/*create a function to add to the attribute table, create a function that returnsd the value of an attribute,
+create a function that changes an atttribute,
+
+*/
+
+#endif
---
+++ src/game-state/src/item.c
@@ -0,0 +1,144 @@
+/* Implementations of the object struct */
+#include <stdio.h>
+#include <stdlib.h.h>
+#include "item.h"
+#include <assert.h>
+
+item_t *item_new(char *item_id, char *short_desc, char *long_desc)
+{
+  item_t *new_item;
+  int check;
+
+  new_item = malloc(sizeof(item_t));
+  new_item->item_id = malloc(10*sizeof(char)); // tentative size allocation
+  new_item->short_desc = malloc(50*sizeof(char));
+  new_item->long_desc = malloc(300*sizeof(char));
+
+  if(new_item == NULL) {
+      error("Could not allocate memory!");
+      return NULL;
+  }
+
+  check = item_init(new_item, short_desc, long_desc);
+
+  if(check != SUCCESS) {
+      error("Could not initialize item!");
+      return NULL;
+  }
+
+  return new_item;
+
+}
+
+int item_init(item_t *new_item, char *item_id, char *short_desc, char *long_desc)
+{
+
+  assert(new_item != NULL);
+  new_item->item_id = item_id;
+  new_item->short_desc = short_desc;
+  new_item->long_desc = long_desc;
+
+  return SUCCESS;
+}
+
+/* the following functions retrieve specific information from desired object
+in anticipation of supporting player demands */
+
+char *get_id(item_t *item)
+{
+  char *my_item_id = item->item_id;
+
+  return my_item_id;
+}
+
+
+char *get_short_desc(item_t *item)
+{
+  char *item_shortd = item->short_desc;
+
+  return item_shortd;
+}
+
+
+char *get_long_desc(item_t *item)
+{
+  char *item_longd = item->long_desc;
+
+  return item_longd;
+}
+
+
+int *get_obj_type(enum object_type_t obj_t)
+{
+
+    /* TO DO */
+
+    return NULL;
+}
+
+
+int take_object(object_t obj)
+{
+
+    /* TO DO */
+
+    return 0;
+}
+
+
+/* adding item to room inventory hash */
+int add_item_to_room(all_items_t all_items, int item_id, item_t *item) {
+    item_t *s;
+    HASH_FIND_STR(all_items, &item_id, s);
+    if (s != NULL) {
+        /* WARNING */
+        /* SHOULD BE ABLE TO SUPPORT STACKING MULTIPLE OBJECTS */
+        printf("FATAL: item_id already used!\n");
+        exit(0);
+    }
+    HASH_ADD_INT(all_items, item_id, s);
+    return 1;
+}
+
+
+/* adding item attributes to item attr hash, in progress */
+int add_attr_to_item(item_t *item, char *attribute, attribute_value_t item_attr) {
+    item_t *s;
+    attribute_table_t all_attrs = item->attributes;
+    HASH_FIND_STR(all_attrs, attribute, s);
+    if (s != NULL) {
+        printf("This attribute is already present!\n");
+        exit(1);
+    }
+    HASH_ADD_INT(all_attrs, attribute, s);
+    return 1;
+}
+
+//do we want to make the attribute union a tagged union?? 
+attribute_value_t* create_attribute(void* value, int type)
+{
+  attribute_value_t* new_attribute = malloc(attribute_value_t)
+  if (type == INTEGER)
+  {
+    new_attribute->int_val = *value;
+    return new_attribute;
+  }
+  else if (type == DOUBLE)
+  {
+    new_attribute->double_value = *value;
+    return new_attribute;
+  }
+  else if (type == CHARACTER)
+  {
+    new_attribute->char_val = *value;
+    return new_attribute;
+  }
+  else if (type == STRING)
+  {
+    new_attribute->str_val = value;
+    return new_attribute;
+  }
+  error("Attribute could not be created");
+  return NULL;
+
+}
