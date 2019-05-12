5158e1863fdf352df51ca7e4bc5a28240696a86f
Melinda Tang <melindatang@uchicago.edu>
2019-05-12 17:21:51 -0500

    altered some documentation

--- src/action_management/include/actionmanagement.h
+++ src/action_management/include/actionmanagement.h
@@ -39,8 +39,8 @@ enum actions {
     USE_ON
 };
 
-/* Each enum corresponds to a different "KIND" of action
-*/
+
+/* Each enum corresponds to a different "KIND" of action */
 enum action_kind {
     ITEM = 1, // ACTION <item>
     DIRECTION = 2, // ACTION <direction
@@ -48,6 +48,7 @@ enum action_kind {
     ITEM_NPC = 4, // ACTION <item> <npc>
     ITEM_ITEM = 5 // ACTION <item> <item>
 };
+
 
 /* An action struct that contains the following:
  * - act: the encoded enum name for this action
@@ -61,6 +62,7 @@ typedef struct {
 	list_t *synonyms;   // e.g. "drink" -> "use"
 	enum action_kind *kind; // e.g. KIND_1
 } action_t;
+
 
 // ===========================================================
 
@@ -104,59 +106,131 @@ int action_init(action_t *a, enum actions act, char *c_name,
  */
 int action_free(action_t *a);
 
+
 // ===========================================================
 
-/* A function that returns all supported actions
+/* 
+ * A function that returns all supported actions
+ *
  * Parameters:
  * - kind: The enumerated value of KIND, specifying which action KIND necessary
+ * 
  * Returns:
  * - a linked list of action_t structs 
  */
 list_t *get_supported_actions(enum action_kind *kind);
 
-typedef struct {
-    enum actions act;   // e.g. CONSUME
-    char *c_name;  // e.g. "eat"
-    list_t *synonyms;   // e.g. "drink" -> "use"
-    enum action_kind *kind; // e.g. KIND_1
-} action_t;
 
-/* A function that returns the canonical name of an action as a string */
+/* 
+ * A function that returns the canonical name of an action as a string 
+ * 
+ * Parameters:
+ * - a: An action struct
+ * 
+ * Returns:
+ * - the canonical name of the action as a string
+*/
 char *get_action_cname(action_t *a);
 
-/* A function that returns the canonical name of an action as a string */
+
+/* 
+ * A function that returns the synonyms of an action as a list of enums 
+ * 
+ * Parameters:
+ * - a: An action struct
+ * 
+ * Returns:
+ * - a list of the synonyms of the action
+*/
 list_t *get_action_synonyms(action_t *a);
 
+
 /* A function that returns the KIND of an action as an integer 
- * Returns 1 for KIND 1, 2 for KIND 2, etc. 
+ * 
+ * Parameters:
+ * - a: An action struct
+ * 
+ * Returns: 
+ * - 1 for KIND 1
+ * - 2 for KIND 2
+ * - 3 for KIND 3
+ * - 4 for KIND 4
+ * - 5 for KIND 5
  */
 int *get_action_kind(action_t *a);
+
 
 // ===========================================================
 
 /* A function that executes KIND 1 actions (ACTION <item>)
- * Returns 0 on success, 1 on failure. */
+ * 
+ * Parameters:
+ * - g: A game struct consisting of the game state
+ * - a: An action struct
+ * - i: An item struct
+ * 
+ * Returns
+ * - 0 on success
+ * - 1 on failure
+ */
 int action_item(game_t *g, action_t *a, item_t *i);
 
 
 /* A function that executes KIND 2 actions (ACTION <direction>)
- * Returns 0 on success, 1 on failure. */
+ * 
+ * Parameters:
+ * - g: A game struct consisting of the game state
+ * - a: An action struct
+ * - d: A direction struct
+ * 
+ * Returns
+ * - 0 on success
+ * - 1 on failure
+ */
 int action_direction(game_t *g, action_t *a, direction_t *d);
 
 
 /* A function that executes KIND 3 actions (ACTION <npc>)
- * Returns 0 on success, 1 on failure. */
+ * 
+ * Parameters:
+ * - g: A game struct consisting of the game state
+ * - a: An action struct
+ * - n: An npc struct
+ * 
+ * Returns
+ * - 0 on success
+ * - 1 on failure
+ */
 int action_npc(game_t *g, action_t *a, npc_t *n);
 
 
 /* A function that executes KIND 4 actions (ACTION <item> <npc>)
- * Returns 0 on success, 1 on failure. */
+ * 
+ * Parameters:
+ * - g: A game struct consisting of the game state
+ * - a: An action struct
+ * - i: An item struct
+ * - n: An npc struct
+ * 
+ * Returns
+ * - 0 on success
+ * - 1 on failure
+ */
 int action_item_npc(game_t *g, action_t *a, item_t *i, npc_t *n);
 
 
 /* A function that executes KIND 5 actions (ACTION <item> <item>)
- * The first input is considered the DIRECT object, whereas the second is considered INDIRECT
- * Returns 0 on success, 1 on failure. */
+ * 
+ * Parameters:
+ * - g: A game struct consisting of the game state
+ * - a: An action struct
+ * - direct: An item struct containing the direct object (the "actor")
+ * - indirect: An item struct containing the indirect object (the "actee")
+ * 
+ * Returns
+ * - 0 on success
+ * - 1 on failure
+ */
 int action_item_item(game_t *g, action_t *a, item_t *direct, item_t *indirect);
 
 
