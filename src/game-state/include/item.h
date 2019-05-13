4dcf3a87ab3961a14993f6840502a7b7821c9f6c
lhsi <lhsi@uchicago.edu>
2019-05-13 18:23:19 -0500

    reformatted header file and added new functions #52

--- src/game-state/include/item.h
+++ src/game-state/include/item.h
@@ -3,16 +3,47 @@
 
 #include "game_state_common.h"
 
+// ITEM STRUCTURE DEFINITION --------------------------------------------------
+typedef struct item {
+    UT_hash_handle hh; //makes thuis struct hashable for the room struct (objects in rooms) and player struct (inventory)
+    char *item_id;
+    char *short_desc;
+    char *long_desc;
+    // bool condition; /* reserved for future expansion */
+    attribute_hash_t attributes; // a hashtable for all attributes
+} item_t;
+
+typedef struct item* item_hash_t;
+
+/* item_new() allocates a space for an item struct in memory
+*  Parameters:
+*    a unique item id
+*  Returns:
+*    A pointer to a new item struct.
+*/
+item_t *item_new(char *item_id, char *short_desc, char *long_desc);
+
+
+/* item_new() allocates a space for an item struct in memory
+*  Parameters:
+*    a unique item id
+*  Returns:
+*    A pointer to a new item struct.
+*/
+int item_free(item_t *item);
+
+
+// ATTRIBUTE STUCTURE DEFINITION ----------------------------------------------
 // values will be loaded from WDL/provided by action management
 typedef union attribute_value {
     double double_val;
     char char_val;
-    bool boole_val;
+    bool bool_val;
     char* str_val;
     int int_val;
 } attribute_value_t;
 
-enum attribute_tag {BOOL, CHARACTER, STRING, INTEGER};
+enum attribute_tag {DOUBLE, BOOLE, CHARACTER, STRING, INTEGER};
 
 typedef struct attribute {
     UT_hash_handle hh;
@@ -28,61 +59,57 @@ f3args myunion;
 
 typedef struct attribute* attribute_hash_t;
 
-/* this object struct will include a door object to be used between rooms,
-i.e. included in the room struct in its list of exits */
-typedef struct item {
-    UT_hash_handle hh; //makes thuis struct hashable for the room struct (objects in rooms) and player struct (inventory)
-    char *item_id;
-    char *short_desc;
-    char *long_desc;
-    // bool condition; /* reserved for future expansion */
-    attribute_hash_t attributes; // a hashtable for all attributes
-} item_t;
-
-typedef struct item* item_hash_t;
-/* item_new() allocates a space for an item struct in memory
-  Parameters:
-    a unique item id
-    a short description of the item
-    a long description of the item
-  Returns:
-    A pointer to a new item struct.
-*/
-item_t *item_new(char *item_id, char *short_desc, char *long_desc);
-
-
-/* get_item_type() retrieves the type of the item
-  Parameters:
-    the item struct in question
-  Returns:
-
-*/
-// int *get_item_type(enum item_type_t item);
-
-int take_item(item_t *item);
-
+// ATTRIBUTE FUNCTIONS (FOR ITEMS) --------------------------------------------
 // attribute_t* create_attribute(void* value, int type);
 //attribute_t* create_attribute(attribute_value_t value, enum attribute_tag type);
 attribute_t* create_attribute(void* value, enum attribute_tag type);
 
-<<<<<<< HEAD
-=======
-/* this has to be in interface as room and player modlues use this */
-int add_item_to_hash(item_hash_t item_hash, char *item_id, item_t *item);
->>>>>>> d2232e23f8789646481628bb298c7024d4bdeefb
-
-int item_free(item_t *item);
-
 int attribute_free(attribute_t *attribute);
 
+void* get_attribute(item_t* item, char* attribute_key);
+
+// ATTRIBUTE ADDITION FUNCTIONS -----------------------------------------------
+// the following functions allow their users to add attributes to the given item
+// ex. change a door from locked to unlocked
+
+/* this has to be in interface as room and player modules use this */
+int add_item_to_hash(item_hash_t item_hash, item_t *item);
+
+
+
+
+
+
+
+
+
+// ATTRIBUTE REPLACEMENT FUNCTIONS --------------------------------------------
+// the following functions allow their users to replace (read: change)
+// attributes associated
+
+int replace_str_attr(item_t *item, attribute_t* attribute);
+
+int replace_int_attr(item_t *item, attribute_t* attr);
+
+int replace_char_attr(item_t *item, attribute_t* attr);
+
+int replace_double_attr(item_t *item, attribute_t* attr);
+
+int replace_bool_attr(item_t *item, attribute_t* attr);
+
+
+
+
+
+
+
+// DELETION FUNCTIONS -----------------------------------------------
 /* this has to be in the interace as room and player modules use this */
 int delete_all_items(item_hash_t items);
 
 int delete_all_attributes(attribute_hash_t attributes);
 
-void* get_attribute(item_t* item, char* attribute_key);
 
-int change_attribute(item_t* item, char* attribute_key, void* new_attribute);
 /*create a function to add to the attribute table, create a function that returnsd the value of an attribute,
 create a function that changes an atttribute,
 
