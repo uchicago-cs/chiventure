59f3ce569bb32004fdc6674cf89a58bb89e95375
Olivia Morkved <oliviamorkved@cs.uchicago.edu>
2019-05-13 22:57:27 -0500

    Final touches to coordinate.c now that find_coord and add_coord both work

--- src/assigncoords/coordinate.c
+++ src/assigncoords/coordinate.c
@@ -5,6 +5,7 @@
 
 coord_record_t *coordmap;
 
+
 void coord_init(coordinate_t *c, int x, int y)
 {
   assert (c != NULL);
@@ -14,15 +15,16 @@ void coord_init(coordinate_t *c, int x, int y)
 }
 
 /* find_coord:
+ *
  * See coordinate.h for more details
  * Returns NULL if coord not found in hash
  */
 coord_record_t *find_coord(int x, int y)
 {
   coordinate_t *key = malloc(sizeof(coordinate_t));
-
   memset(key, 0, sizeof(coordinate_t));
   coord_init(key, x, y);
+
   coord_record_t *cr = malloc(sizeof(coord_record_t));
   memset(cr, 0, sizeof(coord_record_t));
   HASH_FIND(hh, coordmap, key, sizeof(coordinate_t), cr);
@@ -31,35 +33,20 @@ coord_record_t *find_coord(int x, int y)
 }
 
 /* add_coord:
- * - Implementation  Will use HASH_FIND to check uniqueness
- *   If unique, creates a new coord_key_t and add to map using HASH_ADD
  *
  * - returns SUCCESS if does not find coordinate and add its
  * - returns FAILURE if it finds coordinate already and 
  * - the coord is mapped to a different room
- *
- * Note: Will implement more extensive tests of add_coord once 
- * find_coord works (as this seems to be the best way to test if
- * a coord has been added). For now, it seems add coord does indeed work 
- * -- when I run the main in gdb, a call to
- * print coordmap->key 
- * after the add_coord function is called 
- * returns = {x = 5, y = 6}, which shows that the first coord has been added
  */
 int add_coord(int x, int y, room_t *r)
 {
-  //coordinate_t *key = malloc(sizeof(coordinate_t));
-  //memset(key, 0, sizeof(coordinate_t));
-  
   coord_record_t *cr = find_coord(x, y);
-
+  /* Only runs if find_coord does not find coord
+    already existing in hashtable */
+  
   if (cr == NULL) {
     fprintf(stderr,"Adding coord (%d, %d) to hash\n", x, y);
-    
-    /* Only runs if find_coord does not find coord
-    already existing in hashtable */
 
-    //coord_init(key, x, y);
     cr = malloc(sizeof(coord_record_t));
     memset(cr, 0, sizeof(coord_record_t));    
     cr->key.x = x;
@@ -73,7 +60,8 @@ int add_coord(int x, int y, room_t *r)
     if (cr->r->id == r->id)
       return SUCCESS;
   }
-  
+  fprintf(stderr,
+	  "add_coord(): This coordinate has already been assigned.\n");  
   return FAILURE;
 }
 
@@ -85,7 +73,7 @@ int main()
   coordmap = NULL;
   coordmap = find_coord(1, 2);
   if (coordmap == NULL)
-    fprintf(stdout,
+    fprintf(stderr,
 	    "find_coord: Successfully returns NULL when not found\n");
 
   room_t *r = malloc(sizeof(room_t));
@@ -100,7 +88,7 @@ int main()
   if (example == NULL)
     fprintf(stderr,"Failure to find coord (%d, %d)\n", 5, 6);
   else
-    fprintf(stdout,"Found coordinate of room with room id %d",
+    fprintf(stderr,"Found coordinate of room with room id %d\n",
 	    example->r->id);
 
   coord_record_t *ex2 = find_coord(-1, -2);
@@ -111,9 +99,11 @@ int main()
     fprintf(stderr,"Found coordinate of room with room id %d\n",
 	    ex2->r->id);
   
-  //coord_record_t *a = NULL;
-  //coord_record_t *b = NULL;
-  //  HASH_ITER(hh, coordmap, a, b);
+  room_t *z = malloc(sizeof(room_t));
+  z->id = 3;
+  fprintf(stderr,
+	  "Test to see if add_coord() correctly blocks double-assigning:\n");
+  add_coord(5, 6, z);
 
   free(r);
   free(g);
