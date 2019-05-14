53240887ca45ce44242d4316eb470dae582553fe
Kate <kate755@gmail.com>
2019-05-13 22:46:54 -0500

    updated parse_document.h to reflect new function in .c file

--- src/libwdl/include/parse_document.h
+++ src/libwdl/include/parse_document.h
@@ -18,11 +18,16 @@ void create_game(obj_t *obj);
 /* Creates the various attributes of the rooms within 
  * the game state team's structs
  */
-void create_rooms(attr_list_t *ls);
+void create_rooms(attr_list_t *ls, game_t *g);
 
 /* Creates the various attributes of the items within 
  * the game state team's structs
  */
 void create_items(attr_list_t *ls);
 
+/* Given a room id and the list of all items in a game returns
+ * a list with all the items in that room */
+attr_list_t* get_items_in_room(char* room_id, attr_list_t* all_items);
+
 #endif /*INCLUDE_CONNECT_GAME_H*/
+
