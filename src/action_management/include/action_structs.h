9b6e47c98ca6beade9736f65b97b4d29936b0766
Melinda Tang <melindatang@uchicago.edu>
2019-05-16 12:56:15 -0500

    removed outdated description of action struct

--- src/action_management/include/action_structs.h
+++ src/action_management/include/action_structs.h
@@ -62,7 +62,7 @@ enum action_kind {
  * - act: the encoded enum name for this action
  * - c_name: the 'canonical' string that should call the enum
  * - synonyms: the synonyms that would also be allowed to call action
- * - parameters: an order-sensitive linked list of object_type enums
+ * - kind: an enumeration of the kind of action
 */
 typedef struct {
     enum actions act;   // e.g. CONSUME
