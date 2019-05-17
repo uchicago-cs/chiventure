034392d9b873b2ea879ae6e48caa35d7f75cc02f
Patrick Koopmans <patrickkoopmans@cs.uchicago.edu>
2019-05-17 15:34:07 -0500

    Issue #133 refactored code into multiple common files

--- src/game-state/src/common-item.h
+++ src/game-state/src/common-item.h
@@ -1,7 +1,6 @@
-#ifndef _COMMON_H
-#define _COMMON_H
+#ifndef _ITEM_COMMON_H
+#define _ITEM_COMMON_H
 
-#include "room.h"
 #include "item.h"
 
 /* add_item_to_hash() adds an item to the hash table of items
@@ -23,24 +22,5 @@ int add_item_to_hash(item_hash_t item_hash, char *item_id, item_t *item);
  */
 int delete_all_items(item_hash_t items);
 
-/* Adds a room to the given hashtable of rooms
- *
- * Parameters:
- *  hashtable the room is added to
- *  room id
- *  pointer to the room
- * Returns:
- *  SUCCESS if successful, FAILURE if failed
- */
-int add_room_to_hash(room_hash_t all_rooms, char *room_id, room_t *room);
 
-/* Deletes a hashtable of rooms
- * Implemented with macros provided by uthash.h
- *
- * Parameters:
- *  hashtable of rooms that need to be deleted
- * Returns:
- *  SUCCESS if successful, FAILURE if failed
- */
-int delete_all_rooms(room_hash_t rooms);
 #endif
