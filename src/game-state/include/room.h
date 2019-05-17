5546f00cb31ba8364c661c2115c5f3db260c74fc
Patrick Koopmans <patrickkoopmans@cs.uchicago.edu>
2019-05-17 15:13:45 -0500

    refactored hash references in room module

--- src/game-state/include/room.h
+++ src/game-state/include/room.h
@@ -48,17 +48,6 @@ room_t *room_new(char *room_id, char *short_desc, char *long_desc, item_hash_t i
  */
 int room_free(room_t *room);
 
-/* Adds a room to the given hashtable of rooms
- *
- * Parameters:
- *  hashtable the room is added to
- *  room id
- *  pointer to the room
- * Returns:
- *  SUCCESS if successful, FAILURE if failed
- */
-int add_room_to_hash(room_hash_t all_rooms, char *room_id, room_t *room);
-
 /* Adds an item to the given room
  *
  * Parameters:
@@ -80,16 +69,5 @@ int add_item_to_room(room_t *room, item_t *item);
  *  SUCCESS if successful, FAILURE if failed
  */
 int add_path_to_room(room_t *room, path_t *path);
-
-/* Deletes a hashtable of rooms
- * Implemented with macros provided by uthash.h
- *
- * Parameters:
- *  hashtable of rooms that need to be deleted
- * Returns:
- *  SUCCESS if successful, FAILURE if failed
- */
-int delete_all_rooms(room_hash_t rooms);
-
 
 #endif
