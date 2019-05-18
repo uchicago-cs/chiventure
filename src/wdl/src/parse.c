fde623d6753d1ad5663fd039042beae9ee12b013
Victor <jesusaa@uchicago.edu>
2019-05-18 15:30:00 -0500

    incorporated typechecking into extract_objects()

--- src/wdl/src/parse.c
+++ src/wdl/src/parse.c
@@ -29,59 +29,28 @@ attr_list_t *get_obj_list(obj_t *obj, char *str)
 }
 
 /* see parse.h */
-//attr_list_t *extract_objects(obj_t *obj, char *str)
-//{
-//    char *attributes[3] = {"ROOMS", "ITEMS", "PLAYERS"};
-//    attr_list_t *ls = get_obj_list(obj, str);
-//    bool valid = false;
-//
-//    if (ls == NULL)
-//        return NULL;
-//
-//    if (strcmp(str, attributes[0]))
-//        valid = list_check_type(ls, room_type_check);
-//   else if (strcmp(str, attributes[1]))
-//       valid = list_check_type(ls, item_type_check);
-//    else
-//        valid = list_check_type(ls, player_type_check);
-//
-//    if (valid)
-//        return ls;
-//    else
-//        return NULL;
-//}
-
-
-/* make_char_id()
- * a helper function for extract_ids();
- * used to add a char id to the id list
- *
- * parameters:
- *  - obj: the object for which to get the id
- *
- * returns:
- *  - a single id item to be added to the list by extract_ids()
- *
-attr_list_t *make_char_id(obj_t *obj)
+attr_list_t *extract_objects(obj_t *obj, char *str)
 {
+    char *attributes[3] = {"ROOMS", "ITEMS", "PLAYERS"};
+    bool valid = false;
 
-}*/
+    attr_list_t *ls = get_obj_list(obj, str);
 
+    if (ls == NULL)
+        return NULL;
 
-/* make_str_id()
- * a helper function for extract_ids();
- * used to add a str id to the id list
- *
- * parameters:
- *  - obj: the object for which to get the id
- *
- * returns:
- *  - a single id item to be added to the list by extract_ids()
- *
-attr_list_t *make_str_id(obj_t *obj)
-{
-    
-}*/
+    if (strcmp(str, attributes[0]))
+        valid = list_check_type(ls, room_type_check);
+    else if (strcmp(str, attributes[1]))
+       valid = list_check_type(ls, item_type_check);
+    else
+        valid = list_check_type(ls, player_type_check);
+
+    if (valid)
+        return ls;
+    else
+        return NULL;
+}
 
 
 /* see parse.h */
