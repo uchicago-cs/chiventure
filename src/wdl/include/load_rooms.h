6a21a86205c0b2ab7cfa5e54cb6b54076509b8fb
Matthew Yang <mtyang@cs.uchicago.edu>
2019-05-23 17:14:26 -0500

    added load_rooms header file

---
+++ src/wdl/include/load_rooms.h
@@ -0,0 +1,14 @@
+/*
+ * Header file for loading rooms into game_state structs
+ */
+
+#ifndef INCLUDE_LOAD_ROOMS_H
+#define INCLUDE_LOAD_ROOMS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+
+#endif
