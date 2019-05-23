3a0c6193a252619020ddb89b8ef589d6d1666c8c
Christian Bjerre-Fernandes <chrisfernandes18@cs.uchicago.edu>
2019-05-23 16:06:21 -0500

    adding test_save.c file

---
+++ src/checkpointing/tests/test_save.c
@@ -0,0 +1,44 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "save.h"
+
+Test(save, attribute_value)
+{
+
+}
+
+Test(save, attribute)
+{
+
+}
+
+Test(save, item)
+{
+
+}
+
+Test(save, condition)
+{
+
+}
+
+Test(save, path)
+{
+
+}
+
+Test(save, room)
+{
+
+}
+
+Test(save, player)
+{
+
+}
+
+Test(save, game)
+{
+
+}
