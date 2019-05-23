be16ff7c9ea527ab5517a9b8f2fa5b640bb56e33
Matthew Yang <mtyang@cs.uchicago.edu>
2019-05-23 17:26:05 -0500

    created load_rooms header file #140

--- src/wdl/include/load_rooms.h
+++ src/wdl/include/load_rooms.h
@@ -8,7 +8,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "validate.h"
 
+/*
+ * add_rooms_to_game
+ * accesses the document object and obtain list of rooms to parse into game_state
+ * structs and add to the inputted game pointer
+ *
+ * parameters:
+ * - obj: the document object
+ * - g: a game pointer as a return parameter
+ *
+ * side effects:
+ * - adds rooms into the inputted game pointer
+ */
 
+/*
+ * add_connections_to_rooms
+ * accesses the document object and obtains connections for each room to parse intro
+ * game_state structs and add to the inputted game pointer
+ * parameters:
+ * - obj: the document object
+ * - g: a game pointer as a return parameter
+ *
+ * side effects:
+ * - adds connections into the inputted game parameter
+ */
 
 #endif
