4e57d89d58bda9fb990a598d78455f4a04a02efc
Thor Anderskow <tanderskow@cs.uchicago.edu>
2019-05-23 22:37:38 -0500

    created game_set_start

--- src/wdl/include/load_game.h
+++ src/wdl/include/load_game.h
@@ -20,7 +20,21 @@
  *   - a pointer to a game_t struct
  *
  */
-game_t *create_game(obj_t *game);
+game_t *create_game(obj_t *doc);
+
+
+/*
+ * Function set start room in a game
+ *
+ * input: 
+ *   - room id, pointer to game_t struct
+ *
+ * output:
+ *   - none
+ *
+ */
+void game_set_start(char *room_id, game_t *game);
+
 
 
 
--- src/wdl/src/load_game.c
+++ src/wdl/src/load_game.c
@@ -5,10 +5,18 @@
 #include "load_game.h"
 #include "parse_document.h"
 
-game_t *create_game(obj_t *game)
+game_t *create_game(obj_t *doc)
 {
+  // to be used when game_state supports an intro in their game_t struct
   //  char *intro = obj_get_str(game,"GAME.1.intro");
   
   game_t *game_ret = game_new();
   return game_ret;
+}
+
+//uses find_room specified in game.c
+void game_set_start(char *room_id, game_t *game)
+{
+  game->curr_room = find_room(game, room_id);
+  return;
 }
