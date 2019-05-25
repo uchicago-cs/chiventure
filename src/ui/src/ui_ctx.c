ee35d50a3a2ae52130a6ce9998599c4264a0378c
Olivia Morkved <oliviamorkved@cs.uchicago.edu>
2019-05-25 18:54:46 -0500

    changed creation of initial coord

--- src/ui/src/ui_ctx.c
+++ src/ui/src/ui_ctx.c
@@ -60,7 +60,8 @@ int ui_ctx_init(ui_ctx_t *ui_ctx, game_t *game)
     ui_ctx->coord_hash = create_valid_map();
 
     // Initial room coordinates set to 0, 0
-    coord_init(ui_ctx->player_loc, 0, 0);
+    coord_t initial_coord = coord_new(0, 0);
+    ui_ctx->player_loc = initial_coord;
 
     /* Valid maps cannot be created for illogical map directions or for maps
      * with logical distances of more than unit one
