c35e76aa0dba387856df7fab0b3335a9df416e5f
Jessica Xia <jessicax@cs.uchicago.edu>
2019-05-25 14:40:04 -0500

    issue #135, branch game-state/game-tests: tests for game module functions not involving hash tables

--- src/game-state/tests/test_game.c
+++ src/game-state/tests/test_game.c
@@ -3,7 +3,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "game.h"
+#include "player.h"
+#include "room.h"
 
+/*
 void setup(void) {
     game_t *game = game_new();
     player_t *plyr = player_new(100);
@@ -13,13 +16,52 @@ void setup(void) {
 void teardown(void) {
     puts("Runs after the test");
 }
+*/
 
-Test(game, new_free)
+Test(game, new)
 {
     game_t *game = game_new();
+
     cr_assert_not_null(game, "game_new() failed");
-    cr_assert_eq(game_free(game), SUCCESS, "game_free() failed");
 }
+
+Test(game, free)
+{
+    game_t *game = game_new();
+    int res = game_free(game);
+
+    cr_assert_eq(res, SUCCESS, "game_free() failed");
+}
+
+Test(game, move_room)
+{
+    game_t *game = game_new();
+    room_t *room1 = room_new("a", "b", "c");
+    room_t *room2 = room_new("b", "c", "d");
+    
+    game->curr_room = room1;
+    move_room(game, room2);
+    
+    cr_assert_not_null(game, "game_new() failed");
+    cr_assert_not_null(room1, "room_new() failed");
+    cr_assert_not_null(room2, "room_new() failed");
+  
+    cr_assert_eq(room2, game->curr_room, "move_room() failed to move rooms");
+}
+
+Test(game, set_curr_player)
+{
+    game_t *game = game_new();
+    player_t *player1 = player_new(99);
+    player_t *player2 = player_new(99);
+
+    game->curr_player = player1;
+    int res = set_curr_player(game, player2);
+
+    cr_assert_eq(res, SUCCESS, "set_curr_player() failed to return success");
+    cr_assert_eq(game->curr_player, player2, "set_curr_player() failed to set new current player");
+}
+
 
 /*need to figure out how to feed in test struct into criterion
 //assuming that is completed
