f6b89471451b34307b986e89211974cbecc9dde1
Ellyn Liu <ellynliu@cs.uchicago.edu>
2019-05-25 10:00:09 -0500

    added new functions in load.h

--- src/checkpointing/include/load.h
+++ src/checkpointing/include/load.h
@@ -1,8 +1,7 @@
 #ifndef LOAD_H
 #define LOAD_H
-
 #include "game.pb-c.h"
-#include "dummy.h"
+
 
 /* 
  * Takes a file, reads it, and returns the length of file
@@ -17,38 +16,83 @@
  */
 size_t read_file(char *filename, unsigned max_length, uint8_t *out);
 
-/*
- * Loads object into game struct
- * 
- * Parameters:
- * - o: pointer to object in protofile
- * - o_t: pointer to an empty object in game struct to be filled from protofile
- *
- * Returns:
- * - 0 if successful
- * - -1 if unsuccessful
- */
-int load_object(Object *o, object_t *o_t);
 
 /*
- * Loads player into game struct
+ * Loads attribute value into game struct
  * 
  * Parameters:
- * - p: pointer to player in protofile
- * - p_t: pointer to an empty player in game struct to be filled from protofile
+ * - av: pointer to Attribute_value in protofile
+ * - av_t: pointer to an empty attribute_value_t in game struct to be filled from protofile
  *
  * Returns:
  * - 0 if successful
  * - -1 if unsuccessful
  */
-int load_player(Player *p, player_t *p_t);
+int load_attribute_value(Attribute_value *av, attribute_value_t *av_t);
+
+
+/*
+ * Loads attribute into game struct
+ * 
+ * Parameters:
+ * - a: pointer to Attribute in protofile
+ * - a_t: pointer to an empty attribute_t in game struct to be filled from protofile
+ *
+ * Returns:
+ * - 0 if successful
+ * - -1 if unsuccessful
+ */
+int load_attribute(Attribute *a, attribute_t *a_t);
+
+
+/*
+ * Loads item into game struct
+ * 
+ * Parameters:
+ * - i: pointer to Item in protofile
+ * - i_t: pointer to an empty item_t in game struct to be filled from protofile
+ *
+ * Returns:
+ * - 0 if successful
+ * - -1 if unsuccessful
+ */
+int load_item(Item *i, item_t *i_t);
+
+
+/*
+ * Loads condition into game struct
+ * 
+ * Parameters:
+ * - c: pointer to Condition in protofile
+ * - c_t: pointer to an empty condition_t in game struct to be filled from protofile
+ *
+ * Returns:
+ * - 0 if successful
+ * - -1 if unsuccessful
+ */
+int load_condition(Condition *c, condition_t *c_t);
+
+
+/*
+ * Loads path into game struct
+ * 
+ * Parameters:
+ * - p: pointer to Path in protofile
+ * - p_t: pointer to an empty path_t in game struct to be filled from protofile
+ *
+ * Returns:
+ * - 0 if successful
+ * - -1 if unsuccessful
+ */
+int load_path(Path *p, path_t *p_t);
+
 
 /*
  * Loads room into game struct
  * 
  * Parameters:
- * - r: pointer to room in protofile
- * - r_t: pointer to an empty room in game struct to be filled from protofile
+ * - r: pointer to Room in protofile
+ * - r_t: pointer to an empty room_t in game struct to be filled from protofile
  *
  * Returns:
  * - 0 if successful
@@ -56,12 +100,27 @@ int load_player(Player *p, player_t *p_t);
  */
 int load_room(Room *r, room_t *r_t);
 
+
+/*
+ * Loads player into game struct
+ * 
+ * Parameters:
+ * - p: pointer to Player in protofile
+ * - p_t: pointer to an empty player_t in game struct to be filled from protofile
+ *
+ * Returns:
+ * - 0 if successful
+ * - -1 if unsuccessful
+ */
+int load_player(Player *p, player_t *p_t);
+
+
 /*
  * Loads game into game struct
  * 
  * Parameters:
- * - g: pointer to game in protofile
- * - g_t: pointer to an empty game in game struct to be filled from protofile
+ * - g: pointer to Game in protofile
+ * - g_t: pointer to an empty game_t in game struct to be filled from protofile
  *
  * Returns:
  * - 0 if successful
