ddfcca2a719ffce3d5ef2fbf638f49a83c5b4039
Christian Bjerre-Fernandes <chrisfernandes18@cs.uchicago.edu>
2019-05-26 12:37:05 -0500

    test_load skeleton

---
+++ src/checkpointing/tests/test_load.c
@@ -0,0 +1,44 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "load.h"
+
+Test(load, attribute_value)
+{
+
+}
+
+Test(load, attribute)
+{
+
+}
+
+Test(load, item)
+{
+
+}
+
+Test(load, condition)
+{
+
+}
+
+Test(load, path)
+{
+
+}
+
+Test(load, room)
+{
+
+}
+
+Test(load, player)
+{
+
+}
+
+Test(load, game)
+{
+
+}
