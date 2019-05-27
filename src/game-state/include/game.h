3a711b8f1b28b2122fcfaa1164701051bf40fe8e
Anant M <anantm@Anants-MacBook-Pro.local>
2019-05-27 00:00:49 -0500

    #163 okay NOW it's here

--- src/game-state/include/game.h
+++ src/game-state/include/game.h
@@ -99,6 +99,18 @@ int add_player_to_game(game_t *game, player_t *player);
  */
 int add_room_to_game(game_t *game, room_t *room);
 
+/*
+* Function to connect two rooms
+* Parameters:
+* game, Source room_id, destination room_id, direction
+* 
+* Returns:
+* SUCCESS if all okay, exit(1) if src room_id not found, exit(2) if dest not found
+* 
+* WARNING: CREATES PATH BUT DOES NOT FILL PATH CONDITIONS AT THE MOMENT AS PARAMETERS NOT GIVEN
+*/
+int create_connection(game_t *game, char* src_room, char* dest_room, char* direction);
+
 
 /*
 *
@@ -133,17 +145,6 @@ player_t *get_player(game_t *game, char *player_id);
 */
 room_t *find_room(game_t *game, char* room_id);
 
-/*
-* Function to connect two rooms
-* Parameters:
-* game, Source room_id, destination room_id, direction
-* 
-* Returns:
-* SUCCESS if all okay, exit(1) if src room_id not found, exit(2) if dest not found
-* 
-* WARNING: CREATES PATH BUT DOES NOT FILL PATH CONDITIONS AT THE MOMENT AS PARAMETERS NOT GIVEN
-*/
-int create_connection(game_t *game, char* src_room, char* dest_room, char* direction);
 
 
 #endif
