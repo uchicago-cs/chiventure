9b21e939e05dde4ff42a1862af150e976f7abaa2
yoonsjeong <yoonsjeong@uchicago.edu>
2019-05-27 18:17:03 -0500

    revamped the tests bc they were not working previously

--- src/action_management/tests/test_get_actions.c
+++ src/action_management/tests/test_get_actions.c
@@ -1,46 +1,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 #include <criterion/criterion.h>
 #include "utlist.h"
 #include "actionmanagement.h"
 
-static action_type_t valid_actions[] = {
-    // KIND 1
-    {"open", ITEM},
-    {"close", ITEM},
-    {"push", ITEM},
-    {"pull", ITEM},
-    {"examine", ITEM},
-    {"turn on", ITEM},
-    {"turn off", ITEM},
-    {"take", ITEM},
-    {"drop", ITEM},
-    {"consume", ITEM},
-    // KIND 2
-    {"go", PATH},
-    {"enter", PATH},
-    // KIND 3
-    {"use on", ITEM_ITEM},
-    {"put on", ITEM_ITEM}
-};
-
-void check_search(list_action_type_t *head, char *query, int expected_index)
+action_type_t *search_supported_actions(char *query)
 {
-    action_type_t* elt;
-    LL_SEARCH_SCALAR(head,elt,c_name,query);
-    int strcmp_result = strcmp(elt->c_name,valid_actions[expected_index].c_name);
-    cr_assert_eq(strcmp_result,0,
-                 "Searched using query %s. Found the element %s of the kind %d.\n",
-                 query, elt->c_name, elt->kind);
+    list_action_type_t *head, *temp;
+    head = get_supported_actions();
+    for (temp = head; temp != NULL; temp = temp->next)
+    {
+        if (strcmp(temp->c_name,query) == 0)
+        {
+            return temp;
+        }        
+    }
+    return NULL;
 }
 
 /* Checks to see if the action list called can be iterated over using string */
 Test(get_actions, search)
 {
-    list_action_type_t *actions = get_supported_actions();
-    cr_assert(actions != NULL, "get_supported_actions() returned a NULL pointer.\n");
-    check_search(actions, "open", 0);
-    check_search(actions, "close", 1);
-    check_search(actions, "put on", 13);
+    action_type_t *open, *consume, *go, *use_on;
+    open = search_supported_actions("open");
+    consume = search_supported_actions("consume");
+    go = search_supported_actions("go");
+    use_on = search_supported_actions("use on");
+
+    cr_assert_neq(open, NULL, 
+                  "search_supported_actions returned a null for query \"open\".\n");
+    cr_assert_neq(consume, NULL, 
+                  "search_supported_actions returned a null for query \"consume\".\n");
+    cr_assert_neq(go, NULL, 
+                  "search_supported_actions returned a null for query \"go\".\n");
+    cr_assert_neq(use_on, NULL, 
+                  "search_supported_actions returned a null for query \"use on\".\n");
 }
