f7bcdc60825d937160f2dfc2fbf4f366d9a181ea
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-28 15:26:51 -0500

    changed expected_output #182

--- src/action_management/tests/test_item_item_actions.c
+++ src/action_management/tests/test_item_item_actions.c
@@ -6,13 +6,22 @@
 #include "item.h"
 #include "game.h"
 
-bool execute_do_item_item_action(char *c_name, enum action_kind kind, char *expected_output)
+bool execute_do_item_item_action(char *c_name, enum action_kind kind)
 {
     game_t *g = game_new;
     action_type_t *a = action_type_new(c_name, kind);
     item_t *direct = item_new("direct", "The direct item", "The directmost object of interest");
     item_t *indirect = item_new("indirect", "The indirect item", "The indirectmost object of interest");
-    
+
+    char *expected_output = malloc(100); // buffer
+    expected_output[0] = '\0';
+    strcat(expected_output, "Requested action ");
+    strcat(expected_output, a->c_name);
+    strcat(expected_output, " with ");
+    strcat(expected_output, direct->item_id);
+    strcat(expected_output, " on ");
+    strcat(expected_output, indirect->item_id);
+
     bool rc;
     if (strcmp(do_item_item_action(g, a, direct, indirect), expected_output) == 0) 
     {
@@ -23,6 +32,7 @@ bool execute_do_item_item_action(char *c_name, enum action_kind kind, char *expe
         rc = false;
     }
 
+    free(expected_output);
     item_free(direct);
     item_free(indirect);
     action_type_free(a);
