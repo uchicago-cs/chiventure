852dad3353261ea648d23d8c7fdc77665c11a1a8
Olivia Morkved <oliviamorkved@cs.uchicago.edu>
2019-05-27 23:22:34 -0500

    Updated map_example

--- src/ui/examples/map_example.c
+++ src/ui/examples/map_example.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "map.h"
+#include "ctx.h"
+#include "ui_ctx.h"
 
 int main()
 {
@@ -12,19 +13,45 @@ int main()
     int cur_y = 0;
     int cur_z = 0;
     int incr = 1;
-    int num_rooms = 100;
-    room_t ** rooms = get_test_rooms(num_rooms);
 
-    // Allocates & Initializes map struct
-    map_t *map = map_init(rooms, num_rooms);
+    // Sample Rooms
+    room_t *room1 = room_new("One", "", "");
+    room_t *room2 = room_new("Two", "", "");
+    room_t *room3 = room_new("Three", "", "");
+    room_t *room4 = room_new("Four", "", "");
+
+    //  Must set hash to NULL (see uthash documentation)
+    coord_record_t *coordmap = NULL;
+    
+    coord_record_t *head = malloc(sizeof(coord_record_t));
+    memset(cr, 0, sizeof(coord_record_t));
+
+    // Initial coordinate is arbitrarily set to be (0,0)
+    head->key.x = 0;
+    head->key.y = 0;
+    head->r = room1;
+    HASH_ADD(hh, coordmap, key, sizeof(coordinate_t), head);
+
+    try_add_coord(coordmap, 0, 1, room2);
+    try_add_coord(coordmap, 0, 2, room3);
+    try_add_coord(coordmap, 1, 1, room4);
+    game_t *game = game_new();
+    ui_ctx_t *ui_ctx = ui_ctx_new(game);
+
+    /* Game State functions needed for DFS not complete at this stage,
+     * so we will disregard the coord hash created in ui_ctx_new 
+     * (which does not work yet) and instead create use own mini coord hash
+     */
+    ui_ctx->coord_hash = coordmap;
+    
     // Set the screen location of the map
-    map_set_displaywin(map, 0, 0, COLS, LINES-1);
+    map_set_displaywin(ui_ctx->map, 0, 0, COLS, LINES-1);
     // Refresh the map centered on room 0,0,0
-    map_center_on(map, 0, 0, 0);
+    map_center_on(ui_ctx->map, 0, 0, 0);
 
     char ch;
     while (true) {
-        ch = wgetch(map->pad);
+        ch = wgetch(ui_ctx->map->pad);
         ch = tolower(ch);
 
         if (ch == 'w') {
@@ -48,7 +75,7 @@ int main()
         if (ch == 'q') {
             break;
         }
-        map_center_on(map,cur_x,cur_y,cur_z);
+        map_center_on(ui_ctx->map, cur_x, cur_y, cur_z);
     }
 
     //Ends the ncurses UI
