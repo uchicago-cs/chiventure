e3400bfb9b703d97d1163fd8a39836a0d333829e
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-28 15:22:23 -0500

    untested work for testing function #182

--- src/action_management/tests/test_item_item_actions.c
+++ src/action_management/tests/test_item_item_actions.c
@@ -6,14 +6,27 @@
 #include "item.h"
 #include "game.h"
 
-char *execute_do_item_item_action(char *c_name, enum action_kind kind)
+bool execute_do_item_item_action(char *c_name, enum action_kind kind, char *expected_output)
 {
     game_t *g = game_new;
+    action_type_t *a = action_type_new(c_name, kind);
     item_t *direct = item_new("direct", "The direct item", "The directmost object of interest");
     item_t *indirect = item_new("indirect", "The indirect item", "The indirectmost object of interest");
-    action_type_t *action = action_type_new(c_name, kind);
+    
+    bool rc;
+    if (strcmp(do_item_item_action(g, a, direct, indirect), expected_output) == 0) 
+    {
+        rc = true;
+    }
+    else 
+    {
+        rc = false;
+    }
 
     item_free(direct);
     item_free(indirect);
+    action_type_free(a);
     game_free(g);
+
+    return rc;
 }
