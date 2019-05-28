09465ca98132943335484929880ac403ace8a4b2
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-28 15:07:22 -0500

    skeleton function with basic news and frees

--- src/action_management/tests/test_item_item_actions.c
+++ src/action_management/tests/test_item_item_actions.c
@@ -5,3 +5,12 @@
 #include "action_structs.h"
 #include "item.h"
 #include "game.h"
+
+char *execute_do_item_item_action(action_type_t *a) 
+{
+    game_t *g = game_new;
+    item_t *direct = item_new("direct", "The direct item", "The directmost object of interest");
+    item_t *indirect = item_new("indirect", "The indirect item", "The indirectmost object of interest");
+
+    game_free(g);
+}
