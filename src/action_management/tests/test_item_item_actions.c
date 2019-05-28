dfa2e0acf6d367f07226e58b0c3ae0e583368bee
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-28 15:12:00 -0500

    added actions

--- src/action_management/tests/test_item_item_actions.c
+++ src/action_management/tests/test_item_item_actions.c
@@ -6,13 +6,12 @@
 #include "item.h"
 #include "game.h"
 
-char *execute_do_item_item_action(action_type_t *a) 
+char *execute_do_item_item_action(char *c_name, enum action_kind kind)
 {
     game_t *g = game_new;
     item_t *direct = item_new("direct", "The direct item", "The directmost object of interest");
     item_t *indirect = item_new("indirect", "The indirect item", "The indirectmost object of interest");
-
-
+    action_type_t *action = action_type_new(c_name, kind);
 
     item_free(direct);
     item_free(indirect);
