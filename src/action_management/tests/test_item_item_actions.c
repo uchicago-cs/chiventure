e99551f8bccfe19d9535d65a4b350b7c5ce4de84
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-28 16:32:16 -0500

    #182 changed checks

--- src/action_management/tests/test_item_item_actions.c
+++ src/action_management/tests/test_item_item_actions.c
@@ -16,11 +16,11 @@ bool execute_do_item_item_action(char *c_name, enum action_kind kind)
     char *expected_output = malloc(100); // buffer
     expected_output[0] = '\0';
     strcat(expected_output, "Requested action ");
-    strcat(expected_output, a->c_name);
+    strcat(expected_output, c_name);
     strcat(expected_output, " with ");
-    strcat(expected_output, direct->item_id);
+    strcat(expected_output, "direct");
     strcat(expected_output, " on ");
-    strcat(expected_output, indirect->item_id);
+    strcat(expected_output, "indirect");
 
     bool rc;
     if (strcmp(do_item_item_action(g, a, direct, indirect), expected_output) == 0) 
@@ -39,4 +39,30 @@ bool execute_do_item_item_action(char *c_name, enum action_kind kind)
     game_free(g);
 
     return rc;
+}
+
+Test(item_item_actions, wrong_kind_1)
+{
+    bool rc = execute_do_item_item_action("dummy", ITEM);
+
+    cr_assert_eq(rc, false,
+                  "execute_do_item_item_action returned true for wrong kind 1");
+}
+
+Test(item_item_actions, wrong_kind_2)
+{
+
+    bool rc = execute_do_item_item_action("dummy", PATH);
+
+    cr_assert_eq(rc, false,
+                 "execute_do_item_item_action returned true for wrong kind 2");
+    ;
+}
+
+Test(item_item_actions, correct_kind_3)
+{
+    bool rc = execute_do_item_item_action("dummy", ITEM_ITEM);
+
+    cr_assert_eq(rc, true,
+                 "execute_do_item_item_action returned false for correct kind 3");
 }
