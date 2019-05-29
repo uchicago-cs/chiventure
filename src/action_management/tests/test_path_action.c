c6635a6261506b6b7b571ea39ba2e37028ac24c8
Elly Choi <ellychoi@uchicago.edu>
2019-05-28 20:00:09 -0500

    preliminary tests

---
+++ src/action_management/tests/test_path_action.c
@@ -0,0 +1,67 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "actionmanagement.h"
+#include "action_structs.h"
+#include "item.h"
+#include "game.h"
+
+bool execute_do_item_action(char *c_name, enum action_kind kind,
+  char *direction, char *dest)
+{
+    game_t *g = game_new;
+    action_type_t *a = action_type_new(c_name, kind);
+    path_t *path = path_new(direction);
+
+
+    char *expected_output = malloc(100); // buffer
+    expected_output[0] = '\0';
+    strcat(expected_output, "Requested action ");
+    strcat(expected_output, c_name);
+    strcat(expected_output, " in direction ");
+    strcat(expected_output, direction);
+    strcat(expected_output, " into room");
+    strcat(expected_output, dest);
+
+    bool rc;
+    if (strcmp(do_path_action(g, a, path), expected_output) == 0)
+    {
+        rc = true;
+    }
+    else
+    {
+        rc = false;
+    }
+
+    free(expected_output);
+    path_free(path);
+    action_type_free(a);
+    game_free(g);
+
+    return rc;
+}
+
+Test(item_actions, wrong_kind_1)
+{
+    bool rc = execute_do_item_action("dummy", ITEM);
+
+    cr_assert_eq(rc, false,
+                  "execute_do_item_item_action returned true for wrong kind 1");
+}
+
+Test(item_actions, correct_kind_2)
+{
+
+    bool rc = execute_do_item_action("dummy", PATH);
+
+    cr_assert_eq(rc, false,
+                 "execute_do_item_item_action returned false for wrong kind 2");
+}
+
+Test(item_actions, wrong_kind_3)
+{
+    bool rc = execute_do_item_action("dummy", ITEM_ITEM);
+
+    cr_assert_eq(rc, true,
+                 "execute_do_item_item_action returned true for correct kind 3");
+}
