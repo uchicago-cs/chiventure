d30e51d4b58bbbff0d9c20e13a19006e4205945d
ecoble <elizabethjiamei@gmail.com>
2019-05-28 19:30:06 -0500

    Reverting action management's makefile to their master version #106

--- src/cli/example/testshell.c
+++ src/cli/example/testshell.c
@@ -31,7 +31,7 @@ char *trim_newline(char *s)
 
 int main()
 {
-    lookup_t ** table = initialize_lookup();
+    lookup_t **table = initialize_lookup();
     int quit = 1;
     char *cmd_string;
     greet();
