dc8db4c299f04c038a34ee48a77b380f3a96e4c5
Matthew Yang <mtyang@uchicago.edu>
2019-05-29 00:58:28 -0500

    oops

--- src/wdl/tests/test_rooms.c
+++ src/wdl/tests/test_rooms.c
@@ -24,12 +24,12 @@ Test(rooms, add_rooms)
     room_t *r = find_room(g, "room A");
     char *s = get_sdesc(r);
     char *scmp = "This is room A";
-    rc = strncmp(s, scmp, 14);
+    rc = strncmp(s, scmp, strlen(scmp));
     cr_assert_eq(rc, SUCCESS, "failed to parse room sdesc");
 
     char *l = get_ldesc(r);
     char *lcmp = "This is room A long";
-    rc = strncmp(l, lcmp, 19);
+    rc = strncmp(l, lcmp, strlen(lcmp));
     cr_assert_eq(rc, SUCCESS, "failed to parse room ldesc");
 
     //TODO: make sure we can check all attribute fields
@@ -49,7 +49,7 @@ Test(rooms, add_connections)
     path_t *p = path_search(r, "north");
     char *to_id = p->dest->room_id;
     char *s = "room B";
-    rc = strncmp(to_id, s, 6);
+    rc = strncmp(to_id, s, strlen(s));
     cr_assert_eq(rc, SUCCESS, "failed to parse connection");
 
     //TODO: make sure we can check every single connection
