7b8666f220d4a3b80230e2cc57a0de4ff1372305
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-31 17:16:22 -0500

    #241 changed tests to account for allowed actions

--- src/action_management/tests/test_item_actions.c
+++ src/action_management/tests/test_item_actions.c
@@ -6,11 +6,13 @@
 #include "item.h"
 #include "game.h"
 
-bool execute_do_item_action(char *c_name, enum action_kind kind)
+bool execute_do_item_action(char *act_name, enum action_kind kind, char *allowed_act_name, enum action_kind allowed_kind)
 {
     game_t *g = game_new;
-    action_type_t *a = action_type_new(c_name, kind);
-    item_t *item = item_new("direct", "The direct item", "The directmost object of interest");
+    action_type_t *a = action_type_new(act_name, kind);
+    action_type_t *allowed_a = action_type_new(act_name, kind);
+    item_t *item = item_new("dummy", "The dummy item", "The dummy object of interest");
+    add_allowed_action(item, allowed_act_name, allowed_a);
 
 
     char *expected_output = malloc(100); // buffer
@@ -19,6 +21,7 @@ bool execute_do_item_action(char *c_name, enum action_kind kind)
     strcat(expected_output, c_name);
     strcat(expected_output, " on ");
     strcat(expected_output, "item");
+    strcat(ret_string, item->item_id);
 
     bool rc;
     if (strcmp(do_item_action(g, a, item), expected_output) == 0)
@@ -33,23 +36,24 @@ bool execute_do_item_action(char *c_name, enum action_kind kind)
     free(expected_output);
     item_free(item);
     action_type_free(a);
+    action_type_free(allowed_a);
     game_free(g);
 
     return rc;
 }
 
-Test(item_actions, correct_kind_1)
+Test(item_actions, correct_kind_1_and_allowed_action)
 {
-    bool rc = execute_do_item_action("dummy", ITEM);
+    bool rc = execute_do_item_action("dummy", ITEM, "dummy", ITEM);
 
     cr_assert_eq(rc, false,
-                  "execute_do_item_action returned false for wrong kind 1");
+                  "execute_do_item_action returned false for correct kind 1");
 }
 
 Test(item_actions, wrong_kind_2)
 {
 
-    bool rc = execute_do_item_action("dummy", PATH);
+    bool rc = execute_do_item_action("dummy", PATH, "dummy", PATH);
 
     cr_assert_eq(rc, false,
                  "execute_do_item_action returned true for wrong kind 2");
@@ -57,8 +61,24 @@ Test(item_actions, wrong_kind_2)
 
 Test(item_actions, wrong_kind_3)
 {
-    bool rc = execute_do_item_action("dummy", ITEM_ITEM);
+    bool rc = execute_do_item_action("dummy", ITEM_ITEM, "dummy", ITEM_ITEM);
 
     cr_assert_eq(rc, true,
-                 "execute_do_item_action returned true for correct kind 3");
+                 "execute_do_item_action returned true for wrong kind 3");
+}
+
+Test(item_actions, action_not_allowed_name)
+{
+    bool rc = execute_do_item_action("dummy", ITEM, "dummy_allow", ITEM);
+
+    cr_assert_eq(rc, false,
+                 "execute_do_item_action returned true for action name that is not allowed");
+}
+
+Test(item_actions, action_not_allowed_struct)
+{
+    bool rc = execute_do_item_action("dummy", ITEM, "dummy_allow", ITEM_ITEM);
+
+    cr_assert_eq(rc, false,
+                 "execute_do_item_action returned true for action struct that is not allowed");
 }
