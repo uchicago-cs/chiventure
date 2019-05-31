a3a4bdc246378b1ff1b4b76e0039d4bbb8fc300b
liuyifan <cnjsnjlyf@gmail.com>
2019-05-31 02:29:49 -0500

    Revert "fixed problems with merge conflicts"
    This reverts commit 4cc91171bd18fda20b45e4c0080c32cea2300ed7.

--- src/game-state/include/item.h
+++ src/game-state/include/item.h
@@ -165,7 +165,11 @@ int set_int_attr(item_t* item, char* attr_name, int value);
  */
 int set_double_attr(item_t* item, char* attr_name, double value);
 
+<<<<<<< HEAD
 /* set_char_attr() sets the value of an attribute of an item to the given char
+=======
+/* set_char_attr() sets the value of an attribute of an item to the given int
+>>>>>>> master
  * Parameters:
  *  a pointer to the item
  *  the attribute with value to be changed
@@ -176,7 +180,11 @@ int set_double_attr(item_t* item, char* attr_name, double value);
  */
 int set_char_attr(item_t* item, char* attr_name, char value);
 
+<<<<<<< HEAD
 /* set_bool_attr() sets the value of an attribute of an item to the given boolean
+=======
+/* set_bool_attr() sets the value of an attribute of an item to the given int
+>>>>>>> master
  * Parameters:
  *  a pointer to the item
  *  the attribute with value to be changed
@@ -247,6 +255,7 @@ char get_char_attr(item_t *item, char* attr_name);
  */
 bool get_bool_attr(item_t *item, char* attr_name);
 
+<<<<<<< HEAD
 /* get_act_attr() returns the string value of an attribute of an item
  * Parameters:
  *  a pointer to the item
@@ -273,5 +282,40 @@ int add_allowed_action(item_t* item, char *act_name, action_type_t *act_type);
  *  SUCCESS if item contains action, FAILURE if it does not
  */
 int allowed_action(item_t* item, char* action_name);
+
+=======
+/*
+ * Function to get a linked list (utlist) of all the attributes in the item
+ *
+ * Parameters:
+ *  item
+ *
+ * Returns:
+ *  linked list of pointers to attributes (the head element)
+ */
+attribute_list_t *get_all_attributes(item_t *item);
+
+/*
+ * Function to delete a linked list (utlist) retrieved from get_all_attributes()
+ *
+ * Parameters:
+ *  linked list of pointers to attributes
+ *
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int delete_attribute_llist(attribute_list_t *head);
+
+/*
+ * Function to delete a linked list (utlist) retrieved from get_all_items()
+ *
+ * Parameters:
+ *  linked list of pointers to items
+ *
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int delete_item_llist(item_list_t *head);
+>>>>>>> master
 
 #endif
