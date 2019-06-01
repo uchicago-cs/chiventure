3cc32e9e6f5d297c22fb2a26cac1351b26738535
Dante Gil-Marin <drgilmarin@icloud.com>
2019-05-31 21:31:37 -0500

    quick fixes from Melinda

--- src/action_management/tests/test_item_actions.c
+++ src/action_management/tests/test_item_actions.c
@@ -5,10 +5,14 @@
 #include "action_structs.h"
 #include "item.h"
 #include "game.h"
+#include "player.h"
 
 bool execute_do_item_action(char *act_name, enum action_kind kind, char *allowed_act_name, enum action_kind allowed_kind)
 {
+    player_t *player = player_new("player", 1);
     game_t *g = game_new("this is a dummy game");
+    add_player_to_game(g, player);
+    set_curr_player(g, player);
     action_type_t *a = action_type_new(act_name, kind);
     action_type_t *allowed_a = action_type_new(allowed_act_name, allowed_kind);
     item_t *item = item_new("dummy", "The dummy item", "The dummy object of interest");
@@ -38,6 +42,7 @@ bool execute_do_item_action(char *act_name, enum action_kind kind, char *allowed
     action_type_free(a);
     action_type_free(allowed_a);
     game_free(g);
+    player_free(player);
 
     return rc;
 }
@@ -46,7 +51,7 @@ Test(item_actions, correct_kind_1_and_allowed_action)
 {
     bool rc = execute_do_item_action("dummy", ITEM, "dummy", ITEM);
 
-    cr_assert_eq(rc, false,
+    cr_assert_eq(rc, true,
                   "execute_do_item_action returned false for correct kind 1");
 }
 
@@ -63,7 +68,7 @@ Test(item_actions, wrong_kind_3)
 {
     bool rc = execute_do_item_action("dummy", ITEM_ITEM, "dummy", ITEM_ITEM);
 
-    cr_assert_eq(rc, true,
+    cr_assert_eq(rc, false,
                  "execute_do_item_action returned true for wrong kind 3");
 }
 
