e15cf7c5dd524daaf6b4874ea7efb3c88d07205c
ecoble <elizabethjiamei@gmail.com>
2019-06-02 22:19:55 -0500

    Update CLI's use of action management's functions #245

--- src/cli/src/operations.c
+++ src/cli/src/operations.c
@@ -9,6 +9,9 @@
 // remove the comment as soon as checkpointing removes their dummy struct
 //#include "../../checkpointing/include/save.h"
 
+/* Buffer size of 100 determined by action management's buffer size, also of 100*/
+#define BUFFER_SIZE (100)
+
 char *quit_operation(char *tokens[TOKEN_LIST_SIZE], game_t *game, lookup_t **table)
 {
     return NULL;
@@ -68,7 +71,10 @@ char *kind1_action_operation(char *tokens[TOKEN_LIST_SIZE], game_t *game, lookup
         if (strcmp(curr_item->item_id,tokens[1]) ==0 )
         {
             action_type_t *action = find_action(tokens[0], table);
-            do_item_action(game, action, curr_item);
+
+            char *str = malloc(BUFFER_SIZE);
+            do_item_action(game, action, curr_item, str);
+            printf("%s", str);
             return "The object is found\n";
         }
     }
@@ -84,7 +90,10 @@ char *kind2_action_operation(char *tokens[TOKEN_LIST_SIZE], game_t *game, lookup
         if (strcmp(curr_path->direction,tokens[1]) == 0)
         {
             action_type_t *action = find_action(tokens[0], table);
-            do_path_action(game, action, curr_path);
+
+            char *str = malloc(BUFFER_SIZE);
+            do_path_action(game, action, curr_path, str);
+            printf("%s", str);
             return "Direction available!\n";
         }
     }
@@ -120,7 +129,10 @@ char *kind3_action_operation(char *tokens[TOKEN_LIST_SIZE], game_t *game, lookup
     }
 
     action_type_t *action = find_action(tokens[0], table);
-    do_item_item_action(game, action, item1, item2);
+
+    char *str = malloc(BUFFER_SIZE);
+    do_item_item_action(game, action, item1, item2, str);
+    printf("%s", str);
     return "is an action!";
 }
 
