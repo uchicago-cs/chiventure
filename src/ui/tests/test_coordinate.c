0d3e92b8f65f49899609e39da34ab863d25a70e5
CS Student <adomiter@uchicago.edu>
2019-06-04 11:47:13 -0500

    debugged unit tests, makefile is still not working

--- src/ui/tests/test_coordinate.c
+++ src/ui/tests/test_coordinate.c
@@ -7,8 +7,8 @@
 // coordinate struct
 Test(coordinate, new)
 {
-    coord_t *c;
-    c = malloc(sizeof(coord_t));
+    //coord_t *c;
+    //c = malloc(sizeof(coord_t));
 
     coord_t *rc;
     rc = malloc(sizeof(coord_t));
@@ -28,6 +28,7 @@ int check_equal(coord_record_t *one, coord_record_t *two)
 	if (one->r->room_id == NULL && two->r->room_id == NULL) {
 		return 1;
 	}
+
 	int one_x = one->key.x;
 	int one_y = one->key.y;
 	int two_x = two->key.x;
@@ -38,6 +39,8 @@ int check_equal(coord_record_t *one, coord_record_t *two)
     if (one_x == two_x && one_y == two_y && one_id == two_id)
     {
         return 1;
+    } else {
+        return 0;
     }
 
 }
@@ -69,6 +72,7 @@ Test(coordinate, not_found)
     memset(cr, 0, sizeof(coord_record_t));
     cr->key.x = 0;
     cr->key.y = 0;
+    cr->key.z = 0;
 
     room_t *initial = malloc(sizeof(room_t));
     initial->room_id = "123";
@@ -76,7 +80,7 @@ Test(coordinate, not_found)
 
     HASH_ADD(hh, coordmap, key, sizeof(coord_t), cr);
 
-    check_find_coord(coordmap, 1, 2, NULL, 0);
+    check_find_coord(coordmap, 1, 2, 1, NULL, 0);
 }
 
 /* Checks that a room present in the hash table returns the correct
@@ -88,30 +92,31 @@ Test(coordinate, found)
     memset(cr, 0, sizeof(coord_record_t));
     cr->key.x = 0;
     cr->key.y = 0;
+    cr->key.z = 0;
 
     room_t *initial = malloc(sizeof(room_t));
-    initial->id = 125;
+    initial->room_id = "125";
     cr->r = initial;
 
     HASH_ADD(hh, coordmap, key, sizeof(coord_t), cr);
 
-    check_find_coord(coordmap, 0, 0, coordmap, 1);
+    check_find_coord(coordmap, 0, 0, 0, coordmap, 1);
 }
 
 //Returns SUCCESS if a coordinate is not present and adds it
-void check_add_coord(coord_record_t *coordmap, int x, int y, room_t *r,
+void check_add_coord(coord_record_t *coordmap, int x, int y, int z, room_t *r,
 						int expected)
 {
 	int rc;
 
-    rc = try_add_coord(coordmap, x, y, r)
+    rc = try_add_coord(coordmap, x, y, z, r);
 
     cr_assert_eq(rc, expected,
                  " Room %.02f has %s but "
                  "try_add_room returned %s",
-                 r->id,
+                 r->room_id,
                  expected? "not been added":"been added",
-                 equal? "false":"true");
+                 rc? "false":"true");
 }
 
 /* Checks that a room not present in the hash table is added */
@@ -122,17 +127,18 @@ Test(coordinate, added_successfully)
     memset(cr, 0, sizeof(coord_record_t));
     cr->key.x = 0;
     cr->key.y = 0;
+    cr->key.z = 0;
 
     room_t *initial = malloc(sizeof(room_t));
-    initial->id = 125;
+    initial->room_id = "125";
     cr->r = initial;
 
     HASH_ADD(hh, coordmap, key, sizeof(coord_t), cr);
 
     room_t *r = malloc(sizeof(room_t));
-    r->id = 24;
+    r->room_id = "24";
 
-    check_add_coord(coordmap, 3, 4, r, 1)
+    check_add_coord(coordmap, 3, 4, 1, r, 1);
 }
 
 /* Checks that a room present in the hash table is not added */
@@ -143,44 +149,44 @@ Test(coordinate, add_failure )
     memset(cr, 0, sizeof(coord_record_t));
     cr->key.x = 0;
     cr->key.y = 0;
+    cr->key.z = 0;
 
     room_t *initial = malloc(sizeof(room_t));
-    initial->id = 125;
+    initial->room_id = "125";
     cr->r = initial;
 
     HASH_ADD(hh, coordmap, key, sizeof(coord_t), cr);
 
-    check_add_coord(coordmap, 0, 0, initial, 0)
+    check_add_coord(coordmap, 0, 0, 0, initial, 0);
 }
 
 //Returns SUCCESS if all rooms are assigned coordinates
-void check_assign_dfs(coord_record_t *coordmap, int vertical_hops,
-           int horizontal_hops, room_t* room, ctx_t *context, int expected)
+/*void check_assign_dfs(coord_record_t *coordmap, int vertical_hops, 
+           game_t *game, int horizontal_hops, room_t* room, int expected)
 {
     int rc;
 
-    rc = assign(coordmap, vertical_hops, horizontal_hops, room, context)
+    rc = assign(coordmap, vertical_hops, horizontal_hops, room, game);
 
     cr_assert_eq(rc, expected,
                  "The map is %s but "
                  "create_valid_map returned %s",
                  expected? "is not a valid map":"is a valid map",
-                 equal? "failure":"success");
-}
+                 rc? "failure":"success");
+}*/
 
 //Returns SUCCESS if all rooms are assigned coordinates
-void check_create_valid_map(coord_record_t *coordmap,
+void check_create_valid_map(game_t *game,
 					int expected)
 {
     coord_record_t *end_map;
     end_map = malloc(sizeof(coord_record_t));
 
-    end_map = create_valid_map();
+    end_map = create_valid_map(game);
 
-    int
-    cr_assert_eq(rc, expected,
+    cr_assert_eq(end_map, expected,
                  "The map is %s but "
                  "create_valid_map returned %s",
                  expected? "is not a valid map":"is a valid map",
-                 equal? "failure":"success");
+                 end_map? "failure":"success");
 }
