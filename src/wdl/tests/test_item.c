c5f4c4acba2a35f1e34ada623080d39d3ed46b77
Victor <jesusaa@uchicago.edu>
2019-06-04 22:18:46 -0500

    #179 Wrote function to automate testing of load_items()

--- src/wdl/tests/test_item.c
+++ src/wdl/tests/test_item.c
@@ -3,62 +3,34 @@
 #include <stdbool.h>
 #include "load_item.h"
 
-//TEMPORARILY ADDING FUNCTIONS FOR TESTING; WILL REMOVE BEFORE PR
-//**********************************************
-/* see item.h */
-char *get_sdesc_item(item_t *item)
-{
-  if (item == NULL) {
-    return NULL;
-  }
-  return item->short_desc;
-}
-
-/* see item.h */
-char *get_ldesc_item(item_t *item)
-{
-  if (item == NULL) {
-    return NULL;
-  }
-  return item->long_desc;
-}
-//**********************************************
-
-Test(items, test1)
+/* a function used to automate the testing of loading items 
+ * 
+ * parameters:
+ *  - item: the id of the item as in the wdl file that is being loaded and tested
+ *  - room: the id of the room in which the item is located, as written in the wdl file
+ *  - index: a number indicating the position of the item in the list of items
+ */
+void item_check(char* room, char *item, char *index)
 {
     obj_t *doc = get_doc_obj();
     game_t *g = game_new("Welcome to UChicago");
-    
+
     // check adding items to game
-    int rc = load_items(doc, g);
-    cr_assert_eq(rc, SUCCESS, "loading items failed");
-}
-
-
-Test(items, correct_fields_chair)
-{
-    obj_t *doc = get_doc_obj();
-    game_t *g = game_new("Welcome to UChicago");
-
-    // check adding rooms to game
     int rc = add_rooms_to_game(doc, g);
     cr_assert_eq(rc, SUCCESS, "adding rooms to game failed");
 
-    // check adding items to game
-    rc = load_items(doc, g);
-    cr_assert_eq(rc, SUCCESS, "loading items failed");
-
     // checking fields were correctly filled
-    room_t *r = find_room_from_game(g, "room A");
-    item_t *i = get_item_in_room(r, "chair");
+    room_t *r = find_room_from_game(g, room);
+    item_t *i = get_item_in_room(r, item);
 
-    char *s = get_sdesc_item(i);
-    char *scmp = "This is a chair";
-    rc = strncmp(s, scmp, strlen(scmp));
-    cr_assert_eq(rc, SUCCESS, "failed to parse item sdesc");
+    char *item_path = malloc(100); // setting up string buffer
+    strncat(item_path, "ITEMS.", 6);
+    strncat(item_path, index, 94);
+    obj_t item_obj = obj_get_attr(doc, item_path, false);
 
-    char *l = get_ldesc_item(i);
-    char *lcmp = "This is a chair long";
-    rc = strncmp(l, lcmp, strlen(lcmp));
-    cr_assert_eq(rc, SUCCESS, "failed to parse item ldesc");
+    rc = strcmp(obj_get_str(item_obj, "short_desc"), get_sdesc_item(i));
+    cr_assert_eq(rc, 0, "failed to load item short_desc");
+
+    rc = strcmp(obj_get_str(item_obj, "long_desc"), get_ldesc_item(i));
+    cr_assert_eq(rc, 0, "failed to load item long_desc");
 }
