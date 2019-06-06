231880a2f3205911c7dbb8f0687cc0dd46dcc38c
yoonsjeong <yoonsjeong@uchicago.edu>
2019-06-06 15:15:13 -0500

    added actions and items to the sample game, as per issue #278

--- src/common/src/sample_game.c
+++ src/common/src/sample_game.c
@@ -1,4 +1,21 @@
 #include "sample_game.h"
+#include "item.h" // to get item_t
+#include "action_structs.h" // to get action_type_t
+#include "actionmanagement.h" // for get_supported_actions
+#include "string.h" // for strcmp
+
+
+/* this is a function used to get the actions needed*/
+action_type_t *search_supported_actions(list_action_type_t *head, char *query)
+{
+    list_action_type_t *temp;
+    for (temp = head; temp != NULL; temp = temp->next) {
+        if (strcmp(temp->act->c_name, query) == 0) {
+            return temp->act;
+        }
+    }
+    return NULL;
+}
 
 /* See sample_game.h */
 game_t* create_sample_game()
@@ -22,6 +39,24 @@ game_t* create_sample_game()
     /* initialize items */
     item_t *item_orb = item_new("orb", "magical orb", "an orb that can be held, placed, or tossed");
     item_t *item_table = item_new("table", "a regular table", "this table can have things placed on it");
+    
+    /* introduce actions */
+    list_action_type_t *head = get_supported_actions();
+    action_type_t *actiontype_take = search_supported_actions(head, "TAKE");
+    action_type_t *actiontype_drop = search_supported_actions(head, "DROP");
+    action_type_t *actiontype_puton = search_supported_actions(head, "PUT_ON");
+
+    /* add valid actions to each item */
+    // NOTE: I based this off *NEW* code from game-state/develop-actions
+    // it will not work in this branch because develop-actions is not merged to master
+    add_action(item_orb, "TAKE", actiontype_take, 
+               "You have placed the orb in your pocket.", "Can't perform that action!");
+    add_action(item_orb, "DROP", actiontype_drop,
+               "You have dropped the orb.", "Can't perform that action!");
+    add_action(item_orb, "PUT_ON", actiontype_puton,
+               "You have placed the orb on the table", "Can't perform that action!");
+    add_action(item_table, "PUT_ON", actiontype_puton,
+                "You have placed the orb on the table", "Can't perform that action!");
 
     /* add items to room */
     add_item_to_room(room1, item_orb);
