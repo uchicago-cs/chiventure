1b3e0abaa81b079940457520d571bbb0b8aa2f86
Elly Choi <ellychoi@uchicago.edu>
2019-06-07 16:17:08 -0500

    updated strings

--- src/action_management/tests/test_path_actions.c
+++ src/action_management/tests/test_path_actions.c
@@ -23,7 +23,7 @@ int execute_do_path_action(char *c_name, enum action_kind kind)
     char *string = malloc(BUFFER_SIZE);
 
     int rc = do_path_action(g, a, p, &string);
- 
+
     path_free(p);
     action_type_free(a);
     game_free(g);
@@ -37,7 +37,7 @@ Test(path_actions, kind_ITEM)
     int rc = execute_do_path_action("dummy", ITEM);
 
     cr_assert_eq(rc, WRONG_KIND,
-                 "execute_do_item_item_action returned %d for wrong kind ITEM, expected WRONG_KIND (1)", rc);
+                 "execute_do_path_action returned %d for wrong kind ITEM, expected WRONG_KIND (1)", rc);
 }
 
 
@@ -47,7 +47,7 @@ Test(path_actions, kind_PATH)
     int rc = execute_do_path_action("dummy", PATH);
 
     cr_assert_eq(rc, SUCCESS,
-                 "execute_do_item_item_action returned %d for correct kind PATH expected SUCCESS (0)", rc);
+                 "execute_do_path_action returned %d for correct kind PATH expected SUCCESS (0)", rc);
 }
 
 
@@ -56,5 +56,5 @@ Test(path_actions, kind_ITEM_ITEM)
     int rc = execute_do_path_action("dummy", ITEM_ITEM);
 
     cr_assert_eq(rc, WRONG_KIND,
-                 "execute_do_item_item_action returned %d for wrong kind ITEM_ITEM expected WRONG_KIND (1)");
+                 "execute_do_path_action returned %d for wrong kind ITEM_ITEM expected WRONG_KIND (1)");
 }
