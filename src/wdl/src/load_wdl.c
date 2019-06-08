f0e5e3c6aa59d03caea2152b8a06730e4a6c3e4e
Thor Anderskow <tanderskow@cs.uchicago.edu>
2019-06-08 15:41:42 -0500

    fixed obj_get_str in load_wdl.c #312

---
+++ src/wdl/src/load_wdl.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "parse.h"
+#include "load_wdl.h"
+
+/*
+ * load_wdl, function that loads a wdl into all the game-state structs
+ *
+ * has no parameters, automatically load yaml found in FILE_PATH
+ *
+ *
+ */
+int load_wdl()
+{
+  obj_t *big_document = get_doc_obj(PATH_TO_YAML);
+  game_t *game = create_game(big_document);
+
+  int rooms = add_rooms_to_game(big_document, game);
+  int connections =  add_connections_to_rooms(big_document, game);
+  char *start_room = obj_get_str(big_document, "start");
+  game_set_start(start_room, game);
+  int items = load_items(big_document, game);
+
+  if((rooms + connections + items) == 0){
+    return 0;
+  }else{
+    return 1;
+  }
+}
+
+    
