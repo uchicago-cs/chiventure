863793f75fc3dfb4ab317f59b4d387c743b1b6a2
Tianle Liu <liutianle@cs.uchicago.edu>
2019-06-07 23:58:56 -0500

    Added test for cmd_new() #216

--- src/cli/tests/test_cmd.c
+++ src/cli/tests/test_cmd.c
@@ -1,12 +1,12 @@
 #include <criterion/criterion.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "cmd.c"
+#include "cmd.h"
 
 /* Checks if the memory for a lookup table is successfully allocated */
 Test(lookup, new)
 {
-    lookup_t *l;
+    lookup_t **l;
 
     l = lookup_t_new();
 
@@ -16,10 +16,10 @@ Test(lookup, new)
 /* Checks if a lookup table is be successfully initialized */
 Test(lookup, init)
 {
-    lookup_t l;
+    lookup_t *l;
     int rc;
 
-    rc = lookup_t_init(&p, 42, 37);
+    rc = lookup_t_init(&l);
 
     cr_assert_eq(rc, SUCCESS, "lookup_t_init() failed");
 }
@@ -27,7 +27,7 @@ Test(lookup, init)
 /* Checks if a lookup table is successfully freed */
 Test(lookup, free)
 {
-    lookup_t *l;
+    lookup_t **l;
     int rc;
 
     l = lookup_t_new();
@@ -39,13 +39,27 @@ Test(lookup, free)
     cr_assert_eq(rc, SUCCESS, "lookup_t_free() failed");
 }
 
-/* */
+/* Checks if a new cmd struct is successfully created */
 Test(cmd, new)
 {
+    char *token[1] = {"LOOK"}, *tokens[2] = {"Take", "orb"};
+    cmd *c1, *c2;
+    c1 = cmd_new(token);
+    c2 = cmd_new(tokens);
+
+    cr_assert_not_null(c1, "cmd_new() failed");
+    cr_assert_not_null(c2, "cmd_new() failed");
+
+    cr_assert_eq(c1->tokens, token, 
+                 "cmd_new() didn't set the tokens field correctly");
+    cr_assert_eq(c2->tokens, token, 
+                 "cmd_new() didn't set the tokens field correctly");
 }
 
 /* */
 Test(cmd, init)
+{
+}
 
 /* */
 Test(cmd, free)
