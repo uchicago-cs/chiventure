dfa2712d7892cbb1df7933a187ff2ecb7f5ba68a
Victor <jesusaa@uchicago.edu>
2019-06-08 19:14:17 -0500

    #312 Added tests to check that load_wdl loads rooms and items

--- src/wdl/tests/test_wdl.c
+++ src/wdl/tests/test_wdl.c
@@ -12,3 +12,27 @@ Test(wdl, load_wdl)
   cr_assert_str_eq(start_desc, "This is the intro", "Intro was not loaded corr\
 ectly");
 }
+
+/*
+ * checks that rooms were loaded
+ */
+Test(wdl, loading_rooms)
+{
+    game_t *game = load_wdl();
+
+    room_list_t *rc = get_all_rooms(game);
+
+    cr_assert_not_null(rc, "load_wdl did not load rooms");
+}
+
+/*
+ * checks that items were loaded
+ */
+Test(wdl, loading_items)
+{
+    game_t *game = load_wdl();
+
+    item_list_t *rc = get_all_items_in_game(game);
+
+    cr_assert_not_null(rc, "load_wdl did not load items");
+}
