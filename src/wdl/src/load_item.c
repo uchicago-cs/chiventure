cdec779fa83738398eb7ba7e40674c12228b8936
Shriya Bansal <bansals@uchicago>
2019-06-10 14:22:12 -0500

    finished loading attributes #320

--- src/wdl/src/load_item.c
+++ src/wdl/src/load_item.c
@@ -94,6 +94,13 @@ int load_items(obj_t *doc, game_t *g)
         //load attributes into item
         // getting a list of actions from item
         attr_list_t *attr_ls = get_item_attributes(doc);
+        while (attr_ls != NULL)
+        {
+            char* attr_name = obj_get_str(attr_ls->obj, "attribute");
+            char* val = obj_get_str(attr_ls->obj, "value");
+            set_str_attr(item, attr_name, val);
+            attr_ls = attr_ls->next;
+        }
 
         
 
@@ -109,6 +116,7 @@ int load_items(obj_t *doc, game_t *g)
 
         // add item to room
         add_item_to_room(item_room, item);
+        //add_item_to_game(g, item);
         curr = curr->next;
     }
     return 0;
