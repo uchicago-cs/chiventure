53c8d3e7e99917b48acc660bc21092b69140da70
Thor Anderskow <tanderskow@cs.uchicago.edu>
2019-06-10 23:44:00 -0500

    added end condition stuff in load_wdl.c

--- src/wdl/src/load_wdl.c
+++ src/wdl/src/load_wdl.c
@@ -17,8 +17,14 @@ game_t *load_wdl()
   obj_t *game_document = obj_get_attr(big_document, "GAME.0", false);
   char *start_room = obj_get_str(game_document, "start");
   game_set_start(start_room, game);
+  char *start_room = obj_get_str(game_document, "end");
+  room_t *final_room = find_room_from_game(game, final_room);
+  int final = add_final_room_to_game(game, final_room);
   int items = load_items(big_document, game);
 
+  if(final == FAILURE){
+    return NULL;
+  }
   if((rooms + connections) == 0){
     if(items == -1){//load_items is the only one that returns -1 on failure
       return NULL;
