6c332a9de9db43c18dcf2c93daf24d8674ed10df
Christian Bjerre-Fernandes <chrisfernandes18@cs.uchicago.edu>
2019-06-11 17:57:50 -0500

    fixed tests

--- src/checkpointing/tests/test_load.c
+++ src/checkpointing/tests/test_load.c
@@ -18,10 +18,10 @@ Test(load, item)
     int success = load_item(candy, candy_t);
 
     cr_assert_eq(success, 0, "save_item failed");
-    cr_assert_eq(candy_t->item_id, "1234", "load_item: loading item_id failed");
-    cr_assert_eq(candy_t->short_desc, "salt water taffy",
+    cr_assert_str_eq(candy_t->item_id, "1234", "load_item: loading item_id failed");
+    cr_assert_str_eq(candy_t->short_desc, "salt water taffy",
 		 "load_item: loading short_desc failed");
-    cr_assert_eq(candy_t->long_desc, "glues mouth shut",
+    cr_assert_str_eq(candy_t->long_desc, "glues mouth shut",
 		 "load_item: loading long_desc failed");
 }
 
@@ -32,54 +32,57 @@ Test(load, room)
     dorm->short_desc = "college dorm";
     dorm->long_desc = "there are clothes and empty ramen cups everywhere";
 
-    Item **items = malloc(sizeof(Item*) * 2);
+    Item **items = malloc(sizeof(Item*) * 3);
     items[0] = malloc(sizeof(Item));
     items[0]->item_id = "1234";
     items[0]->short_desc = "cup ramen";
     items[0]->long_desc = "has already been eaten";
     items[1] = malloc(sizeof(Item));
-    items[1]->item_id = "1234";
     items[1]->item_id = "4321";
     items[1]->short_desc = "black sweatshirt";
     items[1]->long_desc = "wrinkled and probably needs to be washed";
+    items[2] = malloc(sizeof(Item));
+    items[2]->item_id = "5678";
+    items[2]->short_desc = "salt water taffy";
+    items[2]->long_desc = "glues mouth shut";
     dorm->items = items;
 
     item_t** all_items = malloc(sizeof(item_t*) * 3);
     int itemsucc;
-    for(int i = 0; i < 2; i++){
+    for(int i = 0; i < 3; i++){
         all_items[i] = malloc(sizeof(item_t));
         itemsucc = load_item(items[i], all_items[i]);
         cr_assert_eq(itemsucc, 0,
                      "load_item: loading item %d in load_room failed", i);
     }
-    Item *candy = malloc(sizeof(Item));
-    candy->item_id = "1234";
-    candy->short_desc = "salt water taffy";
-    candy->long_desc = "glues mouth shut";
-    all_items[2] = malloc(sizeof(item_t));
-    itemsucc = load_item(candy, all_items[2]);
-    cr_assert_eq(itemsucc, 0, "load_item: loading item 2 in load_room failed");
 
-    room_t* room_t = malloc(sizeof(room_t));
-    int succ = load_room(dorm, room_t, all_items, 3);
+    room_t room_t;
+
+    int succ = load_room(dorm, &room_t, all_items, 3);
 
     cr_assert_eq(succ, 0, "load_room failed");
-    cr_assert_eq(room_t->room_id, "5", "load_room: loading room_id failed");
-    cr_assert_eq(room_t->short_desc, "college dorm",
+
+    cr_assert_str_eq(room_t.room_id, "5", "load_room: loading room_id failed");
+
+    cr_assert_str_eq(room_t.short_desc, "college dorm",
 		 "load_room: loading short_desc failed");
-    cr_assert_eq(room_t->long_desc,
+
+    cr_assert_str_eq(room_t.long_desc,
 		 "there are clothes and empty ramen cups everywhere",
 		 "load_room: loading long_desc failed");
+
     /* We potentially need to talk to game-state about a function that let's us
      * check the contents of the item hashtable within a room.
      */
+
 }
 
 Test(load, player)
 {
     Player *chad = malloc(sizeof(Player));
     chad->health = 1;
-
+    chad->player_id = "Chad";
+    
     Item **items = malloc(sizeof(Item*) * 2);
     items[0] = malloc(sizeof(Item));
     items[0]->item_id = "1234";
@@ -237,8 +240,8 @@ Test(load, game)
     success = load_game(game, g_t);
 
     cr_assert_eq(success, 0, "load_game failed");
-    cr_assert_eq(g_t->curr_room->room_id, "5", "load_game: loading curr_room failed %s", g_t->curr_room->room_id);
-    cr_assert_eq(g_t->curr_player->player_id, "chad", "load_game: loading curr_player failed");
+    cr_assert_str_eq(g_t->curr_room->room_id, "5", "load_game: loading curr_room failed %s", g_t->curr_room->room_id);
+    cr_assert_str_eq(g_t->curr_player->player_id, "chad", "load_game: loading curr_player failed");
     // need hashtable searching options to check all_players and all_rooms
     room_list_t * all_rooms1 = get_all_rooms(g_t);
     int count_r = 0;
