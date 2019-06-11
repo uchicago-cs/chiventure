529c7acda96902a58f2524526cf65d647607da65
Jonah Fleishhacker <jfleishhacker@cs.uchicago.edu>
2019-06-11 13:50:31 -0500

    Revert "style edits"
    This reverts commit 5026411993277edeaed1e4b2bfa398a6bc0b72ab.

--- src/cli/src/operations.c
+++ src/cli/src/operations.c
@@ -28,43 +28,47 @@ bool validate_filename(char *filename)
 {
   int len = strlen(filename);
   int min_filename_length = 4;
-  if (len < min_filename_length) {
+  if(len < min_filename_length)
+    {
       return false;
-  }
+    }
   const char *ending = &filename[len-4];
   int cmp = strcmp(ending, ".dat");
-  if (cmp == 0) {
+  if(cmp == 0)
+    {
       return true;
-  } else {
+    }
+  else
+    {
       return false;
-  }
+    }
 }
 
 /* See operations.h */
 char *save_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
-  if (tokens[1] == NULL) {
+  if(tokens[1] == NULL){
     return "Invalid Input, Save failed\n";
   }
-  if (validate_filename(tokens[1]) == true) {
+  if (validate_filename(tokens[1]) == true){
     int sv = save(ctx->game, tokens[1]);
     return "Game Saved\n";
-  } else {
-    return "Improper filename, Save failed\n";
   }
+  else
+    return "Improper filename, Save failed\n";
 }
 
 char *load_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
-  if (tokens[1] == NULL) {
-      return "Invalid Input, Load failed\n";
+  if(tokens[1] == NULL){
+    return "Invalid Input, Load failed\n";
   }
-  if (validate_filename(tokens[1]) == true) {
-      int ld = load(tokens[1], ctx->game);
-      return "Load Succesful\n!";
-  } else {
+  if (validate_filename(tokens[1]) == true){
+    int ld = load(tokens[1], ctx->game);
+    return "Load Succesful\n!";
+  }
+  else
     return "Improper filename, Load failed\n";
-  }
   
 }
 
@@ -72,14 +76,18 @@ bool validate_filename(char *filename)
 {
     int len = strlen(filename);
     int min_filename_length = 4;
-    if (len < min_filename_length) {
+    if(len < min_filename_length)
+    {
         return false;
     }
     const char *ending = &filename[len-4];
     int cmp = strcmp(ending, ".dat");
-    if (cmp == 0) {
+    if(cmp == 0)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
@@ -89,7 +97,8 @@ cmd *assign_action(char **ts, lookup_t ** table)
 {
     cmd *output = cmd_new(ts);
     output->func_of_cmd = find_operation(ts[0], table);
-    if(output->func_of_cmd == NULL) {
+    if(output->func_of_cmd == NULL)
+    {
         output->func_of_cmd = action_error_operation;
     }
     return output;
@@ -98,19 +107,24 @@ cmd *assign_action(char **ts, lookup_t ** table)
 char *look_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
     game_t *game = ctx->game;
-    if (game == NULL || game->curr_room == NULL) {
+    if(game == NULL || game->curr_room == NULL)
+    {
         return "Room not found! Error! We need a room to be loaded to LOOK!\n";
     }
-    if (tokens[1] == NULL) {   
-        if (game != NULL) {
+    if(tokens[1] == NULL)
+    {   
+        if(game != NULL){
             return game->curr_room->long_desc;
-        } else {
+        }
+        else
+        {
             return "Error !";
         }
     }
     item_t *curr_item = NULL;
     curr_item = get_item_in_room(game->curr_room, tokens[1]);
-    if (curr_item != NULL) {
+    if(curr_item != NULL)
+    {
         return curr_item->long_desc;
     }
     return "specified item not found\n";
@@ -120,20 +134,23 @@ char *look_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 char *kind1_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
     game_t *game = ctx->game;
-    if (game == NULL || game->curr_room == NULL) {
+    if(game == NULL || game->curr_room == NULL)
+    {
         print_to_cli(ctx, tokens[0]);
         return ( "Error! We need a loaded room to do the above action. \n");
     }
     lookup_t **table = ctx->table;
 
-    if (tokens[1] == NULL) {
+    if(tokens[1] == NULL)
+    {
         return "You must identify an object to act on\n";
     }
     item_t *curr_item;
     curr_item = get_item_in_room(game->curr_room, tokens[1]);
-    if (curr_item != NULL) {
+    if(curr_item != NULL)
+    {
         action_type_t *action = find_action(tokens[0], table);
-	
+
         char *str;
         do_item_action(game, action, curr_item, &str);
         printf("%s", str);
@@ -146,21 +163,24 @@ char *kind1_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
 char *kind2_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
     game_t *game = ctx->game;
-    if (game == NULL || game->curr_room == NULL) {
+    if(game == NULL || game->curr_room == NULL)
+    {
         print_to_cli(ctx, tokens[0]);
         return "Error! We need a loaded room to do the above action. \n";
     }
     lookup_t **table = ctx->table;
 
     path_t *curr_path;
-    ITER_ALL_PATHS(game->curr_room, curr_path) {
-        if (strcmp(curr_path->direction,tokens[1]) == 0) {
+    ITER_ALL_PATHS(game->curr_room, curr_path)
+    {
+        if(strcmp(curr_path->direction,tokens[1]) == 0)
+        {
             action_type_t *action = find_action(tokens[0], table);
-	    
+
             char *str;
             do_path_action(game, action, curr_path, &str);
             printf("%s", str);
-	    
+
             return "Direction available!\n";
         }
     }
@@ -171,13 +191,15 @@ char *kind2_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
 char *kind3_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
     game_t *game = ctx->game;
-    if (game == NULL || game->curr_room == NULL) {
+    if(game == NULL || game->curr_room == NULL)
+    {
         print_to_cli(ctx, tokens[0]);
         return "Error! We need a loaded room to do the above action. \n";
     }
     lookup_t **table = ctx->table;
 
-    if (tokens[1] == NULL || tokens[3] == NULL) {
+    if(tokens[1] == NULL || tokens[3] == NULL)
+    {
         return "You must identify two objects to act on";
     }
 
@@ -185,7 +207,8 @@ char *kind3_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
     item1 = get_item_in_room(game->curr_room, tokens[1]);
     item2 = get_item_in_room(game->curr_room, tokens[3]);
     
-    if (item1 == NULL || item2 == NULL) {
+    if(item1 == NULL || item2 == NULL)
+    {
         return "The object(s) could not be found";
     }
     action_type_t *action = find_action(tokens[0], table);
@@ -205,7 +228,8 @@ char *action_error_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
 char *inventory_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
     game_t *game = ctx->game;
-    if (game == NULL || game->curr_player == NULL) {
+    if(game == NULL || game->curr_player == NULL)
+    {
         print_to_cli(ctx, tokens[0]);
         return "Error! We need a loaded player to check inventory.\n";
     }
