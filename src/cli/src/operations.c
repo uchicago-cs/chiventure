5987d9ce9fd55b3930e364d7bc0b8930763118a5
ecoble <elizabethjiamei@gmail.com>
2019-06-11 13:15:50 -0500

    Update strings returned from am to print to window #356

--- src/cli/src/operations.c
+++ src/cli/src/operations.c
@@ -109,8 +109,7 @@ char *kind1_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
         action_type_t *action = find_action(tokens[0], table);
         char *str;
         do_item_action(action, curr_item, &str);
-        printf("%s", str);
-        return "The object is found\n";
+        return str;
     }
     return "The object could not be found\n";
 }
@@ -135,9 +134,8 @@ char *kind2_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
 
             char *str;
             do_path_action(ctx, action, curr_path, &str);
-            printf("%s", str);
 
-            return "Direction available!\n";
+            return str;
         }
     }
     return "You cannot go in this direction\n";
@@ -171,8 +169,8 @@ char *kind3_action_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ct
 
     char *str;
     do_item_item_action(action, item1, item2, &str);
-    printf("%s", str);
-    return "is an action!";
+    
+    return str;
 }
 
 
