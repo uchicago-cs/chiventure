2f453ad72f3433a044cb90a2c66dcfadbf1d7589
yoonsjeong <yoonsjeong@uchicago.edu>
2019-06-11 23:38:53 -0500

    changed snprint

--- src/action_management/tests/test_path_actions.c
+++ src/action_management/tests/test_path_actions.c
@@ -20,9 +20,9 @@ void check_do_path(chiventure_ctx_t *c, action_type_t *a, path_t *p, room_t *roo
     rc = do_path_action(c, a, p, &ret_string);
     room_output = c->game->curr_room;
 
-    // cr_expect_eq(room_expected, room_output,
-    //              "The room expected, %s, did not match the actual room output, %s.",
-    //              room_expected->room_id, room_output->room_id);
+    cr_expect_eq(room_expected, room_output,
+                 "The room expected, %s, did not match the actual room output, %s.",
+                 room_expected->room_id, room_output->room_id);
     cr_assert_eq(rc_expected, rc,
                  "The expected return code was %d, but the actual return code was %d. The return string given: %s.",
                  rc_expected, rc, ret_string);
