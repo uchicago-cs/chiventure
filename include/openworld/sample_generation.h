39e722c9ba7030de587a9ff2ab6d2e841a259234
Chanik Bryan Lee <bryan22lee@gmail.com>
2020-05-08 16:33:44 -0500

    RPG-openworld: opened include and src directies with header and source code files (respectively) for designing and implementing a simple sample room generation module

---
+++ include/openworld/sample_generation.h
@@ -0,0 +1,12 @@
+/* Team RPG-Openworld
+ * 
+ * sample_generation.h: This file: function prototypes and purposes from 
+ * the functions defined in chiventure/src/sample_generation.c
+ * 
+ * Sprint 2: 
+ * Create a high level room generation module that pulls the lower-level 
+ * room generation modules together
+ * 
+ * See chiventure/include/sample_generation.c source code file to see function 
+ * prototypes and purposes
+ */
