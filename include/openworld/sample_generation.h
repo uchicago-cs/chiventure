7599f35d4f789a9acabb7ba8d9bfb8cfb2cbf519
Chanik Bryan Lee <bryan22lee@gmail.com>
2020-05-08 22:21:36 -0500

    Added two functions to sample_generation. Together, they will eventually auto-add a hard-coded room when a new room is entered based on autogen algorithm

--- include/openworld/sample_generation.h
+++ include/openworld/sample_generation.h
@@ -1,5 +1,7 @@
 /* Team RPG-Openworld
  * 
+ * Sample-Generation header file
+ * 
  * sample_generation.h: This file: function prototypes and purposes from 
  * the functions defined in chiventure/src/sample_generation.c
  * 
@@ -10,3 +12,64 @@
  * See chiventure/include/sample_generation.c source code file to see function 
  * prototypes and purposes
  */
+
+#ifndef INCLUDE_SAMPLE_GENERATION_H
+#define INCLUDE_SAMPLE_GENERATION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+// INCLUDE HARD-CODE ROOMS AND AUTOGENERATION MODULE
+#include "game-state/game.h"
+#include "wdl/load_room.h"
+#include "game-state/game_state_common.h"
+#include "common/uthash.h"
+
+// ASSUME HARD-CODED ROOMS AVAILABLE FROM ANOTHER MODULE
+
+/*
+ * enter_new_room
+ * Did the player move from one room to another in the same game?
+ * Compares two rooms based on UT_hash_handle to see if they are different
+ * Assume that the input rooms are from the same game in different states
+ * 
+ * parameters:
+ * - r1: a room pointer for input room 1 (old game state)
+ * - g2: a room pointer for input room 2 (new game state, after entering room)
+ *
+ * side effects:
+ * - None. Does not alter room/game states. Just compares them to determine if 
+ * a new room was entered. Assume old room state is saved on the heap before a 
+ * path action is taken, so that they can be compared after a path action.
+ *
+ * returns:
+ * - true if a new room was entered
+ * - false if a new room was not entered
+ */
+bool enter_new_room(room_t *r1, room_t *r2);
+
+/*
+ * room_generate
+ * Given two game states and a room to add, add the room to the new game 
+ * state (connected to the current room) if and only if the curr_room field 
+ * is different between the two games
+ * 
+ * Freeing gameOld is to be handled outside of this function
+ * 
+ * parameters:
+ * - gameOld: a game pointer for the old game state
+ * - gameNew: a game pointer for the old game state
+ * - addRoom: a room pointer for the room to add if a new room was entered
+ *
+ * side effects:
+ * - Changes gameNew to add a room to the list of all_rooms and edits the 
+ * current_room struct to have a path to the new room
+ *
+ * returns:
+ * - SUCCESS if the new room (addRoom) was added
+ * - FAILURE if the new room (addRoom) was not added
+ */
+int room_generate(game_t* gameOld, game_t* gameNew, room_t* addRoom);
+
+#endif /* INCLUDE_SAMPLE_GENERATION_H */
---
+++ src/openworld/sample_generation/sample_generation.c
@@ -0,0 +1,46 @@
+/* Team RPG-Openworld
+ * 
+ * sample_generation.c: This file. Function definitions of the functions 
+ * specified in chiventure/include/sample_generation.h
+ * 
+ * Sprint 2: 
+ * Create a high level room generation module that pulls the lower-level 
+ * room generation modules together
+ * 
+ * See chiventure/include/sample_generation.h header file to see function 
+ * prototypes and purposes
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../../../include/sample_generation.h"
+
+/* See sample_generation.h */
+bool enter_new_room(room_t *r1, room_t *r2) {
+    // Check if they have the same hash handle pointer
+    return (bool) (r1->hh.key == r2->hh.key)
+}
+
+/* See sample_generation.h */
+int room_generate(game_t* gameOld, game_t* gameNew, room_t* addRoom) {
+    /* BELOW: implement algo from autogenerate algorithm module */
+    if (enter_new_room(gameOld->curr_room, gameNew->curr_room)) {
+        /* BELOW: TO BE EDITED */
+
+        // Add addRoom to gameNew
+        HASH_ADD_TO_TABLE(addRoom->hh, gameNew->all_rooms, addRoom->hh.key, 
+        addRoom->hh.keylen, addRoom->hh.hashval, add, oomed); 
+        // (HASH_ADD_TO_TABLE) I have no idea what these "add" and "oomed" parameters are
+        
+        // Add path from the current room to addRoom
+        path_t* path_to_room = path_new(addRoom, "to new room");
+        assert (0 == add_path_to_room(gameNew->curr_room, path_to_room));
+
+        /* ABOVE: TO BE EDITED */
+
+        return 0; /* SUCCESS - room added */
+    }
+
+    return 1; /* room not added */
+}
