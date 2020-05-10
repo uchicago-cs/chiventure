4763a6ea2afac2616714bec78338d49f2fccddc9
Sophie Veys <sophiev@cs.uchicago.edu>
2020-05-10 13:44:12 -0400

    wohoo lez go fellas

--- src/battle/sandbox/players_stats.c
+++ src/battle/sandbox/players_stats.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "structs.h"
-#include "player_stats.h"
+//#include "player_stats.h"
 
 
+/** dummy stats **/
 stats_t * create_test_stats()
 {
 	stats_t *test_stats = malloc(sizeof(stats_t));
@@ -15,12 +16,14 @@ stats_t * create_test_stats()
 	return test_stats;
 }
 
+/** make empty inventory list **/
 ilist_t *create_test_inventory()
 {
 	ilist_t *inv_list = malloc(sizeof(ilist_t));
-	return inv_list;	
+	return inv_list;
 }
 
+/** package item info into item struct **/
 item_t *create_item(int id, int quan, int durab, char* descrip, bool bat, int attack, int defense, int hp)
 {
 	item_t *new_item = (item_t*) malloc(sizeof(item_t));
@@ -29,16 +32,17 @@ item_t *create_item(int id, int quan, int durab, char* descrip, bool bat, int at
 	new_item->durability = durab;
 	new_item->description = (char*) malloc(sizeof(char) * 25);
 	strcpy(new_item->description, descrip);
-	new_item->id = ID;
-	new_item->quantity = quantity;
-	new_item->durability = durability;
-	new_item->battle = battle;
+	new_item->id = id;
+	new_item->quantity = quan;
+	new_item->durability = durab;
+	new_item->battle = bat;
 	new_item->attack = attack;
 	new_item->defense = defense;
 	new_item->hp = hp;
 	return new_item;
 }
 
+/** add item to inventory list **/
 ilist_t *add_item_to_inventory(ilist_t *inv, item_t *item)
 {
 	ilist_t *new_item = (ilist_t*) malloc(sizeof(ilist_t));
@@ -48,59 +52,65 @@ ilist_t *add_item_to_inventory(ilist_t *inv, item_t *item)
 	return inv;
 }
 
+/** make empty armor list **/
 alist_t *create_test_armor()
 {
-        alist_t *arm_list = malloc(sizeof(alist_t));
-        return arm_list;
+	alist_t *arm_list = malloc(sizeof(alist_t));
+    return arm_list;
 }
 
+/** package armor info into armor struct **/
 armor_t *create_armor(char* descrip, int defense, int weight)
 {
-        armor_t *new_armor = (armor_t*) malloc(sizeof(armor_t));
-        new_armor->defense = defense;
-        new_armor->weight = weight;
-	new_armor->description = (char*) malloc(sizeof(char) * 25);
-        strcpy(new_armor->description, descrip);
-        return new_armor;
+    armor_t *new_armor = (armor_t*) malloc(sizeof(armor_t));
+    new_armor->defense = defense;
+    new_armor->weight = weight;
+    new_armor->description = (char*) malloc(sizeof(char) * 25);
+    strcpy(new_armor->description, descrip);
+    return new_armor;
 }
 
+/** add armor to armor list **/
 alist_t *add_armor(alist_t *arm, armor_t *armor)
 {
-        alist_t *new_armor = (alist_t*) malloc(sizeof(alist_t));
-        new_armor->armor = armor;
-        new_armor->next = arm;
-        arm = new_armor;
-        return arm;
+    alist_t *new_armor = (alist_t*) malloc(sizeof(alist_t));
+    new_armor->armor = armor;
+    new_armor->next = arm;
+	arm = new_armor;
+    return arm;
 }
 
+/** determines if item can be used for battle **/
 bool item_battle(item_t *item)
 {
 	return item->battle;
 }
 
-
-
-player_t *create_test_player(ilist_t *inv, alist_t *arm) 
+/** creates a dummy player given an inventory and armor list **/
+player_t *create_test_player(ilist_t *inv, alist_t *arm)
 {
-        player_t *test_player = malloc(sizeof(player_t));
-        test_player->cl = 3;
-        test_player->inventory = inv;
-        test_player->armor = arm;
+    player_t *test_player = malloc(sizeof(player_t));
+    test_player->cl = 3;
+    test_player->inventory = inv;
+	test_player->armor = arm;
 	return test_player;
 }
 
+/** awards xp **/
 stats_t *award_xp(stats_t *stats, int xp)
 {
 	stats->xp += xp;
 	return stats;
 }
 
+/** creates empty move list **/
 mlist_t *create_mlist()
 {
 	mlist_t *mov_list = malloc(sizeof(mlist_t));
         return mov_list;
 }
 
+/** packages move info into struct **/
 move_t *create_move(item_t *item, bool atk, int dmg, int df)
 {
 	move_t *move = (move_t*) malloc(sizeof(move_t));
@@ -111,20 +121,23 @@ move_t *create_move(item_t *item, bool atk, int dmg, int df)
 	return move;
 }
 
+/** returns true if item is better for attack **/
 bool attack_item(item_t *item)
 {
 	return (item->attack >= item->defense);
 }
 
+/** adds move to movelist **/
 mlist_t *add_move(mlist_t *moves, move_t *move)
 {
-        mlist_t *new_move = (mlist_t*) malloc(sizeof(mlist_t));
-        new_move->move = move;
-        new_move->next = moves;
-        moves= new_move;
-        return moves;
+    mlist_t *new_move = (mlist_t*) malloc(sizeof(mlist_t));
+    new_move->move = move;
+	new_move->next = moves;
+    moves= new_move;
+    return moves;
 }
 
+/** generates list of enemy moves based on their inventory **/
 mlist_t *enemy_moves(ilist_t *inv)
 {
 	mlist_t *moves = create_mlist();
@@ -133,16 +146,17 @@ mlist_t *enemy_moves(ilist_t *inv)
 	{
 		if(tmp->item->battle)
 		{
-			move_t *move = create_move(tmp->item, attack_item(tmp->item), tmp->item->attack, tmp->item->defense);
+			move_t *move = create_move(tmp->item, attack_item(tmp->item),
+			tmp->item->attack, tmp->item->defense);
 			moves = add_move(moves, move);
 		}
 	tmp = tmp->next;
 	}
 	return moves;
 }
-		
 
 
+/** uses helper functions to make dummy player and stats **/
 int main()
 {
 	printf("Meow\n");
@@ -163,5 +177,5 @@ int main()
 	arm = add_armor(arm,arm2);
 	arm = add_armor(arm,arm3);
 	player_t *player = create_test_player(inv, arm);
-
+	stats_t *stats = create_test_stats();
 }
