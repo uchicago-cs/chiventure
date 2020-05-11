f9b49d1784e34031862ba44b86c219cbd13146c3
Your Name <you@example.com>
2020-05-11 04:41:30 -0500

    added stats.c and stubbed first set of stats functions

--- include/game-state/stats.h
+++ include/game-state/stats.h
@@ -25,8 +25,138 @@ typedef struct effects{
 
 typedef struct effects effects_hash_t;
 
+/*
+ * Initializes a Stat with specified value and modifier 0
+ *
+ * Parameters:
+ *  s: A stats struct Must point to already allocated memory.
+ *  stats_name: the unique string ID of the stat
+ *  init: starting value
+ *
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int stats_init(stats_t *s, char* stats_name, int init);
 
+/*
+ * Allocates a new stat
+ *
+ * Parameters:
+ * stats_name: the unique string ID of the stat
+ * init: starting value
+ * 
+ * Returns:
+ *  Pointer to allocated stats struct
+ */
+stats_t *stats_new(char* stats_name, int init);
 
+/*
+ * Assigns a table of stats to a player
+ *
+ * Parameters:
+ *  player_id: the player's id
+ *  sh: the stats hashtable
+ *
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int assign_stats(char* player_id, stats_hash_t sh);
 
+/*
+ * Assigns a table of stats to a player
+ *
+ * Parameters:
+ *  player_id: the player's id
+ *  stat: the name of the stat
+ *  change: the value to add to the stat
+ *
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int modify_stat(char* player_id, char* stat, int change);
+
+/*
+ * function that gets integer value of a specified
+ * stat of a player
+ *
+ * Parameters:
+ *  player_id: the player's id
+ *  stat: the name of the stat
+ *
+ * Returns:
+ *  integer value of a players stat
+ */
+int get_stat(char* player_id, char* stat);
+
+/*
+ * function that gets integer value of  the modifier of 
+ * a specified stat of a player
+ *
+ * Parameters:
+ *  player_id: the player's id
+ *  stat: the name of the stat
+ *
+ * Returns:
+ *  integer value of a players stat mod
+ */
+int get_stat_mod(char* player_id, char* stat);
+
+/*
+ * Adds a stat to a player by adding it to just their
+ * hash table
+ *
+ * Parameters: 
+ * player_id: the player's id
+ * s: pointer to a stats struct 
+ * 
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int add_stat_player(char* player_id, stats_t *s);
+
+/*
+ * Adds a stat to the game by adding 
+ * it on every players stats hashtable
+ *
+ * Parameters: 
+ * s: pointer to a stats struct
+ * 
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int add_stat_global(stats_t *s);
+
+/*
+ * Displays a players stats
+ *
+ * Parameters: 
+ * player_id: the player's ID
+ * 
+ * Returns:
+ *  Void
+ */
+void display_stats(char* player_id);
+
+/*
+ * Frees a stats struct
+ *
+ * Parameters: 
+ * s: pointer to the stats struct to be freed
+ * 
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int free_stats(stats *s);
+
+/*
+ * Frees a stats struct
+ *
+ * Parameters: 
+ * s: pointer to the stats struct to be freed
+ * 
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs.
+ */
+int delete_stats(stats_hash_t *sh);
 
 #endif
---
+++ src/game-state/src/stats.c
@@ -0,0 +1,78 @@
+#include "game-state/stats.h"
+
+/* See stats.h */
+int stats_init(stats_t *s, char* stats_name, int init)
+{
+    printf("stats_init: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+stats_t *stats_new(char* stats_name, int init)
+{
+    printf("stats_new: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int assign_stats(char* player_id, stats_hash_t sh)
+{
+    printf("assign_stats: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int modify_stat(char* player_id, char* stat, int change)
+{
+    printf("modify_stat: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int get_stat(char* player_id, char* stat)
+{
+    printf("get_stat: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int get_stat_mod(char* player_id, char* stat)
+{
+    printf("get_stat_mod: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int add_stat_player(char* player_id, stats_t *s)
+{
+    printf("add_stat_player: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int add_stat_global(stats_t *s)
+{
+    printf("add_stat_global: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+void display_stats(char* player_id)
+{
+    printf("display_stats: function not yet implemented\n");
+    return; // still needs to be implemented
+}
+
+/* See stats.h */
+int free_stats(stats *s)
+{
+    printf("free_stats: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
+
+/* See stats.h */
+int delete_stats(stats_hash_t *sh)
+{
+    printf("delete_stats: function not yet implemented\n");
+    return 0; // still needs to be implemented
+}
