7fc6ba17954f6938244e494352d36fda8fec3d86
Noah Elliot Chewbacca Klowden <noahklowden@linux2.cs.uchicago.edu>
2020-05-10 21:38:26 -0500

    Added first test to ensure information could be printed. Added asserts to code. Fixed warnings of unused inputs in code.

--- src/wdl/sandbox/parser.c
+++ src/wdl/sandbox/parser.c
@@ -1,27 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 /* necessary includes to use JSON-C */
 #include <json-c/json.h>
 
-int main(int argc, char **argv) {
+/* DEBUG is 0 normally, 1 to print debugging statements */
+#define DEBUG 1
+
+/* Helper function: to print debugging statements only when debugging */
+int my_print(char *string) {
+    
+    if (DEBUG) {
+        printf("%s\n",string);
+    }
+
+    return 0;
+
+}
+
+int main() {
    FILE *fp;
-   char buffer[2048];
+   char buffer[4096];
    
+   /* The main json_objects for storing top level information*/
    struct json_object *game_document;
    struct json_object *game;
    struct json_object *rooms;
    struct json_object *items;
    
+   /* json_objects for storing game object information */
+   struct json_object *intro;
+
+   /* reads the input file */
    fp = fopen("connected_rooms.json", "r");
-   fread(buffer, 2048, 1, fp);
+   assert(fp);
+   fread(buffer, 4096, 1, fp);
    fclose(fp);
 
    game_document = json_tokener_parse(buffer);
    
+   assert(game_document);
+
    json_object_object_get_ex(game_document, "GAME", &game);
    json_object_object_get_ex(game_document, "ROOMS", &rooms);
    json_object_object_get_ex(game_document, "ITEMS", &items);
+
+   assert(game);
+
+   json_object_object_get_ex(game, "intro", &intro);
+
+   assert(intro);
+
+   my_print("Intro:");
+   my_print((char*)json_object_get_string(intro));
 
 }
