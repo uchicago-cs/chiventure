dfccaef8c5a05410ef40f1d2ec1ed425920e8c06
Noah Elliot Chewbacca Klowden <noahklowden@linux2.cs.uchicago.edu>
2020-05-11 23:12:49 -0500

    Rewrote parser so that it would return game_t. Added in a parser that will add rooms to the game.

--- src/wdl/sandbox/parser.c
+++ src/wdl/sandbox/parser.c
@@ -5,6 +5,7 @@
 
 #include "parser.h"
 
+
 /* DEBUG is 0 normally, 1 to print debugging statements */
 #define DEBUG 1
 
@@ -19,43 +20,84 @@ int my_print(char *string) {
 
 }
 
-json_object *parse(char* filename) {
-   FILE *fp;
-   char buffer[4096];
-   
-   /* The main json_objects for storing top level information*/
-   struct json_object *game_document;
-   struct json_object *game;
-   struct json_object *rooms;
-   struct json_object *items;
-   
-   /* json_objects for storing game object information */
-   struct json_object *intro;
 
-   /* reads the input file */
-   fp = fopen(filename, "r");
-   assert(fp);
-   fread(buffer, 4096, 1, fp);
-   fclose(fp);
+int add_rooms_to_game(json_object *rooms, game_t *g) {
 
-   game_document = json_tokener_parse(buffer);
-   
-   assert(game_document);
+    //initializes json_objects for use in making room_t
+    int arr_len = json_object_array_length(rooms);
+    struct json_object room_obj;
+    struct json_object id_obj;
+    struct json_object long_desc_obj;
+    struct json_object short_desc_obj;
 
-   json_object_object_get_ex(game_document, "GAME", &game);
-   json_object_object_get_ex(game_document, "ROOMS", &rooms);
-   json_object_object_get_ex(game_document, "ITEMS", &items);
+    //if room list is empty, return 1
+    if(rooms != NULL) {
+        fprintf(stderr, "rooms list is empty\n");
+        return FAILURE;
+    }
 
-   assert(game);
+    //for loops list of rooms, creates a new game_struct room, add room to game
+    for (int i = 0; i < arr_len; i++) {
 
-   json_object_object_get_ex(game, "intro", &intro);
+        room_obj = json_object_array_get_idx(rooms, i);
 
-   assert(intro);
+        json_object_object_get_ex(room_obj, "id", &id_obj);
+        char *id = json_object_get_string(id_obj);
+        
+        json_object_object_get_ex(room_obj, "short_desc", &short_desc_obj);
+        char *short_desc = json_object_get_string(short_desc_obj);
 
-   my_print("Intro:");
-   my_print((char*)json_object_get_string(intro));
+        json_object_object_get_ex(rooms_obj, "long_desc", &long_desc_obj);
+        char *long_desc = json_object_get_string(long_desc_obj);
 
-   return game_document;
+        room_t *room = room_new(id, short_desc, long_desc);
+
+        add_room_to_game(g, room);
+
+    }
+
+    return SUCCESS;
+
+}
+
+game_t *parse_wdl(char* filename) {
+    FILE *fp;
+    char buffer[4096];
+    
+    /* The main json_objects for storing top level information*/
+    struct json_object *game_document;
+    struct json_object *game_obj;
+    struct json_object *rooms_obj;
+    struct json_object *items_obj;
+    struct json_object *intro_obj;
+    
+    /* reads the input file */
+    fp = fopen(filename, "r");
+    assert(fp);
+    fread(buffer, 4096, 1, fp);
+    fclose(fp);
+
+    game_document = json_tokener_parse(buffer);
+    
+    assert(game_document);
+ 
+    json_object_object_get_ex(game_document, "GAME", &game_obj);
+    json_object_object_get_ex(game_document, "ROOMS", &rooms_obj);
+    json_object_object_get_ex(game_document, "ITEMS", &items_obj);
+
+    assert(game_obj);
+
+    //gets the intro text for the game
+    json_object_object_get_ex(game, "intro", &intro_obj);
+    assert(intro_text);
+
+    char *intro = json_object_get_string(intro_obj);
+
+    game_t *game = game_new(intro);
+
+    add_rooms_to_game(rooms_obj,game);
+
+    return game;
 
 }
 
@@ -63,7 +105,7 @@ int main(int argc, char **argv) {
 
     if(argc > 1) {
 
-         parse(argv[1]);
+         parse_wdl(argv[1]);
 
     }
 
--- src/wdl/sandbox/parser.h
+++ src/wdl/sandbox/parser.h
@@ -2,11 +2,53 @@
  *
  */
 
+#include "game-state/game.h"
+
 /* necessary includes to use JSON-C */
 #include <json-c/json.h>
 
 /* 
- * parse()
+ * add_connections_to_rooms()
+ * Adds the connections to the rooms
+ * 
+ * Parameters:
+ *  - doc: The json_object that it was stored in
+ *  - g: the game object
+ *
+ * Returns:
+ *  - SUCCESS if passes, FAILURE if fails
+ */
+int add_connections_to_rooms(json_object *doc, game_t *g);
+
+/* 
+ * add_items_to_game()
+ * Adds the items to the game object
+ * 
+ * Parameters:
+ *  - doc: The json_object that it was stored in
+ *  - g: the game object
+ *
+ * Returns:
+ *  - SUCCESS if passes, FAILURE if fails
+ */
+int add_items_to_game(json_object *doc, game_t *g);
+
+/* 
+ * add_rooms_to_game()
+ * Adds the rooms to the game object
+ * 
+ * Parameters:
+ *  - doc: The json_object that it was stored in
+ *  - g: the game object
+ *
+ * Returns:
+ *  - SUCCESS if passes, FAILURE if fails
+ */
+int add_rooms_to_game(json_object *doc, game_t *g);
+
+/* 
+ * parse_wdl()
+ * parses the wdl document into a game_t
  * 
  * Parameters:
  *  - filename: the path to the file to be parsed
@@ -14,7 +56,7 @@
  * Returns:
  *  - a zero for success
  */
-json_object *parse(char* filename);
+game_t *parse_wdl(char* filename);
 
 /*
  * main()
