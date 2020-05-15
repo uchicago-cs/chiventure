d0457cb17b93be3f718c547144b11cad1286e689
Isaaq-Khader <ikhader@intrinsicschools.org>
2020-05-15 16:23:32 -0500

    Added additional functions from sandbox into logic (Issue #532) Added feedback files

--- src/battle/include/logic.h
+++ src/battle/include/logic.h
@@ -48,4 +48,24 @@ int mod_stat(stats_t *stat);
  */
 int battle_over(player_t p, enemy_t *e);
 
+/* Function detemines what player goes first
+ *
+ *  Inputs: 
+ *      difficulty: difficulty level of the game
+ *  Outputs:
+ *      -1 - invalid input
+ *      0 - user goes first
+ *      1 - enemy goes first    */
+int goes_first(int difficulty);
+
+/* Uses the item with the given ID on the player
+ *
+ *  Inputs: 
+ *      e - the enemy using the item
+ *      p - the player being attacked
+ *      ID - the ID of the item
+ *  Outputs:
+ *      returns item used    */
+int enemy_use_item(enemy_t *e, player_t *p, int ID);
+
 #endif
--- src/battle/src/logic.c
+++ src/battle/src/logic.c
@@ -1,3 +1,100 @@
 #include "logic.h"
 
+/* check logic.h */
+int check_target(enemy_t *e) {
+    return 0;
+}
 
+/* check logic.h */
+double check_effectiveness(player_t *p, enemy_t *e, move_t *move) {
+    return 1.0;
+}
+
+/* check logic.h */
+int mod_stat(stats_t *stat) {
+    return 0;
+}
+
+/* check logic.h */
+int battle_over(player_t p, enemy_t *e) {
+    return 1;
+}
+
+/* check logic.h */
+int goes_first(int difficulty)
+{
+    int r = rand() % 2; //random number generator
+    switch (difficulty)
+    {
+    case 1:
+        return 0;
+        break;
+    case 2:
+        if (r == 0)
+            return 0;
+        else
+            return 1;
+        break;
+    case 3:
+        return 1;
+        break;
+    default:
+        printf("Invalid difficulty!");
+        return -1;
+    }
+}
+
+/* Creates a dumy armor list with hardcoded values
+ * 	Inputs:
+ * 		inventory - an player or enemy's inventory
+ * 		ID - the ID number of the desired item
+ * 	Outputs:
+ * 		temp->item - pointer to the desired item */
+item_t *find_item(ilist_t *inventory, int ID)
+{
+    ilist_t *temp = inventory;
+    for (temp = inventory; temp != NULL; temp = temp->next)
+    {
+        if (temp->item->id == ID)
+        {
+            return temp->item;
+        }
+        else if ((temp->next == NULL) &&
+                 (temp->item->id != ID))
+        {
+            printf("\nError! You don't have any in inventory!");
+            return NULL;
+        }
+    }
+    return NULL;
+}
+
+/* see logic.h */
+item_t* enemy_use_item(enemy_t *e, int ID)
+{
+    if (e->inv == NULL)
+    {
+        fprintf(stderr, "Noooo! Your inventory is empty!");
+        return NULL;
+    }
+    item_t *desired_item = malloc(sizeof(item_t));
+    desired_item = find_item(e->inv, ID);
+    if (desired_item->quantity == 0)
+    {
+        fprintf(stderr, "ARGH!! You don't have any!");
+        return desired_item;
+    }
+    else
+    {
+        desired_item->quantity -= 1;
+        desired_item->durability -= 1;
+        return desired_item;
+    }
+}
+
+/* Awards xp to the player
+stats_t *award_xp(stats_t *stats, int xp)
+{
+    stats->xp += xp;
+    return stats;
+}*/
