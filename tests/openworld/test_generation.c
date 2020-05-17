023193be91b54d70d09d6737bccf39df022b05a9
Chanik Bryan Lee <bryan22lee@gmail.com>
2020-05-16 20:52:00 -0500

    Implemented single-room autogen. Now testing. Compiles so far

--- tests/openworld/test_generation.c
+++ tests/openworld/test_generation.c
@@ -1,7 +1,7 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "sample_generation.h"
+#include "../../include/openworld/sample_generation.h"
 
 /* Tests the functions in sample_generation.h */
 
@@ -16,9 +16,9 @@ Test(sample_generation, enter_new_room1) {
     "enter_new_room() interpreted two different rooms as the same");
 }
 
-Test(sample_generation, enter_new_room1) {
+Test(sample_generation, enter_new_room2) {
     room_t *sampleRoom = room_new("string1", "string2", "string3");
-    cr_assert_not_null(sampleRoom1, "sampleRoom1 is NULL");
+    cr_assert_not_null(sampleRoom, "sampleRoom1 is NULL");
 
     cr_assert_eq(enter_new_room(sampleRoom, sampleRoom), false, 
     "enter_new_room() interpreted two same rooms as different");
