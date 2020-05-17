8a1a8247661af3dacd3c6de728fa8ce90c877b64
Emma Mary Medeiros <emedeiros@linux2.cs.uchicago.edu>
2020-05-17 16:04:49 -0500

    control_block_t tests

--- tests/custom-actions/test_struct.c
+++ tests/custom-actions/test_struct.c
@@ -194,3 +194,154 @@ Test(AST_block_t, free_CONDITIONAL)
 
     cr_assert_eq(rc, SUCCESS, "AST_block_free() failed");
 }
+
+/* Checks that a new IF/ELSE control block is created without interruption */
+Test(control_block_t, new_IF/ELSE)
+{
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+    enum control_type control_type = IF/ELSE;
+
+    control_block_t* new_control = control_block_new(next, control_type);
+
+    cr_assert_not_null(new_control, "control_block_new failed");
+
+    cr_assert_eq(new_control->next, next, "control_block_new() didn't set 
+                 new_control->next");
+    cr_assert_eq(new_control->control_type, control_type, "control_block_new() didn't
+                 set new_control->control_type");
+
+    control_block_free(new control);
+}
+
+/* Checks that a new WHILE/ENDWHILE control block is created without interruption */
+Test(control_block_t, new_WHILE/ENDWHILE)
+{
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+    enum control_type control_type = WHILE/ENDWHILE;
+
+    control_block_t* new_control = control_block_new(next, control_type);
+
+    cr_assert_not_null(new_control, "control_block_new failed");
+
+    cr_assert_eq(new_control->next, next, "control_block_new() didn't set 
+                 new_control->next");
+    cr_assert_eq(new_control->control_type, control_type, "control_block_new() didn't
+                 set new_control->control_type");
+
+    control_block_free(new control);
+}
+
+/* Checks that a new FOR/ENDFOR control block is created without interruption */
+Test(control_block_t, new_FOR/ENDFOR)
+{
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+    enum control_type control_type = FOR/ENDFOR;
+
+    control_block_t* new_control = control_block_new(next, control_type);
+
+    cr_assert_not_null(new_control, "control_block_new failed");
+
+    cr_assert_eq(new_control->next, next, "control_block_new() didn't set 
+                 new_control->next");
+    cr_assert_eq(new_control->control_type, control_type, "control_block_new() didn't
+                 set new_control->control_type");
+
+    control_block_free(new control);
+}
+
+/* Checks that a new IF/ELSE control block is initialized without interruption */
+Test(control_block_t, init_IF/ELSE)
+{
+    control_block_t control;
+    int rc;
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+    enum control_type control_type = IF/ELSE;
+
+    rc = control_block_init(&control, next, control_type);
+
+    cr_assert_eq(rc, SUCCESS, "control_block_init() failed");
+    cr_assert_eq(control.next, next, "control_block_init() didn't set control.next");
+    cr_assert_eq(control.control_type, control_type, "control_block_init()
+                 didn't set control.control_type");
+}
+
+/* Checks that a new WHILE/ENDWHILE control block is initialized without interruption */
+Test(control_block_t, init_WHILE/ENDWHILE)
+{
+    control_block_t control;
+    int rc;
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+    enum control_type control_type = WHILE/ENDWHILE;
+
+    rc = control_block_init(&control, next, control_type);
+
+    cr_assert_eq(rc, SUCCESS, "control_block_init() failed");
+    cr_assert_eq(control.next, next, "control_block_init() didn't set control.next");
+    cr_assert_eq(control.control_type, control_type, "control_block_init()
+                 didn't set control.control_type");
+}
+
+/* Checks that a new FOR/ENDFOR control block is initialized without interruption */
+Test(control_block_t, init_FOR/ENDFOR)
+{
+    control_block_t control;
+    int rc;
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+    enum control_type control_type = FOR/ENDFOR;
+
+    rc = control_block_init(&control, next, control_type);
+
+    cr_assert_eq(rc, SUCCESS, "control_block_init() failed");
+    cr_assert_eq(control.next, next, "control_block_init() didn't set control.next");
+    cr_assert_eq(control.control_type, control_type, "control_block_init()
+                 didn't set control.control_type");
+}
+
+
+/* Checks that a new IF/ELSE control block is freed without interruption */
+Test(control_block_t, free_IF/ELSE)
+{
+    control_block_t *control;
+    int rc;
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+
+    control = control_block_new(next, IF/ELSE);
+
+    cr_assert_not_null(control, "control_block_new() failed");
+
+    rc = control_block_free(control);
+
+    cr_assert_eq(rc, SUCCESS, "control_block_free() failed");
+}
+
+/* Checks that a new WHILE/ENDWHILE control block is freed without interruption */
+Test(control_block_t, free_WHILE/ENDWHILE)
+{
+    control_block_t *control;
+    int rc;
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+
+    control = control_block_new(next, WHILE/ENDWHILE);
+
+    cr_assert_not_null(control, "control_block_new() failed");
+
+    rc = control_block_free(control);
+
+    cr_assert_eq(rc, SUCCESS, "control_block_free() failed");
+}
+
+/* Checks that a new FOR/ENDFOR control block is freed without interruption */
+Test(control_block_t, free_FOR/ENDFOR)
+{
+    control_block_t *control;
+    int rc;
+    AST_block_t *next = AST_block_new(control_block, CONTROL);
+
+    control = control_block_new(next, FOR/ENDFOR);
+
+    cr_assert_not_null(control, "control_block_new() failed");
+
+    rc = control_block_free(control);
+
+    cr_assert_eq(rc, SUCCESS, "control_block_free() failed");
+}
