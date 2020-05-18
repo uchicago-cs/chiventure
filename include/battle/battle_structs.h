fbd0116efe447d2673abddf4b9c02e72d3105f53
Isaaq-Khader <ikhader@intrinsicschools.org>
2020-05-17 19:07:29 -0500

    Moved structs that we had in src/battle/include to include/battle

--- include/battle/battle_structs.h
+++ include/battle/battle_structs.h
@@ -1,28 +1,106 @@
-#ifndef STRUCTS_H
-#define STRUCTS_H
-
 #include <stdbool.h>
 #include <string.h>
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
 
 /* max name length */
 #define MAX_NAME_LEN (50)
 
-/* items stub */
-typedef struct item {
+typedef struct
+{
+    double speed;
+    double strength;
+    double dexterity;
+    double charisma;
+    double hp;
+    double max_hp;
+    double xp;
+    unsigned int level;
+} stats_t;
+
+typedef struct
+{
     int id;
     int quantity;
-    int durability;
+    double durability;
     char* description;
     bool battle;
-    int attack;
-    int defense;
-    int hp;
+    double attack;
+    double defense;
+    double hp;
     struct item *next;
     struct item *prev;
 } item_t;
 
-/* moves stub */
-typedef struct move {
+typedef struct
+{
+    char* description;
+    double defense;
+    double weight;
+} armor_t;
+
+
+typedef struct _ilist_t ilist_t;
+struct _ilist_t
+{
+    item_t *item;
+    ilist_t *next;
+};
+
+typedef struct _alist_t alist_t;
+struct _alist_t
+{
+    armor_t *armor;
+    alist_t *next;
+};
+
+
+/** Players get stat bonuses based on their class.
+This enum type is part of the class struct. Whichever stat
+is stored in class_t.st is increased by class_t.bonus **/
+enum stats{spd, str, dex, chrsma};
+
+/* class stub */
+typedef enum class
+{
+    bard,
+    cleric,
+    paladin,
+    wizard
+} class_t;
+
+/** The player class struct, which includes the enum class, a short
+description of the class, which stat the class gets a bonus for, and
+how much that bonus is **/
+typedef struct
+{
+    enum class cl;
+    char *info;
+    enum stats st;
+    int bonus;
+} class_t;
+
+
+typedef struct
+{
+    class_t cl;
+    ilist_t *inventory;
+    alist_t *armor;
+    stats_t *stats;
+} player_t;
+
+typedef struct
+{
+    char *name;
+    stats_t *stats;
+    class_t cl;
+    ilist_t *inv;
+    alist_t *armor;
+} enemy_t;
+
+typedef struct
+{
     item_t *item;
     bool attack;
     int damage;
@@ -31,26 +109,12 @@ typedef struct move {
     struct move *prev;
 } move_t;
 
-/* stats stub */
-typedef struct stat {
-    int speed;
-    int strength;
-    int dexterity;
-    int hp;
-    int max_hp;
-    int xp;
-} stat_t;
-
-/* class stub */
-typedef enum class {
-    bard,
-    cleric,
-    paladin, 
-    wizard
-} class_t;
-
-
+typedef struct _mlist_t mlist_t;
+struct _mlist_t
+{
+    move_t *move;
+    mlist_t *next;
+};
 
 
 #endif
-
