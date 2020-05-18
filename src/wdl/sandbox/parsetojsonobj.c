0664e032bdf6b8f36a08bcdf623fd6c237f12298
David Thomas Bukowski <dtbukowski@linux1.cs.uchicago.edu>
2020-05-17 23:34:03 -0500

    My progress on getting a zip library to work. Libzip seems to not be installed as I can only find shared libraries of it. I will ask CS Techstaff tonight and for now I will try to implement this using a different library that is installed, zziplib

---
+++ src/wdl/sandbox/parsetojsonobj.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "parsetojsonobj.h"
+
+
+
+int main() {
+
+    //FILE *fp;
+    //char buffer[4096];
+    int* p;
+    zip_t* ziparchive = zip_open("game1.wdz", 0, p);
+    printf(ziparchive);
+    /* 
+    struct json_object *game_document;
+    struct json_object *game_obj;
+    struct json_object *rooms_obj;
+    struct json_object *items_obj;
+    struct json_object *intro_obj;
+
+    fp = fopen(filename, "r");
+    assert(fp);
+    fread(buffer, 4096, 1, fp);
+    fclose(fp);
+
+    game_document = json_tokener_parse(buffer);
+    
+    assert(game_document);
+ 
+    json_object_object_get_ex(game_document, "GAME", &game_obj);
+    json_object_object_get_ex(game_document, "ROOMS", &rooms_obj);
+    json_object_object_get_ex(game_document, "ITEMS", &items_obj);
+
+    assert(game_obj);
+    */
+    
+    return 0;
+}
---
+++ src/wdl/sandbox/parsetojsonobj.h
@@ -0,0 +1,4 @@
+//#include <zip.h>
+
+/* Main function that has libobj to parser*/
+int main(char **argv);
