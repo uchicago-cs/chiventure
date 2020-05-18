4e16aa1cb4171819a9b7e3c57d398dad6d98e1e6
Sophie Veys <sophiev@cs.uchicago.edu>
2020-05-18 18:13:49 -0400

    okay made test funcs for init and new

---
+++ tests/battle/test_moves.c
@@ -0,0 +1,34 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "moves.h"
+
+Test(moves, new)
+{
+    char *test_str;
+    test_str = "Test description";
+    
+    class_t* class;
+    class = new_class(0, test_str, 1, 3.0);
+    
+    cr_assert_not_null(class, "new_class() failed");
+
+    cr_assert_eq(class->info, test_str, "new_class() didn't set info");
+}
+
+
+Test(moves, init)
+{
+    class_t class;
+    int rc; 
+  
+    char *test_str;
+    test_str = "Test description";
+
+    rc = init_class(&class, 0, test_str, 1, 3.0);
+
+    cr_assert_eq(rc, 0, "init_class() failed");
+
+    cr_assert_eq(class.info, test_str, "init_class() didn't set info");
+}
