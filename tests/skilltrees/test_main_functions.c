f6071ed4af4252f7872f868b506da6cfee203e2d
vannstrasen <vannstrasen@gmail.com>
2020-05-17 23:48:59 -0500

    Testing basic functions of tests

---
+++ tests/skilltrees/test_main_functions.c
@@ -0,0 +1,12 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "skilltrees/skilltrees.h"
+
+// First test for skilltrees
+Test(main_functions, test1)
+{
+    print("Hello\n")
+    cr_assert_eq(1, 1, "Failed to equalize 1 with 1")
+}
