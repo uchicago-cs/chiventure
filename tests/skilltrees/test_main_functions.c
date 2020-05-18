c5f5e471af3e828a3b21b0e2a3a73bc056cd342b
vannstrasen <vannstrasen@gmail.com>
2020-05-18 10:20:31 -0500

    bugfixes

--- tests/skilltrees/test_main_functions.c
+++ tests/skilltrees/test_main_functions.c
@@ -23,9 +23,9 @@ Test(skilltrees_main_functions, create_skill_list)
 
 
     for (int i = 0; i < 5; i++) {
-        cr_assert_null(skill_list->active_skill[i],
+        cr_assert_null(skill_list->active_skills[i],
                        "Active skill %d is not null", i);
-        cr_assert_null(skill_list->passive_skill[i],
+        cr_assert_null(skill_list->passive_skills[i],
                        "Passive skill %d is not null", i);
     }
 }
@@ -49,7 +49,7 @@ skill_t* create_dummy_skill()
 
     skill_t* skill = new_skill(name, cur_level, experience,
                                description, &skill_func);
-    return skill
+    return skill;
 }
 
 
@@ -59,13 +59,13 @@ Test(skilltrees_main_functions, new_skill)
     skill_t* skill = create_dummy_skill();
     cr_assert_not_null(skill, "new_skill() failed: null skill");
 
-    cr_assert_str_eq(name, skill->name,
+    cr_assert_str_eq("fireball", skill->name,
                      "new_skill() failed at creating the name.");
-    cr_assert_str_eq(description, skill->description,
+    cr_assert_str_eq("shoots fire but, like, super fast.", skill->description,
                      "new_skill() failed at creating the description.");
-    cr_assert_eq(cur_level, skill->cur_level,
+    cr_assert_eq(0, skill->cur_level,
                  "new_skill() failed at creating the current level.");
-    cr_assert_eq(experience, skill->experience,
+    cr_assert_eq(10, skill->experience,
                  "new_skill() failed at creating the experience parameter.");
     cr_assert_eq(6, skill->execute_skill(2, 4),
                  "new_skill() failed when testing the function pointer.");
@@ -90,8 +90,8 @@ Test(skilltrees_main_functions, free_skill_lists_manually)
     allskills_t* skill_list = create_skill_list();
     cr_assert_not_null(skill_list, "create_skill_list() failed");
 
-    rc = free_passive_skills(skill_list);
-    rc2 = free_active_skills(skill_list);
+    int rc = free_passive_skills(skill_list);
+    int rc2 = free_active_skills(skill_list);
 
     cr_assert_eq(rc, 0, "free_passive_skills() failed.");
     cr_assert_eq(rc2, 0, "free_active_skills() failed.");
