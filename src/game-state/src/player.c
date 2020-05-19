adcdd85ed931ca1c1635b8aeefe81ef34100aea3
Your Name <you@example.com>
2020-05-19 01:52:09 -0500

    fixing style errors and changing assign_stats_player to take a pointer to a player

--- src/game-state/src/player.c
+++ src/game-state/src/player.c
@@ -146,7 +146,7 @@ item_list_t *get_all_items_in_inventory(player_t *player)
 }
 
 /* See player.h */
-int assign_stats_player(player_t plyr, stats_hash_t *sh)
+int assign_stats_player(player_t *plyr, stats_hash_t *sh)
 {
     printf("assign_stats_player: function not yet implemented\n");
     return 0; // still needs to be implemented
