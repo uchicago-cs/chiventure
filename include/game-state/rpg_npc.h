ca18f2ec3b3b86fde59c5ec5ffe08a583edb7656
Anshika <anshikab@uchicago.edu>
2020-05-21 13:34:28 +0530

    added functions

--- include/game-state/rpg_npc.h
+++ include/game-state/rpg_npc.h
@@ -1,5 +1,5 @@
-#ifndef _RPG_NPC_H
-#define _RPG_NPC_H
+#ifndef _NPC_H
+#define _NPC_H
 
 #include "game_state_common.h"
 #include "item.h"
@@ -34,6 +34,7 @@ typedef struct npc
  *  npc_id: string referring to npc id; passed implicitly
             from npc_new 
  *  dialogue: pointer to a convo struct for the npc
+ *  stats: pointer to the stats struct for the npc
  *   // placeholder for incoming dialogue module
  *
  * Returns:
@@ -49,6 +50,7 @@ int npc_init(npc_t *npc, char *npc_id, int health, convo_t *dialogue, stats_hash
  *  npc_id: the unique string ID of the npc
  *  health: the starting health of the npc
  *  dialogue: pointer to convo struct for the npc
+ *  stats: pointer to the stats struct for the npc
  *   // placeholder for incoming dialogue module
  *
  * Returns:
@@ -56,5 +58,72 @@ int npc_init(npc_t *npc, char *npc_id, int health, convo_t *dialogue, stats_hash
  */
  npc_t *npc_new(char *npc_id, int health, convo_t *dialogue, stats_hash_t *stats);
  
+ /*
+ * Frees resources associated with an npc.
+ *
+ * Parameters:
+ *  npc: the npc to be freed
+ *
+ * Returns:
+ *  SUCCESS if successful, FAILURE if an error occurs.
+ */
+int npc_free(npc_t *npc);
+
+/*
+ * Returns the health of an npc.
+ *
+ * Parameters:
+ *  npc: the npc
+ *
+ * Returns:
+ *  int, the npc's health
+ */
+int get_npc_health(npc_t *npc);
  
+ /*
+ * Changes the health of the npc. 
+ *
+ * Parameters:
+ *  npc: the npc
+ *
+ * Returns:
+ *  int, updated health
+ */
+int change_npc_health(npc_t *npc, int change, int max);
+
+/*
+ * Returns the inventory list
+ *
+ * Parameters:
+ *  npc: the npc
+ *
+ * Returns:
+ *  hashtable of items, the inventory
+ */
+item_hash_t* get_npc_inventory(npc_t *npc);
+
+
+/* Adds an item to the given npc
+ *
+ * Parameters:
+ *  npc struct
+ *  item struct
+ *
+ * Returns:
+ *  SUCCESS if successful, FAILURE if an error occured.
+ */
+int add_item_to_npc(npc_t *npc, item_t *item);
+
+
+/*
+ * Function to get a linked list (utlist) of all the items in the player's inventory
+ *
+ * Parameters:
+ *  npc: the npc
+ *
+ * Returns:
+ *  linked list of pointers to items (the head element)
+ */
+item_list_t *get_all_items_in_inv_npc(npc_t *npc);
+
  #endif
