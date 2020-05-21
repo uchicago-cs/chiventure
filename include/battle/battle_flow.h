7143fec1ddf57777a3ff5703b639fbbc113ffc95
Celia A <andersonca@uchicago.edu>
2020-05-20 21:04:11 -0700

    one more small fix

--- include/battle/battle_flow.h
+++ include/battle/battle_flow.h
@@ -20,7 +20,7 @@ typedef struct player {
 /* Stub for the game_t struct in game-state */
 typedef struct game {
     // Would have other fields (eg hash tables for players, rooms, items)
-    player_t curr_player;
+    player_t *curr_player;
 } game_t;
 
 /* Stub, similar to chiventure_ctx_t except adding in battle mode component */
