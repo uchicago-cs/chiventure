4f38dd2b648d47cdcc3c344cc396e7f8e5fb331d
PaulTreitel <paul.treitel@gmail.com>
2020-05-20 21:41:39 -0700

    added very basic action interpreter

--- src/playerclass/sandbox/custom_skills.c
+++ src/playerclass/sandbox/custom_skills.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
 #include "common/utlist.h"
 
 // enum for supported commands
@@ -50,8 +51,8 @@ typedef struct {
     int patk; // physical attack
     int ratk; // ranged attack
     int mdef; // magic defense
-    int matck; // magic attack
-    int energ; // energy
+    int matk; // magic attack
+    int energy;
 } stats_stub;
 
 
@@ -62,6 +63,7 @@ custom_action_t* action_new(custom_command comm, custom_param_t p1,
     custom_param_t p2, param_type p1_type, param_type p2_type) {
 
     custom_action_t *act = malloc(sizeof(custom_action_t));
+    assert(act != NULL);
     act->comm = comm;
     act->param1 = p1;
     act->p1_type = p1_type;
@@ -76,11 +78,151 @@ custom_action_t* action_new(custom_command comm, custom_param_t p1,
  * creates and initializes a new custom action block
  */
 custom_block_t* block_new(custom_action_t* act, custom_block_t* next) {
-    
+    assert(act != NULL);
     custom_block_t *b = malloc(sizeof(custom_block_t));
+    assert(b != NULL);
     b->action = act;
     b->next = next;
     return b;
+}
+
+
+/* 
+ * compares a stat with an integer constant
+ * the only comparison supported is equality (==)
+ */
+bool compare_stat(stats_stub* stats, stat_param sp, int to_compare) {
+    assert(sp != NO_PARAM);
+    assert(stats != NULL);
+
+    int stat_value = 0;
+    switch (sp) {
+        case HEALTH:
+            stat_value = stats->health;
+            break;
+        case XP:
+            stat_value = stats->xp;
+            break;
+        case SPEED:
+            stat_value = stats->speed;
+            break;
+        case PDEF:
+            stat_value = stats->pdef;
+            break;
+        case PATK:
+            stat_value = stats->patk;
+            break;
+        case RATK:
+            stat_value = stats->ratk;
+            break;
+        case MDEF:
+            stat_value = stats->mdef;
+            break;
+        case MATK:
+            stat_value = stats->matk;
+            break;
+        case ENERG:
+            stat_value = stats->energy;
+            break;
+        default:
+            return false;
+    }
+    return to_compare == stat_value;
+}
+
+
+/*
+ * adds an integer to a specified stat in the stats struct
+ *
+ * returns 0 on success, 1 on failure
+ */
+int add_to_stat(stats_stub* stats, stat_param sp, int to_add) {
+    assert(stats != NULL);
+    assert(sp != NO_PARAM);
+
+    switch (sp) {
+        case HEALTH:
+            stats->health += to_add;
+            break;
+        case XP:
+            stats->xp += to_add;
+            break;
+        case SPEED:
+            stats->speed += to_add;
+            break;
+        case PDEF:
+            stats->pdef += to_add;
+            break;
+        case PATK:
+            stats->patk += to_add;
+            break;
+        case RATK:
+            stats->ratk += to_add;
+            break;
+        case MDEF:
+            stats->mdef += to_add;
+            break;
+        case MATK:
+            stats->matk += to_add;
+            break;
+        case ENERG:
+            stats->energy += to_add;
+            break;
+        default:
+            return 1;
+    }
+    return 0;
+}
+
+
+/* 
+ * executes a custom action block. Logic is very simple.
+ * assumes the block is well-formed and valid
+ * assumes that all actions in the block with parameters have a stat
+ *      followed by an integer constant
+ */
+int execute(stats_stub* stats, custom_block_t* block) {
+    assert(stats != NULL);
+    assert(block != NULL);
+
+    custom_block_t* curr;
+    custom_action_t* curr_act;
+    bool if_result = false;
+    int rc, status = 0;
+
+    for (curr = block; curr != NULL; curr = curr->next) {
+        curr_act = curr->action;
+
+        if (curr_act->comm == ADD) {
+            /* handles ADD actions */
+            rc = add_to_stat(stats, curr_act->param1.st,
+                curr_act->param2.constant);
+            if (rc)
+                status = 1;
+
+        } else if (curr_act->comm == SUB) {
+            /* handles SUB actions - just adds a negative number */
+            rc = add_to_stat(stats, curr_act->param1.st,
+                - curr_act->param2.constant);
+            if (rc)
+                status = 1;
+
+        } else if (curr_act->comm == IF) {
+            /* evaluates an if action and, if false, skips to ELSE or ENDIF */
+            if_result = compare_stat(stats, curr_act->param1.st,
+                curr_act->param2.constant);
+
+            if (!if_result) {
+                // skip over the rest of the if section
+                while (curr_act != NULL && curr_act->comm != ELSE && 
+                    curr_act->comm != ENDIF) {
+                    curr = curr->next;
+                    curr_act = curr->action;
+                }
+            }
+        }
+    }
+    return status;
 }
 
 
