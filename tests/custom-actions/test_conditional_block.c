02e655ff1897850904337e9d4e9bfd47f1053a54
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-05-21 20:44:43 -0500

    undo conditional

--- tests/custom-actions/test_conditional_block.c
+++ tests/custom-actions/test_conditional_block.c
@@ -1,7 +1,7 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "custom-actions/conditional_block_functions.h"
+#include "custom-actions/conditional_block.h"
 
 /* Checks that a new conditional block with type EQ is created without interruption */
 Test(conditional_block_t, new_EQ)
