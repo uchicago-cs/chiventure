22285c760043d8fdb8edb89f3046a87074ab40f9
Sophie Veys <sophiev@cs.uchicago.edu>
2020-05-22 19:47:15 -0400

    made header file

---
+++ include/battle/battle_moves.h
@@ -0,0 +1,43 @@
+#ifndef BATTLE_MOVES_H
+#define BATTLE_MOVES_H
+
+#include <stdbool.h>
+#include <string.h>
+
+/* items stub */
+typedef struct item {
+    int id;
+    int quantity;
+    int durability;
+    char* description;
+    bool battle;
+    int attack;
+    int defense;
+    int hp;
+    struct item *next;
+    struct item *prev;
+} item_t;
+
+
+/* moves stub */
+typedef struct move {
+    char *info;
+    int id;
+    item_t *item;
+    bool attack;
+    int damage;
+    int defense;
+    struct move *next;
+    struct move *prev;
+} move_t;
+
+test_move_bard();
+
+test_move_cleric();
+
+test_move_paladin();
+
+test_move_wizard();
+
+#endif
+
