3f1ae14819529652268247b07d78b928a5d7b7c2
Jacqueline Lee <jacquelinelee@uchicago.edu>
2020-05-23 07:29:19 -0400

    pointer fix

--- tests/skilltrees/test_inventory.c
+++ tests/skilltrees/test_inventory.c
@@ -53,60 +53,60 @@ Test(inventory, free) {
 }
 
 Test(inventory, add_has_remove) {
-    skill_t skill1, skill2, skill3, skill4;
+    skill_t *skill1, *skill2, *skill3, *skill4;
     int init1, init2, init3, init4;
     int add1, add2, add3, add4;
     int has1, has2, has3;
     int rem1, rem2, rem3, rem4;
 
-    init1 = skill_init(&skill1, UNLOCK_DOOR, ACTIVE, "Unlock door",
-                    "A skill that allows a player to unlock a locked door",
-                    1, 0, &effect_unlock);
-    cr_assert_eq(init1, SUCCESS, "skill_init() failed");
-    init2 = skill_init(&skill2, DEFUSE_BOMB, ACTIVE, "Defuse bomb",
-                    "A skill that allows a player to defuse a bomb",
-                    1, 0, &effect_defuse_bomb);
-    cr_assert_eq(init2, SUCCESS, "skill_init() failed");
-    init3 = skill_init(&skill3, CHOP_TREE, ACTIVE, "Chop tree",
-                    "A skill that allows a player to chop a tree down",
-                    1, 0, &effect_chop_tree);
-    cr_assert_eq(init3, SUCCESS, "skill_init() failed");
-    init4 = skill_init(&skill4, INNER_PEACE, PASSIVE, "Inner peace",
-                    "A skill that allows a player to maintain inner peace",
-                    1, 0, &effect_inner_peace);
-    cr_assert_eq(init4, SUCCESS, "skill_init() failed");
+    skill1 = skill_new(UNLOCK_DOOR, ACTIVE, "Unlock door",
+                       "A skill that allows a player to unlock a locked door",
+                       1, 0, &effect_unlock);
+    cr_assert_not_null(skill1, "skill_new() failed");
+    skill2 = skill_new(DEFUSE_BOMB, ACTIVE, "Defuse bomb",
+                       "A skill that allows a player to defuse a bomb",
+                       1, 0, &effect_defuse_bomb);
+    cr_assert_not_null(skill2, "skill_new() failed");
+    skill3 = skill_new(CHOP_TREE, ACTIVE, "Chop tree",
+                       "A skill that allows a player to chop down a tree",
+                       1, 0, &effect_chop_tree);
+    cr_assert_not_null(skill3, "skill_new() failed");
+    skill4 = skill_new(INNER_PEACE, PASSIVE, "Inner peace",
+                       "A skill that allows a player to maintain inner peace",
+                       1, 0, &effect_inner_peace);
+    cr_assert_not_null(skill4, "skill_new() failed");
 
     inventory_t* inventory = inventory_new(2, 5);
     cr_assert_not_null(inventory, "inventory_new() failed");
 
-    add1 = inventory_skill_add(inventory, &skill1);
+    add1 = inventory_skill_add(inventory, skill1);
     cr_assert_eq(add1, SUCCESS, "inventory_skill_add() failed");
     cr_assert_eq(inventory->active[0]->sid, UNLOCK_DOOR, "inventory_skill_add() failed");
 
-    add2 = inventory_skill_add(inventory, &skill2);
+    add2 = inventory_skill_add(inventory, skill2);
     cr_assert_eq(add2, SUCCESS, "inventory_skill_add() failed");
     cr_assert_eq(inventory->active[1]->sid, DEFUSE_BOMB, "inventory_skill_add() failed");
 
-    add3 = inventory_skill_add(inventory, &skill3);
+    add3 = inventory_skill_add(inventory, skill3);
     cr_assert_eq(add3, FAILURE, "inventory_skill_add() failed");
 
-    add4 = inventory_skill_add(inventory, &skill4);
+    add4 = inventory_skill_add(inventory, skill4);
     cr_assert_eq(add4, SUCCESS, "inventory_skill_add() failed");
     cr_assert_eq(inventory->passive[0]->sid, INNER_PEACE, "inventory_skill_add() failed");
 
-    has1 = inventory_has_skill(inventory, &skill4);
+    has1 = inventory_has_skill(inventory, skill4);
     cr_assert_eq(has1, 0, "inventory_has_skill() failed");
 
-    has2 = inventory_has_skill(inventory, &skill3);
+    has2 = inventory_has_skill(inventory, skill3);
     cr_assert_eq(has2, -1, "inventory_has_skill() failed");
 
-    has3 = inventory_has_skill(inventory, &skill2);
+    has3 = inventory_has_skill(inventory, skill2);
     cr_assert_eq(has3, 1, "inventory_has_skill() failed");
 
-    rem1 = inventory_skill_remove(inventory, &skill1);
+    rem1 = inventory_skill_remove(inventory, skill1);
     cr_assert_eq(rem1, SUCCESS, "inventory_skill_remove() failed");
 
-    rem2 = inventory_skill_remove(inventory, &skill2);
+    rem2 = inventory_skill_remove(inventory, skill2);
     cr_assert_eq(rem2, SUCCESS, "inventory_skill_remove() failed");
 
     rem3 = inventory_skill_remove(inventory, &skill3);
