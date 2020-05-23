a1c85e2f9bd6043180667bc3ddf792fce92fa05e
Carolina Elizabeth Calderon <carocalderon@linux2.cs.uchicago.edu>
2020-05-23 13:41:08 -0500

    Working out more bugs. Switched to LL functions

--- src/openworld/sample_generation/sample_rooms.c
+++ src/openworld/sample_generation/sample_rooms.c
@@ -13,17 +13,17 @@ roomspec_t **get_allowed_room(char *bucket, char *sh_desc, char *l_desc,
 	roomspec_t *barn = roomspec_new("barn", "A red barn",
 		"A red barn with stables inside",
 		get_allowed_items("barn", llist), NULL, NULL);
-	roomspec_t *cafeteria = roomspec_new(("cafeteria", "A grungy cafeteria",
+	roomspec_t *cafeteria = roomspec_new("cafeteria", "A grungy cafeteria",
 		"A messy high school cafeteria with trays and tables out",
 		get_allowed_items("cafeteria", llist), NULL, NULL);
 	roomspec_t *classroom = roomspec_new("classroom",
 		"A medium-sized classroom with 30 desks",
+		"A geography teacher's classroom with 30 desks",
 		get_allowed_items("classroom", llist), NULL, NULL);
-	"A geography teacher's classroom with 30 desks");
-	roomspec_t *closet = roomspec_new(("closet", "A broom closet",
+	roomspec_t *closet = roomspec_new("closet", "A broom closet",
 		"A small broom closet with supplies",
 		get_allowed_items("closet", llist), NULL, NULL);
-	roomspec_t *dungeon = roomspec_new(("dungeon", "A dark dungeon",
+	roomspec_t *dungeon = roomspec_new("dungeon", "A dark dungeon",
 		"A dank, dark dungeon with traps and enemies to battle",
 		get_allowed_items("dungeon", llist), NULL, NULL);
 	roomspec_t *field = roomspec_new("open field",
@@ -32,7 +32,7 @@ roomspec_t **get_allowed_room(char *bucket, char *sh_desc, char *l_desc,
 		get_allowed_items("open field", llist), NULL, NULL);
 	roomspec_t *hallway = roomspec_new("hallway", "A well-lit hallway",
 		"A sterile, white hallway with no windows",
-		get_alowed_items("hallway", llist), NULL, NULL);
+		get_allowed_items("hallway", llist), NULL, NULL);
 	roomspec_t *kitchen = roomspec_new("kitchen", "A 60s era (outdated) kitchen",
 		"An outdated kitchen with obvious wear-and-tear",
 		get_allowed_items("kitchen", llist), NULL, NULL);
@@ -49,21 +49,35 @@ roomspec_t **get_allowed_room(char *bucket, char *sh_desc, char *l_desc,
 	assert(bucket != NULL);
 
 	if (!strcmp(bucket, "school")) {
-		roomspec_t **school_rooms = { cafeteria, classroom, closet,
-		field, hallway,  library };
+		roomspec_t **school_rooms = calloc(5, sizeof(roomspec_t*));
+		school_rooms[0] = cafeteria;
+		school_rooms[1] = classroom;
+		school_rooms[2] = closet;
+		school_rooms[3] = hallway;
+		school_rooms[4] = library;
 		return school_rooms;
 	}
 	else if (!strcmp(bucket, "farmhouse")) {
-		roomspec_t **farm_rooms = { barn, closet , field,
-			kitchen, hallway, living_room };
+		roomspec_t **farm_rooms = calloc(5, sizeof(roomspec_t*));
+		farm_rooms[0] = barn;
+		farm_rooms[1] = field;
+		farm_rooms[2] = kitchen;
+		farm_rooms[3] = hallway;
+		farm_rooms[4] = living_room;
 		return farm_rooms;
 	} else if(!strcmp(bucket, "castle")) {	
-		roomspec_t **castle_rooms = { closet, dungeon, living_room,
-			library, hallway, throne_room };
+		roomspec_t **castle_rooms = calloc(5, sizeof(roomspec_t*));
+		castle_rooms[0] = closet;
+		castle_rooms[1] = dungeon;
+		castle_rooms[2] = library;
+		castle_rooms[3] = hallway;
+		castle_rooms[4] = throne_room;
 		return castle_rooms;
 	} else{
-		return &roomspec_new(bucket, sh_desc, l_desc,
+		roomspec_t **rooms = calloc(1, sizeof(roomspec_t*));
+		rooms[0] = roomspec_new(bucket, sh_desc, l_desc,
 			get_allowed_items(bucket, llist), NULL, NULL);
+		return rooms;
 	}
 }
 
@@ -73,25 +87,22 @@ roomspec_t *make_default_room(char *bucket, char *sh_desc, char *l_desc,
 	roomspec_t *hash = NULL;
 
 	//get allowed rooms and defined descriptions
-	roomspec_t **rooms = get_allowed_room(bucket, sh_desc, l_desc, item_list_t *items);
+	roomspec_t **rooms = get_allowed_room(bucket, sh_desc, l_desc, items);
 
 	int i = 0;
 
 	while (rooms[i] != NULL) {
 		//count number of allowed items
-		unsigned int count = DL_COUNT(rooms[i]->allowed_items);
-
+		unsigned int count = LL_COUNT(rooms[i]->allowed_items);
+		item_list_t *allowed = get_allowed_items(rooms[i]->room_name, items);
 		//generate actual item list from allowed items
 		for (unsigned int i = 0; i < count; i++) {
-			//get the item description strings given an id  from allowed items
-			char **details = get_desc_item(rooms[i]->allowed_items->item->item_id);
-			//add the "random" item details from allowed_items to a new item
-			item_t *new_item = item_new(details[0], details[1], details[2]);
 			//append this to item_hash_t for this room spec
-			DL_APPEND(rooms[i]->allowed_items, new_item);
+			LL_APPEND(rooms[i]->allowed_items, allowed->item);
 			HASH_ADD_STR(hash, room_name, rooms[i]);
 			//iterate to next allowed item
 			rooms[i]->allowed_items = rooms[i]->allowed_items->next;
+			allowed = allowed->next;
 		}
 		i++;
 	}
