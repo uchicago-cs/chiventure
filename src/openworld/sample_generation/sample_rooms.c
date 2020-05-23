d2f1f9482d34e54bea25017148c5ebe417f907a4
root <root@Calliope.localdomain>
2020-05-23 16:35:21 -0500

    Fixed function names in room module

--- src/openworld/sample_generation/sample_rooms.c
+++ src/openworld/sample_generation/sample_rooms.c
@@ -16,13 +16,13 @@ roomspec_t **get_allowed_rooms(char *bucket, char *sh_desc, char *l_desc,
 	//these types of rooms are shared by more than one themed room group
 	roomspec_t *closet = roomspec_new("closet", "A broom closet",
 		"A small broom closet with supplies",
-		get_allowed_items("closet", llist), NULL, NULL);
+		make_default_items("closet", llist), NULL, NULL);
 	roomspec_t *hallway = roomspec_new("hallway", "A well-lit hallway",
 		"A sterile, white hallway with no windows",
-		get_allowed_items("hallway", llist), NULL, NULL);
+		make_default_items("hallway", llist), NULL, NULL);
 	roomspec_t *library = roomspec_new("library", "This is a library room with resources",
 		"An old, dusty library with skill-boosting resources like books and potions",
-		get_allowed_items("library", llist), NULL, NULL);
+		make_default_items("library", llist), NULL, NULL);
 
 	assert(bucket != NULL);
 
@@ -34,7 +34,7 @@ roomspec_t **get_allowed_rooms(char *bucket, char *sh_desc, char *l_desc,
 		school_rooms[1] = roomspec_new("classroom",
 			"A medium-sized classroom with 30 desks",
 			"A geography teacher's classroom with 30 desks",
-			get_allowed_items("classroom", llist), NULL, NULL);
+			make_default_items("classroom", llist), NULL, NULL);
 		school_rooms[2] = closet;
 		school_rooms[3] = hallway;
 		school_rooms[4] = library;
@@ -44,18 +44,18 @@ roomspec_t **get_allowed_rooms(char *bucket, char *sh_desc, char *l_desc,
 		roomspec_t **farm_rooms = calloc(5, sizeof(roomspec_t*));
 		farm_rooms[0] = roomspec_new("barn", "A red barn",
 			"A red barn with stables inside",
-			get_allowed_items("barn", llist), NULL, NULL);
+			make_default_items("barn", llist), NULL, NULL);
 		farm_rooms[1] = roomspec_new("open field",
 			"An open field outside",
 			"An open field with grass and a clear view",
-			get_allowed_items("open field", llist), NULL, NULL);
+			make_default_items("open field", llist), NULL, NULL);
 		farm_rooms[2] = roomspec_new("kitchen", "A 60s era (outdated) kitchen",
 			"An outdated kitchen with obvious wear-and-tear",
-			get_allowed_items("kitchen", llist), NULL, NULL);
+			make_default_items("kitchen", llist), NULL, NULL);
 		farm_rooms[3] = hallway;
 		farm_rooms[4] = roomspec_new("living room", "A living room with basic items",
 			"A plain, unremarkable living room",
-			get_allowed_items("living room", llist), NULL, NULL);
+			make_default_items("living room", llist), NULL, NULL);
 		roomspec_free(closet);
 		roomspec_free(library);
 		return farm_rooms;
@@ -64,17 +64,17 @@ roomspec_t **get_allowed_rooms(char *bucket, char *sh_desc, char *l_desc,
 		castle_rooms[0] = closet;
 		castle_rooms[1] = roomspec_new("dungeon", "A dark dungeon",
 			"A dank, dark dungeon with traps and enemies to battle",
-			get_allowed_items("dungeon", llist), NULL, NULL);
+			make_default_items("dungeon", llist), NULL, NULL);
 		castle_rooms[2] = library;
 		castle_rooms[3] = hallway;
 		castle_rooms[4] = roomspec_new("throne room", "This is a throne room",
 			"A regal throne room decked out with lavish items",
-			get_allowed_items("throne room", llist), NULL, NULL);
+			make_default_items("throne room", llist), NULL, NULL);
 		return castle_rooms;
 	} else{
 		roomspec_t **rooms = calloc(1, sizeof(roomspec_t*));
 		rooms[0] = roomspec_new(bucket, sh_desc, l_desc,
-			get_allowed_items(bucket, llist), NULL, NULL);
+			make_default_items(bucket, llist), NULL, NULL);
 		return rooms;
 	}
 }
