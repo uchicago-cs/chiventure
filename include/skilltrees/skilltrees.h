77e54f6d922b9d8aaf0250ec514779f4e5302c6c
Jacqueline Lee <jacquelinelee@uchicago.edu>
2020-05-23 02:50:55 -0400

    pr changes and increased skill effect flexibility #601 #559

--- include/skilltrees/skilltrees.h
+++ include/skilltrees/skilltrees.h
@@ -1,76 +1,195 @@
+/*
+ * Skill trees library for chiventure
+ */
 
-// Player’s skill, unique to each character:
-struct skill_t {
+#ifndef INCLUDE_SKILLTREES_H_
+#define INCLUDE_SKILLTREES_H_
+
+/* Unique skill IDs for each skill */
+typedef enum sid {
+    UNLOCK_DOOR,
+    DEFUSE_BOMB,
+} sid_t;
+
+/* Skill type */
+typedef enum skill_type {
+    ACTIVE,
+    PASSIVE,
+} skill_type_t;
+
+/*
+ * Skill effect function type
+ *
+ * Parameters:
+ *  - Contained within a string for function pointer uniformity. Each skill
+ *    effect function can read the string for parameters using sscanf.
+ *
+ * Returns:
+ *  - A string describing the consequence of the skill execution, for the CLI
+ */
+typedef char* (*effect_t)(char*);
+
+/* An INDIVIDUAL skill, belonging to a player */
+typedef struct skill {
+    // The skill ID that uniquely identifies the skill
+    sid_t sid;
+
+    // The skill type
+    skill_type_t type;
+
+    // The name of the skill
     char* name;
-    unsigned int cur_level;
-    unsigned int experience;
-    char* description;
-    int (*execute_skill)(int param1, int param2);
-};
 
-typedef struct skill_t skill_t;
+    // The description of the skill
+    char* desc;
 
-// Struct which holds a player's every skill, differentiated by passive and
-// active.
-// When not holding a skill, each space in the passive or active skill lists is
-// defined as NULL.
-struct allskills_t {
-    skill_t** active_skills;
-    unsigned int cur_num_active;
-    unsigned int max_num_active;
-    skill_t** passive_skills;
-    unsigned int cur_num_passive;
-    unsigned int max_num_passive;
-};
+    // The player's current level of the skill
+    unsigned int level;
 
-typedef struct allskills_t allskills_t;
+    // The player's current experience points associated with the skill
+    unsigned int xp;
 
-// Creates a new skill when given all of the parameters needed for a skill.
-// Returns a fully malloc'd skill.
-skill_t* new_skill(char* name, unsigned int cur_level, unsigned int experience,
-                   int (*given_skill)(int, int));
+    // The pointer to the function that will execute the skill effect
+    effect_t effect;
 
-// Frees a skill.
-// Returns an error code.
-int free_skill(skill_t* skill);
+} skill_t;
 
-// Creates a new skill list, initially mallocing space for 5 active and 5
-// passive skills.
-// Returns the malloc'd list.
-allskills_t* create_skill_list();
+/* ALL the skills belonging to a player */
+typedef struct skill_inventory {
+    // An array of active skills belonging to a player
+    skill_t** active;
 
-// Frees all skills in the skill list.
-// Returns an error code.
-int free_all_skills(allskills_t* list);
+    // The number of active skills belonging to a player
+    unsigned int nactive;
 
-// Pushes an active skill into the active skill list.
-// Returns an error code.
-int push_active_skill(allskills_t* list, skill_t* skill);
+    // The maximum number of active skills a player can possess
+    // (This field helps to enforce skill tree balancing)
+    unsigned int max_active;
 
-// Pushes a passive skill into the passive skill list.
-// Returns an error code.
-int push_passive_skill(allskills_t* list, skill_t* skill);
+    // An array of passive skills belonging to a player
+    skill_t** passive;
 
-// Pops an active skill from the active skill list.
-// Returns 1 when there wasn't a skill to be found, else, 0.
-int pop_active_skill(allskills_t* list, skill_t* skill);
+    // The number of passive skills belonging to a player
+    unsigned int npassive;
 
-// Pops a passive skill from the passive skill list.
-// Returns 1 when there wasn't a skill to be found, else, 0.
-int pop_passive_skill(allskills_t* list, skill_t* skill);
+    // The maximum number of passive skills a player can possess
+    // (This field helps to enforce skill tree balancing)
+    unsigned int max_passive;
 
-// Tests to see if there is an active skill inside of the current skill list.
-// Returns the location of the skill in the active_skill list  when the skill
-// was found, else, -1.
-int has_active_skill(allskills_t* list, skill_t* skill);
+} skill_inventory_t;
 
-// Tests to see if there is a passive skill inside of the current skill list.
-// Returns the location of the skill in the passive_skill list  when the skill
-// was found, else, -1.
-int has_passive_skill(allskills_t* list, skill_t* skill);
+/*
+ * Allocates a new skill in the heap.
+ *
+ * Parameters:
+ *  - sid: The skill ID that uniquely identifies the skill
+ *  - type: The skill type, ACTIVE or PASSIVE
+ *  - name: The name of the skill
+ *  - desc: The description of the skill
+ *  - effect: The skill effect
+ *
+ * Returns:
+ *  - A pointer to the skill, or NULL if a skill cannot be allocated
+ */
+skill_t* skill_new(sid_t sid, skill_type_t type, char* name, char* desc,
+                   effect_t effect);
+
+/*
+ * Initializes a skill.
+ *
+ * Parameters:
+ *  - skill: A skill. Must point to skill allocated with skill_new
+ *  - sid: The skill ID that uniquely identifies the skill
+ *  - type: The skill type, ACTIVE or PASSIVE
+ *  - name: The name of the skill
+ *  - desc: The description of the skill
+ *  - level: The skill level
+ *  - xp: The xp points associated with the skill
+ *  - effect: The skill effect
+ *
+ * Returns:
+ *  - 0 on success, 1 if an error occurs
+ */
+int skill_init(skill_t* skill, sid_t sid, skill_type_t type, char* name,
+               char* desc, unsigned int level, unsigned int xp,
+               effect_t effect);
+
+/*
+ * Frees the resources associated with a skill.
+ *
+ * Parameters:
+ *  - skill: A skill. Must point to skill allocated with skill_new
+ *
+ * Returns:
+ *  - Always returns 0
+ */
+int skill_free(skill_t* skill);
+
+/*
+ * Allocates a new skill inventory in the heap.
+ *
+ * Parameters:
+ *  - max_active: The maximum number of active skills a player can possess
+ *  - max_passive: The maximum number of passive skills a player can possess
+ *
+ * Returns:
+ *  - A pointer to the skill inventory, or NULL if the skill inventory cannot be
+ *    allocated
+ */
+skill_inventory_t* skill_inventory_new(unsigned int max_active,
+                                       unsigned int max_passive);
+
+/*
+ * Frees the resources associated with a skill inventory.
+ *
+ * Parameters:
+ *  - inventory: A skill inventory. Must point to skill inventory allocated with
+ *               skill_inventory_new
+ *
+ * Returns:
+ *  - Always returns 0
+ */
+int skill_inventory_free(skill_inventory_t* inventory);
+
+/*
+ * Adds a skill to a player's skill inventory.
+ *
+ * Parameters:
+ *  - inventory: A skill inventory
+ *  - skill: A skill
+ *
+ * Returns:
+ *  - 0 on success, 1 if an error occurs
+ */
+int skill_add(skill_inventory_t* inventory, skill_t* skill);
+
+/*
+ * Removes a skill from a player's skill inventory.
+ *
+ * Parameters:
+ *  - inventory: A skill inventory
+ *  - skill: A skill
+ *
+ * Returns:
+ *  - 0 on success, 1 if an error occurs
+ */
+int skill_remove(skill_inventory_t* inventory, skill_t* skill);
+
+/*
+ * Searches for a skill in a player's skill inventory.
+ *
+ * Parameters:
+ *  - inventory: A skill inventory
+ *  - skill: A skill
+ *
+ * Returns:
+ *  - If in the inventory, the position of the skill in the inventory;
+ *    -1 otherwise
+ */
+int has_skill(skill_inventory_t* inventory, skill_t* skill);
 
 // Functions needed in regards to this:
-// 5) Activate passive skills — Activates all passive skills in the list
+// 5) Activate passive skills — Activates all passive skills in the inventory
 
 
 // // Overall skill tree, shared by every player:
@@ -88,3 +207,5 @@ int has_passive_skill(allskills_t* list, skill_t* skill);
 // get a skill.
 // 2) find_unlockables — A player calls this when they want to know
 // what they can get after they unlock this skill.
+
+#endif /* INCLUDE_SKILLTREES_H_ */
--- src/skilltrees/skilltrees.c
+++ src/skilltrees/skilltrees.c
@@ -1,216 +1,193 @@
+/*
+ * Skill trees library for chiventure
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <string.h>
 #include "skilltrees.h"
 
-// Creates an empty list of passive and active skills to use in the game.
-allskills_t* create_skill_list() {
-    allskills_t* res = (allskills_t*)malloc(sizeof(allskills_t));
-    if (res == NULL) {
-        fprintf(stderr, "Invalid memory allocation in create_skill_list #1");
-        exit(1);
+/* See skilltrees.h */
+skill_t* skill_new(sid_t sid, skill_type_t type, char* name, char* desc,
+                   effect_t effect) {
+    skill_t* skill;
+    int rc;
+
+    skill = (skill_t*)malloc(sizeof(skill_t));
+    if (skill == NULL) {
+        fprintf(stderr, "skill_new: could not allocate memory\n");
+        return NULL;
     }
 
-    res->cur_num_active = 0;
-    res->cur_num_passive = 0;
-    res->cur_num_active = 5;
-    res->cur_num_passive = 5;
-    skill_t** active_skills = (skill_t**)malloc(5 * sizeof(skill_t*));
-    if (active_skills == NULL) {
-        fprintf(stderr, "Invalid memory allocation in create_skill_list #2");
-        exit(1);
+    rc = skill_init(skill, sid, type, name, desc, 0, 0, effect);
+    if (rc) {
+        fprintf(stderr, "skill_new: could not initialize skill\n");
+        return NULL;
     }
-    skill_t** passive_skills = (skill_t**)malloc(5 * sizeof(skill_t*));
-    if (passive_skills == NULL) {
-        fprintf(stderr, "Invalid memory allocation in create_skill_list #3");
-        exit(1);
-    }
-    res->active_skills = active_skills;
-    res->passive_skills = passive_skills;
 
-    // Make sure skills are initialized as null.
-    for (int i = 0; i < 5; i++) {
-        res->active_skills[i] = NULL;
-        res->passive_skills[i] = NULL;
-    }
-    return res;
+    return skill;
 }
 
-// Creates a new skill structure.
-skill_t* new_skill(char* name, unsigned int cur_level, unsigned int experience,
-                   int (*given_skill)(int, int)) {
+/* See skilltrees.h */
+int skill_init(skill_t* skill, sid_t sid, skill_type_t type, char* name,
+               char* desc, unsigned int level, unsigned int xp,
+               effect_t effect) {
+    assert(skill != NULL);
 
-    skill_t* res = (skill_t*)malloc(sizeof(skill_t));
-    if (res == NULL) {
-        fprintf(stderr, "Invalid memory allocation in new_skill");
-        exit(1);
-    }
+    skill->sid = sid;
+    skill->type = type;
+    skill->name = strdup(name);
+    skill->desc = strdup(desc);
+    skill->level = level;
+    skill->xp = xp;
+    skill->effect = effect;
 
-    res->name = name;
-    res->cur_level = cur_level;
-    res->experience = experience;
-    res->execute_skill = given_skill;
-    return res;
+    return 0;
 }
 
-// Frees a new skill
-int free_skill(skill_t* skill) {
+/* See skilltrees.h */
+int skill_free(skill_t* skill) {
+    assert(skill != NULL);
+
+    free(skill->name);
+    free(skill->desc);
     free(skill);
+
     return 0;
 }
 
-// Frees all active skills in the skill list.
-// Returns an error code.
-int free_active_skills(allskills_t* list) {
-    for (unsigned int i = 0; i < list->cur_num_active; i++) {
-        int status = free_skill(list->active_skills[i]);
-        if (status != 0) {
-            return status;
-        }
+/* See skilltrees.h */
+skill_inventory_t* skill_inventory_new(unsigned int max_active,
+                                       unsigned int max_passive) {
+    skill_inventory_t* inventory;
+    skill_t** active;
+    skill_t** passive;
+    unsigned int i;
+
+    inventory = (skill_inventory_t*)malloc(sizeof(skill_inventory_t));
+    if (inventory == NULL) {
+        fprintf(stderr, "skill_inventory_new: could not allocate memory\n");
+        return NULL;
     }
-    free(list->active_skills);
+    active = (skill_t**)malloc(sizeof(skill_t*) * max_active);
+    if (active == NULL) {
+        fprintf(stderr, "skill_inventory_new: could not allocate memory\n");
+        return NULL;
+    }
+    passive = (skill_t**)malloc(sizeof(skill_t*) * max_passive);
+    if (passive == NULL) {
+        fprintf(stderr, "skill_inventory_new: could not allocate memory\n");
+        return NULL;
+    }
+
+    for (i = 0; i < max_active; i++) {
+        active[i] = NULL;
+    }
+    for (i = 0; i < max_passive; i++) {
+        passive[i] = NULL;
+    }
+
+    inventory->active = active;
+    inventory->nactive = 0;
+    inventory->max_active = max_active;
+    inventory->passive = passive;
+    inventory->npassive = 0;
+    inventory->max_passive = max_passive;
+
+    return inventory;
+}
+
+/* See skilltrees.h */
+int skill_inventory_free(skill_inventory_t* inventory) {
+    assert(inventory != NULL);
+
+    free(inventory->active);
+    free(inventory->passive);
+    free(inventory);
+
     return 0;
 }
 
-// Frees all passive skills in the skill list.
-// Returns an error code.
-int free_passive_skills(allskills_t* list) {
-    for (unsigned int i = 0; i < list->cur_num_passive; i++) {
-        int status = free_skill(list->passive_skills[i]);
-        if (status != 0) {
-            return status;
-        }
+/* See skilltrees.h */
+int skill_add(skill_inventory_t* inventory, skill_t* skill) {
+    assert(inventory != NULL && skill != NULL);
+
+    switch (skill->type) {
+        case ACTIVE:
+            if (inventory->nactive >= inventory->max_active) {
+                fprintf(stderr, "skill_add: at max active skills\n");
+                return 1;
+            }
+            inventory->active[inventory->nactive] = skill;
+            inventory->active += 1;
+            return 0;
+        case PASSIVE:
+            if (inventory->npassive >= inventory->max_passive) {
+                fprintf(stderr, "skill_add: at max passive skills\n");
+                return 1;
+            }
+            inventory->active[inventory->nactive] = skill;
+            inventory->active += 1;
+            return 0;
+        default:
+            fprintf(stderr, "skill_add: not a valid skill type\n");
+            return 1;
     }
-    free(list->passive_skills);
-    return 0;
 }
 
-int free_all_skills(allskills_t* list) {
-    int passive_status = free_passive_skills(list);
-    if (passive_status != 0) {
-        return passive_status;
-    }
-    int active_status = free_active_skills(list);
-    if (active_status != 0) {
-        return active_status;
-    }
-    free(list);
-    return 0;
-}
+/* See skilltrees.h */
+int skill_remove(skill_inventory_t* inventory, skill_t* skill) {
+    assert(inventory != NULL && skill != NULL);
 
-int push_active_skill(allskills_t* list, skill_t* skill) {
-    if (list->cur_num_active < list->max_num_active) {
-        list->active_skills[list->cur_num_active] = skill;
-        list->cur_num_active += 1;
-        return 0;
-    } else if (list->cur_num_active == list->max_num_active) {
-        list->active_skills = (skill_t**)realloc(
-            list->active_skills, (list->max_num_active + 5) * sizeof(skill_t*));
-        list->active_skills[list->cur_num_active] = skill;
-        list->cur_num_active += 1;
-        // Initialize all empty spaces in new list as null.
-        for (unsigned int i = list->cur_num_active; i < list->max_num_active;
-             i++) {
-            list->active_skills[i] = NULL;
-        }
-        return 0;
-    } else {
-        // cur_num_active is somehow > max_num_active, which is a bug.
+    int pos = has_skill(inventory, skill);
+    if (pos < 0) {
+        fprintf(stderr, "skill_remove: skill is not in inventory");
         return 1;
     }
-}
 
-int push_passive_skill(allskills_t* list, skill_t* skill) {
-    if (list->cur_num_passive < list->max_num_passive) {
-        list->passive_skills[list->cur_num_passive] = skill;
-        list->cur_num_passive += 1;
-        return 0;
-    } else if (list->cur_num_passive == list->max_num_passive) {
-        list->passive_skills = (skill_t**)realloc(
-            list->passive_skills, (list->max_num_passive + 5)*sizeof(skill_t*));
-        list->passive_skills[list->cur_num_passive] = skill;
-        list->cur_num_passive += 1;
-        for (unsigned int i = list->cur_num_passive; i < list->max_num_passive;
-             i++) {
-            list->passive_skills[i] = NULL;
-        }
-        return 0;
-    } else {
-        // cur_num_passive is somehow > max_num_passive, which is a bug.
-        return 1;
+    switch (skill->type) {
+        case ACTIVE:
+            inventory->active[pos] = NULL;
+            inventory->nactive -= 1;
+            inventory->active[pos] = inventory->active[inventory->nactive];
+            inventory->active[inventory->nactive] = NULL;
+            return 0;
+        case PASSIVE:
+            inventory->passive[pos] = NULL;
+            inventory->npassive -= 1;
+            inventory->passive[pos] = inventory->passive[inventory->npassive];
+            inventory->passive[inventory->npassive] = NULL;
+            return 0;
+        default:
+            fprintf(stderr, "skill_remove: not a valid skill type\n");
+            return 1;
     }
 }
 
-int has_active_skill(allskills_t* list, skill_t* skill) {
-    for (unsigned int i = 0; i < list->cur_num_active; i++) {
-        if (strcmp(list->active_skills[i]->name, skill->name) == 0) {
-            return i;
-        }
+/* See skilltrees.h */
+int has_skill(skill_inventory_t* inventory, skill_t* skill) {
+    assert(inventory != NULL && skill != NULL);
+
+    unsigned int i;
+
+    switch (skill->type) {
+        case ACTIVE:
+            for (i = 0; i < inventory->nactive; i++) {
+                if (inventory->active[i]->sid == skill->sid) {
+                    return i;
+                }
+            }
+            return -1;
+        case PASSIVE:
+            for (i = 0; i < inventory->npassive; i++) {
+                if (inventory->passive[i]->sid == skill->sid) {
+                    return i;
+                }
+            }
+            return -1;
+        default:
+            fprintf(stderr, "has_skill: not a valid skill type\n");
+            return -1;
     }
-    return -1;
-}
-
-int has_passive_skill(allskills_t* list, skill_t* skill) {
-    for (unsigned int i = 0; i < list->cur_num_passive; i++) {
-        if (strcmp(list->passive_skills[i]->name, skill->name) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int pop_active_skill(allskills_t* list, skill_t* skill) {
-    int loc = has_active_skill(list, skill);
-    if (loc == -1) {
-        // Skill to be popped isn't in the active_skill list.
-        return -1;
-    } else {
-        int status = free_skill(list->active_skills[loc]);
-        if (status != 0) {
-            return status;
-        }
-        list->active_skills[loc] = NULL; // Make sure nothing's left there.
-        // To make sure we don't have empty pockets in our skill list, we
-        // have to move the last skill in the full list to where we just
-        // removed a skill.
-        list->cur_num_active -= 1;
-        list->active_skills[loc] = list->active_skills[list->cur_num_active];
-        list->active_skills[list->cur_num_active] = NULL;
-        return 0;
-    }
-}
-
-int pop_passive_skill(allskills_t* list, skill_t* skill) {
-    int loc = has_passive_skill(list, skill);
-    if (loc == -1) {
-        // Skill to be popped isn't in the passive_skill list.
-        return -1;
-    } else {
-        int status = free_skill(list->passive_skills[loc]);
-        if (status != 0) {
-            return status;
-        }
-        list->passive_skills[loc] = NULL; // Make sure nothing's left there.
-        // To make sure we don't have empty pockets in our skill list, we
-        // have to move the last skill in the full list to where we just
-        // removed a skill.
-        list->cur_num_passive -= 1;
-        list->passive_skills[loc] = list->passive_skills[list->cur_num_passive];
-        list->passive_skills[list->cur_num_passive] = NULL;
-        return 0;
-    }
-}
-
-
-
-int test_skill(int param1, int param2) {
-    printf("Testing test_skill: %i, %i\n", param1, param2);
-    return 0;
-}
-
-int main() {
-    char name[30] = "Hello";
-    skill_t* our_struct = new_skill(name, 1, 0, &test_skill);
-    int res = our_struct->execute_skill(1, 2);
-    printf("Result: %i\n", res);
 }
