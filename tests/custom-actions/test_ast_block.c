dcb5fa49f2b514395389acc9f999b52b22e6b373
Emma Mary Medeiros <emedeiros@linux4.cs.uchicago.edu>
2020-05-22 22:20:34 -0500

    pointer?

--- tests/custom-actions/test_ast_block.c
+++ tests/custom-actions/test_ast_block.c
@@ -6,10 +6,10 @@
 /* Checks that a new AST block with control type is created without interruption */
 Test(AST_block_t, new_CONTROL)
 {
-    block_t* block = control_block;
+    block_t block = control_block; 
     enum block_type block_type = CONTROL;
 
-    AST_block_t* new_ast = AST_block_new(*block, block_type);
+    AST_block_t* new_ast = AST_block_new(&block, block_type);
 
     cr_assert_not_null(new_ast, "AST_block_new failed");
 
