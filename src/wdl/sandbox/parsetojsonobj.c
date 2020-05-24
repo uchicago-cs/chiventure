9a35bfc213d7ca4772e51a79db7b64b1fc174218
David Thomas Bukowski <dtbukowski@linux2.cs.uchicago.edu>
2020-05-24 13:52:43 -0500

    Make a method to parse the current 5 json files to json objects but not specific properties in them yet. I will need to figure out a way to loop through them.

--- src/wdl/sandbox/parsetojsonobj.c
+++ src/wdl/sandbox/parsetojsonobj.c
@@ -4,44 +4,75 @@
 #include <assert.h>
 
 #include "parsetojsonobj.h"
+#define MAX_DIRECTORY_LENGTH 100
+#define MAX_BYTES 4096
 
 
-
+void makejsonobj(struct json_object *jobject, char filename, zip_t* dir){
+    zip_file_t* zipfile = zip_fopen(dir, filename, 0);
+    assert(zipfile);
+    char*  game_buffer = malloc(sizeof(char) * MAX_BYTES);
+    zip_int64_t chars =  zip_fread(file, buffer, MAX_BYTES);
+    if(chars == 0){
+        printf("Tried making a JSON obj but file is empty! \n");
+    }
+    jobject = json_tokener_parse(buffer);
+    assert(jobject);
+    free(game_buffer);
+    zip_fclose(zipfile);
+}
 
 int main() {
 
-    //FILE *fp;
-   // char buffer[4096];
+    char game_buffer[MAX_BYTES];
+    //This pointer will hold an int value if zip_t fails.
     int* p = malloc(sizeof(int));
     zip_t* dir = zip_open("game1.wdz", 0, p);
-   // zip_name_locate(dir, "actions.json", 0);
-   zip_file_t* file = zip_fopen(dir, "game1/game/items.json", 0);
-   assert(file);
-   // zip_close(dir);
-    
-    /* 
-    struct json_object *game_document;
-    struct json_object *game_obj;
-    struct json_object *rooms_obj;
+
+    struct json_object *actions_obj;
+    struct json_object *globalconditions_obj;
     struct json_object *items_obj;
-    struct json_object *intro_obj;
-    */
-    char* name = malloc(sizeof(char)*100);
-    name = zip_get_name(dir, 7,0);
-   // zip_int64_t chars =  zip_fread(file, buffer, 4096);
-    printf("Name is: %s", name);
-   // zip_fclose(file);
-    /*
-    game_document = json_tokener_parse(buffer);
+    struct json_object *player_obj;
+    struct json_object *rooms_obj;
+
+    makejsonobj(actions_obj, "game1/game/actions.json", dir);
+    makejsonobj(globalconditions_obj, "game1/game/globalconditions.json", dir);
+    makejsonobj(items_obj, "game1/game/items.json", dir);
+    makejsonobj(player_obj, "game1/game/player.json", dir);
+    makejsonobj(rooms_obj, "game1/game/rooms.json", dir);
     
-    assert(game_document);
- 
+
+
+    
+    /*
+    Alternative implementation that is more adaptable for larger number of files
+    Gets files for .JSON files. This is inefficent and may need to use something like:
+    
+    And loop through the indexes to look for names that end in .json
+    
+    zip_int64_t zipfiles = zip_get_num_entries(zip_t *archive, zip_flags_t flags);
+    for (int i = 0; i < zipfiles; i++){
+        char* name = malloc(sizeof(char)*MAX_DIRECTORY_LENGTH);
+
+        name = zip_get_name(dir, i,0);
+        //Some way to check for .json and other types of files
+        free(name)
+    }
+
+    */
+    
+
+
+    
+  
+    
+    /*
     json_object_object_get_ex(game_document, "GAME", &game_obj);
     json_object_object_get_ex(game_document, "ROOMS", &rooms_obj);
     json_object_object_get_ex(game_document, "ITEMS", &items_obj);
-
-    assert(game_obj);
     */
+  
+    zip_close(dir);
     
     return 0;
 }
