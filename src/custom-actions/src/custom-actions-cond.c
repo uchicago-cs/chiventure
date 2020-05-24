9906c87a8a854c60f0df7088ff086dc544b41f23
Brent Palmer DeVries <devriesbp@linux3.cs.uchicago.edu>
2020-05-24 10:58:10 -0500

    Updated include statements

--- src/custom-actions/src/custom-actions-cond.c
+++ src/custom-actions/src/custom-actions-cond.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/custom-actions-cond.h"
+#include "../include/custom-actions-common.h"
 #include "../../../include/game-state/item.h"
 
 
