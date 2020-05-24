10bf5febea8d6ef221b568b416343e16b52d0548
root <root@Calliope.localdomain>
2020-05-24 06:01:04 -0500

    Added items tests

--- tests/openworld/test_items.c
+++ tests/openworld/test_items.c
@@ -2,127 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../include/openworld/default_items.h"
-
+#include "../../include/common/uthash.h"
 
 /* Tests the functions in default_items.h */
 
-/* Test item_list_new */
-Test(item, new_list){
-	item_list_t *llist = item_list_new();
-	cr_assert_not_null(llist, "item_list_new failed");
+Test(item, default_hat) {
+	item_hash_t *default = get_default_items();
+	cr_assert_not_null(default, "get_default_items() NULL, and failed");
+
+	item_t *hat;
+	HASH_FIND_STR(default, "hat", hat);
+	cr_assert_not_null(default, "get_default_items(hat) NULL, and failed");
+
+	cr_assert_str_eq(hat->item_id, "hat", 
+		"get_default_items(hat) wrong item id");
+	cr_assert_str_eq(hat->short_desc, "a top hat", 
+		"get_default_items(hat) wrong item id");
+	cr_assert_str_eq(hat->long_desc, "A black top hat with monogrammed A. L.",
+		"get_default_items(hat) wrong item id");
+}
+Test(item, default_gold) {
+	item_hash_t *default = get_default_items();
+	cr_assert_not_null(default, "get_default_items() NULL, and failed");
+
+	item_t *gold;
+	HASH_FIND_STR(default, "gold", gold);
+	cr_assert_not_null(default, "get_default_items(gold) NULL, and failed");
+
+	cr_assert_str_eq(hat->item_id, "gold", 
+		"get_default_items(gold) wrong item id");
+	cr_assert_str_eq(hat->short_desc, "three gold coins",
+		"get_default_items(gold) wrong short desc");
+	cr_assert_str_eq(hat->long_desc, "Three shiny gold coins the size of your eye",
+		"get_default_items(gold) wrong long desc");
 }
 
-/* Test free_item_list */
-Test(item, free_list) {
-	item_list_t *llist = item_list_new();
-	cr_assert_not_null(llist, "item_list_new failed");
-	item_list_free(llist);
-	cr_assert_null(llist, "item_list_free failed");
-}
+Test(item, default_kettle) {
+	item_hash_t *default = get_default_items();
+	cr_assert_not_null(default, "get_default_items() NULL, and failed");
 
-/* test add_items_to_llist on empty list*/
-Test(item, add_list_empty) {
-	item_list_t *llist = item_list_new();
-	cr_assert_not_null(llist, "item_list_new failed");
+	item_t *kettle;
+	HASH_FIND_STR(default, "kettle", kettle);
+	cr_assert_not_null(default, "get_default_items(kettle) NULL, and failed");
 
-	item_t *babies[1] = { item_new("baby", "baby", "baby") };
-	llist = add_items_to_llist(llist, babies, 1);
-
-	cr_assert_not_null(llist, "add_items_to_llist failed");
-	cr_assert_str_eq(llist->item->item_id, "baby", "add_items_to_llist failed");
-	cr_assert_null(llist->next, "add_items_to_llist failed");
-}
-
-/* Test add_items_to_llist on nonempty list */
-Test(item, add_list_not_empty) {
-	item_list_t *llist = item_list_new();
-
-	cr_assert_not_null(llist, "item_list_new failed");
-
-	llist->item = item_new("babies", "babies", "babies");
-	cr_assert_not_null(llist->item, "item_new failed");	
-	cr_assert_null(llist->next, "item_list_new failed");
-	
-	item_t *baby[1] = { item_new("baby", "baby", "baby") };
-	llist = add_items_to_llist(llist, baby, 1);
-
-	cr_assert_not_null(llist, "add_items_to_llist failed");
-
-	cr_assert_str_eq(llist->item->item_id, "babies", "add_items_to_llist failed");
-	cr_assert_not_null(llist->next, "add_items_to_llist failed");
-	cr_assert_str_eq(llist->item->item_id, "baby", "add_items_to_llist failed");
-	cr_assert_null(llist->next, "add_items_to_llist failed");
-}
-
-/* test make_default_items on defined room type with empty list */
-Test(item, allowed_dungeon_empty){
-	item_list_t *ret = make_default_items("dungeon", item_list_new());
-	cr_assert_not_null(ret, "make_default_items failed");
-
-	cr_assert_str_eq(ret->item->item_id, "door", 
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->item->item_id, "gold", 
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->item->item_id, "hat", 
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->next->item->item_id, "ladder", 
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->next->next->item->item_id, "nail",
-		"make_default_items failed");
-	cr_assert_null(ret->next->next->next->next->next,
-		"make_default_items failed");
-}
-
-/* Test make_default_items on defined room type with nonempty list */
-Test(item, allowed_dungeon_not_empty){
-	item_t *items[1] = { item_new("parrot", "parrot", "parrot") };
-
-	item_list_t *llist = add_items_to_llist(item_list_new(), items, 1);
-	cr_assert_not_null(llist, "add_items_to_llist failed");
-
-	item_list_t *ret = make_default_items("dungeon", llist);
-	cr_assert_not_null(ret, "make_default_items failed");
-
-
-	cr_assert_str_eq(ret->item->item_id, "parrot",
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->item->item_id, "door",
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->item->item_id, "gold",
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->next->item->item_id, "hat",
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->next->next->item->item_id, "ladder",
-		"make_default_items failed");
-	cr_assert_str_eq(ret->next->next->next->next->next->item->item_id, "nail",
-		"make_default_items failed");
-	cr_assert_null(ret->next->next->next->next->next->next,
-		"make_default_items failed");
-}
-
-/* Test make_default_items on undefined room type and empty list */
-Test(item, allowed_pharmacy_empty) {
-	item_list_t *llist = item_list_new();
-	cr_assert_not_null(llist, "item_list_new failed");
-
-	item_list_t *ret = make_default_items("pharmacy", item_list_new());
-	cr_assert_null(ret, "make_default_items failed");
-}
-
-/* Test make_default_items on undefined room type and nonempty list */
-Test(item, allowed_pharmacy_not_empty){
-	item_t *items[1] = { item_new("parrot", "parrot", "parrot") };
-
-	item_list_t *llist = item_list_new();
-	cr_assert_not_null(llist, "item_list_new failed");
-
-	llist = add_items_to_llist(llist, items, 1);
-	cr_assert_not_null(llist, "add_items_to_llist failed");
-
-	item_list_t *ret = make_default_items("pharmacy", llist);
-	cr_assert_null(ret, "make_default_items failed");
-
-	cr_assert_str_eq(ret->item->item_id, "parrot",
-		"make_default_items failed");
-	cr_assert_null(ret->next, "make_default_items failed");
+	cr_assert_str_eq(hat->item_id, "kettle", 
+		"get_default_items(kettle) wrong item id");
+	cr_assert_str_eq(hat->short_desc, "a black kettle", 
+		"get_default_items(kettle) wrong short desc");
+	cr_assert_str_eq(hat->long_desc, "A black kettle on a stove",
+		"get_default_items(kettle) wrong long desc");
 }
