4f4bbbeebe2c70904622aa65ce7e1f13f2f5520c
Christopher Zhu <chz@uchicago.edu>
2020-05-24 09:39:59 -0700

    include player to resolve warning

--- src/game-state/src/game_action.c
+++ src/game-state/src/game_action.c
@@ -1,5 +1,6 @@
 #include "game-state/game_action.h"
 #include "common-game-action.h"
+#include "game-state/player.h"
 
 /* See common_game_action.h */
 int game_action_init(game_action_t *new_action, char *act_name,
