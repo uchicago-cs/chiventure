b957221c32b66fecf2b13676fcd8c9c825b1cf22
Brent Palmer DeVries <devriesbp@linux3.cs.uchicago.edu>
2020-05-24 10:40:34 -0500

    Adding custom actions conditions header and common header

---
+++ src/custom-actions/include/custom-actions-common.h
@@ -0,0 +1,8 @@
+#ifndef CUSTOM_ACTIONS_COMMON_H
+#define CUSTOM_ACTIONS_COMMON_H
+
+#define TRUE 0
+#define FALSE 1
+#define FAILURE 2
+
+#endif /* CUSTOM_ACTIONS_COMMON_H */
--- src/custom-actions/src/custom-actions-cond.c
+++ src/custom-actions/src/custom-actions-cond.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "structs.h"
+#include <string.h>
+#include "../include/custom-actions-cond.h"
+#include "../../../include/game-state/item.h"
 
 
+/*
+ * Atomic Conditionals: functions to check different conditions of an action
+ */ 
+
+
+/* Enumerative type for numerically comparative conditions */
 typedef enum {
     LT,
     GT,
@@ -11,21 +19,7 @@ typedef enum {
 } num_comp;
 
 
-/*
- * Atomic Conditionals: functions to check different conditions of an action
- */ 
-
-
-/* 
- * check_eq - determine whether the value of an attribute is equal to another
- *
- * Arguments:
- *  - a1, a2: pointers to attributes to be compared
- *
- * Returns:
- *  - TRUE/FALSE int status code
- *  - FAILURE status code if attribute types cannot be compared
- */
+/* See custom-actions-cond.h */
 int check_eq(attribute_t *a1, attribute_t *a2)
 {
     if (a1->attribute_tag != a2->attribute_tag) {
@@ -45,6 +39,16 @@ int check_eq(attribute_t *a1, attribute_t *a2)
 }
 
 
+/* num_comp - compares two attributes with numerical values
+ *
+ * Arguments:
+ *  - a1, a2: attributes to be compared
+ *  - op: comparative operator to be used
+ *
+ * Returns:
+ *  - TRUE/FALSE int status code
+ *  - FAILURE status code if attribute types cannot be compared
+ */
 int num_comp(attribute_t *a1, attribute_t *a2, num_comp op)
 {
     if (a1->attribute_tag != a2->attribute_tag) {
@@ -76,95 +80,35 @@ int num_comp(attribute_t *a1, attribute_t *a2, num_comp op)
 }
 
 
-/* 
- * check_lt - determine whether attribute a1 is less than attribute a2
- *
- * Notes:
- *  - supports attributes of type int, double, or character
- *
- * Arguments:
- *  - a1, a2: pointers to attributes to be compared
- *
- * Returns:
- *  - TRUE/FALSE int status code
- *  - FAILURE status code if attribute types cannot be compared
- */
+/* See custom-actions-cond.h */
 int check_lt(attribute_t *a1, attribute_t *a2)
 {
     num_comp(a1, a2, LT);
 }
 
 
-/* 
- * check_gt - determine whether attribute a1 is greater than attribute a2
- *
- * Notes:
- *  - supports attributes of type int or double only
- *
- * Arguments:
- *  - a1, a2: pointers to attributes to be compared
- *
- * Returns:
- *  - TRUE/FALSE int status code
- *  - FAILURE status code if attribute types cannot be compared
- */
+/* See custom-actions-cond.h */
 int check_gt(attribute_t *a1, attribute_t *a2)
 {
     num_comp(a1, a2, GT);
 }
 
 
-/* 
- * check_lte - determine whether attribute a1 is less than or equal to
- *  attribute a2
- *
- * Notes:
- *  - supports attributes of type int or double only
- *
- * Arguments:
- *  - a1, a2: pointers to attributes to be compared
- *
- * Returns:
- *  - TRUE/FALSE int status code
- *  - FAILURE status code if attribute types cannot be compared
- */
+/* See custom-actions-cond.h */
 int check_lte(attribute_t *a1, attribute_t *a2)
 {
     num_comp(a1, a2, LTE);
 }
 
 
-/* 
- * check_gte - determine whether attribute a1 is greater than or equal to
- *  attribute a2
- *
- * Notes:
- *  - supports attributes of type int or double only
- *
- * Arguments:
- *  - a1, a2: pointers to attributes to be compared
- *
- * Returns:
- *  - TRUE/FALSE int status code
- *  - FAILURE status code if attribute types cannot be compared
- */
+/* See custom-actions-cond.h */
 int check_gte(attribute_t *a1, attribute_t *a2)
 {
     num_comp(a1, a2, GTE);
 }
 
 
-/*
- * check_in - determine whether a certain item is in the container
- *
- * Arguments:
- *  - a: string attribute with the name of the item to be checked for
- *  - c: container item to be checked in
- *
- * Returns:
- *  - TRUE/FALSE int status code
- *  - FAILURE status code if invalid input
- */
+/* See custom-actions-cond.h */
 int check_in(attribute_t *a, item_t *c)
 {
         return 0;
