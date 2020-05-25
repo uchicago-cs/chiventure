e5ab7d18c1433b6f903598449512cf2e144085b2
Simren Kshetrapal <simrenk@uchicago.edu>
2020-05-24 22:31:25 -0700

    Git Issues w/ Previous rpg-playerclass/class-item-restriction.c branch – Moving relevant changes to this branch

---
+++ src/playerclass/src/class_item_restriction.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include "playerclass/class_structs.h"
+#include "playerclass/class.h"
+#include "game-state/item.h"
+
+int set_item_restriction(item_t* item, class_t* class) {
+    if ((item != NULL) && (class != NULL)) {
+        set_bool_attr(item, class->name, true);
+    return SUCCESS;
+    } else {
+        fprintf(stderr, "Item or Class provided are NULL");
+	    return FAILURE;
+    }
+}
+
+bool get_class_restriction(item_t* item, class_t* class) {
+    if (get_bool_attr(item, class->name)) {
+        return true;
+    }
+    return false;
+}
