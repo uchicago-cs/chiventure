d57da74d8a023198b5f810601cbe6100e2b2ff7d
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-05-24 21:18:50 -0500

    action.h

--- src/custom-actions/include/action_block.h
+++ src/custom-actions/include/action_block.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "game-state/item.h"
+#include "ast_block.h"
 
 /* An enumeration type for an action block */
 typedef enum action_type {
@@ -17,7 +18,7 @@ typedef enum action_type {
     ADDSUBMULTDIV, 
     GEN, 
     EXEC
-} action_type_t;
+} action_enum_t;
 
 /* A block that holds an action, as well as corresponding attributes and actions */
 typedef struct action_block {
@@ -37,7 +38,7 @@ typedef struct action_block {
  * Returns: 
  * - An action block. 
  */
-action_block_t* action_block_new(action_type_t action_type, int num_args, 
+AST_block_t* action_block_new(action_type_t action_type, int num_args, 
 attribute_t** args);
 
 /* 
