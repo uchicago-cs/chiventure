82729ddf146fa40665732c233246a8e9b419c0f0
David Thomas Bukowski <dtbukowski@linux4.cs.uchicago.edu>
2020-05-26 01:13:46 -0500

    Changed pseudocode into mostly real code, will write list of things still needed

--- src/wdl/sandbox/parsetojsonobj.c
+++ src/wdl/sandbox/parsetojsonobj.c
@@ -22,6 +22,57 @@ void makejsonobj(struct json_object *jobject, char filename, zip_t* dir){
     zip_fclose(zipfile);
 }
 
+objtype getobj_type(char* name){
+    int n = strlen(name);
+    //takes file to see if it includes keynames "action", "globalconditions", etc.
+    for(int i = n; i > 0; i--){
+        if (s[i] == '.'){
+            char subbuff[17];
+            memcpy( subbuff, &s[i-7], 7);
+            subbuff[6] = '\0';
+            printf("\nSub is: %s", subbuff);
+            if(!strcmp("action", subbuff)){
+                return TYPE_ACTION
+            }
+            memcpy( subbuff, &s[i-16], 16);
+            subbuff[16] = '\0';
+            if(!strcmp("globalconditions", subbuff)){
+                 return TYPE_GCONDITION
+            }
+            memcpy( subbuff, &s[i-6], 6);
+            subbuff[6] = '\0';
+            if(!strcmp("player", player)){
+                 return TYPE_PLAYER
+            }
+            memcpy( subbuff, &s[i-4], 4);
+            subbuff[4] = '\0';
+            if(!strcmp("item", subbuff)){
+                 return TYPE_ITEM
+            }
+            memcpy( subbuff, &s[i-4], 4);
+            subbuff[4] = '\0';
+            if(!strcmp("room", subbuff)){
+                 return TYPE_ROOM
+            }
+            /*SLOTS FOR NPC AND DIALOG TREE TYPES*/
+            /*
+            memcpy( subbuff, &s[i-16], 16);
+            subbuff[16] = '\0';
+            if(!strcmp("globalconditions", subbuff)){
+                 printf("This is OK");
+            }
+            memcpy( subbuff, &s[i-16], 16);
+            subbuff[16] = '\0';
+            if(!strcmp("globalconditions", subbuff)){
+                 printf("This is OK");
+            }
+            */
+        }
+        
+    }
+    return TYPE_NONE
+}
+
 int main() {
 
     char game_buffer[MAX_BYTES];
@@ -36,6 +87,10 @@ int main() {
     struct json_object *rooms_obj;
 
     makejsonobj(actions_obj, "game1/game/actions.json", dir);
+    assert(actions_obj)
+    json_object_object_get_ex(action_list, "actions", &actions_obj);
+    assert(action_list)
+
     makejsonobj(globalconditions_obj, "game1/game/globalconditions.json", dir);
     makejsonobj(items_obj, "game1/game/items.json", dir);
     makejsonobj(player_obj, "game1/game/player.json", dir);
@@ -49,26 +104,58 @@ int main() {
     Gets files for .JSON files. This is inefficent and may need to use something like:
     
     And loop through the indexes to look for names that end in .json
-    
-    zip_int64_t zipfiles = zip_get_num_entries(zip_t *archive, zip_flags_t flags);
+    */
+    zip_int64_t zipfiles = zip_get_num_entries(dir, 0);
     for (int i = 0; i < zipfiles; i++){
+        //Gets name of each directory
         char* name = malloc(sizeof(char)*MAX_DIRECTORY_LENGTH);
 
         name = zip_get_name(dir, i,0);
-        //Some way to check for .json and other types of files
-            //If it is JSON
-            //Parse into a JSON file
-            json_object_object_foreach(THEJSONOBJ, key, val ){
-                json_object_get_type (val)
-                //Test each scenario, if val is int, double, another object, string, array
-                //And then add that object
-                printf("For each is working, key is %s\n and the value type is %d \n", key, json_object_get_type(val));
+        int n = strlen(name);
+        //takes file character by character to see if it ends in .json or other endings
+        for(int i = 0; i < n; i++){
+            if (s[i] == '.'){
+                if(s[i+1] == 'j' && s[i+2] == 's' && s[i+3] == 'o' && s[i+4] == 'n' && s[i+5] == '\0'){
+                    //Parse into a JSON file
+                    struct json_object *THEJSONOBJ;
+                    makejsonobj(THEJSONOBJ), name, dir);
 
-            }	
+                    //Get objtype e type using its name
+                    objtype filetype = get_objtype(name);
+
+                    //Gets each object in the .json file (Such as actions.json) and adds to obj_store
+                    json_object_object_foreach(THEJSONOBJ, key, val){
+                        if (json_object_is_type(json_object_get_type (val), json_type_boolean )	){
+                            //add boolean object with id key and objtype filetype
+                        }
+                        else if (json_object_is_type(json_object_get_type (val), json_type_int )	){
+                            //add int object with id key and objtype filetype
+                        }
+                        else if (json_object_is_type(json_object_get_type (val), json_type_double )	){
+                            //add double object with id key and objtype filetype
+                        }
+                        else if (json_object_is_type(json_object_get_type (val), json_type_string )	){
+                            //add string object with id key and objtype filetype
+                        }
+                        else if (json_object_is_type(json_object_get_type (val), json_type_array )	){
+                            //add array object with id key and objtype filetype
+                        }
+                        else if (json_object_is_type(json_object_get_type (val), json_type_object )	){
+                            //add object with id key and objtype filetype
+                            //will need to make this object (a json object) in a libobj
+                        }
+
+                    }	
+                }
+            }
+            
+        }
+      
+                
         free(name)
     }
 
-    */
+    
     
 
 
--- src/wdl/sandbox/parsetojsonobj.h
+++ src/wdl/sandbox/parsetojsonobj.h
@@ -1,3 +1,4 @@
 #include <zip.h>
+//Include directories that make it possible to add objects
 
 int main();
