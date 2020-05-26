c521c24efe3a11c360e04b847134cdd6ddefa6e1
Sumedh Sarwate <sumedhsarwate@uchicago.edu>
2020-05-25 19:01:44 -0500

    adding tests for display_stat #591

--- tests/game-state/test_stats.c
+++ tests/game-state/test_stats.c
@@ -19,7 +19,7 @@ Test (stats, add_stat_player)
     s.max = 75;
     s.modifier = 1.1;
 
-    int rc = add_stat_player(sh, s);
+    int rc = add_stat_player(sh, &s);
 
     cr_assert_eq(rc, SUCCESS, "add_stat_player failed");
 
@@ -27,4 +27,56 @@ Test (stats, add_stat_player)
     HASH_FIND(hh, sh, g, sizeof(stats_global_t), test);
 
     cr_assert_not_null(test, "add_stat_player did not add the stat");
+}
+
+/* Checks that display_stat returns the correct list of stats */
+Test(stats, display_stat)
+{
+    stats_hash_t *sh = NULL;
+
+    stats_global_t health;
+    health.name = "health";
+    health.max = 100;
+
+    stats_t s1;
+    s1.global = &health;
+    s1.val = 50;
+    s1.max = 75;
+    s1.modifier = 1.1;
+
+    int rc = add_stat_player(sh, &s1);
+
+    cr_assert_eq(rc, SUCCESS, "add_stat_player failed");
+
+    stats_t *test;
+    HASH_FIND(hh, sh, health, sizeof(stats_global_t), test);
+
+    cr_assert_not_null(test, "add_stat_player did not add the stat");
+
+    stats_global_t speed;
+    speed.name = "speed";
+    speed.max = 100;
+
+    stats_t s2;
+    s2.global = &speed;
+    s2.val = 25;
+    s2.max = 50;
+    s2.modifier = 0.9;
+
+    int rc = add_stat_player(sh, &s2);
+
+    cr_assert_eq(rc, SUCCESS, "add_stat_player failed");
+
+    stats_t *test2;
+    HASH_FIND(hh, sh, speed, sizeof(stats_global_t), test);
+
+    cr_assert_not_null(test2, "add_stat_player did not add the stat");
+
+    char *list = display_stats(sh);
+
+    cr_assert_str_eq(list, "health, speed",
+                     "expected: health, speed, but display_stat returned %s",
+                     list);
+
+    free(list);
 }
