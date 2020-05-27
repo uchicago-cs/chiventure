3e0a93fcbfca26f717119901cb9107e001d98f86
ela-ine <elainewan@uchicago.edu>
2020-05-27 17:04:09 -0500

    added basic implementation (not tested and doesn't compile). ran into some issues with structs in obj_documentation.h, going to go fix.

--- include/wdl/attributes.h
+++ include/wdl/attributes.h
@@ -1,5 +1,5 @@
 #include "../../src/libobj/sandbox/sample_obj_documentation.h"
-#include "common/common.h"
+#include "../common/common.h"
 
 #ifndef INCLUDE_ATTRIBUTES_H
 #define INCLUDE_ATTRIBUTES_H
@@ -25,12 +25,40 @@
 
 } attribute_t; */
 
+/* new_attr - allocates and inits an attr_t
+ *
+ * params:
+ *   - id: attr id
+ *   - data: atrr data
+ * returns: ptr to new attribute_t
+ */
 attribute_t *new_attr(char *id, data d);
 
-int *add_attr(char *id, data d);
+/* find_attr - given id, find attribute in hash
+ *
+ * params:
+ *   - attrs: attribute_t hash
+ *   - id: attr id
+ * returns: ptr to attribute if found, else NULL
+ */
+attribute_t *find_attr(attribute_t **attrs, char *id);
 
-attribute_t *find_attr(char *id, data d);
+/* add_attr - if attr is unique, add new attr. else, update attr data.
+ *
+ * params:
+ *   - attrs: attribute_t hash
+ *   - id: attr id
+ *   - data: atrr data
+ * returns: SUCCESS on completion, else FAILURE
+ */
+int add_attr(attribute_t **attrs, char *id, data d);
 
-attribute_t *free_attr(attribute_t *a);
+/* free_attr - frees & deletes attr from hash table
+ *
+ * params:
+ *   - attr: attribute
+ * returns: SUCCESS
+ */
+int free_attr(attribute_t **attrs, attribute_t *a);
 
 #endif
--- src/wdl/src/attributes.c
+++ src/wdl/src/attributes.c
@@ -1,1 +1,52 @@
-#include "wdl/attributes.h"
+#include "../../../include/wdl/attributes.h"
+
+/* See attributes.h for documentation */
+attribute_t *new_attr(char *id, data d)
+{
+    if (id == NULL) {
+        return NULL;
+    }
+
+    attribute_t *new = malloc(sizeof(attribute_t));
+    memset(new, 0, sizeof(*new)); // to accommodate padding in struct
+
+    strcpy(new->id, id);
+    new->data = data;
+    return new;
+}
+
+/* See attributes.h for documentation */
+attribute_t *find_attr(attribute_t **attrs, char *id)
+{
+    attribute_t *res;
+    char tmp[MAXLEN_ID];
+    strcpy(tmp, id);
+
+    HASH_FIND(hh, *attrs, &tmp, sizeof(tmp), res);
+
+    return res;
+}
+
+/* See attributes.h for documentation */
+int add_attr(attribute_t **attrs, char *id, data d)
+{
+    if (id == NULL) {
+        return FAILURE;
+    }
+    attribute_t *new = find_objstore(attrs, id); // see if key already exists in hash
+    if (new == NULL) {
+        new =  new_attr(id, d);
+        if (new == NULL) return FAILURE;
+        HASH_ADD(hh, *attrs, id, sizeof(id), new);
+    } else {
+        new->o = o;
+    }
+    return SUCCESS;
+}
+
+/* See attributes.h for documentation */
+int *free_attr(attribute_t **attrs, attribute_t *a)
+{
+    HASH_DEL(attrs, a);
+    free(a);
+}
