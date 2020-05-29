17af83e1f11c4e0a0d6b8bf0944722e0eeae15c6
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-05-28 23:35:51 -0500

    Update main.c

--- tests/custom-actions/main.c
+++ tests/custom-actions/main.c
@@ -6,10 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> origin/dev
     struct criterion_test_set *tests = criterion_initialize();
 
     criterion_options.jobs = 1;
@@ -20,19 +16,4 @@ int main(int argc, char *argv[])
 
     criterion_finalize(tests);
     return result;
-<<<<<<< HEAD
-=======
-        struct criterion_test_set *tests = criterion_initialize();
-
-        criterion_options.jobs = 1;
-
-        int result = 0;
-        if (criterion_handle_args(argc, argv, true))
-            result = !criterion_run_all_tests(tests);
-
-        criterion_finalize(tests);
-        return result;
->>>>>>> origin/dev
-=======
->>>>>>> origin/dev
 }
