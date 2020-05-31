51094d6caeb5ae16788f40bba4c0108608419440
PaulTreitel <paul.treitel@gmail.com>
2020-05-30 21:37:04 -0700

    reduced class args; fixed tests that broke; started class_skills_init

--- include/playerclass/class.h
+++ include/playerclass/class.h
@@ -7,6 +7,7 @@
 #include "libobj/obj.h"
 #include "common/utlist.h"
 #include "playerclass/class_structs.h"
+#include "game-state/stats.h"
 
 /* 
  * Allocates memory for a new player class. Only creates a deep copies of the 
@@ -24,9 +25,8 @@
  *  - a pointer to the allocated class memory
  *  - NULL on error
  */
-class_t* class_new(char* name, char* shortdesc, char* longdesc, obj_t* attr,
-                   stats_stub_t* stat, skilltree_stub_t* skilltree,
-                   skill_inventory_t* combat, skill_inventory_t* noncombat);
+class_t* class_new(char* name, char* shortdesc, char* longdesc,
+                   obj_t* attr, stats_stub_t* stat);
 
 /* 
  * Initializes values for a player class. Only creates a deep copies of the 
@@ -46,8 +46,7 @@ class_t* class_new(char* name, char* shortdesc, char* longdesc, obj_t* attr,
  *  - EXIT_FAILURE otherwise
  */
 int class_init(class_t* class, char* name, char* shortdesc, char* longdesc,
-               obj_t* attr, stats_stub_t* stat, skilltree_stub_t* skilltree,
-               skill_inventory_t* combat, skill_inventory_t* noncombat);
+               obj_t* attr, stats_stub_t* stat);
 
 /*
  * Frees a class and strings created by class_new.
--- src/playerclass/src/class.c
+++ src/playerclass/src/class.c
@@ -12,10 +12,14 @@
 #include "playerclass/class.h"
 #include "common/utlist.h"
 
+/* define constants for how many skills a class can have in their active skill lists
+ * numbers are utterly arbitrary */ 
+#define MAX_ACTIVE_SKILLS (10)
+#define MAX_PASSIVE_SKILLS (10)
+
 /* See class.h */
 class_t* class_new(char* name, char* shortdesc, char* longdesc,
-                   obj_t* attr, stats_stub_t* stat, skilltree_stub_t* skilltree,
-                   skill_inventory_t* combat, skill_inventory_t* noncombat)
+                   obj_t* attr, stats_stub_t* stat)
 {
     int rc;
     class_t* c;
@@ -28,7 +32,7 @@ class_t* class_new(char* name, char* shortdesc, char* longdesc,
         return NULL;
     }
     
-    rc = class_init(c, name, shortdesc, longdesc, attr, stat, skilltree, combat, noncombat);
+    rc = class_init(c, name, shortdesc, longdesc, attr, stat);
 
     if (rc == EXIT_FAILURE){
         fprintf(stderr, "Could not initalize values for class_new.\n");
@@ -37,10 +41,51 @@ class_t* class_new(char* name, char* shortdesc, char* longdesc,
     return c;
 }
 
+
+/*
+ * initializes the class skilltree and active combat and noncombat skill lists
+ *
+ * Parameters:
+ *  - class. the class the skilltree and lists should be placed in
+ *
+ * Returns:
+ *  - status code
+ */
+int class_skills_init(class_t* class)
+{
+    skill_inventory_t *combat, *noncombat;
+    skilltree_stub_t *tree;
+
+    combat = skill_inventory_new(MAX_ACTIVE_SKILLS, MAX_PASSIVE_SKILLS);
+    noncombat = skill_inventory_new(MAX_ACTIVE_SKILLS, MAX_PASSIVE_SKILLS);
+    // stub placeholder allocation
+    tree = calloc(1, sizeof(skilltree_stub_t));
+
+    if (tree == NULL)
+    {
+        fprintf(stderr, "Could not allocate memory for skill trees "
+                        "in class_skills_init\n");
+        return EXIT_FAILURE;
+    }
+    class->skilltree = tree;
+    class->combat = combat;
+    class->noncombat = noncombat;
+
+    /*
+     * TODO
+     *  - set up a system for filling in the skill structures
+     *    based on the class name
+     *  - designate some skills as baseline and automatically add them
+     *    to the relevant skill inventories
+     */
+    
+    return EXIT_SUCCESS;
+}
+
+
 /* See class.h */
 int class_init(class_t* class, char* name, char* shortdesc, char* longdesc,
-                   obj_t* attr, stats_stub_t* stat, skilltree_stub_t* skilltree,
-                   skill_inventory_t* combat, skill_inventory_t* noncombat)
+                   obj_t* attr, stats_stub_t* stat)
 {
     if (class == NULL)
     {
@@ -74,12 +119,14 @@ int class_init(class_t* class, char* name, char* shortdesc, char* longdesc,
     }
     strncpy(class->longdesc, longdesc, MAX_LONG_DESC_LEN);
     
+    if (class_skills_init(class) == EXIT_FAILURE)
+    {
+        return EXIT_FAILURE;
+    }
+
     class->attributes = attr;
     class->stats = stat;
-    class->skilltree = skilltree;
-    class->combat = combat;
-    class->noncombat = noncombat;
-
+    
     return EXIT_SUCCESS;
 }
 
@@ -102,6 +149,18 @@ int class_free(class_t* class)
     if (class->longdesc != NULL)
     {
         free(class->longdesc);
+    }
+    if (class->skilltree != NULL)
+    {
+        free(class->skilltree); // stub placeholder free
+    }
+    if (class->combat != NULL)
+    {
+        skill_inventory_free(class->combat);
+    }
+    if (class->noncombat != NULL)
+    {
+        skill_inventory_free(class->noncombat);
     }
 
     free(class);
