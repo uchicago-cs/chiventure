d837f37a02428054867e8796f533dc4829d6eb45
Eric Chang <ericyc16@gmail.com>
2020-06-02 19:13:19 -0400

    Running demo runs chiventure game; wrote basic sample game; #667

--- src/action_management/examples/spring2020-demo.c
+++ src/action_management/examples/spring2020-demo.c
@@ -7,7 +7,21 @@ const char *banner = "THIS IS AN EXAMPLE PROGRAM";
 
 int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
-    
+    /*
+    struct winsize w;
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &w);
+    int ncols = w.ws_col, nrows = w.ws_row;
+    if (ncols < MIN_COLS || nrows < MIN_ROWS) {
+        fprintf(stderr, "Chiventure prefers to run in terminals of at least %d columns and %d rows. Please resize your terminal!\n", MIN_COLS, MIN_ROWS);
+        exit(1);
+    }*/
+
+    chiventure_ctx_t *ctx = chiventure_ctx_new("../src/action_management/examples/test_game.wdl");
+
+    /* Add calls to component-specific initializations here */
+
+    start_ui(ctx, banner);
+
+    game_free(ctx->game);
     return 0;
 }
