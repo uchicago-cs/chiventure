2cf00c158a9aa54e65eeff866ce6435fd58bdba5
Eric Chang <ericyc16@gmail.com>
2020-06-02 17:23:03 -0400

    Created demo file & updated CMakeLists; #667

---
+++ src/action_management/examples/spring2020-demo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cli/operations.h>
+#include "common/ctx.h"
+#include "ui/ui.h"
+
+const char *banner = "THIS IS AN EXAMPLE PROGRAM";
+
+int main(int argc, char **argv)
+{
+    printf("Hello world!\n");
+    
+    return 0;
+}
