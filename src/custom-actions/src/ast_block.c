38211774bb02637d7d930576d693cee487c6eb02
Emma Mary Medeiros <emedeiros@linux4.cs.uchicago.edu>
2020-06-03 16:24:20 -0500

    need ast_block_free

--- src/custom-actions/src/ast_block.c
+++ src/custom-actions/src/ast_block.c
@@ -11,7 +11,7 @@
 #include "ast_block.h"
 
 /* See ast_block.h */
-AST_block_t* AST_block_new(block_t *block, block_type_t block_type)
+AST_block_t* AST_block_new(block_t *block, block_type_t block_type, AST_block_t **next)
 {
     AST_block_t *ast = malloc(sizeof(AST_block_t));
     int new_ast;
@@ -22,7 +22,7 @@ AST_block_t* AST_block_new(block_t *block, block_type_t block_type)
         return NULL;
     }
 
-    new_ast = AST_block_init(ast, block, block_type);
+    new_ast = AST_block_init(ast, block, block_type, *next);
     if (new_ast != SUCCESS)
     {
         fprintf(stderr, "Could not initialize AST_block_t");
@@ -33,14 +33,15 @@ AST_block_t* AST_block_new(block_t *block, block_type_t block_type)
 }
 
 /* See ast_block.h */
-int AST_block_init(AST_block_t *ast, block_t *block, block_type_t block_type)
+int AST_block_init(AST_block_t *ast, block_t *block, block_type_t block_type, AST_block_t **next)
 {
     assert(ast != NULL); 
     assert(block != NULL);
-
+    
     ast->block = block;
     ast->block_type = block_type;
-    
+    ast->next = *next;
+
     return SUCCESS; 
 }
 
