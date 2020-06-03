840463a62171dbaf44dfad8ebbf4600aad11a1f5
michaelleestuy <leemichael@linux1.cs.uchicago.edu>
2020-06-03 18:56:44 -0500

    headers for test files

---
+++ tests/skilltrees/skilltree_test.c
@@ -0,0 +1,8 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "skilltrees/skilltree.h"
+#include "skilltrees/skill.h"
+#include "skilltrees/stdst.h"
+#include "skilltrees/inventory.h"
