b6850e04b995e1e9af89690abe6c95040bace934
Isaaq <isaaqkhader@linux5.cs.uchicago.edu>
2020-06-02 20:00:42 -0500

    *actually* fixed the tests

--- tests/battle/test_battle_flow.c
+++ tests/battle/test_battle_flow.c
@@ -210,12 +210,11 @@ Test(battle_flow, do_damage_battle_flow)
     ctx = battle_flow(ctx, move, "Enemy");
     cr_assert_not_null(ctx, "battle_flow returned NULL");
     cr_assert_eq(ctx->game->battle->enemy->stats->hp,
-                 5, 
+                 10, 
                  "battle_flow() did not compute damage correctly");
-    printf("%d\n", ctx->game->battle->player->stats->hp);
-        cr_assert_eq(ctx->game->battle->player->stats->hp,
-                     10,
-                     "battle_flow() did not compute damage correctly");
+    cr_assert_eq(ctx->game->battle->player->stats->hp,
+                 5,
+                 "battle_flow() did not compute damage correctly");
     cr_assert_eq(ctx->status, BATTLE_IN_PROGRESS,
                  "battle_flow() failed: battle is not in progress");
 }
@@ -247,7 +246,6 @@ Test(battle_flow, battle_over_by_player)
     cr_assert_not_null(ctx, "battle_flow returned NULL");
     ctx = battle_flow(ctx, move, "Enemy");
     cr_assert_not_null(ctx, "battle_flow returned NULL");
-    printf("%d\n", ctx->game->battle->player->stats->hp);
     cr_assert_eq(ctx->game->battle->player->stats->hp,
                  -29,
                  "battle_flow() did not compute damage correctly");
