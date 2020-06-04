380b20aafda193204a5a891e2639c22c4b559951
Leon Chen <lychen@cs.uchicago.edu>
2020-06-04 07:29:58 -0500

    renamed inventory declarations for clarity

--- src/playerclass/examples/class-list.c
+++ src/playerclass/examples/class-list.c
@@ -33,20 +33,20 @@ move_list_t* move_list() {
     // Bard
 
     class_t *bard = class_new("Bard", "Song-singer", "Song-singer and tale-teller", NULL, NULL, NULL, NULL, NULL, NULL);
-    skill_inventory_t* bard_inventory = bard_inventory();
-    move_list_t* bardlist = new_move_list(bard, bard_inventory->active[0]);
+    skill_inventory_t* bard_inv = bard_inventory();
+    move_list_t* bardlist = new_move_list(bard, bard_inv->active[0]);
 
     // Wizard
 
     class_t *wizard = class_new("Wizard", "Wise", "Old and wise", NULL, NULL, NULL, NULL, NULL, NULL);
-    skill_inventory_t* wizard_inventory = wizard_inventory();
-    move_list_t* wizardlist = new_move_list(wizard, wizard_inventory->active[0]);
+    skill_inventory_t* wizard_inv = wizard_inventory();
+    move_list_t* wizardlist = new_move_list(wizard, wizard_inv->active[0]);
     
     // Knight
 
     class_t *knight = class_new("Knight", "Brave", "Brave and shiny", NULL, NULL, NULL, NULL, NULL, NULL);
-    skill_inventory_t* knight_inventory = knight_inventory();
-    move_list_t* knightlist = new_move_list(knight, knight_inventory->active[0]);
+    skill_inventory_t* knight_inv = knight_inventory();
+    move_list_t* knightlist = new_move_list(knight, knight_inv->active[0]);
 
     DL_APPEND(head, bardlist);
     DL_APPEND(head, wizardlist);
--- src/skilltrees/examples/skill-example.c
+++ src/skilltrees/examples/skill-example.c
@@ -29,35 +29,35 @@ char* effect_sword_slash(char* args){
 
 /* See skill-example.h */
 skill_inventory_t* bard_inventory() {
-    skill_inventory_t* bard_inventory = inventory_new(1, 0);
+    skill_inventory_t* bard_inv = inventory_new(1, 0);
 
     skill_t* diss_track = skill_new(0, ACTIVE, "Diss Track", "A Bard attack spell that deals 8 damage.", 2, 5, effect_diss_track);
 
-    inventory_skill_add(bard_inventory, diss_track);
+    inventory_skill_add(bard_inv, diss_track);
 
-    return bard_inventory;
+    return bard_inv;
 }
 
 
 /* See skill-example.h */
 skill_inventory_t* wizard_inventory() {
-    skill_inventory_t* wizard_inventory = inventory_new(1, 0);
+    skill_inventory_t* wizard_inv = inventory_new(1, 0);
 
     skill_t* fireball = skill_new(1, ACTIVE, "Fireball", "A Wizard attack spell that deals 10 damage.", 2, 5, effect_fireball);
 
-    inventory_skill_add(wizard_inventory, fireball);
+    inventory_skill_add(wizard_inv, fireball);
 
-    return wizard_inventory;
+    return wizard_inv;
 }
 
 
 /* See skill-example.h */
 skill_inventory_t* knight_inventory() {
-    skill_inventory_t* knight_inventory = inventory_new(1, 0);
+    skill_inventory_t* knight_inv = inventory_new(1, 0);
 
     skill_t* sword_slash = skill_new(2, ACTIVE, "Sword Slash", "A Knight attack that deals 9 damage.", 2, 5, effect_sword_slash);
 
-    inventory_skill_add(knight_inventory, sword_slash);
+    inventory_skill_add(knight_inv, sword_slash);
 
-    return knight_inventory;
+    return knight_inv;
 }
