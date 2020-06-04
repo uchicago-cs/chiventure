2e7fa839b7ef41100e3def00a7c60c3c547132cb
Xavier Valencia <xmvalencia@uchicago.edu>
2020-06-03 23:18:05 -0500

    explanation style fix

--- src/ui/sandbox/split_screen.c
+++ src/ui/sandbox/split_screen.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #define MAX_INPUT_CHARS 20
 
-// This sandbox example is demonstrating code that can simulate components in chiventure like
-// the command line interface (CLI) and the ability to take in user input. This code is also 
-// an example of splitting the screen for viewing the CLI and graphics uploaded by the user at
-// the same time.
+/* 	Sandbox Example Summary:
+ *	This sandbox example is demonstrating code that can simulate components in chiventure like
+ *  	the command line interface (CLI) and the ability to take in user input. This code is also 
+ *	an example of splitting the screen for viewing the CLI and graphics uploaded by the user at
+ *	the same time
+ */
 
 typedef struct _cmd {
 	char *cmd;
