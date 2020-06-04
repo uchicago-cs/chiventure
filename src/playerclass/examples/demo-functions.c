9e98c578b90f1ccb8d22c71f2d351204c8b31236
Ethan Tai <etai@uchicago.edu>
2020-06-04 03:33:21 -0500

    finished class_show

--- src/playerclass/examples/demo-functions.c
+++ src/playerclass/examples/demo-functions.c
@@ -2,4 +2,24 @@
  * See demo-functions.h for more information
  */
 
-//TODO
+#include "demo-functions.h"
+
+void class_show(move_list_t* m)
+{
+    if (m == NULL)
+    {
+        fprintf(stderr, "Cannot show information of a NULL movelist.\n");
+        exit(1);
+    }
+    move_list_t* tmp;
+    DL_FOREACH(m, tmp)
+    {
+        fprintf(stdout, "Class info:\n");
+        frpintf(stdout, "    Name: %s\n", tmp->c->name);
+        fprintf(stdout, "    Short Description: %s\n", tmp->c->shortdesc);
+        fprintf(stdout, "    Long Description: %s\n", tmp->c->longdesc);
+        fprintf(stdout, "    Special Skill: %s\n", tmp->spell);
+        fprintf(stdout, "    Skill Damage: %d\n", tmp->damage);
+        frpintf(stdout, "    Skill id: %d\n", tmp->id);
+    }
+}
--- src/playerclass/examples/demo-functions.h
+++ src/playerclass/examples/demo-functions.h
@@ -2,4 +2,23 @@
  * Functions to help demo the playerclass struct in pc_demo.c
  */
 
-//TODO
+#ifndef DEMO_FUNCTIONS_H
+#define DEMO_FUNCTIONS_H
+#include <stdio.h>
+#include "playerclass/class.h"
+#include "playerclass/class_item_restriction.h"
+#include "playerclass/class_structs.h"
+#include "class-list.h"
+
+/* Displays basic information about a class. 
+ * Currently only displays the name, description, and special skill. 
+ * 
+ * Parameters:
+ * - m: a list of classes with their special skills. 
+ * 
+ * Returns:
+ * - void, but prints out information to console
+ */ 
+void class_show(move_list_t* m);
+
+#endif /* DEMO_FUNCTIONS_H */
