ffa61b8db2a7f9fa100a1e906ca0318456a62ce3
Ethan Tai <etai@uchicago.edu>
2020-06-04 03:59:24 -0500

    basic framework for demo

--- src/playerclass/examples/pc_demo.c
+++ src/playerclass/examples/pc_demo.c
@@ -1,3 +1,12 @@
+/* 
+ * A demo of playerclass functionality using x/battle-systems' battle_flow_example.c
+ * This executable aims to demonstrate:
+ * - Some information playerclass stores
+ * - Proof-of-concept class-specific skills
+ * - Class-specific item restrictions
+ * - How playerclass plays a role in the battle flow
+ */ 
+
 #include <criterion/criterion.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,6 +15,8 @@
 #include "playerclass/class.h"
 #include "playerclass/class_item_restriction.h"
 #include "playerclass/class_structs.h"
+#include "demo-functions.h"
+#include "class-list.h"
 #include "../../battle/examples/battle_move_maker.h"
 
 void main()
@@ -13,6 +24,10 @@ void main()
     move_list_t* classlist = move_list();
 
     fprintf(stdout, "Please choose a class from the the following list:\n");
-    
-    
+    list_class_show(classlist);
+
+    fprintf(stdout, "You have chosen: \n");
+    class_show(classlist->c);
+
+    fprintf(stdout, "Proceeding with battle.\n");
 }
