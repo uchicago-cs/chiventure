0ad57c080b897f091ca8045a12abb58365ba7a48
Leon Chen <lychen@cs.uchicago.edu>
2020-06-04 06:28:52 -0500

    modified class-list.c/h to suupport inventory and skill structs

--- src/playerclass/examples/class-list.h
+++ src/playerclass/examples/class-list.h
@@ -6,15 +6,16 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "common/utlist.h"
 #include "playerclass/class.h"
 #include "playerclass/class_structs.h"
+#include "skilltrees/skill.h"
+#include "skilltrees/skill-example.h"
 
 typedef struct move_list {
     class_t* c;
-    char* spell;
-    int damage;
-    int id;
+    skill_t* spell;
     struct move_list *prev;
     struct move_list *next;
 } move_list_t;
@@ -23,15 +24,13 @@ typedef struct move_list {
  *
  * Parameters:
  * - c: the class
- * - spell: the name of the spell
- * - damage: the amount of damage the spell does
- * - id: the id of the spell
+ * - spell: a pointer to a skill that holds name, damage, id
  * 
  * Returns:
  * - move_list_t* A new move_list_t not linked with anything
  * 
  */ 
-move_list_t* new_move_list(class_t* c, char* spell, int damage, int id);
+move_list_t* new_move_list(class_t* c, skill_t* spell);
 
 /* Returns a hard-coded doubly linked list with the class, spell name, damage, 
  * and id number of the spell. 
---
+++ src/skilltrees/examples/skill-example.c
@@ -0,0 +1,63 @@
+/*
+ * Skill examples for battle demo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "skilltrees/skill-example.h"
+
+
+/* See skill-example.h */
+char* effect_diss_track(char* args){
+    return "8";
+}
+
+
+/* See skill-example.h */
+char* effect_fireball(char* args){
+    return "10";
+}
+
+
+/* See skill-example.h */
+char* effect_sword_slash(char* args){
+    return "9";
+}
+
+
+/* See skill-example.h */
+skill_inventory_t* bard_inventory() {
+    skill_inventory_t* bard_inventory = inventory_new(1, 0);
+
+    skill_t* diss_track = skill_new(0, ACTIVE, "Diss Track", "A Bard attack spell that deals 8 damage.", 2, 5, effect_diss_track);
+
+    inventory_skill_add(bard_inventory, diss_track);
+
+    return bard_inventory;
+}
+
+
+/* See skill-example.h */
+skill_inventory_t* wizard_inventory() {
+    skill_inventory_t* wizard_inventory = inventory_new(1, 0);
+
+    skill_t* fireball = skill_new(1, ACTIVE, "Fireball", "A Wizard attack spell that deals 10 damage and increases defense by 1.", 2, 5, effect_fireball);
+
+    inventory_skill_add(wizard_inventory, fireball);
+
+    return wizard_inventory;
+}
+
+
+/* See skill-example.h */
+skill_inventory_t* knight_inventory() {
+    skill_inventory_t* knight_inventory = inventory_new(1, 0);
+
+    skill_t* sword_slash = skill_new(2, ACTIVE, "Sword Slash", "A Knight attack that deals 9 damage and increases defense by 2.", 2, 5, effect_sword_slash);
+
+    inventory_skill_add(wizard_inventory, sword_slash);
+
+    return knight_inventory;
+}
