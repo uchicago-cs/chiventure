77e355dd3a3e9a635606571ff35459ea31c64f04
Isaac Adam Fink <iafink@linux1.cs.uchicago.edu>
2020-06-05 15:25:21 -0500

    strcmp -> strncmp

--- src/action_management/src/actionmanagement.c
+++ src/action_management/src/actionmanagement.c
@@ -83,11 +83,11 @@ int do_item_action(chiventure_ctx_t *c, action_type_t *a, item_t *i, char **ret_
     }
 
     /* use representative c_name for action synonyms */
-    if(strcmp(a->c_name, "PICKUP") == 0) 
+    if(strncmp(a->c_name, "PICKUP", 7) == 0) 
     {
         a->c_name = "TAKE";
     } 
-    else if(strcmp(a->c_name, "USE") == 0 || strcmp(a->c_name, "EAT") == 0 || strcmp(a->c_name, "DRINK") == 0)
+    else if(strncmp(a->c_name, "USE", 4) == 0 || strncmp(a->c_name, "EAT", 4) == 0 || strncmp(a->c_name, "DRINK", 6) == 0)
     {
         a->c_name = "CONSUME";
     }
