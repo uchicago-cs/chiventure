4337b27424e1a7d0bee83def685d892dfea00c01
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-06-05 13:12:33 -0500

    branch init

--- src/custom-actions/src/branch_block.c
+++ src/custom-actions/src/branch_block.c
@@ -48,7 +48,10 @@ AST_block_t* AST_branch_block_new(int num_conditionals, conditional_block_t** co
     branch_block_t *branch;
     int new_branch;
     block_type_t block_type = BRANCH;
+    int num_AST;
+    AST_block_t **next;
 
+    *next = malloc(sizeof(AST_block_t) * (num_AST - 1));
     branch = malloc(sizeof(branch_block_t));
     ast = malloc(sizeof(AST_block_t));
 
