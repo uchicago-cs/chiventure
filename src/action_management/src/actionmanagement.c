6ce6ec88861946010e623e1d7ca1371ae91753dd
Victoria Villalba <vvillalba@linux2.cs.uchicago.edu>
2020-06-05 15:09:10 -0500

    updated do functions in actionmanagement.c to remove conditions

--- src/action_management/src/actionmanagement.c
+++ src/action_management/src/actionmanagement.c
@@ -131,7 +131,10 @@ int do_item_action(chiventure_ctx_t *c, action_type_t *a, item_t *i, char **ret_
 	    int condition;
 	    closed_path = path_search(a->room,a->direction);
 	    condition = remove_condition(closed_path,a);
-	    //if (condition == FAILURE)
+	    if (condition != SUCCESS)
+	    {
+		sprintf(string, "condition was not removed")
+	    }
 
             // successfully carried out action
             sprintf(string, "%s", game_act->success_str);
@@ -184,6 +187,16 @@ int do_path_action(chiventure_ctx_t *c, action_type_t *a, path_t *p, char **ret_
 
     /* PERFORM ACTION */
     int move = move_room(g, room_dest);
+
+    //remove action from any conditions
+    path_t *closed_path;
+    int condition;
+    closed_path = path_search(a->room,a->direction);
+    condition = remove_condition(closed_path,a);
+    if (condition != SUCCESS)
+    {
+        sprintf(string, "condition was not removed")
+    }
 
     if (is_game_over(g)) {
         sprintf(string, "Moved into %s. This is the final room, you've won the game! Press ctrl+D to quit.",
@@ -280,6 +293,16 @@ int do_item_item_action(chiventure_ctx_t *c, action_type_t *a, item_t *direct,
         }
         else if (applied_effect == SUCCESS)
         {
+            //remove action from any conditions
+            path_t *closed_path;
+            int condition;
+            closed_path = path_search(a->room,a->direction);
+            condition = remove_condition(closed_path,a);
+            if (condition != SUCCESS)
+            {
+                sprintf(string, "condition was not removed")
+            }
+
             // successfully carried out action
             sprintf(string, "%s", dir_game_act->success_str);
             if (is_game_over(game))
