06c010cdef443692c31c6a490d3996d16da754cc
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-06-06 15:49:31 -0500

    control ast

--- src/custom-actions/src/control_block.c
+++ src/custom-actions/src/control_block.c
@@ -44,9 +44,9 @@ AST_block_t* AST_control_block_new(control_type_t control_type)
     int new_control;
     block_type_t block_type = CONTROL;
     int num_AST = 1;
-    AST_block_t **next;
+    AST_block_t *next;
     
-    *next = malloc(sizeof(AST_block_t) * (num_AST - 1));
+    next = malloc(sizeof(AST_block_t));
     control = malloc(sizeof(control_block_t));
     ast = malloc(sizeof(AST_block_t));
 
