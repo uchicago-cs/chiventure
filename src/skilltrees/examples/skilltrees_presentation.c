7f632b3fa65d235a608dca0df19ea1d9b6b53c1b
Jacqueline Eunu Hyunah Lee <jacquelinelee@linux2.cs.uchicago.edu>
2020-06-07 18:37:14 -0500

    Prepare for merging dev in

--- src/skilltrees/examples/skilltrees_presentation.c
+++ src/skilltrees/examples/skilltrees_presentation.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include "common/sample_game.h"
 #include "common/ctx.h"
+#include "ui/ui.h"
 
 const char* banner = "SKILLTREES PRESENTATION";
 
+chiventure_ctx_t* create_example_ctx() {
+    game_t* game = create_sample_game();
+
+    chiventure_ctx_t* ctx = chiventure_ctx_new(game);
+
+    return ctx;
+
+}
+
 int main(int argc, char **argv) {
-    printf("SKILLTREES PRESENTATION\n");
+    chiventure_ctx_t* ctx = create_example_ctx();
+
+    start_ui(ctx, banner);
+
+    game_free(
     return 0;
 }
