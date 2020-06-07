9fea2d68daeb0b7a3e7f425c1c547217f7dbd8b2
Christopher Zhu <chz@uchicago.edu>
2020-06-06 23:58:54 -0700

    make new test_condition.c file #577

---
+++ tests/action_management/test_condition.c
@@ -0,0 +1,9 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "game-state/item.h"
+#include "game-state/game.h"
+#include "game-state/player.h"
+#include "game-state/condition.h"
+
