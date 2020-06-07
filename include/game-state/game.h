3b3b478aec71ed74bcbf6118298c7ff141099277
Your Name <you@example.com>
2020-06-07 00:55:10 -0500

    accidentalyl commited early

--- include/game-state/game.h
+++ include/game-state/game.h
@@ -14,6 +14,7 @@ HASH_ITER(hh, (game)->all_rooms, (curr_room), ITTMP_ROOM)
 HASH_ITER(hh, (game)->all_players, (curr_player), ITTMP_PLAYER)
 
 // Forward declaration
+typedef struct stats_global stats_global_t;
 typedef struct stats_global stats_global_hash_t;
 typedef struct effects_global effects_global_t;
 typedef struct effects_global effects_global_hash_t;
