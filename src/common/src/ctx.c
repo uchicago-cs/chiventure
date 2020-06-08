0175e72d58fed6c78b5a3e6efcbc6c864624f243
Your Name <you@example.com>
2020-06-07 20:20:27 -0500

    removing monkey patch stuff  from ctx_init

--- src/common/src/ctx.c
+++ src/common/src/ctx.c
@@ -42,38 +42,9 @@ int chiventure_ctx_init(chiventure_ctx_t *ctx, game_t *game)
 
     lookup_t **table = lookup_t_new();
 
-    stats_global_t *gs1, *gs2, *gs3;
-    *gs1 = stats_global_new("Health", 1000);
-    *gs2 = stats_global_new("XP", 10000);
-    *gs3 = stats_global_new("Stamina", 1000);
-    add_stat_to_game(game, gs1);
-    add_stat_to_game(game, gs2);
-    add_stat_to_game(game, gs3);
-
-    effects_global_t *eg1, *eg2;
-    eg1 = global_effect_new("POISONED");
-    eg2 = global_effect_new("STUNNED");
-    add_effect_to_game(game, eg1);
-    add_effect_to_game(game, eg2);
-
     player_t *player1 = player_new("player1", 100);
     add_player_to_game(ctx->game, player1);
     ctx->game->curr_player = player1;
-
-    stats_hash_t *sh = NULL;
-    effects_hash_t *eh = NULL;
-    stats_t *s1, *s2, *s3;
-    s1 = stats_new(gs1, 100);
-    s2 = stats_new(gs2, 0);
-    s3 = stats_new(gs3, 100);
-    add_stat_player(&sh, s1);
-    add_stat_player(&sh, s2);
-    add_stat_player(&sh, s3);
-
-    obj_t *class_attributes = obj_new("filler_class");
-    class_t *filler = class_new("filler_class", "filler_class", "filler_class",
-                                class_attributes, sh, eh);
-    player1->player_class = filler;
 
     ctx->table = table;
 
