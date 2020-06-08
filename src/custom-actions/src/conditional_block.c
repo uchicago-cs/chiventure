ac76e012b50c6d572a52d5b463c36eead5da702a
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-06-07 20:56:47 -0500

    Update conditional_block.c

--- src/custom-actions/src/conditional_block.c
+++ src/custom-actions/src/conditional_block.c
@@ -47,9 +47,8 @@ AST_block_t* AST_conditional_block_new(conditional_type_t conditional_type,
     int new_conditional;
     block_type_t block_type = CONDITIONAL;
     int num_AST = 1;
-    AST_block_t *next;
+    AST_block_t *ast_sequence = NULL;
   
-    next = malloc(sizeof(AST_block_t));
     conditional = malloc(sizeof(conditional_block_t));
     ast = malloc(sizeof(AST_block_t));
 
@@ -69,7 +68,7 @@ AST_block_t* AST_conditional_block_new(conditional_type_t conditional_type,
 
     block_t *block = malloc(sizeof(block));
     block->conditional_block = conditional;
-    ast = AST_block_new(block, block_type, num_AST, &next);
+    ast = AST_block_new(block, block_type, num_AST, &ast_sequence);
     return ast;
 }
 
