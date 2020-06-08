ea0fec83336d389271dabe9a4947aff49c592c4f
vannstrasen <vannstrasen@gmail.com>
2020-06-08 04:31:22 -0500

    Slamming the game with that string concatenation 152 work

--- include/skilltrees/inventory.h
+++ include/skilltrees/inventory.h
@@ -100,4 +100,6 @@ int inventory_has_skill(skill_inventory_t* inventory, sid_t sid,
  */
 int inventory_skill_remove(skill_inventory_t* inventory, skill_t* skill);
 
+char* current_skills_as_strings(skill_inventory_t* inventory);
+
 #endif /* INCLUDE_INVENTORY_H_ */
