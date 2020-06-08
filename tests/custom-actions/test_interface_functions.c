6d8c8a6fd34711b13193e0769f02fd3d5c2418e1
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 07:14:20 -0400

    Test bugfixes

--- tests/custom-actions/test_interface_functions.c
+++ tests/custom-actions/test_interface_functions.c
@@ -1,15 +1,19 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "action_block.h"
-#include "ast_block.h"
 #include "custom_action.h"
+#include "ast_block.h"
+#include "control_block.h"
+#include "branch_block.h"
+#include "conditional_block.h"
+#include "branch_block.h"
 #include "game-state/item.h"
 #include "interface.h"
 
 /* Checks that an empty custom_action fails */
 Test(Interface,null_custom_action) {
-    custom_action_t* ca = custom_action_new("test","test","test","test",NULL,NULL);  
+    UT_hash_handle hh = hh;
+    custom_action_t* ca = custom_action_new("test","test","test","test",NULL,hh);  
     cr_assert_eq(do_custom_action(NULL),FAILURE, "do_custom_action didn't check for NULL input");
     cr_assert_eq(do_custom_action(ca),SUCCESS, "do_custom_action incorrectly checked for a NULL head");
 }
@@ -41,7 +45,7 @@ Test(Interface,bad_branch_custom_action) {
     // allocates the new branch block
     branch_block_t* new_branch = branch_block_new(1, &conditionals, conditional_type, 2, &controls);
     AST_block_t* ast = AST_block_new(new_branch,BRANCH,1,NULL);
-    custom_action_t* ca = custom_action_new("act_PUSH","item","obj_CHAIR","paladin",ast,NULL);
+    custom_action_t* ca = custom_action_new("act_PUSH","item","obj_CHAIR","paladin",ast,hh);
 
     cr_assert_eq(do_custom_action(ca),FAILURE, "do_custom_action didn't recognize branch block had wrong arguments");
 }
