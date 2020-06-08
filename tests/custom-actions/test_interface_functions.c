a99f362458453c1862df772cdda491bbae785435
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 13:19:06 -0400

    Forgot a semicolon

--- tests/custom-actions/test_interface_functions.c
+++ tests/custom-actions/test_interface_functions.c
@@ -19,7 +19,7 @@ Test(Interface,null_custom_action) {
     ca->item=string;
     ca->type=string;
     UT_hash_handle hh = hh;
-    ca->hh=hh
+    ca->hh=hh;
     ca->head = NULL;
     cr_assert_eq(do_custom_action(NULL),FAILURE, "do_custom_action didn't check for NULL input");
     cr_assert_eq(do_custom_action(ca),SUCCESS, "do_custom_action incorrectly checked for a NULL head");
