5ac923db149356818bfca7a4f177188c05940b12
Jacqueline Eunu Hyunah Lee <jacquelinelee@linux2.cs.uchicago.edu>
2020-06-07 23:11:24 -0500

    Fix for compiler

--- src/skilltrees/examples/skilltrees_presentation.c
+++ src/skilltrees/examples/skilltrees_presentation.c
@@ -53,12 +53,13 @@ chiventure_ctx_t* create_example_ctx() {
     return ctx;
 }
 
-char* design_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t* ctx) {
-    return skill_execute(design_skill, "");
-}
-
 char* effect_design(char* args) {
     return "Good progress on your modules! Keep going!";
+}
+
+char* design_operation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t* ctx) {
+    skill_t* design_skill = skill_new(0, ACTIVE, "Design Skill", "Your software design skill", 4, 1, &effect_design);
+    return skill_execute(design_skill, "");
 }
 
 int main(int argc, char **argv) {
@@ -66,7 +67,6 @@ int main(int argc, char **argv) {
     chiventure_ctx_t* ctx = create_example_ctx();
 
     // Create example skills
-    skill_t* design_skill = skill_new(0, ACTIVE, "Design Skill", "Your software design skill", 4, 1, &effect_design);
 
     // Add DESIGN operation
     add_entry("DESIGN", design_operation, NULL, ctx->table);
