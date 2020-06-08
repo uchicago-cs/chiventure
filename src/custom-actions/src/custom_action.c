8f7b9bc7c510dcfdcabf12f178ba5bc2d644d91d
Sergey Sonkin <62959413+sonkinss@users.noreply.github.com>
2020-06-08 15:38:33 -0400

    Update custom_action.c

--- src/custom-actions/src/custom_action.c
+++ src/custom-actions/src/custom_action.c
@@ -1,68 +1,119 @@
 /* 
- * Basic functions and structs for custom actions to be used in 
- * custom-actions implementation. 
- *
- * Please see "custom_action.h" for function documentation. 
+ * Basic functions and structs for branch blocks to be 
+ * used in custom-actions implementation. 
+ * 
+ * Please see "branch_blocks.h" for function documentation. 
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "game-state/item.h"
-#include "custom_action.h"
+#include "branch_block.h"
+#include "ast_block.h"
 
-/* See custom_action.h */
-custom_action_t* custom_action_new(char *action_name, char *context, char *item, 
-                                   char *type, AST_block_t *head, UT_hash_handle hh)
+/* See branch_block.h */
+branch_block_t* branch_block_new(int num_conditionals, conditional_block_t** conditionals, 
+                                 conditional_type_t conditional_type, int num_controls, 
+                                 control_block_t** controls)
 {
-    custom_action_t *action;
-    int new_action;
+    branch_block_t *branch;
+    int new_branch;
+    block_type_t block_type = BRANCH;
 
-    action = malloc(sizeof(custom_action_t));
+    branch = malloc(sizeof(branch_block_t));
 
-    if (action == NULL)
+    if (branch == NULL)
     {
-        fprintf(stderr, "Error: Could not allocate memory\n");
+        fprintf(stderr,"Could not allocate memory");
         return NULL;
     }
 
-    new_action = custom_action_init(action, action_name, context, item, type, head, hh);
-    if (new_action != SUCCESS)
+    new_branch = branch_block_init(branch, num_conditionals, conditionals, 
+                                   conditional_type, num_controls, controls);
+    if (new_branch != SUCCESS)
     {
-        fprintf(stderr, "Error: Could not initialize custom_action_t\n");
+        fprintf(stderr,"Could not initialize branch_block_t");
         return NULL;
     }
 
-    return action;
+    return branch;
 }
 
-/* See custom_action.h */
-int custom_action_init(custom_action_t *action, char *action_name, char *context, 
-                       char *item, char *type, AST_block_t *head, UT_hash_handle hh)
+/* See branch_block.h */
+AST_block_t* AST_branch_block_new(int num_conditionals, conditional_block_t** conditionals, 
+                                  conditional_type_t conditional_type, int num_controls, 
+                                  control_block_t** controls)
 {
-    assert(action != NULL);
-    assert(action_name != NULL);
-    assert(context != NULL);
-    assert(item != NULL);
-    assert(type != NULL);
-    assert(head != NULL);
+    AST_block_t *ast;
+    branch_block_t *branch;
+    int new_branch;
+    block_type_t block_type = BRANCH;
 
-    action->action_name = action_name;
-    action->context = context;
-    action->item = item;
-    action->type = type;
-    action->head = head;
-    action->hh = hh;
+    branch = malloc(sizeof(branch_block_t));
+    ast = malloc(sizeof(AST_block_t));
 
-    return SUCCESS;
+    if (branch == NULL)
+    {
+        fprintf(stderr,"Could not allocate memory");
+        return NULL;
+    }
+
+    new_branch = branch_block_init(branch, num_conditionals, conditionals, 
+                                   conditional_type, num_controls, controls);
+    if (new_branch != SUCCESS)
+    {
+        fprintf(stderr,"Could not initialize branch_block_t");
+        return NULL;
+    }
+
+    block_t *block = malloc(sizeof(block));
+    block->branch_block = branch;
+    ast = AST_block_new(block, block_type);
+    return ast;
 }
-
-/* See custom_action.h */
-int custom_action_free(custom_action_t *action)
+    
+/* See branch_block.h */
+int branch_block_init(branch_block_t *branch, int num_conditionals, conditional_block_t** conditionals, 
+                      conditional_type_t conditional_type, int num_controls,
+                      control_block_t** controls)
 {
-    assert(action != NULL);
+    assert(branch != NULL);
+    assert(num_conditionals > 0);
+    assert(conditionals != NULL); 
+    assert(num_controls > 0);
+    assert(controls != NULL);
 
-    AST_block_free(action->head);
-    free(action);
+    branch->num_conditionals = num_conditionals;
+    branch->conditionals = conditionals;
+    branch->conditional_type = conditional_type;
+    branch->num_controls = num_controls;
+    branch->controls = controls;
 
-    return SUCCESS;
+    return SUCCESS; 
 }
+
+/* See branch_block.h */
+int branch_block_free(branch_block_t *branch)
+{
+    assert(branch != NULL);
+    
+    if (branch->num_conditionals > 0)
+    {
+       assert(branch->conditionals != NULL);
+       for (int n = 0; n < branch->num_conditionals; n++)
+       {
+           conditional_block_free(branch->conditionals[n]);
+       }
+    }
+    if (branch->num_controls > 0)
+    {
+        assert(branch->controls != NULL);
+        for (int n = 0; n < branch->num_controls; n++)
+        {
+            control_block_free(branch->controls[n]);
+        }
+    }
+    free(branch);
+    
+    return SUCCESS; 
+} 
