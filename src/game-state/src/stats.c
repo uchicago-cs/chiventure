cb83b467f0945929209b0b0f562769efddc338fa
Sumedh Sarwate <sumedhsarwate@uchicago.edu>
2020-06-08 02:53:21 -0500

    removing redundancies

--- src/game-state/src/stats.c
+++ src/game-state/src/stats.c
@@ -273,45 +273,8 @@ char *display_stat_effects(effects_hash_t *hash)
     return display;
 }
 
-
 /* See stats.h */
-char *display_stat_effects(effects_hash_t *hash)
-{
-    stat_effect_t *effect, *tmp;
-    stat_mod_t *mod;
-    int count = 0, list_count = 0;
-
-    HASH_ITER(hh, hash, effect, tmp)
-    {
-        LL_COUNT(effect->stat_list, mod, list_count);
-        count += list_count;
-    }
-
-    int size = MIN_STRING_LENGTH + (MAX_NAME_LENGTH * (count + HASH_COUNT(hash)));
-    char list[size];
-    char *line;
-
-    HASH_ITER(hh, hash, effect, tmp)
-    {
-        sprintf(line, "*** %s ***\n", effect->key);
-        strcat(list, line);
-        LL_FOREACH(effect->stat_list, mod)
-        {
-            sprintf(line, "\t[ %s ] modifier: %f, duration: %d\n", 
-                    mod->stat->key, mod->modifier, mod->duration);
-            strcat(list, line);
-        }
-    }
-
-    char *display = strdup(list);
-    return display;
-}
-
-
-
-
-/* See stats.h */
-int free_stats(stats_hash_t *s)
+int free_stats(stats_hash_t *stat)
 {
     free(stat->key);
     free(stat);
