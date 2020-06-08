e681f7b01947f73d68421c86de886fbb95ca8b93
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 14:25:53 -0400

    Turns out that wasn't a typo

--- src/custom-actions/src/interface.c
+++ src/custom-actions/src/interface.c
@@ -100,13 +100,13 @@ int run_ast_block(AST_block_t *block)
             return FAILURE;
             break;
         case(BRANCH):
-            returnV = do_branch_block(block->block);
+            returnV = do_branch_block(block->block->branch_block);
             if(returnV == -1) return FAILURE;
             if(block->ast_sequence == NULL) return FAILURE;
             return run_ast_block(block->ast_sequence[returnV]);
             break;
         case(ACTION):
-            if(exec_action_block(block->block) == FAILURE) 
+            if(exec_action_block(block->block->action_block) == FAILURE) 
                 return FAILURE;
             if(block->ast_sequence == NULL) return FAILURE;
             return run_ast_block(block->ast_sequence[0]);
