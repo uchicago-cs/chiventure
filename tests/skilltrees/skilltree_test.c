a0add05ed91f0b34529c1e3d28c90ba0adf62769
vannstrasen <vannstrasen@gmail.com>
2020-06-08 03:37:42 -0500

    Testing get missing prereqs

--- tests/skilltrees/skilltree_test.c
+++ tests/skilltrees/skilltree_test.c
@@ -409,11 +409,11 @@ Test(skilltree_tests, skill_prereqs_missing_some)
   skill_node_t** skill_list = get_all_skill_prereqs(tree, 1000, out_prereqs);
   cr_assert_eq(1, (*out_prereqs),
                "Error: failed test skill_prereqs_missing_some\n");
-  // cr_assert_neq(NULL, skill_list,
-  //     "Error: failed test skill_prereqs_missing_some\n");
-  // cr_assert_eq(skill_list[0]->skill->sid, 1001,
-  //              "Error: failed test skill_prereqs_missing_some\n");
-  //
+  cr_assert_neq(NULL, skill_list,
+      "Error: failed test skill_prereqs_missing_some\n");
+  cr_assert_eq(skill_list[0]->skill->sid, 1001,
+               "Error: failed test skill_prereqs_missing_some\n");
+
   // int* out = malloc(sizeof(int));
   //
   // skill_inventory_t* inventory = inventory_new(3,4);
