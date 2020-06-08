32a49dbdcc59c7df1364f904ca8d6b3ab45ec9cf
vannstrasen <vannstrasen@gmail.com>
2020-06-08 00:15:54 -0500

    Working on skilltree tests

--- src/skilltrees/src/stdst.c
+++ src/skilltrees/src/stdst.c
@@ -15,7 +15,7 @@ void** array_element_add(void** array, unsigned int array_len, void* element) {
 
     for (i = 0; i < array_len; i++) {
         if (array[i] == NULL) {
-            array[i] = element;
+            array[i] = (skill_node_t*)element;
             return array;
         }
     }
