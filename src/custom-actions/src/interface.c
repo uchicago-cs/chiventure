1937870049eccbe76eab655106da945a233509c2
Sergey Sonkin <sonkin@uchicago.edu>
2020-06-07 21:19:36 -0400

    BCC functions done

--- src/custom-actions/src/interface.c
+++ src/custom-actions/src/interface.c
@@ -19,6 +19,7 @@ int do_custom_action(custom_action_t *action, char **args, int num_args)
 {
     // need to check if item is there
     // then need to just execute first AST_block in struct, everything will follow from there
+    run_ast_block(action->head);
     return 0;
 }
 
@@ -44,7 +45,6 @@ char *get_custom_action_name(custom_action_t *action)
 
 /* PRIVATE HELPER FUNCTIONS */
 
-<<<<<<< HEAD
 void run_ast_block(AST_block_t *block) 
 {
     if(block == NULL) return;
@@ -52,6 +52,7 @@ void run_ast_block(AST_block_t *block)
     {
         case(CONTROL):
             do_control_block(block->block);
+            run_ast_block(block->next[0]);
             break;
         case(BRANCH):
             int i = do_branch_block(block->block);
@@ -59,6 +60,7 @@ void run_ast_block(AST_block_t *block)
             break;
         case(ACTION):
             do_action_block(block->block);
+            run_ast_block(block->next[0]);
             break;
         case(CONDITIONAL):
             do_conditional_block(block->block);
@@ -67,8 +69,6 @@ void run_ast_block(AST_block_t *block)
 }
 
 
-=======
->>>>>>> 41fa530b3bd07ce55e3831b8d19ce2ff1c024f7d
 /* Note to those writing helper functions - these functions could also take
  * AST_block_t blocks as parameters, if you find that's better suited. - Annabelle*/
 
@@ -82,8 +82,8 @@ void run_ast_block(AST_block_t *block)
  * - SUCCESS on successful execution
  * - FAILURE/TBD on specific types of failure
  */
-int do_action_block(action_block_t *block)
-{
+void do_action_block(action_block_t *block)
+{   
     return 0;
 }
 
@@ -94,19 +94,17 @@ int do_action_block(action_block_t *block)
  * - block: A pointer to the branch block to be executed
  * 
  * Returns:
- * - SUCCESS on successful execution
- * - FAILURE/TBD on specific types of failure
+ * - Integer of what block to execute next
  */
 int do_branch_block(branch_block_t *block)
 {
-<<<<<<< HEAD
     // goes through each of the control blocks
     for(int i = 0; i < block->numcontrols; i++) 
     {
         // will perform the appropriate 
         switch(block->controls[i]->control_type) {
             case IFELSE:
-                if(do_conditional_block(block->conditionals[i]) == SUCCESS) return i;
+                if(do_conditional_block(block->conditionals[i]) == TRUE) return i;
                 break;
             case WHILEENDWHILE:
                 // not implemented yet
@@ -117,9 +115,6 @@ int do_branch_block(branch_block_t *block)
         }
     }
     return i;
-=======
-    return 0;
->>>>>>> 41fa530b3bd07ce55e3831b8d19ce2ff1c024f7d
 }
 
 /* Given an conditiional block and its corresponding arguments, 
@@ -129,32 +124,33 @@ int do_branch_block(branch_block_t *block)
  * - block: A pointer to the conditional block to be evaluated
  * 
  * Returns:
-<<<<<<< HEAD
  * - TRUE if condition true
  * - FALSE if condition false
  */
 bool do_conditional_block(conditional_block_t *block)
-=======
- * - SUCCESS on successful execution
- * - FAILURE/TBD on specific types of failure
- */
-int do_conditional_block(conditional_block_t *block)
 {
-    return 0;
-}
-
-/* Given an control block and its corresponding arguments, 
- * attempt to execute the given block.
- * 
- * Parameters: 
- * - block: A pointer to the control block to be executed
- * 
- * Returns:
- * - SUCCESS on successful execution
- * - FAILURE/TBD on specific types of failure
- */
-int do_control_block(control_block_t *block)
->>>>>>> 41fa530b3bd07ce55e3831b8d19ce2ff1c024f7d
-{
-    return 0;
+    switch(block->conditional_type) {
+        case EQ:
+            return check_eq(block->left,block->right);
+            break;
+        case LTGT: // functioning like case LT
+            return check_lt(block->left,block->right);
+            break;
+        /* 
+        case GT // once conditional_block struct changed
+            return check_gt(block->left,block->right);
+            break;
+        */
+        case LTEGTE: // functioning like case LTE at the moment
+            return check_lte(block->left,block->right);
+            break;
+        /*
+        case GTE // once conditional_block struct changed
+            return check_gte(block->left,block->right);
+            break;
+        */
+        case IN:
+            else check_in(block->left,block->right); // at the time of writing, TBI
+            break;
+    }
 }
