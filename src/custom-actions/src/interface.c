9c69333cb98ea3b5e8e85223060f279c5848447d
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 09:41:14 -0400

    Revert "Apparently I broke something major"
    This reverts commit 68c40e0eee85c236dbb93de36ecf3c9c8634f23a.

--- src/custom-actions/src/interface.c
+++ src/custom-actions/src/interface.c
@@ -6,15 +6,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "ast_block.h"
-#include "action_block.h"
-#include "branch_block.h"
-#include "conditional_block.h"
-#include "control_block.h"
-#include "game-state/game.h"
-#include "common/uthash.h"
-
-
+#include "interface.h"
 
 /* See interface.h */
 int do_custom_action(custom_action_t *action)
