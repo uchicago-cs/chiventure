135167aa93cddcfda2b64d07d42c98e29598592f
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 16:03:18 -0400

    Previous -> prev

--- src/custom-actions/src/interface.c
+++ src/custom-actions/src/interface.c
@@ -108,7 +108,7 @@ int run_ast_block(AST_block_t *block)
             break;
         case(BRANCH):
             returnV = do_branch_block(block->block->branch_block);
-            if(returnV == 0) return block->previous;
+            if(returnV == 0) return block->prev;
             return run_ast_block(block->next);
             break;
         case(ACTION):
