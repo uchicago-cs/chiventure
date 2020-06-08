628c56e26197614fbc635ae81c09b5764c615075
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 13:15:08 -0400

    Rewrote a test

--- tests/custom-actions/test_interface_functions.c
+++ tests/custom-actions/test_interface_functions.c
@@ -12,8 +12,15 @@
 
 /* Checks that an empty custom_action fails */
 Test(Interface,null_custom_action) {
+    custom_action_t* ca = (custom_action_t*)malloc(sizeof(custom_action_t));
+    char* string = "test";
+    ca->action_name=string;
+    ca->context=string;
+    ca->item=string;
+    ca->type=string;
     UT_hash_handle hh = hh;
-    custom_action_t* ca = custom_action_new("test","test","test","test",NULL,hh);  
+    ca->hh=hh
+    ca->head = NULL;
     cr_assert_eq(do_custom_action(NULL),FAILURE, "do_custom_action didn't check for NULL input");
     cr_assert_eq(do_custom_action(ca),SUCCESS, "do_custom_action incorrectly checked for a NULL head");
 }
