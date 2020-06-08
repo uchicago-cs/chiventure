edf82967344b00e430aab83969e82b4f4b5d11c2
emmamedeiros <63020943+emmamedeiros@users.noreply.github.com>
2020-06-08 12:38:05 -0500

    Update action_block.c

--- src/custom-actions/src/action_block.c
+++ src/custom-actions/src/action_block.c
@@ -45,8 +45,6 @@ AST_block_t* AST_action_block_new(action_enum_t action_type, int num_args,
     action_block_t *action;
     int new_action;
     block_type_t block_type = ACTION;
-    int num_AST = 1;
-    AST_block_t *ast_sequence = NULL;
     
     action = malloc(sizeof(action_block_t));
     ast = malloc(sizeof(AST_block_t));
@@ -66,7 +64,7 @@ AST_block_t* AST_action_block_new(action_enum_t action_type, int num_args,
     
     block_t* block = malloc(sizeof(block));
     block->action_block = action;
-    ast = AST_block_new(block, block_type, num_AST, &ast_sequence);
+    ast = AST_block_new(block, block_type);
     return ast;
 }
 
