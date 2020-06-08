3af6cf0c6da0091e6c1641f1add6056fb5ca1213
Sergey Sonkin <mastersmail1@gmail.com>
2020-06-08 16:04:02 -0400

    Previous -> prev

--- tests/custom-actions/test_interface_functions.c
+++ tests/custom-actions/test_interface_functions.c
@@ -58,7 +58,7 @@ Test(Interface,bad_branch_custom_action) {
     AST_block_t* ast = malloc(sizeof(ast));
     ast->block = block;
     ast->block_type = BRANCH;
-    ast->previous = NULL;
+    ast->prev= NULL;
     ast->next = NULL;
     custom_action_t* ca = custom_action_new("act_PUSH","item","obj_CHAIR","paladin",ast,hh);
 
