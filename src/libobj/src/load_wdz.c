178a1ec22330837be3999147cbedba51012ab408
Dawson Whitehead <dwahme1@gmail.com>
2021-04-12 18:29:12 -0500

    Removed printfs

--- src/libobj/src/load_wdz.c
+++ src/libobj/src/load_wdz.c
@@ -56,8 +56,6 @@ int load_obj_zip(obj_t *obj, zip_t *zip)
         return EXIT_FAILURE;
     }
 
-    printf("here\n");
-
     zip_int64_t n_entries = zip_get_num_entries(zip, 0);
     if (n_entries <= 0) // negative number means error, 0 means empty zip
     {
@@ -65,8 +63,6 @@ int load_obj_zip(obj_t *obj, zip_t *zip)
         zip_discard(zip);
         return EXIT_FAILURE;
     }
-
-    printf("here %d\n", n_entries);
 
     for (zip_int64_t i = 0; i < n_entries; i++)
     {
@@ -80,7 +76,6 @@ int load_obj_zip(obj_t *obj, zip_t *zip)
             return EXIT_FAILURE;
         }
 
-    printf("here\n");
         strncpy(name_buf, path, MAX_DEPTH * (MAXLEN_ID + 1) - 1);
 
         // Don't try to load non-json files
@@ -99,20 +94,13 @@ int load_obj_zip(obj_t *obj, zip_t *zip)
             return EXIT_FAILURE;
         }
 
-    printf("here\n");
-
-
-    printf("starting read\n");
         if (zip_fread(curr_file, json_buf, MAXBUFSIZE) == -1)
         {
             free(name_buf);
             free(json_buf);
             zip_discard(zip);
-    printf("oof\n");
             return EXIT_FAILURE;
         }
-
-    printf("here\n");
 
         // Load the json file into an object at <name_buf>
         for (int p = 0; p < strnlen(name_buf, MAX_DEPTH * (MAXLEN_ID + 1) - 1); p++)
@@ -130,8 +118,6 @@ int load_obj_zip(obj_t *obj, zip_t *zip)
             zip_discard(zip);
             return EXIT_FAILURE;
         }
-
-    printf("here\n");
 
         free(name_buf);
         free(json_buf);
