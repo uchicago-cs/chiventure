b8a006934fef8f86317c68a86b93e55ecd0ca3ea
Dawson Whitehead <dwahme1@gmail.com>
2021-04-23 00:24:32 -0500

    Updated all tests and wdl code to be compatible with libobj 3.0

--- tests/wdl/test_item.c
+++ tests/wdl/test_item.c
@@ -48,7 +48,7 @@ static obj_t *__get_doc_obj()
     return obj;
 }
 
-void item_check(char *room, char *item, char *index)
+void item_check(char *room, char *item)
 {
     obj_t *doc = __get_doc_obj();
     game_t *g = game_new("Welcome to UChicago");
@@ -65,37 +65,35 @@ void item_check(char *room, char *item, char *index)
     room_t *r = find_room_from_game(g, room);
     item_t *i = get_item_in_room(r, item);
 
-    // char *item_path = malloc(100);
-    // strcpy(item_path, "ITEMS.");
-    // strncat(item_path, index, 94);
-    // obj_t *item_obj = obj_get_attr(doc, item_path, false);
+    obj_t *item_obj = obj_get_attr(doc, "ITEMS", false);
+    item_obj = obj_get_attr(item_obj, item, false);
 
-    // char *s = get_sdesc_item(i);
-    // char *scmp = obj_get_str(item_obj, "short_desc");
-    // rc = strncmp(s, scmp, strlen(scmp));
-    // cr_assert_eq(rc, SUCCESS, "failed to parse item sdesc");
+    char *s = get_sdesc_item(i);
+    char *scmp = obj_get_str(item_obj, "short_desc");
+    rc = strncmp(s, scmp, strlen(scmp));
+    cr_assert_eq(rc, SUCCESS, "failed to parse item sdesc");
 
-    // char *l = get_ldesc_item(i);
-    // char *lcmp = obj_get_str(item_obj, "long_desc");
-    // rc = strncmp(l, lcmp, strlen(lcmp));
-    // cr_assert_eq(rc, SUCCESS, "failed to parse item sdesc");
+    char *l = get_ldesc_item(i);
+    char *lcmp = obj_get_str(item_obj, "long_desc");
+    rc = strncmp(l, lcmp, strlen(lcmp));
+    cr_assert_eq(rc, SUCCESS, "failed to parse item sdesc");
 
-    // obj_t *actions = obj_get_attr(item_obj, "actions", false);
+    obj_t *actions = obj_get_attr(item_obj, "actions", false);
 
-    // obj_t *curr, *tmp;
-    // HASH_ITER(hh, actions->data.obj.attr, curr, tmp)
-    // {
-    //     rc = possible_action(i, obj_get_str(curr, "action"));
-    //     cr_assert_eq(rc, 0, "failed to load item action");
-    // }    
+    obj_t *curr;
+    DL_FOREACH(actions->data.lst, curr)
+    {
+        rc = possible_action(i, obj_get_str(curr, "action"));
+        cr_assert_eq(rc, 0, "failed to load item action");
+    }    
 }
 
 Test(items, check_chair)
 {
-    item_check("room A", "CHAIR", "0");
+    item_check("room_A", "CHAIR");
 }
 
 Test(items, check_table)
 {
-    item_check("room B", "TABLE", "1");
+    item_check("room_B", "TABLE");
 }
