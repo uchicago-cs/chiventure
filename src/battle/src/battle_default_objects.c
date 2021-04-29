333a870987233d85433db8cb5096d075525f5d13
Laura Zielinski <lzielinski@cs.uchicago.edu>
2021-04-29 13:01:35 -0500

    fixed some fields for default item

--- src/battle/src/battle_default_objects.c
+++ src/battle/src/battle_default_objects.c
@@ -1,39 +1,38 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "battle/battle_structs.h"
 
-/*resource:https://stackoverflow.com/questions/17846212/generate-a-random-number-between-1-and-10-in-c/49099642 */
-/*and: https://stackoverflow.com/questions/1202687/how-do-i-get-a-specific-range-of-numbers-from-rand */
+/* resource: https://stackoverflow.com/questions/17846212/generate-a-random-number-between-1-and-10-in-c/49099642 */
+/* and: https://stackoverflow.com/questions/1202687/how-do-i-get-a-specific-range-of-numbers-from-rand */
 
 
-/*Helper function to generate random numbers from min to max*/
+/* Helper function to generate random integer in range min to max */
 int randnum(int min, int max)
 {
-    //random seed
+    // random seed
     srand((unsigned)time(NULL));
 
     int rv = rand() % (max + 1 - min) + min; 
-
     return rv; 
-
 }
 
-item_t *get_random_default_item
+/* Returns random default item */
+item_t *get_random_default_item()
 {
-    //is there a function to initialize/allocate memory for item_t struct? if not..
-    item_t *rv_item = (item_t)malloc(sizeof(item_t));
+    // is there a function to initialize/allocate memory for item_t struct? if not..
+    item_t *rv_item = calloc(1, sizeof(item_t));
+    assert(rv_item != NULL);
     
-    char name_array[]= {"SWORD", "SHIELD", "SPEAR", "HAMMER", "STICK", "CLUB", "TRIDENT", "BOW & ARROW", "SHIV", "AXE"};
+    char* name_array[]= {"SWORD", "SHIELD", "SPEAR", "HAMMER", "STICK", 
+                         "CLUB", "TRIDENT", "BOW & ARROW", "SHIV", "AXE"};
 
     rv_item->id = randnum(1,10);
-    rv_item->quantity = randnum(1,10);
+    rv_item->quantity = 1;
     rv_item->durability = randnum(20,70);   
     rv_item->name = name_array[randnum(0,9)];
-    //not sure what to do for the description
-    rv_item->description = NULL;
-    //also unsure about the "battle" field in item_t struct
-    rv_item->battle = randnum(0,1);
+    rv_item->description = strcat("It is a ", rv_item->name);
+    rv_item->battle = true;
     rv_item->attack = randnum(20,70);
     rv_item->defense = randnum(20,70);
     rv_item->hp = randnum(50,100);
@@ -41,5 +40,4 @@ item_t *get_random_default_item
     rv_item->prev = NULL;
 
     return rv_item;
-
 }
