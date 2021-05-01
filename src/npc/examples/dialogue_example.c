937c97e9b64fbcc2336c820fde8fe7a827a4a043
James Soh <jeyeong@cs.uchicago.edu>
2021-05-01 11:57:52 -0500

    Added a simple example to dialogue_example.c to illustrate how the module works

--- src/npc/examples/dialogue_example.c
+++ src/npc/examples/dialogue_example.c
@@ -3,8 +3,7 @@
 
 // EXAMPLE PROGRAM ------------------------------------------------------------
 
-/*
- * Builds an in-memory fake game mock-up
+/* Builds an in-memory fake game mock-up
  * to be run through and then executes functions to explore it
  */
 int main()
@@ -13,22 +12,30 @@ int main()
 
     printf("\nWelcome to Chiventure's 'Dialogue Module' Mockup!\n\n\n");
 
-    printf("Dialogue Test\n\n");
+    convo_t *c = create_new_convo();
 
-    // Starting to build the conversation structure
-    convo_t *c = convo_new();
+    add_node(c, "1", "Pick an item: a sword or a shield?");
+    add_node(c, "2a", "I see you prefer the path of offence. Do you prefer a longsword or a scimitar? Or do you change your mind?");
+    add_node(c, "3a", "One longsword coming up.");
+    add_node(c, "3b", "One scimitar coming up.");
+    add_node(c, "2b", "Ah, I see you prefer the path of defence. Clever choice.");
+    add_edge(c, "Sword", "1", "2a");
+    add_edge(c, "Shield", "1", "2b");
+    add_edge(c, "Longsword", "2a", "3a");
+    add_edge(c, "Scimitar", "2a", "3b");
+    add_edge(c, "I change my mind: I want a shield", "2a", "2b");
 
-    // Initialize each node with its NPC dialogue
-    add_node(c, "1", "Test");
-    add_node(c, "2", "Test2");
-    // add_node(c, "3", "Test3");
-
-    // Adding all edge options to each node
-    add_edge(c, "Test 3", "1", "2");
-    add_edge(c, "Test 4", "1", "2");
-
-    // Runs the conversation
     run_conversation(c);
 
     convo_free(c);
+    
+    return 0;
+
+    // Starting to build the conversation structure
+
+    // Initialize each node with its NPC dialogue
+
+    // Adding all edge options to each node
+
+    // Runs the conversation
 }
