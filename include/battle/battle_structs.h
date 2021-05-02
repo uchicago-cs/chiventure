8773aa5126659f0c3a151d0834c3e01e9038700a
Matthew Chang <mchang4@cs.uchicago.edu>
2021-05-02 18:20:57 -0500

    Created stat_changes_t linked list in battle_structs.h

--- include/battle/battle_structs.h
+++ include/battle/battle_structs.h
@@ -48,4 +48,16 @@ typedef struct stat {
     int level;
 } stat_t;
 
+/* stat changes stub */
+typedef struct stat_changes {
+    int speed;
+    int defense;
+    int strength;
+    int dexterity;
+    int hp;
+    int max_hp;
+    int turns_left;
+    struct stat_changes* next;
+} stat_changes_t;
+
 #endif
