645581c32a93fbd63b057c521e628b72349786fa
Gauti Kapoor <grkapoor@cs.uchicago.edu>
2021-05-06 15:28:04 -0500

    Added draw_images.h and .c

---
+++ src/ui/src/draw_images.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "raylib.h"
+
+#include "ui/draw_images.h"
+
+/* See draw_images.h for documentation */
+void draw_room(int room_number)
+{
+
+}
+
+/* See draw_images.h for documentation */
+void draw_object(char *item_id)
+{
+    
+}
