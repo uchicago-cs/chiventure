4c88947fb11f730ce30ecfdb8e93f22b5d272866
Sam Donio <spdonio@cs.uchicago.edu>
2021-05-06 21:58:12 -0500

    Revert "Revert "Added enums and comments to test_item_item_actions.c"" I accidentally reverted the wrong commit
    This reverts commit 5001fb2bd8c01a04024b25ee39dcaca413e1ec1b.

--- tests/action_management/test_item_item_actions.c
+++ tests/action_management/test_item_item_actions.c
@@ -188,12 +188,12 @@ int execute_do_item_item_action(char *act_name, enum action_kind kind, char *all
     /*sometimes the direct and indirect items are not added to a player
      * and must be freed separately. The following if statements should
      * free them in this case */
-    if(choose_condition <= 4){
-            item_free(direct);
+    if (choose_condition <= NOT_ALLOWED_INDIRECT){
+        item_free(direct); //For all tests before test 4 free direct
     }
 
-    if(choose_condition != 8){
-            item_free(indirect);
+    if (choose_condition != END_FAIL){
+        item_free(indirect);  //For all tests expect for test 8 free indirect
     }
 
     action_type_free(a);
