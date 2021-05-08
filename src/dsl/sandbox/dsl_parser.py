3e9aca074257363608bfdfb15d0d4a2621b8af13
Jack Nugent <jacknugent@uchicago.edu>
2021-05-08 14:28:34 -0500

    switch to property:value syntax #931

--- src/dsl/sandbox/dsl_parser.py
+++ src/dsl/sandbox/dsl_parser.py
@@ -9,64 +9,63 @@ dsl_grammar = grammar_f.read()
 grammar_f.close()
 
 class TreeToDict(Transformer):
-    """Transform the parsing tree into a dictionary"""
-    def string(self, s):
-        (s,) = s
-        return str(s)
+    def start_g(self, s):
+        return ("start", s[0])
+    def end_g(self, s):
+        return ("end", s[0])
+    def id(self, s):
+        return ("id", s)
+    def location(self, s):
+        return ("location", s)
 
-    def util__escaped_string(self, s):
-        (s,) = s
-        return s[1:-1]
+    connection = tuple
+    property = tuple
 
     def phrase(self, s):
         return ' '.join(s)
 
-    def start_g(self, s):
-        (s,) = s
-        return ("start", s)
-
-    def end_g(self, s):
-        (s,) = s
-        return ("end", s)
-
-    def id(self, s):
-        (s,) = s
-        return ("id", s)
-
-    def location(self, s):
-        (s,) = s
-        return ("location", s)
-
-    def DIRECTION(self, s):
-        return str(s)
+    def ESCAPED_STRING(self, s):
+        return s[1:-1]
 
     def connections(self, s):
         return ("connections", dict(s))
-    def connection(self, s):
-        return tuple(s)
-    room = dict
 
-    def properties(self, s):
-        return ("properties", dict(s))
+    # input is of the form [('id':value),<properties>]
+    # output is of the form ("ITEM", ("<item id>", <properties>))
+    def item(self, s):
+        item_id = s.pop(0)[1][0]
+        return ('ITEM', (item_id, dict(s)))
 
-    def items(self, s):
-        return ("items", list(s))
+    # we have several objects of the form ('ITEM', (<item id>, <item properties>)) and
+    # we want to group all items into their own dict of the form {<item id>: <item properties>}
+    def room(self, s):
+        room_id = s.pop(0)[1][0]
+        # items = filter(lambda k,v: k == "ITEM", s)
+        d = {}
+        for k,v in s:
+            if k != "ITEM":
+                d[k] = v
+        d["items"] = dict([v for k,v in s if k == "ITEM"])
+        return ('ROOM', (room_id, d))
+    
+    # we have several objects of the form ('ROOM', (<room id>, <room properties>)) and
+    # we want to group all rooms into their own dict of the form {<room id>: <room properties>}
+    def game(self, s):
+        d = {}
+        for k,v in s:
+            if k != "ROOM":
+                d[k] = v
+        d["rooms"] = dict([v for k,v in s if k == "ROOM"])
+        return d
 
-    def rooms(self, s):
-        return ("rooms", list(s))
-    property = tuple
-
-    item = dict
-
-    game = dict
-
+    
 
 parser = Lark(dsl_grammar, parser='lalr')
 
 def main():
     with open(sys.argv[1]) as f:
         tree = parser.parse(f.read())
-        # print(tree.pretty())
+        print(tree.pretty())
         print(json.dumps(TreeToDict().transform(tree), indent=2))
 
         
