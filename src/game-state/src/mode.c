de6b3ed6286280d4c2fabe461a85479c0a63fd2d
Peter Kasner <pkasner@uchicago.edu>
2021-05-09 00:40:45 -0500

    provisional implementation of run_conversation_mode

--- src/game-state/src/mode.c
+++ src/game-state/src/mode.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "game-state/mode.h"
+#include "cli/parser.h"
 
 
 /* see mode.h */
@@ -62,3 +63,39 @@ int load_normal_mode(game_t *g)
 
     return SUCCESS;
 }
+
+
+/* see mode.h */
+int run_conversation_mode(char *input, cli_callback callback_func, 
+                          void *callback_args, chiventure_ctx_t *ctx)
+{
+    char **parsed_input = parse(input);
+    if (parsed_input == NULL)
+    {
+        return FAILURE;
+    }
+    
+    int option;
+    option = atoi(parsed_input[0]);
+
+    /* currently, npcs are not stored by chiventure so that must be
+       fixed before this function can be fully implemented */
+    convo_t *c; //obtained from mode_ctx once npcs in game
+
+    if ((option <= 0) || (option > c->cur_node->num_edges) || 
+        parsed_input[1] != NULL) 
+    {
+        return callback_func(ctx, "Enter a valid dialogue option.", callback_args);
+    }
+
+    char *outstring = (char*)malloc(1000 * sizeof(char)); //use a BUFLEN
+    run_conversation_step(c, option, outstring);
+
+    int rc = callback_func(ctx, outstring, callback_args);
+
+    free(outstring);
+
+    return SUCCESS;
+}
+
+
