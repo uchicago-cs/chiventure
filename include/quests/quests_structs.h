26db8f40bf7cf25c7a1960c68c82e406def4d9bf
Devon Delgado <dtdelgado@cs.uchicago.edu>
2021-05-09 22:02:13 -0500

    Added comments from dev

--- include/quests/quests_structs.h
+++ include/quests/quests_structs.h
@@ -103,6 +103,14 @@ typedef struct quest  {
     int status;  
 } quest_t;
 
+
+/*
+ * This typedef distinguishes between quest_t pointers
+ * which are used to point to quest_t structs in the 
+ * traditional sense and those which are used to hash
+ * quest_t structs with the UTHASH macros as specified
+ * in include/common
+ */
 typedef struct quest quest_hash_t;
 
 
