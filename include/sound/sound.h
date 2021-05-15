8cd66fb20f2a848114157a753b210efbe2bd2ae2
Gyuyoung Park <gypark23@linux3.cs.uchicago.edu>
2021-05-15 17:33:14 -0500

    Changed sound struct header file name

---
+++ include/sound/sound.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <math.h>
+
+typedef struct{
+    // path to sound file
+    char* path;
+    // duration of sound
+    double duration;
+    // name of sound
+    char* name;
+    // id for finding it in hash table
+    int id; 
+    // makes struct hashable
+    UT_hash_handle hh; 
+} sound_t;
+
