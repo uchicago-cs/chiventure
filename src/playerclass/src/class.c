d89f6b00c08f4322df22fc35e4118eb3782d40ef
NameZach <rothwellz@uchicago.edu>
2021-05-15 17:45:08 -0500

    bug fix

--- src/playerclass/src/class.c
+++ src/playerclass/src/class.c
@@ -258,7 +258,7 @@ skill_inventory_t* multiclass_inventory (skill_inventory_t* base_inventory, skil
 class_t* multiclass(class_t* base_class, class_t* second_class, char* name){
     char* new_shortdesc = multiclass_shortdesc(base_class, second_class);
     char* new_longdesc = multiclass_longdesc(base_class, second_class);
-    obj_t* combined_attr = NULL //obj_add_attr(base_class->attributes, second_class->attributes->id, second_class->attributes);
+    obj_t* combined_attr = NULL; //obj_add_attr(base_class->attributes, second_class->attributes->id, second_class->attributes);
     effects_hash_t* combined_effects = multiclass_effects (base_class->effects, second_class->effects);
     
     class_t* new_class = class_new(name, new_shortdesc, new_longdesc, combined_attr, base_class->stats, combined_effects);
