5d19854c1d08a02390624c6940c9797d4cd71920
Alexander Matthew Franzese <afranzese@linux1.cs.uchicago.edu>
2021-05-16 15:38:27 -0500

    Added header comments for the necessary functions corresponding to the skill-trees section in the player-interface integration plan

--- include/game-state/player.h
+++ include/game-state/player.h
@@ -231,4 +231,98 @@ bool item_in_inventory(player_t *player, item_t *item);
  */
 int assign_stats_player(player_t *plyr, stats_hash_t *sh);
 
+/*
+ * Adds a combat skill to a player's respective skill inventory
+ *
+ * Parameters:
+ *  player: A player. Must be allocated with player_new()
+ *  skill: pointer to an already existing skill that is being added to the
+ *         player's combat skill inventory
+ * 
+ * Returns:
+ *  SUCCESS on successful addition of combat skill, FAILURE if an error occurs
+ * 
+ * Note: Same return value as inventory_skill_add()
+ */
+int player_add_combat_skill(player_t *player, skill_t *skill);
+
+/*
+ * Adds a noncombat skill to a player's respective skill inventory
+ *
+ * Parameters:
+ *  player: A player. Must be allocated with player_new()
+ *  skill: pointer to an already existing skill that is being added to the
+ *         player's noncombat skill inventory
+ * 
+ * Returns:
+ *  SUCCESS on successful addition of noncombat skill, FAILURE if an error occurs
+ * 
+ * Note: Same return value as inventory_skill_add()
+ */
+int player_add_noncombat_skill(player_t *player, skill_t *skill);
+
+/*
+ * Remove a noncombat skill from a player's respective skill inventory
+ *
+ * Parameters:
+ *  player: A player. Must be allocated with player_new()
+ *  skill: pointer to an already existing skill that is being removed from the
+ *         player's combat skill inventory
+ * 
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs
+ * 
+ * Note: Same return value as inventory_skill_remove()
+ */
+int player_remove_combat_skill(player_t *player, skill_t *skill);
+
+/*
+ * Remove a noncombat skill from a player's respective skill inventory
+ *
+ * Parameters:
+ *  player: A player. Must be allocated with player_new()
+ *  skill: pointer to an already existing skill that is being removed from the
+ *         player's noncombat skill inventory
+ * 
+ * Returns:
+ *  SUCCESS on success, FAILURE if an error occurs
+ * 
+ * Note: Same return value as inventory_skill_remove()
+ */
+int player_remove_noncombat_skill(player_t *player, skill_t *skill);
+
+/*
+ * Searches for a combat skill in a player's respective skill inventory.
+ *
+ * Parameters:
+ *  player: A player. Must be allocated with player_new()
+ *  sid: The skill ID that uniquely identifies the skill
+ *  type: The skill type, either active or passive
+ * 
+ * Returns:
+ *  The position of the skill in the combat skill inventory, -1 if the 
+ *  skill is not in the skill inventory.
+ *  
+ *  Note: Same return value as inventory_has_skill()
+ */
+int player_has_combat_skill(player_t *player, sid_t sid, skill_type_t type);
+
+
+/*
+ * Searches for a noncombat skill in a player's respective skill inventory.
+ *
+ * Parameters:
+ *  player: A player. Must be allocated with player_new()
+ *  sid: The skill ID that uniquely identifies the skill
+ *  type: The skill type, either active or passive
+ * 
+ * Returns:
+ *  The position of the skill in the noncombat skill inventory, -1 if the 
+ *  skill is not in the skill inventory.
+ *  
+ *  Note: Same return value as inventory_has_skill()
+ */
+int player_has_noncombat_skill(player_t *player, sid_t sid, skill_type_t type);
+
+
 #endif
