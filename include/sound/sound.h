f08472967c02d0b226d17fa15937c96c537bb79b
Leone Fang <leonef@cs.uchicago.edu>
2021-05-17 12:36:00 -0500

    added hashtable related functions and modified CMakeLists.txt to include sound

--- include/sound/sound.h
+++ include/sound/sound.h
@@ -1,8 +1,11 @@
+#ifndef INCLUDE_SOUND_H_
+#define INCLUDE_SOUND_H_
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
 #include <math.h>
-#include "common/uthash"
+#include "common/uthash.h"
 
 typedef struct{
     // path to sound file
@@ -16,3 +19,5 @@ typedef struct{
     // makes struct hashable
     UT_hash_handle hh; 
 } sound_t;
+
+#endif /* INCLUDE_SOUND_H_ */
