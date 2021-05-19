9a132984f493c6d412b4f21af4edcde9b4c74f5b
Tyler Gao <tgao564@cs.uchicago.edu>
2021-05-18 22:21:06 -0500

    Added move effect tests to effect_test.c

--- tests/skilltrees/effect_test.c
+++ tests/skilltrees/effect_test.c
@@ -3,4 +3,23 @@
 #include <criterion/criterion.h>
 #include "skilltrees/effect.h"
 
-/*Tests for for functions in effect.c and effect.h go here*/
+Test(effect_tests, define_move_effect_test)
+{
+    move_t *move = move_new("abc", 1, NULL, true, 10, 55);
+    cr_assert_not_null(move, "Error: move_new failed to create move");
+    move_effect_t* moveeffect = define_move_effect(move);
+    cr_assert_not_null(moveeffect, "Error: define_move_effect failed to create move effect");
+
+}
+
+Test(effect_tests, make_move_effect_test)
+{
+    move_t *move = move_new("abc", 1, NULL, true, 10, 55);
+    cr_assert_not_null(move, "Error: move_new failed to create move");
+    move_effect_t* moveeffect = define_move_effect(move);
+    cr_assert_not_null(moveeffect, "Error: define_move_effect failed to create move effect");
+    effect_t* effect = make_move_effect(moveeffect);
+    cr_assert_eq(effect->effect_type, MOVE_UNLOCK, "Error: make_move_effect failed to set effect type");
+    cr_assert_not_null(effect, "Error: make_move_effect failed to create effect");
+
+}
