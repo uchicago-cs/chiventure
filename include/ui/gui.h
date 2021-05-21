eeecdf6eda29c5fcf47b8cf016da490b595560b0
Gauti Kapoor <grkapoor@cs.uchicago.edu>
2021-05-21 13:21:35 -0500

    Updated gui.h

--- include/ui/gui.h
+++ include/ui/gui.h
@@ -3,18 +3,20 @@
 
 #include "common/ctx.h"
 
+#define MAX_INPUT_CHARS 20
+#define POS_ZERO 0
+#define MIN_COLS 80
+#define MIN_ROWS 24
 
-
-/* start_gui
- * Starts the gui and runs the game loop.
+/* run_gui
+ * Runs the gui and runs the game loop.
  *
  * Parameters:
  *    - ctx : chiventure context struct
- *    - banner : chiventure banner to be shown in the home screen
  *
  * No value is returned
  */
-void start_gui(chiventure_ctx_t *ctx);
+void run_gui(chiventure_ctx_t *ctx);
 
 #endif
 
