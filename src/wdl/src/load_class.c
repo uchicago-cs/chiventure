bb79dfbba72234e8944f8b1ba4672ed692b958e7
Jack Casey <jackcasey067@gmail.com>
2021-05-22 16:36:57 -0500

    I had to modify prefab class code to use game_t instead of chiventure_ctx_t

--- src/wdl/src/load_class.c
+++ src/wdl/src/load_class.c
@@ -1,11 +1,19 @@
 
 #include "wdl/load_class.h"
 #include "wdl/validate.h"
+#include "playerclass/class_prefabs.h"
+
+// RETURNS ERROR CODE
+int load_class(obj_t* class_obj, game_t* game) {
+    if (obj_get_bool(class_obj, "prefab")) {
+        
+    }
+
+    return SUCCESS;
+}
 
 /* See load_class.h */
-/* TODO: Remove [LOAD CLASSES] debug print statements */
 int load_classes(obj_t* obj_store, game_t* game) {
-    printf("[LOAD CLASSES] Trying to load classes.\n");
     obj_t *classes_obj = obj_get_attr(obj_store, "CLASSES", false);
 
     /* Failing to find a CLASSES object is OK, as many games will not define classes */
@@ -18,7 +26,15 @@ int load_classes(obj_t* obj_store, game_t* game) {
         fprintf(stderr, "One or more classes fail type checking.\n");
         return FAILURE;
     }
-    
+
+    /* Iterate over attributes of CLASSES and add them to game */
+    obj_t *class_obj, *tmp;
+    HASH_ITER(hh, classes_obj->data.obj.attr, class_obj, tmp) {
+        if (load_class(class_obj, game) == FAILURE) {
+            fprintf(stderr, "Failed to load class with name %s.\n", class_obj->id);
+            return FAILURE;
+        }
+    }
 
     return SUCCESS;
 }
