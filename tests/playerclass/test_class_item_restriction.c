c1a8642a78e0cac14b1c2882c36b1e991da5b45a
NameZach <rothwellz@uchicago.edu>
2021-05-21 21:52:08 -0500

    updated variable name change (bug fix)

--- tests/playerclass/test_class_item_restriction.c
+++ tests/playerclass/test_class_item_restriction.c
@@ -13,7 +13,7 @@ void check_add_restriction(item_t* item, class_t* class) {
 
     int SIR_check = add_item_restriction(item, class);
     bool GSA_val = false;
-    for (int i = 0; i < class->parent_class_num; i++){
+    for (int i = 0; i < class->num_parent_class; i++){
         if (list_contains_attribute(item->class_restrictions, strndup(class->parent_class_names[i], 100))){
             GSA_val = true;
         }
