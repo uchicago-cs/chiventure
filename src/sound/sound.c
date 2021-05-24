3d7f1e07b11616da3b7b4e79a039537d14e668a6
Gyuyoung Park <gypark23@linux5.cs.uchicago.edu>
2021-05-24 12:00:02 -0500

    Edited structure and added MAKEFILE

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -2,9 +2,9 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <math.h>
-#include "common/uthash"
-#include "include/sound.h"
-
+#include "../../include/common/uthash.h"
+#include "sound.h"
+#include "SDL.h"
 
 int sound_new()
 {
@@ -18,20 +18,20 @@ int sound_init(char* path, double duration, char* name, int id, UT_hash_handle h
 
 int load_wav(sound_t *sound)
 {
-    SDL_LoadWAV(sound->name, &sound->wavSpec, &sound.wavBuffer, &sound.wavLength);
+    SDL_LoadWAV(sound->name, &sound->wavSpec, &sound->wavBuffer, &sound->wavLength);
     return 0;
 }
 
 int play_sound(sound_t *sound)
 {
-    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &sound->wavSpec, NULL, 0);
     int success = SDL_QueueAudio(deviceId, sound->wavBuffer, sound->wavLength);
     SDL_PauseAudioDevice(deviceId, 0);
-    SDL_delay(3000);
+    SDL_Delay(3000);
 
     // clean up
     SDL_CloseAudioDevice(deviceId);
-    SDL_FreeWAV(wavBuffer);
+    SDL_FreeWAV(sound->wavBuffer);
     SDL_Quit();
     return 0;
 }
