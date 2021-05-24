e5be477cc489f7e7fa9929231d05fc01e051c41d
NameZach <rothwellz@uchicago.edu>
2021-05-23 19:34:31 -0500

    two cahnges I missed

--- tests/playerclass/test_class_item_restriction.c
+++ tests/playerclass/test_class_item_restriction.c
@@ -14,11 +14,11 @@ void check_add_restriction(item_t* item, class_t* class) {
     int SIR_check = add_item_restriction(item, class);
     bool GSA_val = false;
     for (int i = 0; i < class->num_parent_class; i++){
-        if (list_contains_attribute(item->class_restrictions, strndup(class->parent_class_names[i], 100))){
+        if (list_contains_attribute(item->class_restrictions, strndup(class->parent_class_names[i], 100))) {
             GSA_val = true;
         }
     }
-    if (list_contains_attribute(item->class_restrictions, strndup(class->name, 100))){
+    if (list_contains_attribute(item->class_restrictions, strndup(class->name, 100))) {
         GSA_val = true;
     }
 
