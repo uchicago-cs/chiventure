4f249da60cfd60658b06ab6890140ac004f0e439
Dillon O'Loughlin <doloughlin@cs.uchicago.edu>
2021-05-24 10:25:54 -0500

    Cleaning up code and adding comments for future changes

--- src/game-state/src/game.c
+++ src/game-state/src/game.c
@@ -116,7 +116,7 @@ int add_quest_to_game(game_t *game, quest_t *quest)
 	}
 
 	HASH_ADD_KEYPTR(hh, game->all_quests, buffer,
-		            strlen(quest->quest_id, MAX_ID_LEN), quest);
+		            strlen(buffer, MAX_ID_LEN), quest);
 
 	return SUCCESS;
 }
