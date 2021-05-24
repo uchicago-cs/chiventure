c3aa94d0634714ba8d32ee11f8c33f7e14bb4b6f
Jack Nugent <jacknugent@uchicago.edu>
2021-05-24 12:41:45 -0500

    Added documentation #998

--- src/dsl/src/vars_test.py
+++ src/dsl/src/vars_test.py
@@ -5,11 +5,13 @@ from lark import Lark, Transformer
 import json
 from vars_parser import evalVars
 
+
 def main():
     with open(sys.argv[1]) as f:
         file_str = f.read()
         vars_evaluated = evalVars(file_str, debug=True)
         print(vars_evaluated)
-        
+
+
 if __name__ == "__main__":
-  main()
+    main()
