a4c1b63645a2cc7da79557ead111e3e87905c227
Laura Zielinski <lzielinski@cs.uchicago.edu>
2021-05-25 17:50:51 -0500

    fixed example:

--- src/battle/examples/battle_command_example.c
+++ src/battle/examples/battle_command_example.c
@@ -342,8 +342,9 @@ int main()
     // npc_enemy_t *e = NULL;
     // DL_APPEND(e, make_npc_enemy("Goblin", make_bard(), e_stats, NULL, NULL, BATTLE_AI_GREEDY));
 
-    npc_t *e = npc_new("Goblin", NULL, NULL, make_bard(), NULL, true);
-    npc_battle_t *npc_b = npc_battle_new(0, e_stats, NULL, BATTLE_AI_GREEDY, HOSTILE, 0);
+    move_t *e_move = move_new("Diss track", 0, NULL, true, 80, 0);
+    npc_t *e = npc_new("Goblin", "Enemy goblin!", "Enemy goblin!", make_bard(), NULL, true);
+    npc_battle_t *npc_b = npc_battle_new(100, e_stats, e_move, BATTLE_AI_GREEDY, HOSTILE, 0);
     e->npc_battle = npc_b;
 
     printf("enemy created!\n");
