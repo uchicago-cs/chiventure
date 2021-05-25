b7af6db514693716bb1fe5f7e115da02c19237bd
Maxine <maxineking@uchicago.edu>
2021-05-25 16:17:26 -0500

    Revert "Integrating graphics with chiventure by displaying a split-screen with a chiventure graphic in the upper half (#1075)" (#1100)
    This reverts commit c17a48d4c0eda47f896096a4d58c30eca3bea6df.
    
    Co-authored-by: Borja Sotomayor <borja@cs.uchicago.edu>

--- src/ui/src/gui.c
+++ src/ui/src/gui.c
@@ -1,156 +1,45 @@
+#include <ncurses.h>
+#include <ctype.h>
+#include <signal.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "action_management/actionmanagement.h"
-#include "ui/gui.h"
-#include "raylib.h"
+#include <locale.h>
 
+#include "common/ctx.h"
+#include "ui/window.h"
+#include "ui/print_functions.h"
 
-void run_gui(chiventure_ctx_t *ctx)
+#define MIN_COLS 80
+#define MIN_ROWS 24
+
+/*Starts up shell of a graphic*/
+void start_gui(chiventure_ctx_t *ctx)
 {
-    /* initialize the window height and width */
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Chiventure");
+    int ch;
 
-    /* creating a rectangle the size of the window */
-    Rectangle window = { POS_ZERO, POS_ZERO, WINDOW_WIDTH, WINDOW_HEIGHT };
+    // starts curses mode
+    setlocale(LC_ALL, "");
+    initscr();
+    // pressed keys are not displayed in the window
+    noecho();
+    // height and width of the terminal window
+    int width = COLS;
+    int height = LINES / 2;
 
-    /* initializing input text box */
-    char name[MAX_INPUT_CHARS + 1] = "\0";
-    int letterCount = 0;
-    int textBoxY = SCREEN_HEIGHT - 30;
-    int textBoxWidth = 225;
-    int textBoxHeight = 30;
+    window_t *map_win = window_new(height, width, 0, 0, print_map, true);
+    window_t *main_win = window_new(height, width, 0, 0, print_info, true);
+    window_t *displayed_win = main_win;
 
-    Rectangle textBox = { POS_ZERO, textBoxY, textBoxWidth, textBoxHeight };
-    bool mouseOnText = false;
+    window_t *cli_win = window_new(height, width, height, 0, print_cli, false);
 
-    /* initializing output text box */
-    int outputX = 10;
-    int outputHeight = 120;
-    int heightbuf = 140;
-    Rectangle output = { outputX, SCREEN_HEIGHT - heightbuf, SCREEN_WIDTH, outputHeight };
-    char *output_text = ctx->game->start_desc;
+    keypad(cli_win->w, TRUE);
+    scrollok(cli_win->w, TRUE);
+    wmove(cli_win->w, 0,0);
 
-    int framesCounter = 0;
-    SetTargetFPS(10);
-
-    /* loop to produce window of image and text box */
-    while (!WindowShouldClose()) {
-        if (CheckCollisionPointRec(GetMousePosition(), window))
-            mouseOnText = true;
-        else
-            mouseOnText = false;
-
-        if (mouseOnText) {
-            /* Get pressed key (character) on the queue */
-            int key = GetKeyPressed();
-
-            /* Check if more characters have been pressed on the same frame */
-            while (key > 0) {
-                /* NOTE: Only allow keys in range [32..125}
-                as these are the printable characters */
-                if ((key >= 0) && (key <= 127) && (letterCount < MAX_INPUT_CHARS))
-                {
-                    name[letterCount] = (char)key;
-                    letterCount++;
-                }
-
-                key = GetKeyPressed();  // Check next character in the queue
-            }
-
-            if (IsKeyPressed(KEY_BACKSPACE)) {
-                letterCount--;
-                name[letterCount] = '\0';
-
-                if (letterCount < 0) 
-                    letterCount = 0;
-            }
-            /* Close the window if ctrl+d keys are pressed */
-            if ((IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) && IsKeyDown(KEY_D)){
-                CloseWindow();
-                exit(1);
-            }
-        }
-
-        if (mouseOnText)
-            framesCounter++;
-        else framesCounter = 0;
-        
-        if (IsKeyPressed(KEY_ENTER)) {
-            /* use the command to string function to turn name into a command */
-            cmd *c = cmd_from_string(name, ctx);
-            /* the output text is taken from the command structs and game context */
-            output_text = (*(c->func_of_cmd))(c->tokens, ctx);
-
-            /* erases text in the text input, clearing the screen */
-            int length = letterCount;
-            for(int i = 0; i < length; i++) {
-                letterCount--;
-                name[letterCount] = '\0';
-            }
-	        name[letterCount] = (char) 32;
-	        letterCount++;
-
-            if (letterCount < 0)
-                letterCount = 0;
-      	}
-
-        int heightbuf2 = 150;
-        int rectHeight = 120;
-
-	    BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        /* Drawing the Image for the upper half of the split screen */
-
-        int width = SCREEN_WIDTH/2;
-        int height = SCREEN_HEIGHT/2;
-        int pos_x = SCREEN_WIDTH/4;
-        int pos_y = SCREEN_HEIGHT/10;
-        
-        /* Loading just one image is a temporary solution
-         In the future, we will use a more generic path so that we can load
-         game-specific images */
-        Image room = LoadImage("../src/ui/src/chiventure.png");   
-            
-        ImageResize(&room, width, height);
-            
-        /* Image converted to texture, uploaded to GPU memory (VRAM) */    
-        Texture2D texture = LoadTextureFromImage(room);
-        
-        /* Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM */
-        UnloadImage(room);   
-        
-        DrawTexture(texture, pos_x, pos_y, WHITE);
-
-        DrawRectangleRec(textBox, WHITE);
-        DrawRectangle(POS_ZERO, SCREEN_HEIGHT - heightbuf2, SCREEN_WIDTH, rectHeight, WHITE);
-        DrawRectangleLines(POS_ZERO, SCREEN_HEIGHT - heightbuf2, SCREEN_WIDTH, heightbuf2, BLACK);
-
-        /* Values related to text and text-box initialized */
-        int xbuf = 5;
-        int ybuf = 8;
-        int lineIndictorY = textBox.y + 10;
-        int lineIndictorX = textBox.x + xbuf + MeasureText(name, 21);
-        int fontSize = 20;
-        int fontSpacing = 5;
-
-        /* Input text and text-box displayed on lower half of split-screen */
-        if (mouseOnText) {
-            DrawRectangleLines(textBox.x, textBox.y, textBox.width, textBox.height, DARKGRAY);
-
-            if (((framesCounter / 5)%2) == 0)
-                DrawText("_", lineIndictorX, lineIndictorY, fontSize, DARKGRAY);
-        }
-            
-        DrawText(name, textBox.x + xbuf, textBox.y + ybuf, fontSize, BLACK);
-        Font test = GetFontDefault();
-        DrawTextRec(test, output_text, output, fontSize, fontSpacing, true, BLACK);
-
-        EndDrawing();
-    }
-    
-    CloseWindow();
-
+    print_homescreen(main_win, "THIS IS GUI");
 }
+
+void stop_gui(chiventure_ctx_t *ctx)
+{
+    //todo
+}
+
