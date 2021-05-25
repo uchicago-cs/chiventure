b4a6d359fb20b937845531af6fd0208b0f7091c4
Jack Casey <jackcasey067@gmail.com>
2021-05-24 20:00:52 -0500

    (#1098) Added a helper function for pausing execution and prompting for input.

--- src/playerclass/examples/2021-demo.c
+++ src/playerclass/examples/2021-demo.c
@@ -4,6 +4,39 @@
 
 #include <stdio.h>
 
+#define BUFFER_SIZE 100
+
+/* A helper function for pausing execution and asking for input. 
+ * 
+ * Parameters:
+ *  - message: The message that will be printed out as a prompt (newline is added).
+ *  - buffer: Pointer to memory to store the input, or NULL (in which case input is ignored).
+ * 
+ * Returns:
+ *  - Nothing.
+ */
+void prompt(char* message, char* input) {
+    printf("%s\n>>> ", message);
+
+    if (input != NULL) {
+        fgets(input, BUFFER_SIZE, stdin);
+
+        /* Delete trailing newline */
+        char* ch_ptr = input;
+        while (*ch_ptr != '\n')
+            ch_ptr += sizeof(char);
+        *ch_ptr = '\0'; 
+    }
+    else {
+        char ignore[BUFFER_SIZE];
+        fgets(ignore, BUFFER_SIZE, stdin);
+    }
+}
+
+/* main function for the 2021-demo executable. */
 int main() {
-    printf("Hello, World!\n");
+    /* test code */
+    char name[BUFFER_SIZE]; 
+    prompt("What is your name?", name); 
+    printf("Hello, %s!\n", name);
 }
