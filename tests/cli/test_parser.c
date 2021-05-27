f61cddda6d309e84e886d56eaa4c5f39bdc9ad46
Beza Desta <bezad@uchicago.edu>
2021-05-27 15:37:06 -0500

    preliminary tests for parse_r

--- tests/cli/test_parser.c
+++ tests/cli/test_parser.c
@@ -7,6 +7,25 @@
 #include "cli/parser.h"
 
 //Tests the parsing of an empty input, which should return NULL.
+Test(parse_r, no_input_r)
+{
+    char str[] = "";
+    tokenized_cmds *words = parse_r(str);
+    cr_assert_null(words, "parser() should return NULL if no input");
+}
+
+//Tests the parsing of a single word.
+Test(parse_r, single_r)
+{
+    char str[] = "LOOK";
+    tokenized_cmds *words = parse_r(str);
+    cr_assert_str_eq(words->cmds,"LOOK", "parse() did not create first token");
+    cr_assert_null(words[1], "parse() should point to NULL for empty tokens");
+    cr_assert_null(words[2], "parse() should point to NULL for empty tokens");
+    cr_assert_null(words[3], "parse() should point to NULL for empty tokens");
+}
+
+//Tests the parsing of an empty input, which should return NULL.
 Test(parse, no_input)
 {
     char str[] = "";
