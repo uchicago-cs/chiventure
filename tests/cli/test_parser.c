3c80947275594758374e5b950f4e92161c942214
Beza Desta <bezad@uchicago.edu>
2021-05-27 16:05:17 -0500

    more progress

--- tests/cli/test_parser.c
+++ tests/cli/test_parser.c
@@ -6,9 +6,63 @@
 #include <ctype.h>
 #include "cli/parser.h"
 
-//Tests the parsing of an empty input, which should return NULL.
-Test(parse_r, no_input_r)
+int compare_list_cmds(char** str, int size, char** expected_words)
 {
+        tokenized_cmds* words = parse_r(str)
+        tokenized_cmds* temp;
+        int i = 0;
+        LL_FOREACH(words,temp)
+        {
+            if (i < size)
+            {
+                if strcmp((expected_words[i], temp->cmds) == 0)
+                {
+                    continue;
+                }
+                else 
+                {
+                    return 0;
+                }
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+        return 1;          
+}
+
+void check_comparison(char** str, int size, char** expected_words, int expected)
+{
+    int check = compare_list_cmds(words, size, expected_words)
+    cr_assert_eq(check, expected, "got %d but expected %d");
+}
+
+Test(parse_r, check_overlap_does_not_intersect)
+{
+    check_circle_overlap(3.0, 4.0, 14.0, 18.0, 5.0, 8.0, 0);
+}
+/* this test should return 2 since the 2 circles inputted are
+ * overlapping 
+ */
+Test(circle, check_overlap_intersects)
+{
+    check_circle_overlap(10.0, 3.0, 1.0, 2.0, 5.0, 8.0, 2);
+}
+
+/* this test should return 1 since the 2 circles inputted are
+ * just touching
+ */
+
+Test(circle, check_overlap_at_singular_point)
+{
+    check_circle_overlap(8.0, 4.0, 18.0, 4.0, 4.0, 6.0, 1);
+}
+
+
+//Tests the parsing of an empty input, which should return NULL.
+//Test(parse_r, no_input_r)
+/*{
     char str[] = "";
     tokenized_cmds *words = parse_r(str);
     cr_assert_null(words, "parser() should return NULL if no input");
@@ -21,8 +75,18 @@ Test(parse_r, single_r)
     tokenized_cmds *words = parse_r(str);
     cr_assert_str_eq(words->cmds,"LOOK", "parse() did not create first token");
     cr_assert_null(words[1], "parse() should point to NULL for empty tokens");
-    cr_assert_null(words[2], "parse() should point to NULL for empty tokens");
-    cr_assert_null(words[3], "parse() should point to NULL for empty tokens");
+    
+}
+
+//Tests the parsing of two words.
+Test(parse_r, two_words_r)
+{
+    char str[] = "LOOK AND AT";
+    tokenized_cmds/* **words = parse(str);
+    cr_assert_str_eq(words[0],"LOOK", "parse() did not create first token");
+    cr_assert_str_eq(words[1], "AT", "parse() did not create second token");
+    cr_assert_null(words[2],"parse() should point to NULL for empty tokens");
+    cr_assert_null(words[3],"parse() should point to NULL for empty tokens");
 }
 
 //Tests the parsing of an empty input, which should return NULL.
@@ -31,7 +95,7 @@ Test(parse, no_input)
     char str[] = "";
     char **words = parse(str);
     cr_assert_null(words, "parser() should return NULL if no input");
-}
+}*/
 
 //Tests the parsing of a single word.
 Test(parse, single)
