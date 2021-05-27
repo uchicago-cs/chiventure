71d3c722af91c1df26985bd58b893df61dd35918
Joshua Shou <jshou@linux6.cs.uchicago.edu>
2021-05-27 16:09:23 -0500

    changes

--- tests/skilltrees/sampleeffects.c
+++ tests/skilltrees/sampleeffects.c
@@ -10,18 +10,30 @@
 //just item_stat and player_stat_effect
 //define_item_att_effect(item_t* item, char* att_id, enum attribute_tag att_tag, attribute_value_t attribute_mod)
 
-item_t* bombitem = item_new("BOMB", "An armed bomb", "5 seconds till detonation")
+item_t* bombitem = item_new("BOMB", "An armed bomb", "5 seconds till detonation");
 enum attribute_tag bombatt_tag = BOOLE;
 attribute_value_t bombmod;
-mod.bool_val = false;
+bombmod.bool_val = false;
 
 item_att_effect_t* itemdefusebombeffect = define_item_att_effect(bombitem, "ARMED", bombtag, bombmod);
 
 effect_t* defusebombeffect = make_item_att_effect(itemdefusebombeffect);
 
+//chop tree effect
+item_t* choptreeitem = item_new("Axe", "An armed Axe", "Use this to cut down trees!");
+enum attribute_tag choptree_tag = BOOLE;
+attribute_value_t choptreemod;
+choptreemod.bool_val = false;
+
+item_att_effect_t* itemchoptreeeffect = define_item_att_effect(choptreeitem, "CHOP", choptree_tag, choptreemod);
+
+effect_t* choptreeeffect = make_item_att_effect(itemdefusebombeffect);
 
 
-define_player_stat_effect(char* player_stat_effect_name, char** stat_names, double* modifications, int* durations, int num_stats, chiventure_ctx_t* ctx)
+
+
+
+
 
 char* innerpeace_stats[] = {"current_health", "peace_level"};
 
@@ -31,7 +43,7 @@ int innerpeace_duration[] = {8, 8};
 
 player_stat_effect_t* playerstatpeace = define_player_stat_effect("innerpeace", innerpeace_stats,
                                                                     innerpeace_mod, innerpeace_duration, 2, ctx);
-effect_t innerpeace = make_player_stat_effect(playerstatpeace);
+effect_t* innerpeace = make_player_stat_effect(playerstatpeace);
 
 
 
