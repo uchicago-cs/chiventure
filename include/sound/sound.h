ef61163187bd90968b0eb746bb4916634cd1e498
Jason Guba <jiguba@uchicago.edu>
2021-05-27 08:58:50 -0500

    Added demo audio and play functions and a compiled sound 'audiofile'

--- include/sound/sound.h
+++ include/sound/sound.h
@@ -1,11 +1,16 @@
 #ifndef INCLUDE_SOUND_H_
 #define INCLUDE_SOUND_H_
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
 #include <math.h>
 #include "common/uthash.h"
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
 
 typedef struct{
     // path to sound file
@@ -19,5 +24,23 @@ typedef struct{
     // makes struct hashable
     UT_hash_handle hh; 
 } sound_t;
+
+
+void load_audio(sound_t *name, sound_t *source);
+void play_audio(sound_t *name);
+
+/* THE FOLLOWING CODE IS FOR DEMO PURPOSES THIS 
+ * EXISTING CODE FROM SDL2 LIBRARIES TO LOAD AND 
+ * PLAY MUSIC
+ */
+void load_audio_demo();
+void play_audio_demo(Mix_Music *backgroundSound,SDL_Window *window, SDL_Renderer *renderer);
+
+
+
+
+
+
+
 
 #endif /* INCLUDE_SOUND_H_ */
