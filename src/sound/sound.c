8ae8831a027a40e21be87580ef46eb2b44cc3e96
Gyuyoung Park <gypark23@linux4.cs.uchicago.edu>
2021-05-27 15:46:21 -0500

    compilable library

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -6,26 +6,25 @@
 #include "sound.h"
 #include "SDL/SDL.h"
 
-int sound_new(char* path, double duration, char* name, int id, UT_hash_handle hh)
-{
-    
-}
-
-int sound_init()
-{
-    SDL_Init(SDL_INIT_AUDIO);
-}
 
 int load_wav(sound_t *sound)
 {
+    /*
     uint32_t wavLength = sound->wavLength;
     uint8_t *wavBuffer = sound->wavBuffer;
     SDL_LoadWAV(sound->name, &(sound->wavSpec), &wavBuffer, &wavLength);
+    */
     return 0;
 }
-/*
-int play_sound(sound_t *sound)
+
+int sound_free(sound_t *sound)
 {
+    return 0;
+}
+
+int play_sound(sound_t *sound, int delay)
+{
+    /*
     SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, sound->wavSpec, NULL, 0);
     int success = SDL_QueueAudio(deviceId, sound->wavBuffer, sound->wavLength);
     SDL_PauseAudioDevice(deviceId, 0);
@@ -35,7 +34,11 @@ int play_sound(sound_t *sound)
     SDL_CloseAudioDevice(deviceId);
     SDL_FreeWAV(sound->wavBuffer);
     SDL_Quit();
+    */
     return 0;
 }
 
-*/
+int main()
+{
+    return 0;
+}
