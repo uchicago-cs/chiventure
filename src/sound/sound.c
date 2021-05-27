38975eb6857d4e9f25744327cc456a302c8e605a
Gyuyoung Park <gypark23@linux6.cs.uchicago.edu>
2021-05-26 22:15:13 -0500

    fixed include sdl library

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -4,27 +4,29 @@
 #include <math.h>
 #include "../../include/common/uthash.h"
 #include "sound.h"
-#include "SDL.h"
+#include "SDL/SDL.h"
 
-int sound_new()
+int sound_new(char* path, double duration, char* name, int id, UT_hash_handle hh)
 {
     
 }
 
-int sound_init(char* path, double duration, char* name, int id, UT_hash_handle hh)
+int sound_init()
 {
-
+    SDL_Init(SDL_INIT_AUDIO);
 }
 
 int load_wav(sound_t *sound)
 {
-    SDL_LoadWAV(sound->name, &sound->wavSpec, &sound->wavBuffer, &sound->wavLength);
+    uint32_t wavLength = sound->wavLength;
+    uint8_t *wavBuffer = sound->wavBuffer;
+    SDL_LoadWAV(sound->name, &(sound->wavSpec), &wavBuffer, &wavLength);
     return 0;
 }
-
+/*
 int play_sound(sound_t *sound)
 {
-    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &sound->wavSpec, NULL, 0);
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, sound->wavSpec, NULL, 0);
     int success = SDL_QueueAudio(deviceId, sound->wavBuffer, sound->wavLength);
     SDL_PauseAudioDevice(deviceId, 0);
     SDL_Delay(3000);
@@ -36,3 +38,4 @@ int play_sound(sound_t *sound)
     return 0;
 }
 
+*/
