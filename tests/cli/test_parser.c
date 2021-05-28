671e98944383fda4703de68bd50b59230cd8e9d2
Beza Desta <bezad@uchicago.edu>
2021-05-28 10:38:54 -0500

    resolved failining issues for issue #1119

--- tests/cli/test_parser.c
+++ tests/cli/test_parser.c
@@ -92,9 +92,9 @@ Test(parse_r, more_than_four_words)
 {
     char* expecting_words[5];
     expecting_words[0] = "LOOK PUSH ";
-    expecting_words[1] = "DESTROY ";
-    expecting_words[2] = "EAT ";
-    expecting_words[3] = "DRINK ";
+    expecting_words[1] = " DESTROY ";
+    expecting_words[2] = " EAT ";
+    expecting_words[3] = " DRINK ";
     expecting_words[4] = "SLEEP";
     char str[] = "LOOK PUSH AND DESTROY AND EAT AND DRINK AND SLEEP";
     check_comparison(str, 5, expecting_words);
