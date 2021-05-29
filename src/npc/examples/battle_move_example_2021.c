9633f36d4d5d2e16de94ca245049ab08cb0da1dc
northofsophia <81777487+northofsophia@users.noreply.github.com>
2021-05-28 20:36:36 -0500

    Added items to hostile_harry #1027

--- src/npc/examples/battle_move_example_2021.c
+++ src/npc/examples/battle_move_example_2021.c
@@ -29,77 +29,77 @@ const char *banner =
 /* Creates a sample class. Taken from test_class.c */
 class_t* generate_test_class()
 {
-class_t* c;
-char *name, *shortdesc, *longdesc;
+	class_t* c;
+	char *name, *shortdesc, *longdesc;
 
-name = "Warrior";
-shortdesc = "Mechanically, the warrior focuses on up-close physical "
-"damage with weapons and survives enemy attacks "
-"using heavy armor.\n";
-longdesc = "The warrior is the ultimate armor and weapons expert,"
-" relying on physical strength and years of training to "
-"deal with any obstacle. Mechanically, the warrior focuses "
-"on up-close physical damage with weapons and survives enemy "
-"attacks using heavy armor.\n";
+	name = "Warrior";
+	shortdesc = "Mechanically, the warrior focuses on up-close physical "
+	"damage with weapons and survives enemy attacks "
+	"using heavy armor.\n";
+	longdesc = "The warrior is the ultimate armor and weapons expert,"
+	" relying on physical strength and years of training to "
+	"deal with any obstacle. Mechanically, the warrior focuses "
+	"on up-close physical damage with weapons and survives enemy "
+	"attacks using heavy armor.\n";
 
-c = class_new(name, shortdesc, longdesc, NULL, NULL, NULL);
+	c = class_new(name, shortdesc, longdesc, NULL, NULL, NULL);
 }
 
 /* Creates a sample npc_mov struct. Taken from test_npc_move.c */
 npc_mov_t *generate_test_npc_mov()
 {
-npc_mov_t *npc_mov;
-room_t *test_room;
-test_room = room_new("test_room", "test", "test test");
-npc_mov = npc_mov_new(NPC_MOV_DEFINITE, test_room);
+	npc_mov_t *npc_mov;
+	room_t *test_room;
+	test_room = room_new("test_room", "test", "test test");
+	npc_mov = npc_mov_new(NPC_MOV_DEFINITE, test_room);
 }
 
 /* Creates example stats. Taken from test_battle_ai.c */
 stat_t* create_enemy_stats()
 {
-stat_t* test_stats = calloc(1, sizeof(stat_t));
+	stat_t* test_stats = calloc(1, sizeof(stat_t));
 
-test_stats->speed = 50;
-test_stats->defense = 20;
-test_stats->strength = 150;
-test_stats->dexterity = 10;
-test_stats->hp = 200;
-test_stats->max_hp = 200;
-test_stats->xp = 0;
-test_stats->level = 5;
+	test_stats->speed = 50;
+	test_stats->defense = 20;
+	test_stats->strength = 150;
+	test_stats->dexterity = 10;
+	test_stats->hp = 200;
+	test_stats->max_hp = 200;
+	test_stats->xp = 0;
+	test_stats->level = 5;
 
-return test_stats;
+	return test_stats;
 }
 
 /* Creates + initializes a move. Taken from test_battle_ai.c */
 move_t *create_move_(int id, battle_item_t* item, bool attack, int damage,
 int defense)
 {
-move_t* move = (move_t*) calloc(1, sizeof(move_t));
+	move_t* move = (move_t*) calloc(1, sizeof(move_t));
 
-move->id = id;
+	move->id = id;
 
-move->item = item;
+	move->item = item;
 
-move->attack = attack;
-move->damage = damage;
-move->defense = defense;
+	move->attack = attack;
+	move->damage = damage;
+	move->defense = defense;
 
-return move;
+	return move;
 }
 
 /* Creates example moves. Taken from test_battle_ai.c */
 move_t* create_enemy_moves()
 {
-move_t *head, *earthquake, *poke, *rock_throw;
-head = NULL;
-earthquake = create_move_(1, NULL, true, 100, 0);
-poke = create_move_(2, NULL, true, 40, 0);
-rock_throw = create_move_(3, NULL, true, 90, 0);
-DL_APPEND(head, earthquake);
-DL_APPEND(head, poke);
-DL_APPEND(head, rock_throw);
-return head;
+	move_t *head, *earthquake, *poke, *rock_throw;
+	head = NULL;
+	earthquake = create_move_(1, NULL, true, 100, 0);
+	poke = create_move_(2, NULL, true, 40, 0);
+	rock_throw = create_move_(3, NULL, true, 90, 0);
+	DL_APPEND(head, earthquake);
+	DL_APPEND(head, poke);
+	DL_APPEND(head, rock_throw);
+	return head;
 }
 
 /* Makes sure the game is loaded */
@@ -199,6 +199,15 @@ chiventure_ctx_t *create_sample_ctx()
     move_t *moves2 = create_enemy_moves();
     add_battle_to_npc(hostile_harry, 10, stats, moves, BATTLE_AI_GREEDY,
                       HOSTILE, 0);
+	
+	/* Add items to hostile npc */
+	item_t *elixir = item_new("ELIXIR","This is an elixir.",
+                   "This is an elixir. Effects: energize and stun.");
+    add_item_to_npc(hostile_harry, elixir);
+
+    item_t *potion = item_new("POTION","This is a health potion.",
+                   "This potion will increase your health. Feel free to take it.");
+    add_item_to_npc(hostile_harry, potion);
     
     /* Add the npcs to the game */
     add_npc_to_game(game, friendly_fiona);
