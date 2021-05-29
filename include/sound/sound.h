06e3de5e1b298921f85eec3b9fb10d2ea5872d1e
Gyuyoung Park <gypark23@linux7.cs.uchicago.edu>
2021-05-28 19:53:22 -0500

    Fixed comments for sound.h functions parameter

--- include/sound/sound.h
+++ include/sound/sound.h
@@ -16,84 +16,70 @@
 typedef enum {BACKGROUND, SOUND_EFFECT} SoundType;
 
 /* This represents our sound data structure */
- typedef struct{
-     // type of sound
-     SoundType type;
-     // name of sound
-     char* name;
-     // wav file information
-     SDL_AudioSpec wavSpec;
-     uint32_t wavLength;
-     uint8_t *wavBuffer;
-     //below for hash table
-     int id; 
-     // makes struct hashable
-     UT_hash_handle hh; 
- } sound_t;
+typedef struct {
+    // type of sound
+    SoundType type;
+    // name of sound
+    char* name;
+    // wav file information
+    SDL_AudioSpec wavSpec;
+    uint32_t wavLength;
+    uint8_t *wavBuffer;
+    //below for hash table
+    int id;
+    // makes struct hashable
+    UT_hash_handle hh;
+} sound_t;
 
-/*
-loads wav on the SDL audio queue
-Inputs: 
-- sound: sound struct 
-returns: 
-- 0 on success 1 on failure
-*/
+/* Loads sound to SDL queue
+ *
+ * Parameters:
+ *      sound: sound struct
+ * Return:
+ *      0 on success 1 on failure
+ */
 int load_wav(sound_t *sound);
 
-/*
-frees sound 
-Inputs: 
-- sound_t* sound to free 
-*/
-int sound_free(sound_t* sound); 
+int sound_free(sound_t* sound);
+
+int play_sound(sound_t* sound, int delay);
 
 
-/*
-Plays sound 
-Input: 
-delay (int): time to delay before playing the sound when the function is called
-sound: (char*) the hash id of the sound
-Returns:
-- 0 on success 1 on failure
-*/
-int play_sound(sound_t* sound, int delay); 
-
-
-/* THE FOLLOWING CODE IS FOR DEMO PURPOSES. THIS 
- * USES EXISTING DATA STRUCTURES AND FUNCTIONS FROM 
+/* THE FOLLOWING CODE IS FOR DEMO PURPOSES. THIS
+ * USES EXISTING DATA STRUCTURES AND FUNCTIONS FROM
  * THE SDL2 LIBRARIES TO LOAD AND PLAY MUSIC
 */
 
-/*  
+/*
  * The purpose of this function is to initiate the loading process of an audio file.
  * We initialize the SDL library, create a window for audio to be played, and locate
  * the audio files to load in the existing SDL sound structures.
- * Parameters: 
+ * Parameters:
  *      type: the type of audio (background, sound effect etc.)
  */
 void load_audio_demo(SoundType type);
 
 /* Audio functions to play larger audio files like .mp3 formats
- * 
+ *
  * Parameters:
  *      backgroundSound: Mix_Music data structure with a loaded audio file
  *      window: the window created with specified dimensions
  *      renderer: a renderering struct that handles rendering for the window
  * Return:
  *      None
- * 
+ *
  */
 void play_audio_demo_bgm(Mix_Music *backgroundSound, SDL_Window *window, SDL_Renderer *renderer);
 
 /* Audio functions to play smaller audio files/music chunks
- * 
+ *
  * Parameters:
  *      soundEffect: Mix_Chunk data structure with a loaded audio file
  *      window: the window created with specified dimensions
  *      renderer: a renderering struct that handles rendering for the window
  * Return:
  *      None
- * 
+ *
  */
 void play_audio_demo_effect(Mix_Chunk *soundEffect,SDL_Window *window, SDL_Renderer *renderer);
 
