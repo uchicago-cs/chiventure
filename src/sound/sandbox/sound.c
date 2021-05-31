7a2a0eb3f1162160315bbb082cb30087095917a7
Jason Guba <jiguba@uchicago.edu>
2021-05-31 13:27:15 -0500

    Added where to find more details for sound.c functions

--- src/sound/sandbox/sound.c
+++ src/sound/sandbox/sound.c
@@ -1,21 +1,25 @@
 #include "common/uthash.h"
 #include "sound/sound.h"
 
+/* See sound.h for details */
 int load_wav(sound_t *sound)
 {
     return 0;
 }
 
+/* See sound.h for details */
 int sound_free(sound_t *sound)
 {
     return 0;
 }
 
+/* See sound.h for details */
 int play_sound(sound_t *sound, int delay)
 {
     return 0;
 }
 
+/* See sound.h for details */
 int main() {
     return 0;
 }
