8f5739a380c9bea501661bee0d90de6cfa11ecb7
Jason Guba <jiguba@uchicago.edu>
2021-05-31 13:25:21 -0500

    Redid file orangization

--- src/sound/sandbox/sound.c
+++ src/sound/sandbox/sound.c
@@ -1,10 +1,5 @@
-#include <stdlib.h>
-#include <stdbool.h>
-#include <assert.h>
-#include <math.h>
 #include "common/uthash.h"
-#include "sound.h"
-#include "SDL/SDL.h"
+#include "sound/sound.h"
 
 int load_wav(sound_t *sound)
 {
@@ -18,5 +13,9 @@ int sound_free(sound_t *sound)
 
 int play_sound(sound_t *sound, int delay)
 {
+    return 0;
+}
+
+int main() {
     return 0;
 }
---
+++ src/sound/src/sound.c
@@ -0,0 +1,21 @@
+#include "common/uthash.h"
+#include "sound/sound.h"
+
+int load_wav(sound_t *sound)
+{
+    return 0;
+}
+
+int sound_free(sound_t *sound)
+{
+    return 0;
+}
+
+int play_sound(sound_t *sound, int delay)
+{
+    return 0;
+}
+
+int main() {
+    return 0;
+}
