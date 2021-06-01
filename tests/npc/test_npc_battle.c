30eea04d9ec2994360cd15768b1efcde07520237
Amy Morrill <amymorrill@linux3.cs.uchicago.edu>
2021-06-01 16:39:14 -0500

    Added Lily's requested changes #1026

--- tests/npc/test_npc_battle.c
+++ tests/npc/test_npc_battle.c
@@ -6,9 +6,9 @@
 #include "battle/battle_moves.h"
 
 /* Creates example stats. Taken from test_battle_ai.c */
-stat_t* create_enemy_stats1()
+stat_t *create_enemy_stats1()
 {
-    stat_t* test_stats = calloc(1, sizeof(stat_t));
+    stat_t *test_stats = calloc(1, sizeof(stat_t));
 
     test_stats->speed = 50;
     test_stats->defense = 20;
@@ -23,9 +23,9 @@ stat_t* create_enemy_stats1()
 }
 
 /* Creates example stats. Taken from test_battle_ai.c */
-stat_t* create_enemy_stats2()
+stat_t *create_enemy_stats2()
 {
-    stat_t* test_stats = calloc(1, sizeof(stat_t));
+    stat_t *test_stats = calloc(1, sizeof(stat_t));
 
     test_stats->speed = 100;
     test_stats->defense = 20;
@@ -43,7 +43,7 @@ stat_t* create_enemy_stats2()
 move_t *create_move(int id, battle_item_t* item, bool attack, int damage, 
 		    int defense)
  {
-     move_t* move = (move_t*) calloc(1, sizeof(move_t));
+     move_t *move = (move_t*) calloc(1, sizeof(move_t));
 
      move->id = id;
 
@@ -57,7 +57,7 @@ move_t *create_move(int id, battle_item_t* item, bool attack, int damage,
  }
 
 /* Creates example moves. Taken from test_battle_ai.c */
-move_t* create_enemy_moves1()
+move_t *create_enemy_moves1()
 {
     move_t *head, *earthquake, *poke, *rock_throw;
     head = NULL;
@@ -71,7 +71,7 @@ move_t* create_enemy_moves1()
 }
 
 /* Creates example moves. Taken from test_battle_ai.c */
-move_t* create_enemy_moves2()
+move_t *create_enemy_moves2()
 {
     move_t *head, *earthquake, *poke, *rock_throw;
     head = NULL;
