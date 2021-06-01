b63c75c6a4273d125bff63588d75dfe348c01945
Jason Guba <jiguba@uchicago.edu>
2021-05-31 19:03:08 -0500

    Added comments stating purpose of hashtable.c

--- src/sound/src/hashtable.c
+++ src/sound/src/hashtable.c
@@ -1,7 +1,17 @@
 #include "sound/sound.h"
 
+/* This code is to test functionality of hash functions in order to 
+    create a hash table to store sound structs. 
+    Source: https://troydhanson.github.io/uthash/userguide.html*/
+
+
+
+/* Declares a hash as a NULL-initalized pointer */ 
+
 sound_t *users = NULL;
 
+
+/* Adds an item to a hash */
 
 void add_user(int user_id, char *name) {
     sound_t *s;
@@ -10,6 +20,8 @@ void add_user(int user_id, char *name) {
     strcpy(s->name, name);
     HASH_ADD_INT(users, id, s);  /* id: name of key field */
 }
+
+/*Finds a structure in the hash based on an int key */
 
 sound_t *find_user(int user_id) {
     sound_t *s;
