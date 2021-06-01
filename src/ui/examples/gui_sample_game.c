2fb74db4917ab7df665cdd268de76663e1e06e97
Perene Wang <perenewang@linux1.cs.uchicago.edu>
2021-06-01 17:08:30 -0500

    fixed key range

--- src/ui/examples/gui_sample_game.c
+++ src/ui/examples/gui_sample_game.c
@@ -75,7 +75,7 @@ int main() {
             while (key > 0) {
                 /* NOTE: Only allow keys in range [32..125}
                 as these are the printable characters */
-                if ((key >= 0) && (key <= 127) && (letterCount < MAX_INPUT_CHARS))
+                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
                 {
                     name[letterCount] = (char)key;
                     letterCount++;
