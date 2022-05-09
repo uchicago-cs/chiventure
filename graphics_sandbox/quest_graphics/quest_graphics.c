e4db8aeda97bbe9b4d1dd2299e5ee4f985044281
周子恺 <zhouzk@uchicago.edu>
2022-05-09 06:57:42 -0500

    added synthesizeTest and adjusted Interface

--- graphics_sandbox/quest_graphics/quest_graphics.c
+++ graphics_sandbox/quest_graphics/quest_graphics.c
@@ -1,16 +1,68 @@
 #include "quest_graphics.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 int getTreeMaxWidth(task_tree_mockup_t* taskTree) {
-    //To do
-    return 6;
-}
-
-int getTreeMaxHeight(task_tree_mockup_t* taskTree) {
     //To do
     return 4;
 }
 
+int getTreeMaxHeight(task_tree_mockup_t* taskTree) {
+    //To do
+    return 3;
+}
 
+task_tree_mockup_t* synthesizeTest() {
+    task_tree_mockup_t *TreeA = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeB = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeC = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeD = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeE = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeF = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeG = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeH = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+    task_tree_mockup_t *TreeI = (task_tree_mockup_t*)malloc(sizeof(task_tree_mockup_t));
+
+    TreeA->task_name="A";
+    TreeA->rsibling=TreeB;
+    TreeA->lmostchild=NULL;
+    TreeA->parent=NULL;
+    TreeB->task_name="B";
+    TreeB->rsibling=TreeC;
+    TreeB->lmostchild=TreeE;
+    TreeB->parent = TreeA;
+    TreeC->task_name= "C";
+    TreeC->rsibling=TreeD;
+    TreeC->lmostchild=TreeG;
+    TreeC->parent=TreeB;
+    TreeD->task_name="D";
+    TreeD->rsibling=NULL;
+    TreeD->lmostchild=TreeG;
+    TreeD->parent=TreeC;
+    TreeE->task_name="E";
+    TreeE->rsibling=NULL;
+    TreeE->lmostchild=TreeF;
+    TreeE->parent=TreeB;
+    TreeF->task_name="F";
+    TreeF->rsibling=TreeH;
+    TreeF->lmostchild=NULL;
+    TreeF->parent=TreeE;
+    TreeG->task_name="G";
+    TreeG->rsibling=NULL;
+    TreeG->lmostchild=NULL;
+    TreeG->parent=TreeC;
+    TreeH->task_name="H";
+    TreeH->rsibling=TreeI;
+    TreeH->lmostchild=NULL;
+    TreeH->parent=TreeF;
+    TreeI->task_name="I";
+    TreeI->rsibling=NULL;
+    TreeI->lmostchild=NULL;
+    TreeI->parent=TreeH;
+
+    return TreeA;
+
+}
 
 
 void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector2 windowsize)
@@ -22,6 +74,9 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
 
     InitWindow(screenWidth, screenHeight, "Quest");
     SetWindowPosition(windowpos.x, windowpos.y);
+
+    double widthSegment = (screenWidth - 100)/getTreeMaxWidth(taskTree) - 30;
+    double heightSegment = (screenWidth - 100)/getTreeMaxHeight(taskTree) - 20;
 
     //Currently Hardcoded, need to be flexible in the future as
     // as we read in from the real quest struct;
@@ -50,11 +105,11 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
 
             ClearBackground(RAYWHITE);
 
-            DrawText(TextFormat("hp: %08i/%08i", hp, hp_requirement), 200, 80, 20, RED);
+            DrawText(TextFormat("hp: %03i/%03i", hp, hp_requirement), 20, 20, 20, RED);
 
-            DrawText(TextFormat("level: %08i/%08i", level, level_requirement), 200, 120, 20, GREEN);
+            DrawText(TextFormat("level: %02i/%02i", level, level_requirement), 20, 50, 20, GREEN);
 
-            DrawText(TextFormat("xp: %02i", xp), 200, 160, 40, BLUE);
+            DrawText(TextFormat("xp: %02i", xp), 20, 80, 20, BLUE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -69,7 +124,7 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
 
 
 int main() {
-
-    runTaskTreeGraphics()
+    task_tree_mockup_t* taskTree = synthesizeTest();
+    runTaskTreeGraphics(taskTree, (Vector2){50,80},(Vector2){600,400});
     return 0;
 }
