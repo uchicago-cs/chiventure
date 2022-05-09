a7cf1cd3da714dc8f4a65c4041bf20e62f1b4150
周子恺 <zhouzk@uchicago.edu>
2022-05-08 19:04:14 -0500

    updated struct in parse.h

--- graphics_sandbox/parse/parse.h
+++ graphics_sandbox/parse/parse.h
@@ -1,13 +1,23 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+#include "raylib.h"
+
+
 typedef struct graphics_component graphics_component_t;
 
-struct graphics_component {
-    
-    
-    
-}
+typedef enum {
+    NPC_GRAPHICS,
+    INPUT_BOX,
+    INVENTORY_GRAPHICS,
+    SCENE_GRAPHICS,
+    QUEST_GRAPHICS,
+} component_name_t;
+
+typedef struct {
+    component_name_t component_name;
+    Rectangle position_and_size;
+} graphics_component_t;
 
 
 
