eda70bdda66536aa3196c69d3ff926fcfdad905e
Ziqi Wei <ziqiwei@linux1.cs.uchicago.edu>
2022-05-09 17:16:34 -0500

    fixed

--- src/quests/src/quests_state.c
+++ src/quests/src/quests_state.c
@@ -485,8 +485,13 @@ int is_quest_completed(quest_t *quest)
 /* Refer to quests_state.h */
 quest_t *get_quest_from_hash(char *quest_id, quest_hash_t *hash_table)
 {
+    // quest_t *q;
+    // HASH_FIND_STR(hash_table, quest_id, q);
+
+    // return q;
     quest_t *q;
-    HASH_FIND_STR(hash_table, quest_id, q);
+    HASH_FIND(hh, hash_table, quest_id,  
+            strnlen(quest_id, MAX_ID_LEN), q);
 
     return q;
 }
