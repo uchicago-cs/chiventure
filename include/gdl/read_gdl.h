7166cc890a716f36fa19ffa930491a231f71ba3f
Gabriel <raffa@Fiki-018.local>
2022-05-09 23:53:00 -0500

    gdl format created

---
+++ include/gdl/read_gdl.h
@@ -0,0 +1,13 @@
+/*
+ * This is a Graphics Description Language
+ * reading device
+ *
+ * Authors can describe preferred graphical
+ * styles and features by writing a "GDL"
+ * file in JSON format according to a set
+ * of just a few rules
+ */
+
+// this will include the pop-up struct I made #include "windows.h"
+
+
