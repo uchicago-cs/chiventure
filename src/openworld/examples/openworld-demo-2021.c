a2aee057a850357d86c5c18f80abbec3ff78e153
William Hu <williamhu2025@uchicago.edu>
2022-05-10 18:58:42 -0500

    Deleting makefile for the demo and moving all code into the commented out demo file

--- src/openworld/examples/openworld-demo-2021.c
+++ src/openworld/examples/openworld-demo-2021.c
@@ -1,4 +1,24 @@
 /*
+Openworld 2021 demo makefile
+
+set(OPENWORLD_EXAMPLES openworld-demo-2022)
+
+add_executable(openworld-demo-2022
+               openworld-demo-2022.c)
+
+# Link with chiventure libraries
+foreach(example ${OPENWORLD_EXAMPLES})
+    foreach(module ${CHIVENTURE_MODULES})
+        target_link_libraries(${example} ${module})
+    endforeach(module)
+endforeach(example)
+
+add_custom_target(openworld-examples
+        DEPENDS ${OPENWORLD_EXAMPLES})
+*/        
+
+
+/*
  * This example program runs a full instance of chiventure with an in-memory game.
  * The CLI is monkey-patched to accept functions that serve to showcase room generation.
  * 
@@ -300,3 +320,5 @@ int main(int argc, char **argv)
 
     return 0;
 }*/
+
+
