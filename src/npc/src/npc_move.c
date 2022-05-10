17fcb4f54c079d96a03b22e55168145dd58e6c8d
William Roy Miller <wrm@linux5.cs.uchicago.edu>
2022-05-09 22:44:35 -0500

    Applying the allman style to all .c files in the src/npc directory

--- src/npc/src/npc_move.c
+++ src/npc/src/npc_move.c
@@ -21,12 +21,12 @@ int npc_mov_init(npc_mov_t *npc_mov, npc_mov_enum_t mov_type,
     if (mov_type == NPC_MOV_DEFINITE)
     {
         LL_APPEND(npc_mov->npc_mov_type.npc_mov_definite->npc_path,
-        room_to_add);
+                  room_to_add);
     }
     else if (mov_type == NPC_MOV_INDEFINITE )
     {
         LL_APPEND(npc_mov->npc_mov_type.npc_mov_indefinite->npc_path
-        ,room_to_add);
+                  ,room_to_add);
         npc_mov->npc_mov_type.npc_mov_indefinite->room_time = NULL;
     }
 
@@ -66,13 +66,17 @@ npc_mov_t *npc_mov_new(npc_mov_enum_t mov_type, room_t *room)
 
 
 /* See npc_move.h */
-int npc_mov_free(npc_mov_t *npc_mov) {
+int npc_mov_free(npc_mov_t *npc_mov)
+{
 
     assert(npc_mov != NULL);
 
-    if (npc_mov->mov_type == NPC_MOV_DEFINITE) {
+    if (npc_mov->mov_type == NPC_MOV_DEFINITE)
+    {
         free(npc_mov->npc_mov_type.npc_mov_definite);
-    } else if (npc_mov->mov_type == NPC_MOV_INDEFINITE) {
+    }
+    else if (npc_mov->mov_type == NPC_MOV_INDEFINITE)
+    {
         free(npc_mov->npc_mov_type.npc_mov_indefinite);
     }
 
@@ -98,13 +102,13 @@ int register_npc_room_time(npc_mov_t *npc_mov, room_t *room, int time)
     new_npc_room_time->time = time;
 
     HASH_REPLACE(hh, npc_mov->npc_mov_type.npc_mov_indefinite->room_time,
-                    room_id, strlen(room->room_id),
-                    new_npc_room_time, return_time);
+                 room_id, strlen(room->room_id),
+                 new_npc_room_time, return_time);
 
     free(return_time);
 
     HASH_ADD_KEYPTR(hh, npc_mov->npc_mov_type.npc_mov_indefinite->room_time,
-            room->room_id, strlen(room->room_id), new_npc_room_time);
+                    room->room_id, strlen(room->room_id), new_npc_room_time);
 
     return SUCCESS;
 }
@@ -121,7 +125,7 @@ int extend_path_definite(npc_mov_t *npc_mov, room_t *room_to_add)
     room_to_add2->room = room_to_add;
 
     LL_APPEND(npc_mov->npc_mov_type.npc_mov_definite->npc_path,
-            room_to_add2);
+              room_to_add2);
 
     return SUCCESS;
 }
@@ -137,7 +141,7 @@ int extend_path_indefinite(npc_mov_t *npc_mov, room_t *room_to_add, int time)
     room_to_add2->room = room_to_add;
 
     LL_APPEND(npc_mov->npc_mov_type.npc_mov_indefinite->npc_path,
-         room_to_add2);
+              room_to_add2);
 
     int check = register_npc_room_time(npc_mov, room_to_add, time);
 
@@ -173,12 +177,16 @@ int flip_npc_path_direction(npc_mov_t *npc_mov)
     room_list_t *head = NULL, *reversed_path_head = NULL;
     room_list_t *room_elt, *room_tmp;
 
-    if (npc_mov->mov_type == NPC_MOV_DEFINITE) {
+    if (npc_mov->mov_type == NPC_MOV_DEFINITE)
+    {
         head = npc_mov->npc_mov_type.npc_mov_definite->npc_path;
-    } else if (npc_mov->mov_type == NPC_MOV_INDEFINITE) {
+    }
+    else if (npc_mov->mov_type == NPC_MOV_INDEFINITE)
+    {
         head = npc_mov->npc_mov_type.npc_mov_indefinite->npc_path;
-    } else return FAILURE;
-        
+    }
+    else return FAILURE;
+
     LL_FOREACH_SAFE(head, room_elt, room_tmp)
     {
         room_list_t *append_room = malloc(sizeof(room_list_t));
@@ -198,17 +206,23 @@ int flip_npc_path_direction(npc_mov_t *npc_mov)
         LL_APPEND(head, reappend_room);
     }
 
-    if (npc_mov->mov_type == NPC_MOV_DEFINITE) {
+    if (npc_mov->mov_type == NPC_MOV_DEFINITE)
+    {
         free(npc_mov->npc_mov_type.npc_mov_definite->npc_path);
         npc_mov->npc_mov_type.npc_mov_definite->npc_path = head;
-    } else if (npc_mov->mov_type == NPC_MOV_INDEFINITE) {
+    }
+    else if (npc_mov->mov_type == NPC_MOV_INDEFINITE)
+    {
         free(npc_mov->npc_mov_type.npc_mov_indefinite->npc_path);
         npc_mov->npc_mov_type.npc_mov_indefinite->npc_path = head;
-    } else return FAILURE;
+    }
+    else return FAILURE;
 
-    if (npc_mov->npc_path_direction == NPC_MOV_ORIGINAL) {
+    if (npc_mov->npc_path_direction == NPC_MOV_ORIGINAL)
+    {
         npc_mov->npc_path_direction == NPC_MOV_REVERSED;
-    } else npc_mov->npc_path_direction == NPC_MOV_ORIGINAL;
+    }
+    else npc_mov->npc_path_direction == NPC_MOV_ORIGINAL;
 
     return SUCCESS;
 }
@@ -216,19 +230,21 @@ int flip_npc_path_direction(npc_mov_t *npc_mov)
 /* See npc_move.h */
 int get_npc_num_rooms(npc_mov_t *npc_mov)
 {
-	room_t *curr_room;
+    room_t *curr_room;
     room_list_t *elt;
 
-	int count = 0;
+    int count = 0;
 
-	if (npc_mov->mov_type == NPC_MOV_DEFINITE) {
+    if (npc_mov->mov_type == NPC_MOV_DEFINITE)
+    {
         LL_COUNT(npc_mov->npc_mov_type.npc_mov_definite->npc_path, elt, count);
-	}
-	else if (npc_mov->mov_type == NPC_MOV_INDEFINITE) {
+    }
+    else if (npc_mov->mov_type == NPC_MOV_INDEFINITE)
+    {
         LL_COUNT(npc_mov->npc_mov_type.npc_mov_indefinite->npc_path, elt, count);
-	}
+    }
 
-	return count;
+    return count;
 }
 
 /* See npc_move.h */
@@ -248,16 +264,23 @@ int move_npc_definite(npc_mov_t *npc_mov)
     npc_path_direction_t direction = npc_mov->npc_path_direction;
     unsigned int path_pos = npc_mov->npc_path_pos;
 
-    if (direction == NPC_MOV_ORIGINAL) {
-        if (path_pos != 0) {
-            for (int i = 0; i < path_pos; i++) {
+    if (direction == NPC_MOV_ORIGINAL)
+    {
+        if (path_pos != 0)
+        {
+            for (int i = 0; i < path_pos; i++)
+            {
                 current_room = current_room->next;
             }
         }
-    } else if (direction == NPC_MOV_REVERSED) {
+    }
+    else if (direction == NPC_MOV_REVERSED)
+    {
         unsigned int path_pos_reversed = list_len-1-path_pos;
-        if (path_pos != (list_len - 1)) {
-            for (int i = 0; i < path_pos_reversed; i++) {
+        if (path_pos != (list_len - 1))
+        {
+            for (int i = 0; i < path_pos_reversed; i++)
+            {
                 current_room = current_room->next;
             }
         }
@@ -284,23 +307,30 @@ int move_npc_definite(npc_mov_t *npc_mov)
 int move_npc_indefinite(npc_mov_t *npc_mov)
 {
     assert(npc_mov->mov_type == NPC_MOV_INDEFINITE);
-    
+
     room_list_t *current_room = malloc(sizeof(room_list_t));
     current_room = npc_mov->npc_mov_type.npc_mov_indefinite->npc_path;
     int list_len = get_npc_num_rooms(npc_mov);
     npc_path_direction_t direction = npc_mov->npc_path_direction;
     unsigned int path_pos = npc_mov->npc_path_pos;
 
-    if (direction == NPC_MOV_ORIGINAL) {
-        if (path_pos != 0) {
-            for (int i = 0; i < path_pos; i++) {
+    if (direction == NPC_MOV_ORIGINAL)
+    {
+        if (path_pos != 0)
+        {
+            for (int i = 0; i < path_pos; i++)
+            {
                 current_room = current_room->next;
             }
         }
-    } else if (direction == NPC_MOV_REVERSED) {
+    }
+    else if (direction == NPC_MOV_REVERSED)
+    {
         unsigned int path_pos_reversed = list_len-1-path_pos;
-        if (path_pos != (list_len - 1)) {
-            for (int i = 0; i < path_pos_reversed; i++) {
+        if (path_pos != (list_len - 1))
+        {
+            for (int i = 0; i < path_pos_reversed; i++)
+            {
                 current_room = current_room->next;
             }
         }
@@ -353,30 +383,35 @@ int auto_gen_movement(npc_mov_t *npc_mov, game_t *game)
     int rc = 0;
     int num_rooms, num_rooms_to_add;
 
-	if(npc_mov == NULL || head == NULL) {
-		return FAILURE;
-	}
+    if(npc_mov == NULL || head == NULL)
+    {
+        return FAILURE;
+    }
 
     num_rooms = get_num_rooms(game);
     num_rooms_to_add = (rand() % num_rooms) + 1;
 
-    for (int i = 0; i < num_rooms_to_add; i++) {
+    for (int i = 0; i < num_rooms_to_add; i++)
+    {
         room_t *room_to_add = malloc(sizeof(room_t));
 
         room_to_add = head->room;
         head->room = head->next->room;
-        if(npc_mov->mov_type == NPC_MOV_DEFINITE) {
-        	rc = extend_path_definite(npc_mov, room_to_add);
+        if(npc_mov->mov_type == NPC_MOV_DEFINITE)
+        {
+            rc = extend_path_definite(npc_mov, room_to_add);
         }
-        else if(npc_mov->mov_type == NPC_MOV_INDEFINITE) {
+        else if(npc_mov->mov_type == NPC_MOV_INDEFINITE)
+        {
             int mintime_in_room = 30000; // min time in room in ms, 30000 ms = 30 s
             int maxtime_in_room = 90000; // max time in room in ms, 90000 ms = 90 s
             int time_in_room = (rand() % (maxtime_in_room - mintime_in_room + 1)) + mintime_in_room;
             rc = extend_path_indefinite(npc_mov, room_to_add, time_in_room);
-	      }
+        }
 
-        if(rc == FAILURE) {
-          return rc;
+        if(rc == FAILURE)
+        {
+            return rc;
         }
     }
 
