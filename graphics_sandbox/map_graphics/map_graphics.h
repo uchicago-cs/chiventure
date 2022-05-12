a94267afe481910d2af9b3d80dae3624481284e6
Urunna Anyanwu <urunnaanyanwu@Urunnas-MacBook-Air.local>
2022-05-12 09:57:03 -0500

    Added more bare requirements to map_graphics.h

--- graphics_sandbox/map_graphics/map_graphics.h
+++ graphics_sandbox/map_graphics/map_graphics.h
@@ -1,3 +1,24 @@
+#ifndef MAP_GRAPHICS_H
+#define MAP_GRAPHICS_H
+
 #include "raylib.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+// An include statement for open_world will be added when the map is 
+// integrated to chiventure as a whole
+
+/* This module will allow the designer to display their player's map */
+
+/*
+ * Main running function for MAP Graphics */
+
+void runMAPGraphics();
+
+
+
+
+
+
+
+#endif
