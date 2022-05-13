8f25ee4f9f5f66f9b3c66c57c02d2ffe9da13c0f
Nikola Alan Bilaver <nkbilaver@linux6.cs.uchicago.edu>
2022-05-13 17:12:37 -0500

    Added interfaces for readers

--- include/skilltrees/reader.h
+++ include/skilltrees/reader.h
@@ -16,5 +16,31 @@
  * Returns:
  *  - A pointer to the reader, or NULL if reader cannot be allocated
  */
-reader_effect_new(reader_type_t type, attr_reader_effect_t att_reader,
+reader_effect_t* reader_effect_new(reader_type_t type, attr_reader_effect_t att_reader,
                   stat_reader_effect_t stat_reader);
+
+/*
+ * Initalizes a reader
+ *
+ * Parameters:
+ *  - reader: Pointer to the reader being initalized
+ *  - type: The reader type, ATTRIBUTE or STATISTIC
+ *  - att_reader: Reader of type ATTRIBUTE
+ *  - stat_reader: Reader of type STATISTIC
+ *
+ * Returns:
+ *  - 1 if initalized succesfully, 0 if failure
+ */
+int reader_effect_init(reader_effect_t* reader, reader_type_t type, attr_reader_effect_t att_reader,
+                  stat_reader_effect_t stat_reader);
+
+/*
+ * Frees a reader in the heap.
+ *
+ * Parameters:
+ *  - reader: Pointer to the reader being initalized
+ *
+ * Returns:
+ *  - 1 if freed succesfully, 0 if failure
+ */
+int reader_effect_init(reader_effect_t* reader);
