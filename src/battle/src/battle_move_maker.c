d3c0fdbea852ee248d34ba89548fbfb4e8bb63d9
Nicholas Wen-Rei Lee <nicholaslee@linux2.cs.uchicago.edu>
2022-05-12 19:01:25 -0500

    Move list changes

--- src/battle/src/battle_move_maker.c
+++ src/battle/src/battle_move_maker.c
@@ -29,8 +29,8 @@ int build_moves(combatant_t *c)
         if(!(strncmp(combatant_class_name, tmp->c->name, MAX_NAME_LEN)))
         {
             appened = true;
-            ret_move = move_new(tmp->id, tmp->name, tmp->info, tmp->dmg_type, tmp->stat_mods,
-			    tmp->effects, tmp->count, tmp->sp_cost, tmp->req_item, tmp->damage,
+            ret_move = move_new(tmp->id, tmp->spell, "", PHYSICAL, NULL,
+			    NO_TARGET, SINGLE, 0, NULL, tmp->damage,
 			    tmp->accuracy, tmp->user_mods, tmp->opponent_mods);
             DL_APPEND(c->moves, ret_move);
         }
