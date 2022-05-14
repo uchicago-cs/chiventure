6f866765f7da1890b31ed20e67d3e630ce28959e
周子恺 <zhouzk@uchicago.edu>
2022-05-14 15:07:22 -0500

    quest dispalyable now with test data

--- graphics_sandbox/quest_graphics/quest_graphics.c
+++ graphics_sandbox/quest_graphics/quest_graphics.c
@@ -27,34 +27,42 @@ task_tree_mockup_t* synthesizeTest1() {
     TreeA->rsibling=TreeB;
     TreeA->lmostchild=NULL;
     TreeA->parent=NULL;
+
     TreeB->task_name="B";
     TreeB->rsibling=TreeC;
     TreeB->lmostchild=TreeE;
     TreeB->parent = NULL;
+
     TreeC->task_name= "C";
     TreeC->rsibling=TreeD;
     TreeC->lmostchild=TreeG;
     TreeC->parent=NULL;
+
     TreeD->task_name="D";
     TreeD->rsibling=NULL;
     TreeD->lmostchild=NULL;
     TreeD->parent=NULL;
+
     TreeE->task_name="E";
     TreeE->rsibling=NULL;
     TreeE->lmostchild=TreeF;
     TreeE->parent=TreeB;
+
     TreeF->task_name="F";
     TreeF->rsibling=TreeH;
     TreeF->lmostchild=NULL;
     TreeF->parent=TreeE;
+
     TreeG->task_name="G";
     TreeG->rsibling=NULL;
     TreeG->lmostchild=NULL;
     TreeG->parent=TreeC;
+
     TreeH->task_name="H";
     TreeH->rsibling=TreeI;
     TreeH->lmostchild=NULL;
     TreeH->parent=TreeE;
+
     TreeI->task_name="I";
     TreeI->rsibling=NULL;
     TreeI->lmostchild=NULL;
@@ -67,63 +75,17 @@ task_tree_mockup_t* synthesizeTest1() {
 pos_in_tree_t* getTaskPos(task_tree_mockup_t* taskTree, char* wanted_task_name) {
 
     pos_in_tree_t *res = (pos_in_tree_t *)malloc(sizeof(pos_in_tree_t));
-    res->movement_tracker = 5;
-    res->totalsteps = 4;
+    res->movement_tracker = 1;
+    res->totalsteps = 3;
 
     return res;
 }
 
 
-void drawTaskTree(task_tree_mockup_t* taskTree, char* current_task_name, float widthSegment, 
-                  float heightSegment,Vector2 startPos, float squareside) 
-{
-    pos_in_tree_t* pos_data = getTaskPos(taskTree, current_task_name);
-    task_tree_mockup_t* current_taskTree = taskTree;
-    char stepsremaining = pos_data->totalsteps;
-    int tracker = pos_data->movement_tracker;
-    float x = startPos.x;
-    float y = startPos.y;
-    float xtmp, ytmp;
-
-    DrawRectangleLines(x,y,squareside,squareside,BLACK);
-    DrawText(current_taskTree->task_name,xtmp+(squareside*0.1),y+(squareside*0.5),20,BLACK);
-
-    while(stepsremaining > 0) {
-        int thismove = tracker & 1;
-        if (thismove == 0) {
-            float xloop = x;
-            float yloop = y;
-
-            //Draw the whole horizontal line
-            //Draw the task names
-            current_taskTree = current_taskTree->lmostchild;
-            ytmp = y;
-            y += heightSegment;
-            DrawLine(x, ytmp, x, y, BLACK);
-            DrawRectangleLines(xtmp-(squareside/2),y,squareside,squareside,BLACK);
-            DrawText(current_taskTree->task_name,xtmp-(squareside*0.4),y+(squareside*0.5),20,BLACK);
-            y += squareside;
-
-        }
-
-        if (thismove == 1) {
-            current_taskTree = current_taskTree->rsibling;
-            xtmp = x;
-            x += widthSegment;
-            DrawLine(xtmp, y, x, y, BLACK);
-            DrawRectangleLines(xtmp,y+(squareside/2),squareside,squareside,BLACK);
-            DrawText(current_taskTree->task_name,xtmp+(squareside*0.1),y,20,BLACK);
-            x += squareside;
-        }  
-        stepsremaining --;
-        tracker = tracker>>1;
-    }
-
-}
-
 
 void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector2 windowsize,          
-                        char*current_taskname)
+                        Vector2 segmentDimension, char*current_task_name, float squareside,
+                        Vector2 drawStartPos)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -133,8 +95,8 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
     InitWindow(screenWidth, screenHeight, "Quest");
     SetWindowPosition(windowpos.x, windowpos.y);
 
-    double widthSegment = (screenWidth - 100)/getTreeMaxWidth(taskTree) - 30;
-    double heightSegment = (screenWidth - 100)/getTreeMaxHeight(taskTree) - 20;
+    double widthSegment = segmentDimension.x;
+    double heightSegment = segmentDimension.y;
 
     //Currently Hardcoded, need to be flexible in the future as
     // as we read in from the real quest struct;
@@ -157,6 +119,15 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
         //xp = getExperience();
         xp = 400;
 
+        pos_in_tree_t* pos_data = getTaskPos(taskTree, current_task_name);
+        task_tree_mockup_t* current_taskTree = taskTree;
+        char stepstotal = pos_data->totalsteps;
+        int tracker = pos_data->movement_tracker;
+        float x = drawStartPos.x;
+        float y = drawStartPos.y;
+        float xtmp, ytmp;
+        int thismove;
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
@@ -169,7 +140,45 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
 
             DrawText(TextFormat("xp: %02i", xp), 20, 80, 20, BLUE);
 
-            drawTaskTree(taskTree,"F",20,20,(Vector2){50,50},10);
+            DrawRectangleLines(x,(y-squareside/2),squareside,squareside,BLACK);
+            DrawText(current_taskTree->task_name,x+(squareside*0.3),y-squareside*0.3,20,BLACK);
+
+            if ((tracker&1)==0) {
+                y += squareside/2;
+            } else {
+                x += squareside/2;
+            }
+
+        for(int i = 0; i < stepstotal; i++) {
+            thismove = tracker & 1;
+            if (thismove == 0) {
+                if((current_taskTree = current_taskTree->lmostchild)) {
+                    y += squareside/2;
+                    ytmp = y;
+                    y += heightSegment;
+                    DrawLine(x, ytmp, x, y, BLACK);
+                    DrawRectangleLines(x-(squareside/2),y,squareside,squareside,BLACK);
+                    DrawText(current_taskTree->task_name,(x-0.2*squareside),(y+0.2*squareside),20,BLACK);
+                    y += squareside/2;
+                } else {
+                    break;
+                }
+            } else {
+                if((current_taskTree = current_taskTree->rsibling)) {
+                    x += squareside/2;
+                    xtmp = x;
+                    x += widthSegment;
+                    DrawLine(xtmp, y, x, y, BLACK);
+                    DrawRectangleLines(x,y-(squareside/2),squareside,squareside,BLACK);
+                    DrawText(current_taskTree->task_name,x+(squareside*0.3),(y-squareside*0.2),20,BLACK);
+                    x += squareside/2;
+                } else {
+                    break;
+                }
+            }
+            tracker >>= 1;
+        }
+
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -184,9 +193,10 @@ void runTaskTreeGraphics(task_tree_mockup_t* taskTree, Vector2 windowpos, Vector
 
 
 int main() {
-    task_tree_mockup_t* taskTree = synthesizeTest1();
-    runTaskTreeGraphics(taskTree, (Vector2){50,80},(Vector2){600,400},"F");
-    task_tree_mockup_t* next;
+    task_tree_mockup_t *TreeA = synthesizeTest1();
+
+    runTaskTreeGraphics(TreeA, (Vector2){80,80},(Vector2){600,400},
+                        (Vector2){50,50},"F",30,(Vector2){150,150});
     //Free
     return 0;
 }
