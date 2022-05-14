dc29470534f9e14288ae1e7b2b38d9b27e75b521
Rohan Reddy Deme <rohandeme@linux2.cs.uchicago.edu>
2022-05-14 08:49:54 -0500

    Added functions to header and c files for load openworld

--- include/wdl/load_openworld.h
+++ include/wdl/load_openworld.h
@@ -10,6 +10,78 @@
 #include "load_condition.h"
 #include "openworld/gen_structs.h"
 
+/* load_itemspecs
+ * takes in wdl object and load loads itemspecs into itemspec struct to be used by openworld 
+*module
+ *
+ * parameters:
+ * - dialogue_obj: the itemspec object
+ * - itemspecs: the itemspec struct it is being loaded into
+ *
+ * returns;
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_itemspecs( obj_t  *doc,  itemspec_t* itemspec);
 
+
+/* load_itemspecs
+ * takes in wdl object and load loads itemspecs into itemspec struct to be used by openworld 
+*module
+ *
+ * parameters:
+ * - dialogue_obj: the itemspec object
+ * - itemspecs: the itemspec struct it is being loaded into
+ *
+ * returns;
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_roomspec( obj_t  *doc,  roomspec_t* roomspec );
+
+
+/* load_itemspecs
+ * takes in wdl object and load loads itemspecs into itemspec struct to be used by openworld 
+*module
+ *
+ * parameters:
+ * - dialogue_obj: the itemspec object
+ * - itemspecs: the itemspec struct it is being loaded into
+ *
+ * returns;
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_gen_context( obj_t  *doc,  gencontext_t* gencontext );
+
+
+/* load_itemspecs
+ * takes in wdl object and load loads itemspecs into itemspec struct to be used by openworld 
+*module
+ *
+ * parameters:
+ * - dialogue_obj: the itemspec object
+ * - itemspecs: the itemspec struct it is being loaded into
+ *
+ * returns;
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_roomlevel ( obj_t  *doc,  roomlevel_t* roomlevel );
+
+
+/* load_itemspecs
+ * takes in wdl object and load loads itemspecs into itemspec struct to be used by openworld 
+*module
+ *
+ * parameters:
+ * - dialogue_obj: the itemspec object
+ * - itemspecs: the itemspec struct it is being loaded into
+ *
+ * returns;
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_levelspecs( obj_t  *doc,  levelspec_t* levelspec);
 
 #endif /* INCLUDE_LOAD_OPENWORLD_H */
--- src/wdl/src/load_openworld.c
+++ src/wdl/src/load_openworld.c
@@ -1,2 +1,50 @@
 #include "wdl/load_openworld.h"
 
+/* load_itemspecs
+ * takes in wdl object and load loads itemspecs into itemspec struct to be used by openworld 
+*module
+ *
+ * parameters:
+ * - dialogue_obj: the itemspec object
+ * - itemspecs: the itemspec struct it is being loaded into
+ *
+ * returns;
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_itemspecs( obj_t  *doc,  itemspec_t* itemspec)
+{
+    return 0;
+}
+
+
+/*takes in wdl format and populates room_specs, returns success if successful*/
+int load_roomspec( obj_t  *doc,  roomspec_t* roomspec )
+{
+    return 0;
+}
+
+
+/*takes in wdl format and loads gencontext into gen context struct.*/
+int load_gen_context( obj_t  *doc,  gencontext_t* gencontext )
+{
+    return 0;
+}
+
+
+
+/*takes in wdl format and loads room_level into room_level struct.*/
+int load_roomlevel ( obj_t  *doc,  roomlevel_t* roomlevel )
+{
+    return 0;
+}
+
+
+
+int load_levelspecs( obj_t  *doc,  levelspec_t* levelspec)
+{
+    return 0;
+}
+
+
+
