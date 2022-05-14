d18dbc3554963e9e3b58e0c01e0f59474e0a2c40
Rohan Reddy Deme <rohandeme@linux2.cs.uchicago.edu>
2022-05-13 22:01:01 -0500

    Updating files

---
+++ include/wdl/load_openworld.h
@@ -0,0 +1,15 @@
+/*
+ * Header file for loading NPC objects into game struct
+ */
+
+#ifndef INCLUDE_LOAD_OPENWORLD_H
+#define INCLUDE_LOAD_OPENWORLD_H
+
+#include "wdl_common.h"
+#include "validate.h"
+#include "load_condition.h"
+#include "openworld/gen_structs.h"
+
+
+
+#endif /* INCLUDE_LOAD_OPENWORLD_H */
