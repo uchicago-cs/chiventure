3c2e672709782993b221e1ed04952893ea0e53ea
周子恺 <zhouzk@uchicago.edu>
2022-05-14 21:13:19 -0500

    added input_box_graphics

--- graphics_sandbox/input_box/input_box.h
+++ graphics_sandbox/input_box/input_box.h
@@ -1,7 +1,7 @@
 #ifndef INPUT_BOX_H
 #define INPUT_BOX_H
 
-
+#include "raylib.h"
 /*
  * Runs the input box window and really enables data input
  *
@@ -14,5 +14,16 @@
  *      Nothing
  */
 void run_input_box(Vector2 windowDimension, Vector2 windowPosition, char* input_buffer);
+
+
+typedef struct input_box_graphics{
+    Vector2 WindowPos;
+    Vector2 WindowSize;
+    unsigned MaxCharNum;
+    Color BoxColor;
+    Color TestColor;
+    Color AnnotationColor;
+    const char* Annotation;
+} input_box_graphics_t;
 
 #endif
