b5de3a08a3442ae184c601b2e52eb152100bfee5
Urunna Anyanwu <urunnaanyanwu@Urunnas-MacBook-Air.local>
2022-05-15 10:15:32 -0500

    implementing inventory_graphics.h function

---
+++ graphics_sandbox/inventory_graphics/inventory_graphics.c
@@ -0,0 +1,10 @@
+#include "inventory_graphics.h"
+
+player_inventory_t *new_player_inventory(graphics_t *graphics)
+{
+    player_inventory_t *inventory = (player_inventory_t)malloc(sizeof(player_inventory_t);
+
+    inventory->display->rows = graphics->inventory->rows;
+    inventory->display->columns = graphics->inventory->columns;
+    inventory->display->color = graphics->inventory->color;
+}
