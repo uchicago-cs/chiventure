6dc69c881db8c657cb6788d3580bba4458cb73e2
Emilio William Levins Page <emiliolevinspage@linux4.cs.uchicago.edu>
2022-05-16 00:03:58 -0500

    included new file

--- include/battle/battle_flow.h
+++ include/battle/battle_flow.h
@@ -15,6 +15,7 @@
 #include "npc/npc.h"
 #include "npc/npc_battle.h"
 #include "ui/print_functions.h"
+#include "common/ctx.h"
 
 /*
  * Starts the battle, sets up battle struct including any associated structs
