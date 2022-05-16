7a5a080a992cd76060f8708a5cbb5fdaab821aa1
Jake Wellington <wellingtj@linux4.cs.uchicago.edu>
2022-05-16 14:09:56 -0500

    #1402 I have implemented a check so that the phrase Multiclass of isnt repeated multiple times in a multicalss shortdesc

--- src/playerclass/src/multiclass.c
+++ src/playerclass/src/multiclass.c
@@ -33,7 +33,9 @@ char* multiclass_shortdesc(class_t* base_class, class_t* second_class, int *succ
 
     char* new_shortdesc = (char*) malloc(MAX_SHORT_DESC_LEN + 1);
     *succ = SUCCESS;
-    strncat(new_shortdesc, "Multiclass of ", 15);
+    if (strstr(base_class->name, "Multiclass of " == NULL)){
+      strncat(new_shortdesc, "Multiclass of ", 15);  
+    }
     strncat(new_shortdesc, base_class->name, strlen(base_class->name));
     if (num_multiclass == 2){
         strncat(new_shortdesc, ", ", 3);
