731de4a379d8bde4b4a31f7e3026e72b7b0aeebb
Miles Boustani Gardner <milesgardner@linux1.cs.uchicago.edu>
2022-05-15 21:01:52 -0500

    Adding first quote test

--- tests/cli/test_parser.c
+++ tests/cli/test_parser.c
@@ -217,5 +217,5 @@ Test(parse_r, two_words_quote)
     char str[] = "GO \"South\"";
     char **words = parse(str);
     cr_assert_str_eq(words[0],"GO", "parse() did not create first token");
-    cr_assert_str_eq(words[1],"\"South\"", "parse() did not create first token");
+    cr_assert_str_eq(words[1],"South", "parse() did not create first token");
 }
