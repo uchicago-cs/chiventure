fc528597fabf46bd7ca5c97fb691a8554b637ed9
Lukas Heinrich Grunzke <lukasg@linux4.cs.uchicago.edu>
2022-05-16 14:52:18 -0500

    completed valid_multi_room3

--- tests/openworld/test_autogenerate.c
+++ tests/openworld/test_autogenerate.c
@@ -504,15 +504,19 @@ Test(autogenerate, valid_multi_room2)
                  "multi_room_generate() returned FAILURE instead of SUCCESS");
 }
 
-/*Test(autogenerate, valid_multi_room2)
+/* Checks that multi_room_generate successfully generates/adds rooms from a
+* context (gencontext_t) struct's specgraph field when multiple (3) rooms are requested */
+Test(autogenerate, valid_multi_room3)
 {
-    rspec_hash_t *hash =make_default_room("school", NULL, NULL);
-    specgraph_t *spec = NULL;
-    specgraph_from_hash(&spec, hash);
+    roomspec_t *roomspec0 = make_default_room("school",NULL,NULL);
+    roomspec_t **roomspecs = (roomspec_t**)malloc(sizeof(roomspec_t*)*3);
+    roomspecs[0] = roomspec0;
+    int **edges = (int**)malloc(sizeof(int*));
+    specgraph_t *specgraph = specgraph_new(1,roomspecs,edges);
 
-    roomspec_t *sample1 = random_room_lookup(spec);
+    roomspec_t *sample1 = random_room_lookup(specgraph);
     room_t *sample_room1 = roomspec_to_room(sample1);
-    roomspec_t *sample2 = random_room_lookup(spec);
+    roomspec_t *sample2 = random_room_lookup(specgraph);
     room_t *sample_room2 = roomspec_to_room(sample2);
 
     // Path to sample_room2
@@ -527,47 +531,35 @@ Test(autogenerate, valid_multi_room2)
     cr_assert_eq(SUCCESS, add_room_to_game(g, sample_room2), "Could not add room sample_room2 to game g");
 
     item_t *sample_item = item_new("item_id", "short_desc", "long_desc");
-    item_t *sample_item2 = item_new("item_id2", "short_desc", "long_desc");
-    item_t *sample_item3 = item_new("item_id3", "short_desc", "long_desc");
 
     cr_assert_eq(SUCCESS, add_item_to_room(sample_room1, sample_item), "Could not add item to room");
 
-    roomspec_t *sample_roomspec = random_room_lookup(spec);
+    roomspec_t *sample_roomspec = random_room_lookup(specgraph);
     cr_assert_not_null(sample_roomspec, "sample_roomspec should not be NULL");
 
-    // 1 roomspec case
     specgraph_t *sample_specgraph = specgraph_new(sample_roomspec);
     cr_assert_not_null(sample_specgraph, "sample_specgraph should not be NULL");
 
     gencontext_t *sample_gencontext = gencontext_new(path_to_room2, 5, 1, sample_specgraph);
     cr_assert_not_null(sample_gencontext, "sample_gencontext should not be NULL");
 
+    roomspec_t *sample_roomspec2 = random_room_lookup(specgraph);
+    cr_assert_not_null(sample_roomspec, "sample_roomspec should not be NULL");
+
+    roomspec_t *sample_roomspec3 = random_room_lookup(specgraph);
+    cr_assert_not_null(sample_roomspec, "sample_roomspec should not be NULL");
+
+    // 3 roomspec case
+    roomspecs[1] = sample_roomspec2;
+    cr_assert_not_null(roomspecs[1],"could not allocate sample_roomspec2 to roomspecs");
+    roomspecs[2] = sample_roomspec3;
+    cr_assert_not_null(roomspecs[2],"could not allocate sample_roomspec3 to roomspecs");
+
     // Ensure game->curr_room does not have paths
     g->curr_room = sample_room1;
+    cr_assert_eq(SUCCESS, multi_room_generate(g, sample_gencontext, "school", 3));
+}
 
-    roomspec_t *sample_roomspec2 = random_room_lookup(spec);
-    cr_assert_not_null(sample_roomspec2, "sample_roomspec2 should not be NULL");
-
-    roomspec_t *sample_roomspec3 = random_room_lookup(spec);
-    cr_assert_not_null(sample_roomspec3, "sample_roomspec3 should not be NULL");
-
-    specgraph_t *mid = specgraph_new(sample_roomspec2);
-    cr_assert_not_null(mid, "Could not create new specgraph");
-    specgraph_t *tail = specgraph_new(sample_roomspec3);
-    cr_assert_not_null(tail, "Could not create new specgraph");
-
-    // Doubly linked
-    specgraph_t *head = NULL;
-    DL_APPEND(head, sample_gencontext->specgraph);
-    DL_APPEND(sample_gencontext->specgraph, mid);
-    DL_APPEND(sample_gencontext->specgraph, tail);
-
-    cr_assert_eq(SUCCESS, multi_room_generate(g, sample_gencontext, "school", 2),
-                 "multi_room_generate() returned FAILURE instead of SUCCESS");
-}*/
-
-/* Checks that multi_room_generate successfully generates/adds rooms from a
-* context (gencontext_t) struct's specgraph field when multiple (3) rooms are requested */
 /*Test(autogenerate, valid_multi_room3)
 {
     rspec_hash_t *hash = make_default_room("school", NULL, NULL);
