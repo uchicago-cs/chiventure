c360a6209096391b21e730ade4b1c06faa4dc1a5
UChicago CS Student <jastarbuck02@gmail.com>
2022-05-16 18:42:09 -0500

    added some comments

--- src/sound/sandbox/test-espeak.c
+++ src/sound/sandbox/test-espeak.c
@@ -1,5 +1,11 @@
 #include <espeak-ng/speak_lib.h>
  
+// in order to run this, in the chiventure/src/sound/sandbox directory run 
+// gcc test-espeak.c -lespeak-ng -o test-espeak
+// especially anytime after a change is made and then use
+// ./test-espeak
+// in order to hear the text
+
  espeak_AUDIO_OUTPUT output = AUDIO_OUTPUT_SYNCH_PLAYBACK;
  char *path = NULL;
  void* user_data;
@@ -7,7 +13,7 @@
  
  int main(int argc, char* argv[]) {
    char voicename[] = {"English"}; // Set voice by its name
-   char text[] = {"Hello Parker!"};
+   char text[] = {"Hello Parker!"}; // text to be output by speaker, if changed recompile as seen above
    int buflength = 500, options = 0;
    unsigned int position = 0, position_type = 0, end_position = 0, flags = espeakCHARS_AUTO;
    espeak_Initialize(output, buflength, path, options);
