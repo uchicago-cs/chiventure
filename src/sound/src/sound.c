b035a50de0540ba6be6bdf8a9f3f0787d03acf6c
UChicago CS Student <jastarbuck02@gmail.com>
2022-05-16 04:03:55 -0500

    created load free and play sound functions

--- src/sound/src/sound.c
+++ src/sound/src/sound.c
@@ -17,5 +17,24 @@ int sound_free(sound_t *sound)
 /* See sound.h for details */
 int play_sound(sound_t *sound, int delay)
 {
+    if (sound == NULL) {
+        fprintf(stderr, "no sound objects present\n");
+        exit(1);
+    }else if (sound->type == BACKGROUND) {
+        SDL_Delay(delay);
+
+        Mix_Music *backgroundSound = Mix_LoadMUS(sound->name);
+        Mix_Chunk *soundEffect = Mix_LoadWAV(sound->name);
+        if (!soundEffect) {
+            printf("MixPlayChannel: %s\n", Mix_GetError()); //Gets error if an audio cannot be loaded properly
+        }
+
+        Mix_PlayMusic(backgroundSound, -1);
+        Mix_PlayChannel(-1, soundEffect, 0)
+    } 
+    // else it is SOUND_EFFECT
+    else {
+
+    }
     return 0;
 }
