919a8761347bdcc1e5cee52e7e30a70f20235e38
Emilio William Levins Page <emiliolevinspage@linux4.cs.uchicago.edu>
2022-05-16 00:19:38 -0500

    maybe fixed undefined ref

--- src/battle/src/battle_flow.c
+++ src/battle/src/battle_flow.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "battle/battle_flow.h"
 #include "battle/battle_print.h"
+#include "ui/print_functions.h"
 
 /* see battle_flow.h */
 int start_battle(battle_ctx_t *ctx, npc_t *npc_enemy, environment_t env)
