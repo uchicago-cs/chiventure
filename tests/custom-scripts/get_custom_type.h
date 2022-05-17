817548f498563f2814f69913ff321e3544c0a130
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-16 19:27:00 -0500

    updated dependency for test file

---
+++ tests/custom-scripts/get_custom_type.h
@@ -0,0 +1,16 @@
+#ifndef _GET_CUSTOM_TYPE_H_
+#define _GET_CUSTOM_TYPE_H_
+
+#include "lua_custom_type.h"
+
+/**
+ * bool_t_get() returns a bool from an object_t struct
+ * Parameters:
+ * - ot: an obj_t
+ * - d: operating data type
+ * Returns:
+ * - bool from ot
+ */
+data_t arg_t_get(object_t *ot);
+
+#endif
