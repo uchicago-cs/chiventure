39a1675c458d7dbd9ec144e970e8b57b6aa90be7
Faruk K Badur <fkb@linux1.cs.uchicago.edu>
2022-05-16 19:42:53 -0500

    renamed  to  in  to avoid compilation error and distinguish this function from the similar one present in test_game.c

--- tests/cli/test_operation.c
+++ tests/cli/test_operation.c
@@ -15,7 +15,7 @@
  *
  * Returns: a chiventure context
  */
-chiventure_ctx_t *create_sample_ctx()
+chiventure_ctx_t *maketest_ctx()
 {
     game_t *game = game_new("Welcome to Chiventure!");
     room_t *room1 = room_new("room1", "This is room 1", "Verily, this is the first room.");
@@ -28,9 +28,9 @@ chiventure_ctx_t *create_sample_ctx()
     /* Create context */
     chiventure_ctx_t *ctx = chiventure_ctx_new(game);
 
-    room_t *room1 = find_room_from_game(ctx->game, "room1");
+    room1 = find_room_from_game(ctx->game, "room1");
     cr_assert_not_null(room1, "Could not fetch room1 from game");
-    room_t *room2 = find_room_from_game(ctx->game, "room2");
+    room2 = find_room_from_game(ctx->game, "room2");
     cr_assert_not_null(room2, "Could not fetch room2 from game");
     /* Check that the game is in the initial room */
     cr_assert_eq(ctx->game->curr_room, room1);
@@ -53,8 +53,8 @@ chiventure_ctx_t *create_sample_ctx()
 Test(operation, kind4_game_null)
 {
     int quit;
-    chiventure_ctx_t *ctx = create_sample_ctx();
-    char *tokens[TOKEN_LIST_SIZE] = (char**)calloc(TOKEN_LIST_SIZE,sizeof(char*));
+    chiventure_ctx_t *ctx = maketest_ctx();
+    char **tokens = (char**)calloc(TOKEN_LIST_SIZE,sizeof(char*));
     tokens[0] = "VIEW";
     tokens[1] = "STATS";
 
