d1c58ce0a781c2c173c5d38303a5a7d1b9ff6f6d
William Hu <williamhu2025@uchicago.edu>
2022-05-16 20:58:37 -0500

    Changing edges yet again to dynamic allocation in both tests

--- tests/openworld/test_structs.c
+++ tests/openworld/test_structs.c
@@ -112,14 +112,12 @@ Test(specgraph, new)
     cr_assert_not_null(spec3, "failed to create new roomspec_t\n");
     roomspec_t *roomspecs[3]={spec1, spec2, spec3};
 
-    int *edges;
-    edges={
-        {5, 4, 5}, 
-        {0, 5, 3}, 
-        {4, 3, 5}
-};
+    int **edges=(int**)malloc(3, sizeof(int*));
+    edges[0]={5, 4, 5};
+    edges[1]={0, 5, 3};
+    edges[2]={4, 3, 5};
 
-    specgraph_t *specgraph = specgraph_new(3, roomspecs, &edges);
+    specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
 
     cr_assert_not_null(specgraph, "failed to create new specgraph_t\n");
     cr_assert_eq(specgraph->num_roomspecs, 3, "specgraph_new() failed gathering num_roomspecs");
