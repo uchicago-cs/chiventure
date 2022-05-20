b7e9136140c53a5ea55b46daaa9cab2b1a4c4193
周子恺 <zhouzk@uchicago.edu>
2022-05-20 10:52:55 -0500

    rewrote new and free

--- graphics_sandbox/scene_graphics/scene_graphics.c
+++ graphics_sandbox/scene_graphics/scene_graphics.c
@@ -1,7 +1,29 @@
 #include "scene_graphics.h"
 #include "raylib.h"
 
+/* See scene_graphics.h*/
+scene_graphics_t* new_scene_graphics(char* SceneName, char* ImagePath, Vector2 WindowPos,
+                       Vector2 WindowSize, Vector2 SceneSize, Vector2 PlayerPosition)
+{
+    scene_graphics_t* s;
+    s = malloc(sizeof(scene_graphics_t));
+    s->SceneName=SceneName;
+    s->ImagePath=ImagePath;
+    s->WindowPos=WindowPos;
+    s->WindowSize=WindowSize;
+    s->SceneSize=SceneSize;
+    s->PlayerPosition=PlayerPosition;
 
+    return s;
+    
+}
+
+/* See scene_graphics.h*/
+int free_scene_graphics(scene_graphics_t* SceneGraphics)
+{
+    free(SceneGraphics);
+    return 1;
+}
 
 void runSceneGraphics(scene_graphics_t* scene_graphics)
 {
@@ -72,3 +94,5 @@ int main(void) {
 
     return 0;
 }
+
+
