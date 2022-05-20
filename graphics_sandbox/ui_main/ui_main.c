b93994e052c453981de45157876fdac1c6b2ef56
周子恺 <zhouzk@uchicago.edu>
2022-05-20 10:33:24 -0500

    still working on ui_main, but need to switch to another branch

---
+++ graphics_sandbox/ui_main/ui_main.c
@@ -0,0 +1,17 @@
+#include "ui_main.h"
+
+void runAllGraphics(ui_linkedlist_t* ui_info) {
+
+    while((ui_info = ui_info->next)) {
+
+        switch(ui_info->WindowType)
+    }
+
+}
+
+
+int main(){
+
+
+
+}
