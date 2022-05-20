7c4553c48aa8732c26213f4337c4b96bae7fd04c
周子恺 <zhouzk@uchicago.edu>
2022-05-20 11:15:38 -0500

    first modification on Makefile and test

--- graphics_sandbox/scene_graphics/test_scene_graphics.c
+++ graphics_sandbox/scene_graphics/test_scene_graphics.c
@@ -9,14 +9,30 @@ Test(scene_graphics, new)
 
     scene_graphics_t* s;
 
-    s = new_scene_graphics("test scene","../nsfw.png",NULL);
+    s = new_scene_graphics("test scene","../nsfw.png", (Vector2){100,150},
+                            (Vector2){500,550}, (Vector2){400,450}, (Vector2){300,320});
 
     cr_assert_not_null(s,"new_scene_graphics() failed");
 
-    cr_assert_eq(s->title, "test scene", "new_scene_graphics() didn't set title");
+    cr_assert_str_eq(s->SceneName, "test scene", "new_scene_graphics() didn't set title");
 
-    cr_assert_eq(s->image_path,"../nsfw.png","new_scene_graphics() didn't set image path");
+    cr_assert_str_eq(s->ImagePath,"../nsfw.png","new_scene_graphics() didn't set image path");
 
+    cr_assert_eq(s->WindowPos.x,100,"new_scene_graphics() didn't set WindowPos.x");
+
+    cr_assert_eq(s->WindowPos.y,150,"new_scene_graphics() didn't set WindowPos.y");
+
+    cr_assert_eq(s->WindowSize.x,500,"new_scene_graphics() didn't set WindowSize.x");
+
+    cr_assert_eq(s->WindowSize.y,550,"new_scene_graphics() didn't set WindowSize.y");
+
+    cr_assert_eq(s->SceneSize.x,400,"new_scene_graphics() didn't set SceneSize.x");
+
+    cr_assert_eq(s->SceneSize.y,450,"new_scene_graphics() didn't set SceneSize.y");
+
+    cr_assert_eq(s->PlayerPosition.x,300,"new_scene_graphics() didn't set PlayerPosition.x");
+
+    cr_assert_eq(s->PlayerPosition.y,320,"new_scene_graphics() didn't set PlayerPosition.y");
 }
 
 Test(scene_graphics, free)
@@ -24,7 +40,8 @@ Test(scene_graphics, free)
     scene_graphics_t *s;
     int rc;
     
-    s = new_scene_graphics("test scene","../nsfw.png",NULL);
+    s = new_scene_graphics("test scene","../nsfw.png", (Vector2){100,100},
+                            (Vector2){500,500}, (Vector2){400,400}, (Vector2){300,300});
 
     cr_assert_not_null(s,"new_scene_graphics() failed");
 
