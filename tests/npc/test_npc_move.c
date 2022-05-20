9c1f59a707284f799592556bc6660607fec9622c
William Miller III <102555942+bvvill@users.noreply.github.com>
2022-05-20 11:28:34 -0500

    last one?

--- tests/npc/test_npc_move.c
+++ tests/npc/test_npc_move.c
@@ -379,10 +379,7 @@ Test(npc_mov, auto_gen_movement_indefinite)
     cr_assert_eq(cnt, num_rooms_in_npc,
                  "room_count returns %d, but there should be %d rooms in npc_mov",
                  cnt, num_rooms_in_npc);
-
-    cr_assert_eq(npc_path_dll_free
-                 (npc_mov->path),
-                 SUCCESS, "delete npc_path_dll failed");
+    cr_assert_eq(npc_mov_free(npc_mov), SUCCESS, "npc_mov_free() failed");
 
     game_free(game);
 }
@@ -394,10 +391,9 @@ Test(npc_mov, check_if_npc_mov_indefinite_needs_moved)
     int rc;
     bool rb;
 
-    npc_mov_t *npc_mov = npc_mov_new(NPC_MOV_INDEFINITE, room1->room_id);
-    rc = register_npc_room_time(npc_mov, room1->room_id, 500);
-    cr_assert_eq(rc, SUCCESS, "register_npc_room_time() failed");
-    rc = extend_path_indefinite(npc_mov, room2->room_id, 1000);
+    npc_mov_t *npc_mov = npc_mov_new(NPC_MOV_INDEFINITE, room1->room_id, 0.5);
+
+    rc = extend_path_indefinite(npc_mov, room2->room_id, 1);
     cr_assert_eq(rc, SUCCESS, "extend_path_indefinite() failed");
 
     rb = check_if_npc_mov_indefinite_needs_moved(npc_mov);
