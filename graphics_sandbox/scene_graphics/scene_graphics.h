0fb1ddb88a0557fa31cd38e26ebd06087f4b6612
周子恺 <zhouzk@uchicago.edu>
2022-05-20 10:39:16 -0500

    working on a reverted version of scene_graphics

---
+++ graphics_sandbox/scene_graphics/scene_graphics.h
@@ -0,0 +1,18 @@
+#include "raylib.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define SCENE_CONTEXT_ID 0;
+
+
+typedef struct scene_graphics scene_graphics_t;
+
+struct scene_graphics {
+    char* SceneName;
+    char* ImagePath;
+    Vector2 WindowPos;
+    Vector2 WindowSize;
+    Vector2 SceneSize;
+    Vector2 PlayerPosition;
+};
