08696d5aa9c71914e2335cced0d05a0ad02eaff7
Harrison S Dong <harrisond@linux1.cs.uchicago.edu>
2022-05-21 14:29:49 -0500

    error checking added to do_self_action

--- src/action_management/src/actionmanagement.c
+++ src/action_management/src/actionmanagement.c
@@ -378,8 +378,11 @@ int do_self_action(chiventure_ctx_t *c, action_type_t *a,
             // retrieve skill tree from the player
             // TO BE IMPLEMENTED
         } else {
-            // TO BE IMPLEMENTED     
+            sprintf(string, "%s cannot be viewed", target);
         }
+    }
+    else {
+        sprintf(string, "No such %s action available", a->c_name);
     }
     *ret_string = string;
     return SUCCESS;
