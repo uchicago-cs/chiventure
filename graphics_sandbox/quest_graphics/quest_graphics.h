8eedca48a6be071dbf6e51791b57ae2a7a2c7d8b
周子恺 <zhouzk@uchicago.edu>
2022-05-20 20:08:00 -0500

    added documentary to structs

--- graphics_sandbox/quest_graphics/quest_graphics.h
+++ graphics_sandbox/quest_graphics/quest_graphics.h
@@ -6,6 +6,15 @@
 
 typedef struct task_tree_mockup task_tree_mockup_t;
 
+
+/*
+ *  This is a mockup struct that has
+ *  identical fields as the real
+ *  task_tree struct
+ * 
+ *  We came up with this mockup struct
+ *  to route around complex dependencies
+ */
 struct task_tree_mockup {
     const char* task_name;
     task_tree_mockup_t *parent;
@@ -15,6 +24,12 @@ struct task_tree_mockup {
 };
 
 
+/*
+ * This is a struct used to store
+ * a priori knowledge of navigating
+ * to a specific struct for drawing
+ * branchs of a binar tree
+ */
 typedef struct pos_in_tree {
     /*
      * Every bit denotes a movement,
@@ -29,14 +44,25 @@ typedef struct pos_in_tree {
 } pos_in_tree_t;
 
 
+/* This is the top-most-level struct
+ * that stores everything (for now)
+ * needed to dispaly quests*/
 typedef struct quest_graphics{
+    /* The upperleft corner position of the window*/
     Vector2 WindowPos;
+    /* The size of the window, width is x field, height is y field*/
     Vector2 WindowSize;
+    /* The length of the segment, horizontal segment is x field, vertical is y*/
     Vector2 SegmentDimension;
+    /* The starting position to draw the task tree, relative to the window's upper-left corner*/
     Vector2 DrawStartPosition;
+    /* The binary task tree that represents the relationship between tasks*/
     task_tree_mockup_t *TaskTree;
+    /* The name/id of the current Task*/
     char* CurrentTaskName;
+    /* If true, we will print out every horizontal siblings of the parents of the current task*/
     bool ShowRemainingHorizontal;
+    /* The length of the side of the square that represents the task*/
     float SquareSide;
 } quest_graphics_t;
 
