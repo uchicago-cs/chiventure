94e09500de3b22c0288e53611b16e996baf66b6e
Jacob Serfaty <jserfaty@linux6.cs.uchicago.edu>
2022-05-22 02:37:04 -0500

    Added wdl files for quests

---
+++ include/wdl/load_quests.h
@@ -0,0 +1,29 @@
+/*
+ * Header file for loading quest objects into game struct
+ */
+
+#ifndef INCLUDE_LOAD_QUESTS_H
+#define INCLUDE_LOAD_QUESTS_H
+
+#include "wdl_common.h"
+#include "quests/quests_state.h"
+
+/* 
+ * Accesses the document object and obtain list of quests to parse into the
+ * game struct
+ * 
+ * parameters:
+ * - doc: the document object
+ * - g: a game pointer as a return parameter
+ *
+ * side effects:
+ * - adds quests from doc into the g->all_quests 
+ *
+ * returns:
+ * - SUCCESS for successful parse
+ * - FAILURE for unsuccessful parse
+ */
+int load_quests(obj_t *doc, game_t *g);
+
+
+#endif /* INCLUDE_LOAD_QUESTS_H */
---
+++ src/wdl/src/load_quests.c
@@ -0,0 +1,1 @@
+#include "wdl/load_quests.h"
