69339b058a8ab1f6e8e2ff7524269b2b52078ebb
Lukas Heinrich Grunzke <lukasg@linux6.cs.uchicago.edu>
2022-05-23 09:49:46 -0500

    updated function roomspec_to_room()

--- src/openworld/src/autogenerate.c
+++ src/openworld/src/autogenerate.c
@@ -58,6 +58,9 @@ room_t* roomspec_to_room(roomspec_t *roomspec)
     /* instead of taking all the items, just take a few of them */
     res->items = generate_items(roomspec);
 
+    /* tag to show roomspec position in roomspec list in specgraph */
+    res->tag = roomspec->tag;
+
     res->paths = NULL;
     return res;
 }
