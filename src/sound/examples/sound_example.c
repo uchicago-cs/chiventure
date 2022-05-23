8f9ee3b9fa1c9fb7d85a7510e1a07e64e2038c97
Rhea Gauri Sanger <sangerr@uchicago.edu>
2022-05-23 16:13:39 -0500

    added sound into example

--- src/sound/examples/sound_example.c
+++ src/sound/examples/sound_example.c
@@ -17,9 +17,47 @@ char *check_game(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
         return "What?\n";
     }
 }
+void load_audio_demo(SoundType type) 
+{
+    // This implementation is referenced from demo.c
+    
+    //This initializes the all SDL2 assets
+    SDL_Init(SDL_INIT_EVERYTHING | SDL_INIT_VIDEO); 
 
-// load audio
-// this is where the implementation of adding sound will go
+    //Basic Audio Format, future teams may want to experiment with this
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT,2, 2048);
+
+    //This creates a window for music to be plated, the dimensions of the window are set by WIDTH and HEIGHT
+    SDL_Window *window = SDL_CreateWindow("This is a music window", SDL_WINDOWPOS_UNDEFINED, 
+                                            SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL); //SDL_WINDOW_HIDDEN HIDES THE WINDOW
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    IMG_Init(IMG_INIT_PNG);
+
+    SDL_Texture *texture = IMG_LoadTexture(renderer, "../../../../sound_library/chiventure.png");
+    
+
+    //Mix Music and MixChunk are data structures in the SDL, this is how we load (.wav//mp3 formats)
+    //The parameter take is the file pathway to the audio file 
+    Mix_Music *backgroundSound = Mix_LoadMUS("../../../../sound_library/Spring Village.mp3");
+
+    
+    Mix_PlayMusic(backgroundSound, -1); //SDL function that plays music, takes in Mix_Music struct
+    bool running = true;                //Setting to -1 plays music in an infinite loop
+    while (running)
+    {
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer); //Closes the window
+    SDL_DestroyWindow(window);
+    Mix_FreeMusic(backgroundSound); //Frees the resources taken up by music/chunks; note
+    Mix_CloseAudio();               
+    SDL_Quit();
+}
+
 
 // create sample game
 chiventure_ctx_t *create_sample_ctx()
@@ -44,7 +82,10 @@ int main() {
     chiventure_ctx_t *ctx = create_sample_ctx();
 
     // sound operations
-    // this is where sound will be
+    sound_t *sound_file = malloc(sizeof(sound_t));
+    sound_file->type = BACKGROUND;
+
+    load_audio_demo(sound_file->type);
 
     start_ui(ctx, banner);
 
