60dcf71c7e9962b8063e08cda6098fc20ce71033
Sabine Salnave <sdsalnave@uchicago.edu>
2022-05-23 16:43:59 -0500

    tests stil failing, added more specific cr_assert()s

--- tests/wdl/test_conditions.c
+++ tests/wdl/test_conditions.c
@@ -21,13 +21,21 @@ void conditions_check(char* item, char* action)
 
     /* get list of conditions from game action (1) */
     item_t *i = get_item_from_game(g, item);
+    cr_assert_not_null(i, "Issue grabbing item %s from game.", item);
+    
     game_action_t *a = get_action(i, action);
+    cr_assert_not_null(a, "Issue grabbing action %s from item %s.", action, item);
     condition_list_t *conditions_ls = a->conditions;
 
     /* get conditions object from wdl (2) */
     obj_t *item_obj = obj_get_attr(doc, "ITEMS", false);
     item_obj = obj_get_attr(item_obj, i->item_id, false);
+    cr_assert_not_null(item_obj, "Issue grabbing item obj %s from wdl.", item);
+
     obj_t *action_obj = obj_get_attr(item_obj, action, false);
+    cr_assert_not_null(action_obj, 
+                       "Issue grabbing action obj %s from item %s in wdl.", 
+                       action, item);
     obj_t *conditions_obj = obj_get_attr(action_obj, "conditions", false);
 
     /* compare (1) and (2) */
@@ -78,7 +86,7 @@ void conditions_check(char* item, char* action)
     else 
     {
         cr_assert_null(conditions_ls, 
-                       "Action %s for item %s has no conditions"
+                       "Action %s for item %s has no conditions "
                        "specified in wdl, but action condition list is not NULL",
                        action, item);
     }
