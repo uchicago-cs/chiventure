63a526deee914cd8352c2c560a79be4d5d1072eb
Jay Hans Sivadas <jhsivadas@linux5.cs.uchicago.edu>
2022-05-22 22:31:28 -0500

    Created functions to parse inputs from wdl file and extract npc data

--- src/dsl/src/graph_validator.py
+++ src/dsl/src/graph_validator.py
@@ -1,6 +1,28 @@
 # Graph validator algorithm
+import sys
+import json
+
+def validate(graph):
+    npcs = {}
+    if "NPCS" in graph:
+        npcs = graph["NPCS"]
+
+
+
+        
+    else:
+        return 0
+    
+
+
+
 
 def main():
+    graph_file = sys.argv[1]
+    if "wdl" in graph_file:
+        f = open(graph_file)
+        graph_json = json.load(f)
+        validate(graph_json)
     return 0
 
 
