a8a3d988a90110bc330c7e330719dbd634ac8c3a
周子恺 <zhouzk@uchicago.edu>
2022-05-22 21:50:27 -0500

    begin testing

---
+++ graphics_sandbox/input_box_graphics/test_input_box.c
@@ -0,0 +1,24 @@
+#include "input_box_graphics.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <criterion/criterion.h>
+
+
+Test(input_box_graphics, new)
+{
+
+
+}
+
+
+Test(input_box_graphics, init)
+{
+
+
+}
+
+Test(input_box_graphics, free)
+{
+    
+
+}
