dc73d2afbb60976a4b4d06293c8078debc6eaa8a
William Miller III <102555942+bvvill@users.noreply.github.com>
2022-05-22 23:54:28 -0500

    i may have done a lot for nothing

--- src/game-state/src/room.c
+++ src/game-state/src/room.c
@@ -422,7 +422,10 @@ int transfer_all_npc_items(npc_t *npc, room_t *room)
     }
     int rc;
     item_t *current_item, *tmp;
-    HASH_ITER(hh_npc, npc->inventory, current_item, tmp)
+    item_hash_t *head = npc->inventory;
+    assert(delete_all_items_from_npc(npc) == SUCCESS);
+
+    HASH_ITER(hh_npc, head, current_item, tmp)
     {
         rc = add_item_to_room(room, current_item);
         if (rc == FAILURE)
@@ -430,8 +433,5 @@ int transfer_all_npc_items(npc_t *npc, room_t *room)
             return rc;
         }
     }
-
-    assert(delete_all_items_from_npc(npc) == SUCCESS);
-
     return SUCCESS;
 }
