3fb28b35e698ef71642c2bac9690aafe060f1412
周子恺 <zhouzk@uchicago.edu>
2022-05-22 22:08:29 -0500

    added test for init

--- graphics_sandbox/input_box_graphics/test_input_box.c
+++ graphics_sandbox/input_box_graphics/test_input_box.c
@@ -23,7 +23,18 @@ Test(input_box_graphics, new)
 
 Test(input_box_graphics, init)
 {
-
+    input_box_graphics i;
+    int rc = initInputboxGraphics(&i,(Vector2){100,110},(Vector2){500,520},BLACK,
+                                  WHITE,GREEN,DARKGREEN,"I am annotation");
+    cr_assert_eq(rc,1,"initInputboxGraphics has failed");
+    cr_assert_eq(&i->WindowPos.x, 100, "input_box_graphics failed to set WindowPos.x");
+    cr_assert_eq(&i->WindowPos.y, 110, "input_box_graphics failed to set WindowPos.y");
+    cr_assert_eq(&i->WindowSize.x, 500, "input_box_graphics failed to set WindowSize.x");
+    cr_assert_eq(&i->WindowSize.y, 520, "input_box_graphics failed to set WindowSize.y");
+    cr_assert_eq(&i->BoxColor,BLACK,"input_box_graphics failed to set BoxColor");
+    cr_assert_eq(&i->TextColor,WHITE,"input_box_graphics failed to set TextColor");  
+    cr_assert_eq(&i->AnnotationColor,GREEN,"input_box_graphics failed to set AnnotationColor"); 
+    cr_assert_eq(&i->OutlineColor,DARKGREEN,"input_box_graphics failed to set OutlineColor"); 
 
 }
 
