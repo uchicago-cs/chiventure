ec755840594e470860502421e388d85838060114
Jay Hans Sivadas <jhsivadas@linux5.cs.uchicago.edu>
2022-05-22 22:52:59 -0500

    Broke apart nodes and edges

--- src/dsl/src/graph_validator.py
+++ src/dsl/src/graph_validator.py
@@ -7,9 +7,14 @@ def validate(graph):
     if "NPCS" in graph:
         npcs = graph["NPCS"]
 
+        for npc, vals in npcs.items():
+            edges = vals['dialogue']['edges']
+            nodes = vals['dialogue']['nodes']
+            print(nodes)
 
 
-        
+
+
     else:
         return 0
     
@@ -19,6 +24,7 @@ def validate(graph):
 
 def main():
     graph_file = sys.argv[1]
+    print(graph_file)
     if "wdl" in graph_file:
         f = open(graph_file)
         graph_json = json.load(f)
