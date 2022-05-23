8d4d487f59cc496d8b8b2f6228d8f968ecc74884
Nicholas Wen-Rei Lee <nicholaslee@linux7.cs.uchicago.edu>
2022-05-23 17:53:58 -0500

    battle flow example changes

--- src/battle/examples/battle_flow_example.c
+++ src/battle/examples/battle_flow_example.c
@@ -3,33 +3,58 @@
 #include "battle/battle_state.h"
 #include "npc/npc.h"
 #include "npc/npc_battle.h"
+#include "move_demo.c"
+#include "demo_items_and_equipment.c"
 
+class_t *make_sorcerer()
+{
+    return class_new("Sorcerer", "Wise", "Old and wise with crazy magecraft ability", NULL, NULL, NULL);
+}
+
+class_t *make_minion()
+{
+    return class_new("Minion", "Stupid", "Unintelligent, but very strong", NULL, NULL, NULL);
+}
 
 int main()
 {
-    /*
+    
     stat_t *p_stats = (stat_t*) calloc(1, sizeof(stat_t));
     p_stats->hp = 100;
+    p_stats->max_hp = 100;
     p_stats->xp = 10;
     p_stats->speed = 10;
     p_stats->level = 3;
-    p_stats->defense = 3;
-    p_stats->strength = 80;
-
-    battle_player_t *p = new_ctx_player("John", NULL, p_stats, NULL, NULL);
+    p_stats->phys_def = 3;
+    p_stats->mag_def = 3;
+    p_stats->phys_atk = 80;
+    p_stats->mag_atk = 80;
+    p_stats->sp = 100;
+    p_stats->max_sp = 100;
+    p_stats->crit = 25;
+    p_stats->accuracy = 100;
+    move_t *user_moves = generate_moves_user_one();
+    battle_player_t *p = new_ctx_player("Sorcerer John", make_sorcerer(), p_stats, user_moves, NULL, 
+                                        make_lunar_wand(), make_omni_crown(), make_cape_of_wisdom());
 
     stat_t *e_stats = (stat_t*) calloc(1, sizeof(stat_t));
     e_stats->hp = 70;
+    e_stats->max_hp = 70;
     e_stats->xp = 10;
     e_stats->speed = 9;
     e_stats->level = 6;
-    e_stats->defense = 2;
-    e_stats->strength = 70;
-
-    move_t *e_move = move_new("Diss Track", 0, NULL, true, 80, 0);
-    npc_t *e = npc_new("Goblin", "Enemy goblin!", "Enemy goblin!", NULL, NULL, true);
-    npc_battle_t *npc_b = npc_battle_new(100, e_stats, e_move,
-                                        BATTLE_AI_GREEDY, HOSTILE, 0, NULL, NULL);
+    e_stats->phys_def = 2;
+    e_stats->mag_def = 2;
+    e_stats->phys_atk = 70;
+    e_stats->mag_atk = 70;
+    e_stats->sp = 100;
+    e_stats->max_sp = 100;
+    e_stats->crit = 25;
+    p_stats->accuracy = 100;
+    move_t *e_moves = generate_moves_enemy_one();
+    npc_t *e = npc_new("Minion", "Enemy minion!", "Enemy minion!", make_minion(), NULL, HOSTILE);
+    npc_battle_t *npc_b = npc_battle_new(70, e_stats, e_moves, BATTLE_AI_GREEDY, HOSTILE, make_minion(), 
+                                        NULL, NULL, NULL, NULL);
     e->npc_battle = npc_b;
 
     battle_ctx_t *ctx = 
@@ -50,7 +75,7 @@ int main()
     while(ctx != NULL && ctx->status == BATTLE_IN_PROGRESS)
     {
 
-        char* res = battle_flow_move(ctx, test_move_bard(), "Goblin");
+        char* res = battle_flow_move(ctx, generate_moves_enemy_one(), "Minion");
         if(strcmp(res, "FAILURE") == 0)
         {
             fprintf(stderr, "Uh oh, the battle flow loop had an error\n");
@@ -78,7 +103,6 @@ int main()
         case NO_BATTLE:
             fprintf(stderr, "ERROR, battle should not return as no_battle");
     }
-    */
     return 0;
 }
 
