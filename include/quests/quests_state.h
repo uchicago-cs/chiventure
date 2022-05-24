f9791c35284cf1cbb76d3fd2d78f8cc0a6bcf479
Sydney Lucina Glenn <sglenn@linux3.cs.uchicago.edu>
2022-05-24 14:54:40 -0500

    ahhh need to copy qctx

--- include/quests/quests_state.h
+++ include/quests/quests_state.h
@@ -564,5 +564,30 @@ int remove_quest_in_hash(quest_hash_t *hash_table, char *quest_id);
  */
 int remove_quest_all(quest_hash_t *hash_table);
 
+// TEMP ADD//
+
+/* Checks to see if the player can start a quest given by the NPC
+ *
+ * Parameter:
+ * - qctx: a quest context struct which includes the player and a list of all quests
+ * - quest_id: a quest id given by the npc
+ *
+ * Returns:
+ * - true: if the player can start the quest
+ * - false: if the player cannot start the quest
+ */
+bool npc_can_give_quest(quest_ctx_t *qctx, char *quest_id);
+
+/* Checks to see if the player can start a task given by the NPC
+ *
+ * Parameter:
+ * - qctx: a quest context struct which includes the player and a list of all quests
+ * - task_id: a quest id given by the npc
+ *
+ * Returns:
+ * - true: if the player can start the quest
+ * - false: if the player cannot start the quest
+ */
+bool npc_can_give_task(quest_ctx_t *qctx, char *task_id);
 
 #endif /* QUESTS_STATE_H */
