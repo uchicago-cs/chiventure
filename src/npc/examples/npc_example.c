61653555eec28f0b9c194b10da061a13604ebc17
Sydney Lucina Glenn <sglenn@linux3.cs.uchicago.edu>
2022-05-24 09:03:09 -0500

    done?

--- src/npc/examples/npc_example.c
+++ src/npc/examples/npc_example.c
@@ -141,7 +141,8 @@ chiventure_ctx_t *create_sample_ctx()
                          "Jim is a shabby man who lives in a shabby house.",
                          "Jim looks just as suspicious as his house. His "
                          "beard appears to be half shaved, and his eyes "
-                         "constantly dart all around.", NULL, NULL, 0);
+                         "constantly dart all around.", NULL, NULL, 0,
+                          NULL, NULL);
     //add_npc_to_game(game, jim);
     convo_t *c = create_sample_convo();
     add_convo_to_npc(jim, c);
