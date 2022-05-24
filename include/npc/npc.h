ccdc4e2bc0be5bc047ac1e416d055aefd73c88fe
Jake Wellington <wellingtj@linux4.cs.uchicago.edu>
2022-05-23 20:59:50 -0500

    #1288 forgot to update parameters for npc new and init in header file

--- include/npc/npc.h
+++ include/npc/npc.h
@@ -79,12 +79,14 @@ typedef struct npc npc_hash_t;
  *  class: a pointer to an existing class_t struct defining the npc's class
            (see /include/playerclass/class_structs.h)
  *  will_fight: a boolean describing whether the npc will engage in battle
+ *  ctx: the chiventure ctx for the given game
  * 
  * Returns:
  *  SUCCESS on success, FAILURE if an error occurs
  */
 int npc_init(npc_t *npc, char *npc_id, char *short_desc, char *long_desc,
-             class_t *class, npc_mov_t *movement, bool will_fight);
+             class_t *class, npc_mov_t *movement, bool will_fight,
+             chiventure_ctx_t* ctx);
 
 /*
  * Allocates a new npc in the heap.
@@ -100,12 +102,14 @@ int npc_init(npc_t *npc, char *npc_id, char *short_desc, char *long_desc,
  *  class: a pointer to an existing class_t struct defining the npc's class
            (see /include/playerclass/class_structs.h)
  *  will_fight: a boolean describing whether the npc will engage in battle
+ *  ctx: the chiventure ctx for the given game
  *
  * Returns:
  *  pointer to allocated npc
  */
 npc_t *npc_new(char *npc_id, char *short_desc, char *long_desc,
-               class_t *class, npc_mov_t *movement, bool will_fight);
+               class_t *class, npc_mov_t *movement, bool will_fight,
+               chiventure_ctx_t* ctx);
 
 /*
  * Frees resources associated with an npc.
