be494a8e0d51511bf66e7cad1be8a1630156ccd4
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-24 16:54:14 -0500

    updating data types

--- tests/custom-scripts/test_types.c
+++ tests/custom-scripts/test_types.c
@@ -345,8 +345,8 @@ Test(custom_type, obj_t_get_int_lua_args)
         "../../../tests/custom-scripts/Lua_file/int_test_args.lua");
     ot = obj_add_arg(obj_add_arg(ot, data4, INT_TYPE), data3, INT_TYPE);
     data_t got = arg_t_get(ot);
-    int rv = got.c;
-    cr_assert_eq(rv, 15, "obj_t_get_int 1: failed int direct retrieval with value %u, %u", data4.i, data3.i);
+    int rv = got.i;
+    cr_assert_eq(rv, 15, "obj_t_get_int 1: failed int direct retrieval with value %u", rv);
 
     // Arguments of an object don't have to be of the same type!
     object_t *ot2 = obj_t_init(data, INT_TYPE,
