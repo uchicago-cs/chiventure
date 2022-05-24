5cf0656a494713dc3531f406a0bfcaefb2edaf06
Sabine Salnave <sdsalnave@uchicago.edu>
2022-05-23 19:34:56 -0500

    all tests pass!

--- tests/wdl/test_conditions.c
+++ tests/wdl/test_conditions.c
@@ -1,3 +1,4 @@
+#include <criterion/logging.h>
 #include "wdl/load_item.h"
 #include "wdl/load_game.h"
 #include "test_wdl.h"
@@ -19,6 +20,7 @@ void conditions_check(char* item, char* action)
     game_t *g = load_game(doc);
     cr_assert_not_null(g, "Game failed to load correctly.");
 
+
     /* get list of conditions from game action (1) */
     item_t *i = get_item_from_game(g, item);
     cr_assert_not_null(i, "Issue grabbing item %s from game.", item);
@@ -27,16 +29,31 @@ void conditions_check(char* item, char* action)
     cr_assert_not_null(a, "Issue grabbing action %s from item %s.", action, item);
     condition_list_t *conditions_ls = a->conditions;
 
+
     /* get conditions object from wdl (2) */
     obj_t *item_obj = obj_get_attr(doc, "ITEMS", false);
-    item_obj = obj_get_attr(item_obj, i->item_id, false);
+    cr_assert_not_null(item_obj, "Issue grabbing all items from wdl.");
+    item_obj = obj_get_attr(item_obj, item, false);
     cr_assert_not_null(item_obj, "Issue grabbing item obj %s from wdl.", item);
 
-    obj_t *action_obj = obj_get_attr(item_obj, action, false);
+    obj_t *actions_ls = obj_get_attr(item_obj, "actions", false);
+    cr_assert_not_null(actions_ls, 
+                       "Issue grabbing actions obj from item %s in wdl.", item);
+    obj_t *action_obj;
+    DL_FOREACH(actions_ls->data.lst, action_obj)
+    {
+        char* curr_name = obj_get_str(action_obj, "action");
+        if (!strcmp(curr_name, action))
+        {
+            break;
+        }
+    }  
     cr_assert_not_null(action_obj, 
                        "Issue grabbing action obj %s from item %s in wdl.", 
                        action, item);
+
     obj_t *conditions_obj = obj_get_attr(action_obj, "conditions", false);
+
 
     /* compare (1) and (2) */
     if (conditions_obj != NULL)
@@ -57,19 +74,23 @@ void conditions_check(char* item, char* action)
                     break;
                 case (INVENTORY): ;
                     /* Check correct condition type */
-                    char *cond_type = obj_get_str(curr, "type");
-                    cr_assert_eq(cond_type, "INVENTORY", 
-                                 "Condition %d type in wdl does not match "
-                                 "condition %d type in condition list for %s",
-                                 cond_index, cond_index, action);
+                    char *cond_type = case_insensitized_string(obj_get_str(curr, "type"));
+                    cr_assert_not_null(cond_type, "type for condition %d is NULL",
+                                       cond_index);
+                    cr_assert_str_eq(cond_type, "inventory", 
+                                 "Condition %d type (%s) in wdl does not match "
+                                 "condition %d type in condition list for %s %s",
+                                 cond_index, cond_type, cond_index, action, item); 
                     
                     /* Check correct item required */
                     char *n1 = conditions_ls->condition.inventory_type->expected_item->item_id;
-                    char *n2 = obj_get_str(curr, "item_id");
-                    cr_assert_eq(n1, n2, 
-                                 "Condition %d item_id in wdl does not match "
-                                 "condition %d item_id in condition list for %s",
-                                 cond_index, cond_index, action);
+                    char *n2 = case_insensitized_string(obj_get_str(curr, "item_id"));
+                    cr_assert_not_null(n2, "item_id for condition %d is NULL",
+                                       cond_index);
+                    cr_assert_str_eq(n1, n2, 
+                                 "Condition %d item_id (%s) in wdl does not match "
+                                 "condition %d item_id (%s) in condition list for %s %s",
+                                 cond_index, n2, cond_index, n1, action, item);
                     break;
                 case (LEVEL):
                     /* TODO when level conditions are fully implemented */
