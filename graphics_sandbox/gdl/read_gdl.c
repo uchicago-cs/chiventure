ea54061039eeea9cef491f79b0aba016936c3f41
Gabriel <raffa@Fiki-018.local>
2022-05-24 17:42:54 -0500

    almost

--- graphics_sandbox/gdl/read_gdl.c
+++ graphics_sandbox/gdl/read_gdl.c
@@ -269,7 +269,7 @@ unsigned int hash(char *str)
     unsigned int hash = 5381;
     int c;
 
-    while (c = *str++)
+    while ((c = *str++))
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
     return hash;
