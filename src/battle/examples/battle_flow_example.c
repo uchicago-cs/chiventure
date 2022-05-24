3dc15fd508cdf7088d5bdec6407c10504f23cbba
Nicholas Wen-Rei Lee <nicholaslee@linux7.cs.uchicago.edu>
2022-05-23 19:00:32 -0500

    battle example flow created

--- src/battle/examples/battle_flow_example.c
+++ src/battle/examples/battle_flow_example.c
@@ -6,6 +6,7 @@
 #include "move_demo.c"
 #include "demo_items_and_equipment.c"
 
+
 class_t *make_sorcerer()
 {
     return class_new("Sorcerer", "Wise", "Old and wise with crazy magecraft ability", NULL, NULL, NULL);
@@ -43,7 +44,7 @@ int main()
     e_stats->xp = 10;
     e_stats->speed = 9;
     e_stats->level = 6;
-    e_stats->phys_def = 2;
+    e_stats->phys_def = 30;
     e_stats->mag_def = 2;
     e_stats->phys_atk = 70;
     e_stats->mag_atk = 70;
@@ -52,32 +53,37 @@ int main()
     e_stats->crit = 25;
     p_stats->accuracy = 100;
     move_t *e_moves = generate_moves_enemy_one();
+    if(e_moves != NULL){
+        printf("Moves are working %s\n", e_moves->name);
+    }
     npc_t *e = npc_new("Minion", "Enemy minion!", "Enemy minion!", make_minion(), NULL, HOSTILE);
-    npc_battle_t *npc_b = npc_battle_new(70, e_stats, e_moves, BATTLE_AI_GREEDY, HOSTILE, make_minion(), 
-                                        NULL, NULL, NULL, NULL);
+    npc_battle_t *npc_b = npc_battle_new(e_stats, e_moves, BATTLE_AI_GREEDY, HOSTILE, make_minion(), 
+                                        make_items(), NULL, NULL, NULL);
     e->npc_battle = npc_b;
+    
+    battle_ctx_t *ctx = (battle_ctx_t*) calloc(1, sizeof(battle_ctx_t));
 
-    // battle_ctx_t *ctx = 
-    //     (battle_ctx_t*) calloc(1, sizeof(battle_ctx_t));
+    battle_game_t *g = new_battle_game();
+    ctx->game = g;
+    
+    ctx->game->player = p;
+    
+    start_battle(ctx, e, ENV_GRASS);
+    int turn = 1;
+    if(npc_b != NULL){
+        printf("Moves are working %s\n", e_moves->name);
+    }
+    
+    printf("Start of battle: \n");
+    printf("    p | hp: %d\n", ctx->game->battle->player->stats->hp);
+    printf("    e | hp: %d\n\n", ctx->game->battle->enemy->stats->hp);
+    printf("User's attack %d\n",ctx->game->battle->player->stats->phys_atk);
+    
+    while(ctx != NULL && ctx->status == BATTLE_IN_PROGRESS)
+    {
 
-    // battle_game_t *g = new_battle_game();
-    // ctx->game = g;
-
-    // ctx->game->player = p;
-
-    // start_battle(ctx, e, ENV_GRASS);
-    // int turn = 1;
-
-    // printf("Start of battle: \n");
-    // printf("    p | hp: %d\n", ctx->game->battle->player->stats->hp);
-    // printf("    e | hp: %d\n\n", ctx->game->battle->enemy->stats->hp);
-
-    // while(ctx != NULL && ctx->status == BATTLE_IN_PROGRESS)
-    // {
-
-<<<<<<< HEAD
-        char* res = battle_flow_move(ctx, generate_moves_enemy_one(), "Minion");
-        if(strcmp(res, "FAILURE") == 0)
+        char* res1 = battle_flow_move(ctx, generate_moves_user_two(), "minion");
+        if(strcmp(res1, "FAILURE") == 0)
         {
             fprintf(stderr, "Uh oh, the battle flow loop had an error\n");
             ctx = NULL;
@@ -85,8 +91,27 @@ int main()
         else
         {
             printf("Turn %d: \n",turn);
+            printf("%s", res1);
             printf("    p | hp: %d\n", ctx->game->battle->player->stats->hp);
             printf("    e | hp: %d\n\n", ctx->game->battle->enemy->stats->hp);
+            printf("User's attack %d\n",ctx->game->battle->player->stats->phys_atk);
+            printf("Enemies's attack %d\n",ctx->game->battle->enemy->stats->phys_atk);
+            turn++;
+        }
+        char* res2 = battle_flow_move(ctx, generate_moves_enemy_one(), "minion");
+        if(strcmp(res2, "FAILURE") == 0)
+        {
+            fprintf(stderr, "Uh oh, the battle flow loop had an error\n");
+            ctx = NULL;
+        }
+        else
+        {
+            printf("Turn %d: \n",turn);
+            printf("%s", res2);
+            printf("    p | hp: %d\n", ctx->game->battle->player->stats->hp);
+            printf("    e | hp: %d\n\n", ctx->game->battle->enemy->stats->hp);
+            printf("User's attack %d\n",ctx->game->battle->player->stats->phys_atk);
+            printf("Enemies's attack %d\n",ctx->game->battle->enemy->stats->phys_atk);
             turn++;
         }
     }
@@ -104,36 +129,6 @@ int main()
         case NO_BATTLE:
             fprintf(stderr, "ERROR, battle should not return as no_battle");
     }
-=======
-    //     char* res = battle_flow_move(ctx, test_move_bard(), "Goblin");
-    //     if(strcmp(res, "FAILURE") == 0)
-    //     {
-    //         fprintf(stderr, "Uh oh, the battle flow loop had an error\n");
-    //         ctx = NULL;
-    //     }
-    //     else
-    //     {
-    //         printf("Turn %d: \n",turn);
-    //         printf("    p | hp: %d\n", ctx->game->battle->player->stats->hp);
-    //         printf("    e | hp: %d\n\n", ctx->game->battle->enemy->stats->hp);
-    //         turn++;
-    //     }
-    // }
-    // battle_status_t winner = battle_over(ctx->game->battle);
-    // switch (winner) {
-    //     case BATTLE_IN_PROGRESS:
-    //         fprintf(stderr, "Uh oh, the battle flow loop had an error\n");
-    //         break;
-    //     case BATTLE_VICTOR_PLAYER:
-    //         fprintf(stderr, "SUCCESS: battle flow loop exited and player won\n");
-    //         break;
-    //     case BATTLE_VICTOR_ENEMY:
-    //         fprintf(stderr, "SUCCESS: battle flow loop exited and enemy won\n");
-    //         break;
-    //     case NO_BATTLE:
-    //         fprintf(stderr, "ERROR, battle should not return as no_battle");
-    // }
->>>>>>> 33c160c0c1328ed032048abcc786a2c45cf02e27
     return 0;
 }
 
