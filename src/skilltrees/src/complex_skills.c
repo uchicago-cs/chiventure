fab8e23cffc3ecd59eb29babd70d8ff6fcea123c
Annika Christensen <anchristensen@linux1.cs.uchicago.edu>
2022-05-24 15:07:24 -0500

    Fixed placement of functions

--- src/skilltrees/src/complex_skills.c
+++ src/skilltrees/src/complex_skills.c
@@ -134,6 +134,52 @@ int complex_skill_xp_up(complex_skill_t* complex_skill, unsigned int xp_gained){
     return SUCCESS;
 }
 
+
+/*See complex_skills.h */
+random_chance_type_t* random_chance_new(complex_skill_t* complex_skill, float chance_failure){
+    return NULL;
+}
+
+/*See complex_skills.h */
+int random_chance_init(random_chance_type_t* random_chance_skill, complex_skill_t* complex_skill, float chance_failure){
+    return 0;
+}
+
+/*See complex_skills.h */
+int random_chance_free(random_chance_type_t* random_chance_skill){
+    return 0;
+}
+
+/*See complex_skills.h */
+random_range_type_t* random_range_new(complex_skill_t* complex_skill, int lower_bound, int upper_bound){
+    return NULL;
+}
+
+/*See complex_skills.h */
+int random_range_init(random_chance_type_t* random_range_skill, complex_skill_t* complex_skill, int lower_bound, int upper_bound){
+    return 0;
+}
+
+/*See complex_skills.h */
+int random_range_free(random_range_type_t* random_range_skill){
+    return 0;
+}
+
+/*See complex_skills.h */
+random_switch_type_t* random_switch_new(complex_skill_t* complex_skill, float* chances){
+    return NULL;
+}
+
+/*See complex_skills.h */
+int random_switch_init(random_chance_type_t* random_range_skill, complex_skill_t* complex_skill, float* chances){
+    return 0;
+}
+
+/*See complex_skills.h */
+int random_switch_free(random_switch_type_t* random_switch_skill){
+    return 0;
+}
+
 /* Takes in a lower and upper value and generates random number within that 
 *  range, helper function for the range execution function
 *  Written with help of https://www.geeksforgeeks.org/generating-random-number-range-c/
@@ -238,49 +284,4 @@ int execute_random_switch_complex_skill(random_switch_type_t* switch_skill, chiv
     }
 
     return SUCCESS;
-}
-
-/*See complex_skills.h */
-random_chance_type_t* random_chance_new(complex_skill_t* complex_skill, float chance_failure){
-    return NULL;
-}
-
-/*See complex_skills.h */
-int random_chance_init(random_chance_type_t* random_chance_skill, complex_skill_t* complex_skill, float chance_failure){
-    return 0;
-}
-
-/*See complex_skills.h */
-int random_chance_free(random_chance_type_t* random_chance_skill){
-    return 0;
-}
-
-/*See complex_skills.h */
-random_range_type_t* random_range_new(complex_skill_t* complex_skill, int lower_bound, int upper_bound){
-    return NULL;
-}
-
-/*See complex_skills.h */
-int random_range_init(random_chance_type_t* random_range_skill, complex_skill_t* complex_skill, int lower_bound, int upper_bound){
-    return 0;
-}
-
-/*See complex_skills.h */
-int random_range_free(random_range_type_t* random_range_skill){
-    return 0;
-}
-
-/*See complex_skills.h */
-random_switch_type_t* random_switch_new(complex_skill_t* complex_skill, float* chances){
-    return NULL;
-}
-
-/*See complex_skills.h */
-int random_switch_init(random_chance_type_t* random_range_skill, complex_skill_t* complex_skill, float* chances){
-    return 0;
-}
-
-/*See complex_skills.h */
-int random_switch_free(random_switch_type_t* random_switch_skill){
-    return 0;
 }
