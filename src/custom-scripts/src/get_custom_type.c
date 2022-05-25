e7637ae753b8bc56577384a2ec83872455b61adf
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-25 17:19:23 -0500

    restoring get module

--- src/custom-scripts/src/get_custom_type.c
+++ src/custom-scripts/src/get_custom_type.c
@@ -3,55 +3,64 @@
 // see get_custom_types.h
 data_t arg_t_get(object_t *ot) {
     data_t d;
-    lua_State *L;
-    char *lua_path;
-
-    if (ot->is_lua) {
-    } else {
-        return ot->data;
-    }
 
     switch (ot->type) {
         case BOOL_TYPE:
             assert(ot->type == BOOL_TYPE);
-            char *lua_path = ot->data.lua;
-            lua_State *L = callLua(ot, lua_path);
-            int result = (int)lua_toboolean(L, -1);
-            lua_pop(L, 1);
-            d.b = (result ? true : false);
-            return d;
+            if (ot->is_lua) {
+                char *lua_path = ot->data.lua;
+                lua_State *L = callLua(ot, lua_path); 
+                int result = (int)lua_toboolean(L, -1);
+                lua_pop(L, 1);
+                d.b = (result ? true : false);
+                return d;
+            } else {
+                return ot->data;
+            }
             break;
         case CHAR_TYPE:
             assert(ot->type == CHAR_TYPE);
-            char *lua_path = ot->data.lua;
-            lua_State *L = callLua(ot, lua_path);            
-            const char *result2 = lua_tostring(L, -1);
-            lua_pop(L, 1);
-            char *result3 = strdup(result2);
-            d.c = *result3;
-            return d;
+            if (ot->is_lua) {
+                char *lua_path = ot->data.lua;
+                lua_State *L = callLua(ot, lua_path);
+                const char *result = lua_tostring(L, -1);
+                lua_pop(L, 1);
+                char *result1 = strdup(result);
+                d.c = *result1;
+                return d;
+            } else {
+                return ot->data;
+            }
             break;
         case INT_TYPE:
             assert(ot->type == INT_TYPE);
-            char *lua_path = ot->data.lua;
-            lua_State *L = callLua(ot, lua_path);            
-            int result4 = (int)lua_tointeger(L, -1);
-            lua_pop(L, 1);
-            d.i = result4;
-            return d;
+            if (ot->is_lua) {
+                char *lua_path = ot->data.lua;
+                lua_State *L = callLua(ot, lua_path);
+                int result = (int)lua_tointeger(L, -1);
+                lua_pop(L, 1);
+                d.i = result;
+                return d;
+            } else {
+                return ot->data;
+            }
             break;
         case STR_TYPE:
             assert(ot->type == STR_TYPE);
-            char *lua_path = ot->data.lua;
-            lua_State *L = callLua(ot, lua_path);            
-            const char *result5 = lua_tostring(L, -1);
-            lua_pop(L, 1);
-            char *result6 = strdup(result5);
-            d.s = result6;
-            return d;
+            if (ot->is_lua) {
+                char *lua_path = ot->data.lua;
+                lua_State *L = callLua(ot, lua_path);
+                const char *result = lua_tostring(L, -1);
+                lua_pop(L, 1);
+                char *result1 = strdup(result);
+                d.s = result1;
+                return d;
+            } else {
+                return ot->data;
+            }
             break;
         default:
             return ot->data;
             break;
-    }
-}
+        }
+    } 
