e558703604d8c30d5ebcae1c4640a738b4a9b7e9
Urunna Anyanwu <urunnaanyanwu@Urunnas-MacBook-Air.local>
2022-05-24 19:39:11 -0500

    Added init tests along with header files

--- graphics_sandbox/gdl/test_stats.c
+++ graphics_sandbox/gdl/test_stats.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include "read_gdl.h"
 
+/* Checks that new_statistics_display_t properly mallocs and inits values
+ * for a statistics_display_t struct */
 Test(statistics, new)
 {
     stats_global_t *global_stat = stats_global_new("Health",10.0);
@@ -27,6 +29,34 @@ Test(statistics, new)
                 "make_statistics_display() didn't set stats_t field");
 }
 
+/* Checks that init_statistics_display properly inits values for a 
+ * statistics_display_t struct */
+Test(statistics, init)
+{
+    stats_global_t *global_stat = stats_global_new("Health", 10.0);
+    stats_t *stat = stats_new(global_stat,10.0);
+
+    corner c = TOP_LEFT;
+    mode m = NUMERICAL;
+    unsigned int num_stat = 1;
+
+    statistics_display_t *stats;
+    int rc;
+
+    rc = init_statistics_display(stats,c,stat,num_stat,m);
+
+    cr_assert_eq(rc, SUCCESS,"init_statistics_display() failed");
+
+    cr_assert_eq(statistics->corner,TOP_LEFT,
+                "init_statistics_display() didn't set corner");
+    cr_assert_eq(statistics->mode,NUMERICAL,
+                "init_statistics_display() didn't set mode");
+    cr_assert_eq(statistics->num_statistics, 1,
+                "init_statistics_display() didn't set num_statistics");
+}
+
+/* Checks that free_statistics_display() properly frees memory used for
+ * statistics_display_t struct */
 Test(statistics, free)
 {
     int rc;
