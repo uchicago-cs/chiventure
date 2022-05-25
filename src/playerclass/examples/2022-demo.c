9222a3b26c8410d1ca4ae99abe29e0c2a8e65c88
Van Olson <vanolson@uchicago.edu>
2022-05-24 20:53:07 -0500

    Removed unnecessary functions from the 2022 demo

--- src/playerclass/examples/2022-demo.c
+++ src/playerclass/examples/2022-demo.c
@@ -209,35 +209,6 @@ void demo_prefab_classes() {
     }
 }
 
-void demo_WDL() {
-    /* This is basically an interactive version of the tests, showing the different 
-     * kinds of classes you can laod */
-    obj_t *obj_store = get_doc_obj();
-    game_t *game = load_game(obj_store);
-
-    /* If the game is NULL, then the WDL example file could not be found. */
-    if (game == NULL) {
-        printf("ERROR! Game object is NULL.\nMake sure to run this executable " 
-               "in root of the build directory so it can properly find the "
-               "example WDL file.\nAborting demo_WDL().\n");
-        return;
-    }
-
-    printf("Game successfully loaded from WDL File: %s\n", CLASSES_WDL_PATH);
-
-    prompt("Loading Knight class, fully defined in the file...", NULL);
-    print_class(find_class(&game->all_classes, "Knight"));
-    
-    prompt("Loading Rogue class, partially defined in the file...", NULL);
-    print_class(find_class(&game->all_classes, "Rogue"));
-    
-    prompt("Loading Monk class, a prefab class declared in the file...", NULL);
-    print_class(find_class(&game->all_classes, "Monk"));
-    
-    prompt("Loading Warrior class, a partially overwritten prefab class declared in the file...", NULL);
-    print_class(find_class(&game->all_classes, "Warrior"));
-}
-
 void demo_multiclasses() {
     game_t* game = game_new("The playerclass demo game!");
 
@@ -256,49 +227,6 @@ void demo_multiclasses() {
     print_class(hexblade_class);
 }
 
-void demo_item_interactions() {
-    /* This is a basic implementation, appropriating ideas from pc-demo.c */
-    game_t* game = game_new("The playerclass demo game!");
-
-    class_t* wizard = class_prefab_new(game, "Wizard");
-    class_t* bard = class_prefab_new(game, "Bard");
-    class_t* warrior = class_prefab_new(game, "Warrior");
-
-    item_t* wand = item_new("wand", " ", " ");
-    item_t* sword = item_new("sword", " ", " ");
-    item_t* guitar = item_new("guitar", " "," ");
-
-    /* Item Restrictions */
-    prompt("Adding Item restriction: The Wizard can't use a sword.", NULL);
-    add_item_restriction(sword, wizard);
-
-    bool allowed_sword = is_restricted(sword, wizard);
-    prompt("You are now playing with class Wizard. Your task is to find a weapon to defeat your archenemy.", NULL);
-    printf("You find a sword lying on a rock, and you are %s to use it.\n", 
-           allowed_sword? "unable": "able");
-
-    prompt("Switching to class Bard...", NULL);
-
-    allowed_sword = is_restricted(sword, bard);
-    printf("%s You are now %s to use the sword.\n", allowed_sword? "Failure.": "Success!", allowed_sword? "unable": "able");
-    printf("The sword %s been added to the inventory, and you continue onwards.", allowed_sword? "hasn't": "has");
-    prompt("", NULL);
-
-    /* Item modifiers */
-    prompt("Adding Item multipliers: The Bard is naturally bad at using the wand. The Wizard is naturally good.", NULL);
-    add_item_multiplier(wand, wizard, 1.5);
-    add_item_multiplier(wand, bard, 0.5);
-    add_item_restriction(wand, warrior);
-
-    prompt("You encounter an evil Goblin. To move forward, you must charm the Goblin by casting a spell.", NULL);
-    double mult = get_class_item_multiplier(wand, bard);
-    printf("You attempt to cast the spell, but you can only use the wand at %.2fx its effectiveness.\n", mult);
-    prompt("You have failed to charm the evil Goblin.", NULL);
-    prompt("Switching to class Wizard...", NULL);
-    mult = get_class_item_multiplier(wand, wizard);
-    printf("Wow! You used the wand at %.2fx its effectiveness and charmed the Goblin.\n", mult);
-}
-
 
 /* Due to having to locate the example WDL file, it is critical that this executable 
  * is run from from a specific location: the root of the build/ directory. Running it anywhere else will cause 
@@ -309,16 +237,10 @@ void demo_item_interactions() {
 
 /* main function for the 2022-demo executable. */
 int main() {
-    /* We decided to break up the ~10 minute demo into 2-3 minute mini demos */
+    /* We broke up the demo into two mini demos */
     printf("***Entering prefab mini-demo***\n");
     demo_prefab_classes();
-    
-    printf("***Entering WDL mini-demo***\n");
-    demo_WDL();
 
     printf("***Entering multiclass mini-demo***\n");
     demo_multiclasses();
-
-    printf("***Entering item interactions mini-demo***\n");
-    demo_item_interactions();
 }
