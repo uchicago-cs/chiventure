60b041c0c5f42a427cdbd264641485f607915868
Kabir Dubey <kabirdubey@mit.cs.uchicago.edu>
2022-05-25 18:59:12 -0500

    updated header file

--- include/skilltrees/complex-skills_examples.h
+++ include/skilltrees/complex-skills_examples.h
@@ -8,19 +8,25 @@
 
 /*
 * adds a combined skill to the player
-* takes in :CTX
+* takes in: CTX
 * Puts out: VOID
 */
-
 void combined_skill(chiventure_ctx_t* ctx);
 
 
 /*
 * adds a sequential skill to the player
-* takes in :CTX
+* takes in: CTX
 * Puts out: VOID
 */
 void sequential_skill(chiventure_ctx_t* ctx);
+
+/*
+* adds a conditional skill to the player
+* takes in: CTX
+* Puts out: VOID
+*/
+void conditional_skill(chiventure_ctx_t* ctx)
 
 
 #endif /* INCLUDE_SKILL_EXAMPLE_H_ */
