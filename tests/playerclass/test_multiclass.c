81d3524c51c96783eba443a8d2e91cf2079bc50d
Jonathan Alfred Roven <jroven@linux3.cs.uchicago.edu>
2022-05-25 00:06:53 -0500

    #1402 debugging

--- tests/playerclass/test_multiclass.c
+++ tests/playerclass/test_multiclass.c
@@ -101,9 +101,9 @@ Test(multiclass, shortdesc_exceeds_max_length){
     int succ;
 
     class_t* c1 = class_prefab_new(ctx->game, "warrior");
-    strcpy(c1->name, "a");
+    strcpy(c1->name, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY");
     class_t* c2 = class_prefab_new(ctx->game, "bard");
-    strcpy(c2->name, "z");
+    strcpy(c2->name, "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA");
 
     check_field_presence(c1);
     check_field_presence(c2);
