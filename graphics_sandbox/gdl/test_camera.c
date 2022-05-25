620a3de2b291f5161babedd59080b038af7264d1
Urunna Anyanwu <urunnaanyanwu@Urunnas-MacBook-Air.local>
2022-05-24 19:27:33 -0500

    added init functions and headers for tests

--- graphics_sandbox/gdl/test_camera.c
+++ graphics_sandbox/gdl/test_camera.c
@@ -18,9 +18,22 @@ Test(camera, new)
     cr_assert_eq(camera->height, 300, "make_camera() didn't set height");
 }
 
+/* Checks that init_camera() properly inits values for camera struct */
 Test(camera, init)
 {
-    unsigned int width = 
+    unsigned int width = 300;
+    unsigned int height = 300;
+
+    camera_t *c;
+    int rc;
+    
+    rc = init_camera(c,width,height);
+
+    cr_assert_eq(rc, SUCCESS, "init_camera() failed");
+
+    cr_assert_eq(camera->width,300,"init_camera() didn't set width");
+    cr_assert_eq(camera->height,300,"init_camera() didn't set height");
+}
 
 /* Tests taht free_camera properly frees a heap allocated camera struct */
 Test(camera, free)
--- graphics_sandbox/gdl/test_dimensions.c
+++ graphics_sandbox/gdl/test_dimensions.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include "read_gdl.h"
 
+/*Checks that new_display_dimensions() properly mallocs and inits a new
+ * display_dimensions_t struct */
 Test(display_dimensions, new)
 {
     unsigned int width = 500;
@@ -17,6 +19,29 @@ Test(display_dimensions, new)
     cr_assert_eq(dimensions->height,400,"make_display_dimensions() didn't set height");
 }
 
+/* Checks that init_display_dimensions properly inits values for a 
+ * display_dimensions_t struct */
+Test(display_dimensions, init)
+{
+    unsigned int width = 500;
+    unsigned int height = 400;
+
+    int rc;
+    display_dimensions_t *dimensions;
+
+    rc = init_display_dimensions(dimensions,width,height);
+    
+    cr_assert_eq(rc, SUCCESS, "init_display_dimensions() failed");
+
+    cr_assert_eq(dimensions->width,500,
+                "init_display dimensions didn't set width");
+    cr_assert_eq(dimensions->height,400,
+                "init_display_dimensions didn't set height");
+}
+
+
+/*Checks that free_display_dimensions() properly frees memory used for 
+ * display_dimensions_t struct */
 Test(display_dimensions, free)
 {
     unsigned int width = 500;
