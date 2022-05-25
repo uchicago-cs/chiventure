424422245f440645f27dc787d0be364980d3f675
Miles Boustani Gardner <milesgardner@linux1.cs.uchicago.edu>
2022-05-25 12:19:38 -0500

    Small test

--- tests/cli/test_parser.c
+++ tests/cli/test_parser.c
@@ -427,7 +427,7 @@ Test(parse, fillers_diff_size)
 {
     char str[] = "go TO tHE south";
     char **words = parse(str);
-    cr_assert_eq("go ",words[0],"parse() did not create first token");
+    cr_assert_eq("go",words[0],"parse() did not create first token");
     cr_assert_eq("south",words[1],"parse() did not create second token");
     cr_assert_null(words[2],"parse() should point to NULL for empty tokens");
 }
