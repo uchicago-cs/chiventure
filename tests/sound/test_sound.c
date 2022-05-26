3be8d82ce26ee1e67e5616d22cc4b7063bd07a87
Victor Jiao <boxuanjiaovictornanjingva21@gmail.com>
2022-05-26 18:41:58 -0500

    Added some more tests, not finished

--- tests/sound/test_sound.c
+++ tests/sound/test_sound.c
@@ -10,7 +10,7 @@
 Test(sound, new_background)
 {
     char song_name[] = "c'est_la_vie_khaled.mp3";
-    sound_t *sound = sound_new(BACKGROUND, song_name);
+    sound_t *sound = sound_new(BACKGROUND, &song_name);
 
      cr_assert_not_null(sound, "sound_new failed");
 }
@@ -19,7 +19,7 @@ Test(sound, new_background)
 Test(sound, new_sound_effect)
 {
     char song_name[] = "wav_example.wav";
-    sound_t *sound = sound_new(SOUND_EFFECT, song_name);
+    sound_t *sound = sound_new(SOUND_EFFECT, &song_name);
 
      cr_assert_not_null(sound, "sound_new failed");
 }
@@ -31,7 +31,7 @@ Test(sound, init_background)
     sound_t *sound;
     char song_name[] = "glamorous_fergie.mp3";
     
-    rc = sound_init(sound, BACKGROUND, song_name);
+    rc = sound_init(sound, BACKGROUND, &song_name);
 
     cr_assert_eq(rc, 1, "sound_init failed");
 }
