7510d22d82984bfdb77820603831b573fb051a8c
William Hu <williamhu2025@uchicago.edu>
2022-05-26 09:21:19 -0500

    Add includes and header for test file

---
+++ tests/openworld/test_game_autogenerate.c
@@ -0,0 +1,10 @@
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "openworld/autogenerate.h"
+#include "openworld/game_autogenerate.h"
+#include "openworld/default_rooms.h"
+#include "openworld/default_items.h"
+#include "openworld/gen_structs.h"
+
+/* Tests the functions in game_autogenerate.h */
