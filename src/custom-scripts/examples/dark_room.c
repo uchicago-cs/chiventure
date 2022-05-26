54159bb09f620681498356e0fab22852e73877fa
Ramarkob <Ramarkob@gmail.com>
2022-05-26 01:52:30 -0500

    Working WDL FIle with Lua Commented out

--- src/custom-scripts/examples/dark_room.c
+++ src/custom-scripts/examples/dark_room.c
@@ -1,61 +1,97 @@
-#include <stdlib.h> 
-#include <stdbool.h>
-#include "lua.h"
-#include "lualib.h"
-#include "dark_room.h"
+/*
+ * This example program runs a full instance of chiventure with an in-memory game,
+ * and where the CLI is monkeypatched to accept a new operation:
+ *
+ *  - TASTE: A "kind 1" action that operates on an item. We support customization for the 
+ *           output string upon running the action.
+ */
 
 #include <stdio.h>
-#include <custom-scripts/custom_type.h>
+#include <custom-scripts/get_custom_type.h>
 #include <cli/operations.h>
 #include "common/ctx.h"
 #include "ui/ui.h"
 
+
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <wdl/load_game.h>
+#include <ui/ui_ctx.h>
+#include "libobj/load.h"
+
+
+#include "game-state/mode.h"
+
 const char *banner = "THIS IS AN EXAMPLE PROGRAM";
 
-/* Helper function to dynamically flip torch state using Lua each time flip is called 
-   Variable st is state of torch before fliping */
 char* flip_state(bool st)
 {
-    /* Use of boolean, switching item torch from 
-    true (lit) to false (unlit), and conversely with Lua */
-    object_t *ot = obj_t_bool("", "../../../../src/custom-scripts/examples/flip.lua");
-    ot = obj_add_arg_bool(ot, st);
-    st = bool_t_get(ot);
+    // /* Use of boolean, switching item torch from 
+    // true (lit) to false (unlit), and conversely with Lua */
+    // object_t *ot = obj_t_bool("", "../../../../src/custom-scripts/examples/flip.lua");
+    // ot = obj_add_arg_bool(ot, st);
+    // st = bool_t_get(ot);
     
-    char* str_state = (char*)malloc(20);
+    // char* str_state = (char*)malloc(20);
     
-    /* output string depending on flip */
-    if (st == false) {
-      str_state = "The torch is unlit.";
-    } else {
-      str_state = "The torch is lit.";
-    }
+    // /* output string depending on flip */
+    // if (st == false) {
+    //   str_state = "The torch is unlit.";
+    // } else {
+    //   str_state = "The torch is lit.";
+    // }
     
-    return str_state;
+    // return str_state;
+    return "The torch is lit.";
 }
 
 /* Creates a sample in-memory game */
 chiventure_ctx_t *create_sample_ctx()
 {
-    game_t *game = game_new("Welcome to Chiventure!");
+    data_t jack;
+    data_t dc;
+
+    object_t *togay = obj_t_init(jack, STR_TYPE,"../../../../src/custom-scripts/examples/dynamic_string.lua");
+   
+
+    /* Create context */
+    char string_num;
+    printf("Enter either 1 or 2 (1 for wizard class, 2 for warrior class): ");
+    scanf("%c", &string_num); 
+    dc.c = string_num;
+    togay = obj_add_arg(togay,dc,CHAR_TYPE);
+    char* custom_string = (char*)malloc(100);
+    data_t temp = arg_t_get(togay);
+    custom_string = temp.s;
+
+    obj_t *obj_store;
+
+    char wizard[] = "Wizard Class", warrior[] = "Warrior Class";
+    if(strcmp(wizard,custom_string)== 0){
+        obj_store = load_obj_store("../../../../src/custom-scripts/examples/demo.wdl");
+    }
+    else{
+        obj_store = load_obj_store("../../../../src/custom-scripts/examples/demo-warrior.wdl");
+    }
+
+    game_t *game = load_game(obj_store);
+    chiventure_ctx_t *ctx = chiventure_ctx_new(game);
+
 
     /* Create one rooms (room1). room1 is the initial room */
     room_t *room1 = room_new("room1", "This is room 1", "Verily, this is the first room.");
     add_room_to_game(game, room1);
-    game->curr_room = room1;
+    create_connection(game, "room_A", "room1", "SOUTH");
 
     /* Create a torch in room1 */
     item_t *torch = item_new("TORCH","It is a torch.",
                    "The torch is nice, and can provide light!");
     add_item_to_room(room1, torch);
-  
+
     /* Associate action "LIGHT" and "UNLIGHT" with the torch.
-     * They have no conditions, so they should succeed unconditionally. */
+    * They have no conditions, so they should succeed unconditionally. */
     add_action(torch, "LIGHT", flip_state(false), "The torch is broken!");
     add_action(torch, "UNLIGHT", flip_state(true), "The torch is broken!");
-
-    /* Create context */
-    chiventure_ctx_t *ctx = chiventure_ctx_new(game);
 
     return ctx;
 }
--- src/custom-scripts/examples/demonstration.c
+++ src/custom-scripts/examples/demonstration.c
@@ -7,65 +7,68 @@
  */
 
 #include <stdio.h>
-#include <custom-scripts/get_custom_type.h>
+#include <custom-scripts/custom_type.h>
 #include <cli/operations.h>
 #include "common/ctx.h"
 #include "ui/ui.h"
 #include <action_management/action_structs.h>
 #include <action_management/actionmanagement.h>
 
-
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <wdl/load_game.h>
-#include <ui/ui_ctx.h>
-#include "libobj/load.h"
-
-
-#include "game-state/mode.h"
-
 const char *banner = "THIS IS AN EXAMPLE PROGRAM";
 
 /* Creates a sample in-memory game */
 chiventure_ctx_t *create_sample_ctx()
 {
-    data_t jack;
-    data_t dc;
+    game_t *game = game_new("Welcome to Chiventure!");
 
-    object_t *togay = obj_t_init(jack, STR_TYPE,"../../../../src/custom-scripts/examples/dynamic_string.lua");
-   
+
+    /* Create two rooms (room1 and room2). room1 is the initial room */
+    room_t *room1 = room_new("room1", "This is room 1", "Verily, this is the first room.");
+    room_t *room2 = room_new("room2", "This is room 2", "Truly, this is the second room.");
+    add_room_to_game(game, room1);
+    add_room_to_game(game, room2);
+    game->curr_room = room1;
+    create_connection(game, "room1", "room2", "NORTH");
+    create_connection(game, "room2", "room1", "SOUTH");
+
+    /* Create a rock in room1 */
+    item_t *rock = item_new("ROCK","It is a rock.",
+                   "You were hoping this was The Rock but, alas, it is just a plain and ordinary rock");
+    add_item_to_room(room1, rock);
+
+    /* Create a key in room1 */
+    item_t *key = item_new("KEY","It is a key.",
+                   "You have acquired a key. You can now enter a new room!");
+    add_item_to_room(room1, key);
+
+
+    /* Where custom_type comes into play, create a dynamic string (hold different values) depending
+       on what the user enters at the start of the game */
+    char string_num;
+    printf("Enter either 1 or 2 (1 for non-caps, 2 for caps): ");
+    scanf("%c", &string_num);  
+    object_t *ot = obj_t_str("", "../../../../src/custom-scripts/examples/dynamic_string.lua");
+    ot = obj_add_arg_char(ot, string_num);
+    char* custom_string = (char*)malloc(500);
+    custom_string = str_t_get(ot);
+
+    /* Associate action "TASTE" with the rock.
+     * It has no conditions, so it should succeed unconditionally. */
+    add_action(rock, "TASTE", custom_string, "It has a gravel-ey bouquet.");
 
     /* Create context */
-    char string_num;
-    printf("Enter either 1 or 2 (1 for wizard class, 2 for warrior class): ");
-    scanf("%c", &string_num); 
-    dc.c = string_num;
-    togay = obj_add_arg(togay,dc,CHAR_TYPE);
-    char* custom_string = (char*)malloc(100);
-    data_t temp = arg_t_get(togay);
-    custom_string = temp.s;
-    char wizard[] = "Wizard Class", warrior[] = "Warrior Class";
-    if(strcmp(wizard,custom_string)== 0){
-        obj_t *obj_store1 = load_obj_store("../../../../src/custom-scripts/examples/demo.wdl");
-        game_t *game1 = load_game(obj_store1);
-        chiventure_ctx_t *ctx1 = chiventure_ctx_new(game1);
-        return ctx1;
-    }
-    else{
-        obj_t *obj_store2 = load_obj_store("../../../../src/custom-scripts/examples/demo-warrior.wdl");
-        game_t *game2 = load_game(obj_store2);
-        chiventure_ctx_t *ctx2 = chiventure_ctx_new(game2);
-        return ctx2;
-    }
+    chiventure_ctx_t *ctx = chiventure_ctx_new(game);
+
+    return ctx;
 }
+
+
 
 
 int main(int argc, char **argv)
 {
     chiventure_ctx_t *ctx = create_sample_ctx();
 
-<<<<<<< HEAD
-=======
     /* Monkeypatch the CLI to add a new "kind 1" action
      * (i.e., an action that operates on an item) */
     action_type_t taste_action = {"TASTE", ITEM};
@@ -77,7 +80,6 @@ int main(int argc, char **argv)
     // how to do that without restarting the whole game
     // how to check if something has been taken into the itenerary
 
->>>>>>> origin/custom-scripts/update-sandbox
     /* Start chiventure */
     start_ui(ctx, banner);
 
