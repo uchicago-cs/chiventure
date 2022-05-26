2efcb7fc70672d6173cb188200e38fbd449e31c3
Urunna Anyanwu <urunnaanyanwu@Urunnas-MacBook-Air.local>
2022-05-26 00:08:12 -0500

    Added tests for inventory structs

--- graphics_sandbox/inventory_graphics/test_inventory_graphics.c
+++ graphics_sandbox/inventory_graphics/test_inventory_graphics.c
@@ -1,17 +1,32 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "inventory_graphics.h"  
 
-int main()
+player_t *synthesize_player(char *player_id)
 {
-    /* This establishes an item to use for player_t */
-    item_t *i1 = item_new("Rock", "Gray Rock", "This is a medium gray rock");
-       
+    chiventure_ctx_t *ctx = chiventure_ctx_new(NULL);
+    
+    player_t *pt = player_new(player_id);
+    
+    item_t *il1 = item_new("Rock", "Gray Rock", "This is a medium gray rock");
+    item_t *il2 = item_new("Orb", "Colorful orb", "This is a rainbow orb");
+    item_t *il3 = item_new("Sword", "Dull sword", 
+                           "This is a very used, dull sword");
+    item_t *il4 = item_new("Feather", "Ashy feather",
+                           "This is an ashy feather, likely from a dead
+                            phoenix");
 
-    player_t *p = player_new("1");
-
-    add_item_to_player(p,i1);
+    add_item_to_player(pt, il1, ctx->game);
+    add_item_to_player(pt, il2, ctx->game); 
+    add_item_to_player(pt, il3, ctx->game); 
+    add_item_to_player(pt, il4, ctx->game); 
    
-    item_hash_t *inv = get_inventory(p);
+    return pt;
+}
 
+graphics_t *synthesize_graphics()
+{
     display_dimensions_t dd;
     inventory_display_t td;
     statistics_display_t sd;
@@ -24,8 +39,8 @@ int main()
     dd.width = 300;
     dd.height = 300;
 
-    td.rows = 10;
-    td.columns = 10;
+    td.rows = 3;
+    td.columns = 5;
     td.color = WHITE;
 
     sd.corner = TOP_LEFT;
@@ -36,10 +51,66 @@ int main()
     g.inventory = &td;
     g.statistics = &sd;
 
-    slot_t **items = populate_items(p,&g);
+    return g;
+} 
 
-    player_inventory_t *pt;
+
+Test(slot, new)
+{
+    status s = FULL;
+    item_t *il = item_new("Rock", "Gray Rock", "This is a medium gray rock");
+    il->inventory_x_pos = 0;
+    il->inventory_y_pos = 0;
+
+    slot_t *slot;
+    slot = new_slot(s,item);
     
-    pt = new_player_inventory(&g, p);
+    cr_assert_not_null(slot, "new_slot() failed");
+    
+    cr_assert_eq(slot->status, FULL, "new_slot() didn't set status");
+    cr_assert_eq(slot->item->short_desc, "Gray Rock",
+                "new_slot() didn't set relation to short item description");
+    cr_assert_eq(slot->item->long_dex, "This is a medium gray rock",
+                "new_slot() didn't set relation to long item description");
+}
 
-}                            
+Test(slot, init)
+{
+    status s = FULL;
+    item_t *il = item_new("Rock", "Gray Rock", "This is a medium gray rock");
+    il->inventory_x_pos = 0;
+    il->inventory_y_pos = 0;
+
+    slot_t slot;
+    int rc; 
+
+    rc = init_slot(&slot, s, item);
+
+    cr_assert_eq(rc, SUCCESS, "init_slot() failed");
+
+    cr_assert_eq(slot.status, FULL, "init_slot() didn't set status");
+    cr_assert_eq(slot.item->short_desc, "Gray Rock",
+                "init_slot() didn't set relation to short item description");
+    cr_assert_eq(slot.item->long_desc, "This is a medium gray rock",
+                "init_slot() didn't set relation to long item description");
+}
+
+Test(slot, free)
+{
+    status s = FULL;
+    item_t *il = item_new("Rock", "Gray Rock", "This is a medium gray rock");
+    il->inventory_x_pos = 0;
+    il->inventory_y_pos = 0;
+    int rc;
+
+    slot_t *slot;
+    slot = new_slot(s,item);
+    
+    cr_assert_not_null(slot, "new_slot() failed");
+    
+    rc = free_slot(slot);
+
+    cr_assert_eq(rc, SUCCESS, "free_slot() failed");
+}
+
+
