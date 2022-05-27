45bdf54f10de3fbcd5fe4ee8d6a3cca9fb8a40e2
Ramarkob <Ramarkob@gmail.com>
2022-05-26 22:56:22 -0500

    Deleted more cache files

--- src/tests/action_management/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maggiex/cs220/chiventure/tests/action_management
-# Build directory: /home/maggiex/cs220/chiventure/src/tests/action_management
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test-action_management "/home/maggiex/cs220/chiventure/src/tests/action_management/test-action_management")
-set_tests_properties(test-action_management PROPERTIES  _BACKTRACE_TRIPLES "/home/maggiex/cs220/chiventure/tests/action_management/CMakeLists.txt;17;add_test;/home/maggiex/cs220/chiventure/tests/action_management/CMakeLists.txt;0;")
