f28d232995ce7739620a394c054d1e6ba05612d6
Ramarkob <Ramarkob@gmail.com>
2022-05-27 00:28:55 -0500

    Almsot finished removing cache files

--- tests/ui/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maggiex/cs220/chiventure/tests/ui
-# Build directory: /home/maggiex/cs220/chiventure/tests/ui
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test-ui "/home/maggiex/cs220/chiventure/tests/ui/test-ui")
-set_tests_properties(test-ui PROPERTIES  _BACKTRACE_TRIPLES "/home/maggiex/cs220/chiventure/tests/ui/CMakeLists.txt;13;add_test;/home/maggiex/cs220/chiventure/tests/ui/CMakeLists.txt;0;")
