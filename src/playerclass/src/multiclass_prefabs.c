eb7a21a7ca5d224b0132694a74840e16bfb1a2a0
Katie Y Gao <katiegao@linux7.cs.uchicago.edu>
2022-05-27 15:56:06 -0500

    some changes to multiclass_prefabs

--- src/playerclass/src/multiclass_prefabs.c
+++ src/playerclass/src/multiclass_prefabs.c
@@ -18,6 +18,45 @@
 #include "common/utlist.h"
 #include "playerclass/multiclass.h"
 
+
+/* Default Multiclasses in alphabetical order. */
+const char* const DEFAULT_MULTICLASS_NAMES[] = {
+    "hexblade",
+    "name1",
+    "name2"
+};
+
+/* Number of predefined default multiclasses (see above). */
+const int DEFAULT_MULTICLASS_COUNT = 3;
+
+/*
+ * Determines the index of name in the DEFAULT_MULTICLASS_NAMES array, 
+ * for use as an internal id.
+ * 
+ * Parameters
+ *  - name: The name of the class.  Case sensitive.
+ * 
+ * Returns:
+ *  - The index of the name in the DEFAULT_CLASS_NAMES array. Returns -1 if the 
+ *    name does not appear or is NULL.
+ */
+int get_multiclass_name_index(char* name) {
+    if (name == NULL)
+        return -1;
+
+    char temp_name[MAX_NAME_LEN + 1];
+    strncpy(temp_name, name, MAX_NAME_LEN + 1);
+    /* make temp_name lowercase */
+    for (int i = 0; i < MAX_NAME_LEN + 1; i++) 
+        temp_name[i] = tolower(temp_name[i]);
+
+    for (int i = 0; i < DEFAULT_MULTICLASS_COUNT; i++) 
+        if (strncmp(temp_name, DEFAULT_MULTICLASS_NAMES[i], MAX_NAME_LEN) == 0) 
+            return i;
+
+    return -1;
+}
+
 /* See multiclass_prefabs.h */
 class_t* multiclass_prefab_new(game_t* game, char* class_name)
 {
