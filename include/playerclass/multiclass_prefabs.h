a950640be0ccdafba96a57f057185842485aad1c
Katie Y Gao <katiegao@linux7.cs.uchicago.edu>
2022-05-27 15:26:46 -0500

    pushing multiclass_prefab files

---
+++ include/playerclass/multiclass_prefabs.h
@@ -0,0 +1,12 @@
+/*
+ * Multiclasss prefabs for demo
+ */
+
+
+#ifndef MULTICLASS_PREFABS_H
+#define MULTICLASS_PREFABS_H
+
+#include "libobj/obj.h"
+#include "common/utlist.h"
+#include "playerclass/class_structs.h"
+#include "game-state/stats.h"
---
+++ src/playerclass/src/multiclass_prefabs.c
@@ -0,0 +1,19 @@
+/*
+ * Enables players to have more than one class at the same time.
+ *
+ * For more information see multiclass_prefabs.h
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "playerclass/class_structs.h"
+#include "playerclass/class.h"
+#include "playerclass/class_prefabs.h"
+#include "multiclass_prefabs.h"
+#include "skilltrees/skilltree.h"
+#include "skilltrees/inventory.h"
+#include "common/utlist.h"
+#include "playerclass/multiclass.h"
