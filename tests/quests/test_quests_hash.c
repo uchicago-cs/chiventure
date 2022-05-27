53595aa7c1417d53ff56c0d1cb9fb5e0337b5448
Jacob Serfaty <jserfaty@linux6.cs.uchicago.edu>
2022-05-27 12:08:07 -0500

    Removed temporary test

--- tests/quests/test_quests_hash.c
+++ tests/quests/test_quests_hash.c
@@ -48,14 +48,6 @@ Test(test, add_quest_test)
     cr_assert_eq(add_quest1, SUCCESS, "Could not sucessfully add quest1"); 
     cr_assert_eq(add_quest2, SUCCESS, "Could not sucessfully add quest2"); 
     cr_assert_eq(add_quest_fail, FAILURE, "Added quest one to the hash twice!"); 
-    
-    // temporary test for show_quests
-    player_t *player = create_sample_player("Steve", 70, 4);
-    quest_ctx_t *qctx = quest_ctx_new(player, test_hash_table);
-    start_quest(quest1, qctx);
-    start_quest(quest2, qctx);
-
-    printf("%s", show_quests(player));
 }
 
 /*test for add task when 2 unique task_ID */ 
