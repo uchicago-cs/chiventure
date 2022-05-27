651981d85ffd5511fb7cad1f4265b6a480befb49
William Hu <williamhu2025@uchicago.edu>
2022-05-27 16:20:15 -0500

    Add more tests for autogeneration

--- tests/openworld/test_game_autogenerate.c
+++ tests/openworld/test_game_autogenerate.c
@@ -9,7 +9,7 @@
 
 /* Tests the functions in game_autogenerate.h */
 
-Test(autogenerate, game_autogenerate_static){
+Test(autogenerate, game_autogenerate_static_random_first_room){
 
     game_t *game=game_new("New Game");
 
@@ -48,4 +48,87 @@ Test(autogenerate, game_autogenerate_static){
     int rc=game_autogenerate_static(game, context, 5, "pickrandomly");
 
     cr_assert_eq(rc, SUCCESS, "failed to autogenerate game with a randomly chosen first room\n");
+}
+
+
+Test(autogenerate, game_autogenerate_static_specific_first_room){
+
+    game_t *game=game_new("New Game");
+
+    roomspec_t *spec1 = roomspec_new("room_name1", "short desc1", "long desc1", NULL);
+    cr_assert_not_null(spec1, "failed to create new roomspec_t\n");
+    spec1->tag=0;
+
+    roomspec_t *spec2 = roomspec_new("room_name2", "short desc2", "long desc2", NULL);
+    cr_assert_not_null(spec2, "failed to create new roomspec_t\n");
+    spec2->tag=1;
+
+    roomspec_t *spec3 = roomspec_new("room_name3", "short desc3", "long desc3", NULL);
+    cr_assert_not_null(spec3, "failed to create new roomspec_t\n");
+    spec3->tag=2;
+    roomspec_t *roomspecs[3]={spec1, spec2, spec3};
+
+    int *matrix=(int*)malloc(9*sizeof(int));
+
+    matrix[0]=5;
+    matrix[1]=4;
+    matrix[2]=5; 
+    matrix[3]=0; 
+    matrix[4]=5;  
+    matrix[5]=3;
+    matrix[6]=4; 
+    matrix[7]=3;  
+    matrix[8]=5;
+
+    int **edges=edges_new(matrix, 3, 3);
+
+    specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
+
+    gencontext_t *context;
+    context->specgraph=specgraph;
+    
+    int rc=game_autogenerate_static(game, context, 5, "room_name1");
+
+    cr_assert_eq(rc, SUCCESS, "failed to autogenerate game with a specific first room\n");
+}
+
+Test(autogenerate, game_autogenerate_static_one_roomspec_game){
+
+    game_t *game=game_new("New Game");
+
+    roomspec_t *spec1 = roomspec_new("room_name1", "short desc1", "long desc1", NULL);
+    cr_assert_not_null(spec1, "failed to create new roomspec_t\n");
+    spec1->tag=0;
+
+    roomspec_t *spec2 = roomspec_new("room_name2", "short desc2", "long desc2", NULL);
+    cr_assert_not_null(spec2, "failed to create new roomspec_t\n");
+    spec2->tag=1;
+
+    roomspec_t *spec3 = roomspec_new("room_name3", "short desc3", "long desc3", NULL);
+    cr_assert_not_null(spec3, "failed to create new roomspec_t\n");
+    spec3->tag=2;
+    roomspec_t *roomspecs[3]={spec1, spec2, spec3};
+
+    int *matrix=(int*)malloc(9*sizeof(int));
+
+    matrix[0]=5;
+    matrix[1]=0;
+    matrix[2]=0; 
+    matrix[3]=0; 
+    matrix[4]=5;  
+    matrix[5]=3;
+    matrix[6]=4; 
+    matrix[7]=3;  
+    matrix[8]=5;
+
+    int **edges=edges_new(matrix, 3, 3);
+
+    specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
+
+    gencontext_t *context;
+    context->specgraph=specgraph;
+    
+    int rc=game_autogenerate_static(game, context, 5, "room_name1");
+
+    cr_assert_eq(rc, SUCCESS, "failed to autogenerate game with only one roomspec\n");
 }
