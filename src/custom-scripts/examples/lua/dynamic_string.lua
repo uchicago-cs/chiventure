f60e7991c4a1ca173ce76a6dceeea7bed5183f58
Ramarkob <Ramarkob@gmail.com>
2022-05-27 17:58:02 -0500

    Forgot to include new folders in last commit. Lua and WDL files are now in their own folders

---
+++ src/custom-scripts/examples/lua/dynamic_string.lua
@@ -0,0 +1,4 @@
+function foo(arg1)
+    if arg1 == '1' then return "../../../../src/custom-scripts/examples/wdl/demo-wizard.wdl" end
+    if arg1 == '2' then return "../../../../src/custom-scripts/examples/wdl/demo-warrior.wdl" end
+end 
