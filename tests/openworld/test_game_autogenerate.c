740b27e442548b40fb2ae6444575c5dc3f4a8450
William Hu <williamhu2025@uchicago.edu>
2022-05-28 14:20:30 -0500

    Where is the error

--- tests/openworld/test_game_autogenerate.c
+++ tests/openworld/test_game_autogenerate.c
@@ -96,7 +96,7 @@ Test(randomroom, random_room_from_game){
 
     cr_assert_eq((0!=tag) && (1!=tag) && (2!=tag), 0, "failed to autogenerate a random first room\n");    
 }
-/*
+
 Test(room, generate_room_in_game){
 
     game_t *game=game_new("New Game");
@@ -133,12 +133,13 @@ Test(room, generate_room_in_game){
     int **edges=edges_new(matrix, 3, 3);
 
     specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
+    cr_assert_eq(0, 1, "failed to autogenerate game with a randomly chosen first room\n");
 
     int rc=autogenerate_room_in_game(game, specgraph);
 
     cr_assert_eq(rc, SUCCESS, "failed to autogenerate game with a randomly chosen first room\n");
 }
-
+/*
 Test(game_autogenerate, game_autogenerate_static_random_first_room){
 
     game_t *game=game_new("New Game");
