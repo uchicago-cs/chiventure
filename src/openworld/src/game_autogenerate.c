76b205f37f78d78933f40479b0d88225eba02a2d
William Hu <williamhu2025@uchicago.edu>
2022-05-28 18:08:35 -0500

    Works up to here?

--- src/openworld/src/game_autogenerate.c
+++ src/openworld/src/game_autogenerate.c
@@ -214,6 +214,8 @@ int autogenerate_room_in_game(game_t* game, specgraph_t *specgraph){
 
     char* direction_to_new=(char*)malloc(6*sizeof(char));
     char* direction_to_curr=(char*)malloc(6*sizeof(char));
+    return SUCCESS;    
+
     int rc=pick_random_direction(game, curr, direction_to_curr, direction_to_new);
     
     if(rc==FAILURE){
@@ -223,7 +225,7 @@ int autogenerate_room_in_game(game_t* game, specgraph_t *specgraph){
 
     room_autogenerate(game, specgraph, curr, currspec, 
                       direction_to_curr, direction_to_new);
-    return SUCCESS;    
+
     path_autogenerate(game, specgraph, curr);
 
     free(roomspecs);
