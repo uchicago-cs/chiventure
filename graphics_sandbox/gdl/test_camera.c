9ef1e070ad0d23f19f3372ba8f821f2e3df9c1c5
Irene Madejski <lauraburakreis@IreneMadejski.local>
2022-05-27 22:42:15 -0500

    additional tests for structs

---
+++ graphics_sandbox/gdl/test_camera.c
@@ -0,0 +1,53 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "read_gdl.h"
+
+/* Checks that new_camera() properly mallocs and inits a new camera struct*/
+Test(camera, new)
+{
+    unsigned int width = 300;
+    unsigned int height = 300;
+
+    camera_t *camera;
+    camera = new_camera(width,height);
+
+    cr_assert_not_null(camera, "make_camera() failed");
+
+    cr_assert_eq(camera->width, 300, "make_camera() didn't set width");
+    cr_assert_eq(camera->height, 300, "make_camera() didn't set height");
+}
+
+/* Checks that init_camera() properly inits values for camera struct */
+Test(camera, init)
+{
+    unsigned int width = 300;
+    unsigned int height = 300;
+
+    camera_t camera;
+    int rc;
+
+    rc = init_camera(&camera, width, height);
+
+    cr_assert_eq(rc, SUCCESS, "init_camera() failed");
+
+    cr_assert_eq(camera.width,300,"init_camera() didn't set width");
+    cr_assert_eq(camera.height,300,"init_camera() didn't set height");
+}
+
+/* Tests taht free_camera properly frees a heap allocated camera struct */
+Test(camera, free)
+{
+    unsigned int width = 300;
+    unsigned int height = 300;
+    int rc;
+
+    camera_t *camera;
+    camera = new_camera(width,height);
+
+    cr_assert_not_null(camera, "make_camera() failed");
+
+    rc = free_camera(camera);
+
+    cr_assert_eq(rc, SUCCESS, "free_camera() failed");
+}
---
+++ graphics_sandbox/gdl/test_dimensions.c
@@ -0,0 +1,60 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "read_gdl.h"
+
+/*Checks that new_display_dimensions() properly mallocs and inits a new
+ * display_dimensions_t struct */
+Test(display_dimensions, new)
+{
+    unsigned int width = 500;
+    unsigned int height = 400;
+
+    display_dimensions_t *dimensions;
+    dimensions =  new_display_dimensions(width,height);
+
+    cr_assert_not_null(dimensions, "make_display_dimensions() failed");
+
+    cr_assert_eq(dimensions->width,500,"make_display_dimensions() didn't set width");
+    cr_assert_eq(dimensions->height,400,"make_display_dimensions() didn't set height");
+}
+
+/* Checks that init_display_dimensions properly inits values for a 
+ * display_dimensions_t struct */
+Test(display_dimensions, init)
+{
+    unsigned int width = 500;
+    unsigned int height = 400;
+
+    int rc;
+    display_dimensions_t dimensions;
+
+    rc = init_display_dimensions(&dimensions,width,height);
+
+    cr_assert_eq(rc, SUCCESS, "init_display_dimensions() failed");
+
+    cr_assert_eq(dimensions.width,500,
+                "init_display dimensions didn't set width");
+    cr_assert_eq(dimensions.height,400,
+                "init_display_dimensions didn't set height");
+}
+
+
+/*Checks that free_display_dimensions() properly frees memory used for 
+ * display_dimensions_t struct */
+Test(display_dimensions, free)
+{
+    unsigned int width = 500;
+    unsigned int height = 400;
+    int rc;
+
+    display_dimensions_t *dimensions;
+    dimensions = new_display_dimensions(width,height);
+
+    cr_assert_not_null(dimensions, "make_display_dimensions() failed");
+
+    rc = free_display_dimensions(dimensions);
+
+    cr_assert_eq(rc,SUCCESS,"free_display_dimensions() failed");
+}
+
