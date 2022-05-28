629b91d7c2fbaa34b377c437a031af146f01e307
Jonathan Alfred Roven <jroven@linux5.cs.uchicago.edu>
2022-05-27 23:01:32 -0500

    #1657 IT WORKS git add -ugit add -u! :)))))))

--- src/battle/src/battle_logic.c
+++ src/battle/src/battle_logic.c
@@ -269,11 +269,11 @@ class_item_stat_multipliers_t* class_multipliers(class_t* class, battle_item_t* 
     if (strncmp(class->name, warrior, 8) == 0) {
         if (strncmp(item->name, strength_up, 12) == 0) {
             mults->phys_atk = 1.5; //1.5
-        }/*
+        }
         if (strcmp(item->name, "Defense Up") == 0) {
             mults->phys_def = 1; //1.2
-        }*/
-    }/*
+        }
+    }
     if (strcmp(class->name, "wizard") == 0) {
         if (strcmp(item->name, "Strength Up") == 0) {
             mults->phys_atk = 1; //0.8
@@ -297,7 +297,7 @@ class_item_stat_multipliers_t* class_multipliers(class_t* class, battle_item_t* 
         if (strcmp(item->name, "Healing Potion") == 0) {
             mults->hp = 1; //1.2
         }
-    }*/
+    }
 
     return mults;
 }
