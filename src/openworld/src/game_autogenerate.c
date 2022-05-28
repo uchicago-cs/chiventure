8c92ba00829be81b24724f1e25d96e527ffe8e33
William Hu <williamhu2025@uchicago.edu>
2022-05-27 19:44:49 -0500

    Add char* malloc

--- src/openworld/src/game_autogenerate.c
+++ src/openworld/src/game_autogenerate.c
@@ -212,8 +212,8 @@ int autogenerate_room_in_game(game_t* game, specgraph_t *specgraph){
     roomspec_t *currspec=(roomspec_t*)malloc(sizeof(roomspec_t));
     currspec=roomspecs[curr->tag];
 
-    char* direction_to_new;
-    char* direction_to_curr;
+    char* direction_to_new=(char*)malloc(6*sizeof(char));
+    char* direction_to_curr(char*)malloc(6*sizeof(char));
     int rc=pick_random_direction(game, curr, direction_to_curr, direction_to_new);
     
     if(rc==FAILURE){
