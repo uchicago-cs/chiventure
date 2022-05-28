f0c62cac9a95f60d4c1189afdadc8c94873698f2
Lukas Heinrich Grunzke <lukasg@linux6.cs.uchicago.edu>
2022-05-27 23:56:34 -0500

    Revert "commenting out parts of test1"
    This reverts commit 72f48e02f25e9ee84dc8e3a48f2603fbc2e79a4c.

--- tests/openworld/test_game_autogenerate.c
+++ tests/openworld/test_game_autogenerate.c
@@ -114,7 +114,6 @@ Test(room, generate_room_in_game){
     spec3->tag=2;
     roomspec_t **roomspecs=(roomspec_t**)malloc(3*sizeof(roomspec_t*));
 
-    /*
     roomspecs[0]=spec1;
     roomspecs[1]=spec2;
     roomspecs[2]=spec3;
@@ -138,7 +137,6 @@ Test(room, generate_room_in_game){
     int rc=autogenerate_room_in_game(game, specgraph);
 
     cr_assert_eq(rc, SUCCESS, "failed to autogenerate game with a randomly chosen first room\n");
-    */
 }
 
 /*
