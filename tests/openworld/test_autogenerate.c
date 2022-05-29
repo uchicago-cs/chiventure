f25580b9a103509d316ec77fac909492078ec15f
William Hu <williamhu2025@uchicago.edu>
2022-05-29 10:45:11 -0500

    Finding where test fails

--- tests/openworld/test_autogenerate.c
+++ tests/openworld/test_autogenerate.c
@@ -377,13 +377,14 @@ Test(room, room_generate_success_zero){
     matrix[7]=3;  
     matrix[8]=5;
 //1
+    cr_assert_eq(0, 1, "failed to generate room\n");
+
     int **edges=edges_new(matrix, 3, 3);
 //2
     specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
 //3
     random_first_room(game, specgraph);
 //4
-    cr_assert_eq(rc, 1, "failed to generate room\n");
     room_t *room=random_room_from_game(game);
 
     int rc;
