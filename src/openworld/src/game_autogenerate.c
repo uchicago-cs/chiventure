ce94be9e6b08cf9f09b27116de3fbb7dac0ebcb5
Lukas Heinrich Grunzke <lukasg@linux7.cs.uchicago.edu>
2022-05-29 15:05:05 -0500

    tryingn something

--- src/openworld/src/game_autogenerate.c
+++ src/openworld/src/game_autogenerate.c
@@ -57,7 +57,9 @@ int random_first_room(game_t* game){
 room_t* random_room_from_game(game_t* game){
 
 //Function seems to be failing right below this line
-    return NULL;
+    // int num = 5;
+    room_t *room = (room_t*)malloc(sizeof(room_t));
+    return room;
     // int num_rooms=HASH_COUNT(game->all_rooms);
     // int num_rooms = game->specgraph->num
     /*
