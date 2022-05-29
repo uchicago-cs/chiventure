0cd8ee5ab5bf5ee5ecac646d3b7cd797d9995b76
William Hu <williamhu2025@uchicago.edu>
2022-05-28 19:43:29 -0500

    Finding where the tests crashes

--- src/openworld/src/autogenerate.c
+++ src/openworld/src/autogenerate.c
@@ -241,10 +241,11 @@ int room_generate(game_t *game, room_t *curr, roomspec_t *rspec_new,
     }
 
     coords_t *coords=coords_new(x, y);    
-    return SUCCESS;
+
     /* create new combination of rooms/items from randomly picked roomspec
     Adds one generated room from the head of specgraph only */
     room_t *new_room = roomspec_to_room(rspec_new, coords);
+    return SUCCESS; 
     assert(add_room_to_game(game, new_room) == SUCCESS);
 
     free(coords);
