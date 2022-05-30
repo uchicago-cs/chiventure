bff3e38046b02a6fba33a4e558da3521f997ad5b
William Hu <williamhu2025@uchicago.edu>
2022-05-30 16:20:10 -0500

    Fixing typo in roomspec correlation test

--- tests/openworld/test_structs.c
+++ tests/openworld/test_structs.c
@@ -554,19 +554,19 @@ Test(specgraph, correlation)
     int correlation2=roomspec_correlation(specgraph, spec1, spec2);
     cr_assert_eq(correlation2, 4, "failed to calculate correlation between spec1 and spec2\n");
     int correlation3=roomspec_correlation(specgraph, spec1, spec3);
-    cr_assert_eq(correlation2, 5, "failed to calculate correlation between spec1 and spec3\n");      
+    cr_assert_eq(correlation3, 5, "failed to calculate correlation between spec1 and spec3\n");      
     int correlation4=roomspec_correlation(specgraph, spec2, spec1);
-    cr_assert_eq(correlation1, 0, "failed to calculate correlation between spec2 and spec1\n");
+    cr_assert_eq(correlation4, 0, "failed to calculate correlation between spec2 and spec1\n");
     int correlation5=roomspec_correlation(specgraph, spec2, spec2);
-    cr_assert_eq(correlation2, 5, "failed to calculate correlation between spec2 and spec2\n");
+    cr_assert_eq(correlation5, 5, "failed to calculate correlation between spec2 and spec2\n");
     int correlation6=roomspec_correlation(specgraph, spec2, spec3);
-    cr_assert_eq(correlation2, 3, "failed to calculate correlation between spec2 and spec3\n");      
+    cr_assert_eq(correlation6, 3, "failed to calculate correlation between spec2 and spec3\n");      
     int correlation7=roomspec_correlation(specgraph, spec3, spec1);
-    cr_assert_eq(correlation1, 4, "failed to calculate correlation between spec3 and spec1\n");
+    cr_assert_eq(correlation7, 4, "failed to calculate correlation between spec3 and spec1\n");
     int correlation8=roomspec_correlation(specgraph, spec3, spec2);
-    cr_assert_eq(correlation2, 3, "failed to calculate correlation between spec3 and spec2\n");
+    cr_assert_eq(correlation8, 3, "failed to calculate correlation between spec3 and spec2\n");
     int correlation9=roomspec_correlation(specgraph, spec3, spec3);
-    cr_assert_eq(correlation2, 5, "failed to calculate correlation between spec3 and spec3\n");      
+    cr_assert_eq(correlation9, 5, "failed to calculate correlation between spec3 and spec3\n");      
 }
 
 /* Tests the roomlevel_new function to validate that a roomlevel
