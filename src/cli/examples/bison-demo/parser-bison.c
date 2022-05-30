d551af3cf61170a551e509e892fd51dfe87b7912
Brianna Lingbao Zeng <bzeng@linux2.cs.uchicago.edu>
2022-05-30 17:47:18 -0500

    Deleted comments

--- src/cli/examples/bison-demo/parser-bison.c
+++ src/cli/examples/bison-demo/parser-bison.c
@@ -5,35 +5,6 @@
 #include "cli/cmd.h"
 #include "cli/parser.h"
 #include "common/utlist.h"
-/*#include <criterion/criterion.h>
-#include <string.h>
-#include <ui/ui.h>
-#include "game-state/game.h"
-#include "common/ctx.h"
-*/
-/* Creates a chiventure context with a sample game
- *
- * Parameters: None
- *
- * Returns: a chiventure context
- */
-/*
-chiventure_ctx_t *create_sample_ctx()
-{
-    game_t *game = game_new("Welcome to Chiventure!");
-    room_t *room1 = room_new("room1", "This is room 1", "Verily, this is the first room.");
-    room_t *room2 = room_new("room2", "This is room 2", "Truly, this is the second room.");
-    add_room_to_game(game, room1);
-    add_room_to_game(game, room2);
-    game->curr_room = room1;
-    create_connection(game, "room1", "room2", "NORTH");
-*/
-    /* Create context */
-/*    chiventure_ctx_t *ctx = chiventure_ctx_new(game);
-
-    return ctx;
-}
-*/
 
 /* create an linked list item of a phrase */
 word_ll* start_phrase(char *word) {
