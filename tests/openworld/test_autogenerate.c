120235eb6d341eb4fb9dc21ea97244768b993fe0
William Hu <williamhu2025@uchicago.edu>
2022-05-30 15:24:10 -0500

    Add test descriptions and more tests for helper

--- tests/openworld/test_autogenerate.c
+++ tests/openworld/test_autogenerate.c
@@ -119,7 +119,65 @@ Test(autogenerate, path_exists_in_direction_mid)
     cr_assert_eq(false, path_exists_in_direction(room, "NOT A DIRECTION"), "Expected false but got true");
 }
 
-Test(autogenerate, room_exists_in_direction_false2){
+/* Checks that room_exists_in_direction outputs false when a room does not exist in a given direction */
+Test(autogenerate, room_exists_in_direction_false){
+
+    game_t *game=game_new("New Game");
+
+    roomspec_t *spec1 = roomspec_new("room_name1", "short desc1", "long desc1", NULL);
+    cr_assert_not_null(spec1, "failed to create new roomspec_t\n");
+    spec1->tag=0;
+
+    roomspec_t *spec2 = roomspec_new("room_name2", "short desc2", "long desc2", NULL);
+    cr_assert_not_null(spec2, "failed to create new roomspec_t\n");
+    spec2->tag=1;
+
+    roomspec_t *spec3 = roomspec_new("room_name3", "short desc3", "long desc3", NULL);
+    cr_assert_not_null(spec3, "failed to create new roomspec_t\n");
+    spec3->tag=2;
+    roomspec_t **roomspecs=(roomspec_t**)malloc(3*sizeof(roomspec_t*));
+
+    roomspecs[0]=spec1;
+    roomspecs[1]=spec2;
+    roomspecs[2]=spec3;
+
+    int *matrix=(int*)malloc(9*sizeof(int));
+
+    matrix[0]=5;
+    matrix[1]=4;
+    matrix[2]=5; 
+    matrix[3]=0; 
+    matrix[4]=5;  
+    matrix[5]=3;
+    matrix[6]=4; 
+    matrix[7]=3;  
+    matrix[8]=5;
+
+    int **edges=edges_new(matrix, 3, 3);
+
+    specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
+
+    coords_t *coords1=coords_new(0,0);
+    coords_t *coords2=coords_new(1,0);    
+    room_t *room1=roomspec_to_room(spec1, coords1);
+    room_t *room1=roomspec_to_room(spec2, coords2);
+
+    add_room_to_game(game, room1);
+    add_room_to_game(game, room2);
+
+    int rc;
+    char* direction_to_new;
+    char* direction_to_curr;
+
+    rc=room_exists_in_direction(game, room1, "east");
+    cr_assert_eq(rc, false, "failed to determine if a room exists north of the current room\n");
+
+    rc=room_exists_in_direction(game, room2, "west");
+    cr_assert_eq(rc, false, "failed to determine if a room exists south of the current room\n");
+}
+
+/* Checks that room_exists_in_direction outputs true when a room does exist in a given direction */
+Test(autogenerate, room_exists_in_direction_false){
 
     game_t *game=game_new("New Game");
 
@@ -242,6 +300,7 @@ Test(autogenerate, roomspec_to_room3)
 
 }
 
+/* Checks that when an open direction exists, pick_random_direction successfully outputs a direction*/
 Test(autogenerate, pick_random_direction){
 
     game_t *game=game_new("New Game");
