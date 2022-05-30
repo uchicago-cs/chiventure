5b46af8cb957e505ab954a45e8b216c91596f17c
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-29 21:31:17 -0500

    fixing tests

--- tests/custom-scripts/Lua_file/3_arg_lua.lua
+++ tests/custom-scripts/Lua_file/3_arg_lua.lua
@@ -1,4 +1,3 @@
 function foo(arg1, arg2, arg3)
-  if arg1 == 'b' and arg2 == 25 and arg3 == false then return tonumber(199) 
-  else return tonumber(tonumber(30) * tonumber(4)) end
+  return 100
 end
