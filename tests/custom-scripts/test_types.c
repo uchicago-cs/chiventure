de2ff3919a21f2b17793957be3c5fb5f9f158a9d
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-30 00:22:38 -0500

    removing extraneous files

--- tests/custom-scripts/test_types.c
+++ tests/custom-scripts/test_types.c
@@ -275,13 +275,13 @@ Test(custom_type, obj_t_get_bool_lua_args)
     ot3 = obj_add_arg(obj_add_arg(obj_add_arg(ot3, data, BOOL_TYPE), data2, BOOL_TYPE), data, BOOL_TYPE);
     data_t got3 = arg_t_get(ot3);
     bool rv3 = got3.b;
-    cr_assert_eq((rv3 ? 1 : 0), 0, "bool_t_get 2: failed bool Lua retrieval");
+    cr_assert_eq((rv3 ? 1 : 0), 0, "bool_t_get 3: failed bool Lua retrieval");
 
     object_t *ot4 = obj_t_init(data, BOOL_TYPE, "../../../tests/custom-scripts/Lua_file/bool_test_args.lua");
     ot4 = obj_add_arg(obj_add_arg(obj_add_arg(ot4, data, BOOL_TYPE), data2, BOOL_TYPE), data, BOOL_TYPE);
     data_t got4 = arg_t_get(ot4);
     bool rv4 = got4.b;
-    cr_assert_eq((rv4 ? 1 : 0), 0, "bool_t_get 2: failed bool Lua retrieval");
+    cr_assert_eq((rv4 ? 1 : 0), 0, "bool_t_get 4: failed bool Lua retrieval");
 }
 
 /** 
