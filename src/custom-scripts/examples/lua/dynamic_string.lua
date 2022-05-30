beac0bf7f7dc026a7d9f7a04140494148e5f60c6
tatmaca <72895724+tatmaca@users.noreply.github.com>
2022-05-30 00:19:46 -0500

    Delete dynamic_string.lua

--- src/custom-scripts/examples/lua/dynamic_string.lua
+++
@@ -1,4 +0,0 @@
-function foo(arg1)
-    if arg1 == '1' then return "../../../../src/custom-scripts/examples/wdl/demo-wizard.wdl" end
-    if arg1 == '2' then return "../../../../src/custom-scripts/examples/wdl/demo-warrior.wdl" end
-end 
