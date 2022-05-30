8f13c6fbe7b4c58e5c36c768d30bf4b907e6c125
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-29 22:00:11 -0500

    further debugging for tests

--- tests/custom-scripts/test_types.c
+++ tests/custom-scripts/test_types.c
@@ -447,19 +447,18 @@ Test(custom_type, lua_three_args_diff_types)
  */
 Test(custom_type, lua_three_args_diff_types_interact)
 {
-    data.s = "";
-    data2.c = 'b';
-    data3.i = 25;
-    data4.b = false;
+    data.i = 99;
+    data2.i = 5;
+    data3.i = 10;
+    data4.i = 5;
 
     data.c = 'X';
     data2.c = 'Y';
 
-    object_t *ot = obj_t_init(data, STR_TYPE, 
-        "../../../tests/custom-scripts/Lua_file/arg_3_lua.lua");
-    ot = obj_add_arg(obj_add_arg(obj_add_arg(ot, data2, CHAR_TYPE), data3, INT_TYPE), data4, BOOL_TYPE);
-    
+    object_t *ot = obj_t_init(data, INT_TYPE, 
+        "../../../tests/custom-scripts/Lua_file/int_test_args.lua");
+    ot = obj_add_arg(obj_add_arg(ot, data4, INT_TYPE), data3, INT_TYPE);
     data_t got = arg_t_get(ot);
     int rv = got.i;
-    cr_assert_eq(rv, 100, "obj_t_get_int 1: failed int direct retrieval with value %u", rv);
+    cr_assert_eq(rv, 15, "obj_t_get_int 1: failed int direct retrieval with value %u", rv);
 }
