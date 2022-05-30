7032b3ffd6385788379eb7b7bb0185b13f5d9c30
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-30 00:43:34 -0500

    readding files

---
+++ tests/custom-scripts/Lua_file/arg_3_lua.lua
@@ -0,0 +1,5 @@
+function foo(arg1, arg2, arg3)
+  if arg1 == 'b' and arg2 == 25 and arg3 == false then return 199 else return 1 end
+  if arg1 == 'b' and arg2 == 25 and arg3 == false then return tonumber(199) 
+  else return tonumber(arg2) * 4 end
+end
