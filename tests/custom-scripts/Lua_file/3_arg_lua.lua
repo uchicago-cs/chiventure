7499c1c538c8dc8fe9be1f87e1244bd9b7f5674c
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-29 21:26:36 -0500

    debugging tests for data types

--- tests/custom-scripts/Lua_file/3_arg_lua.lua
+++ tests/custom-scripts/Lua_file/3_arg_lua.lua
@@ -1,4 +1,4 @@
 function foo(arg1, arg2, arg3)
-  if arg1 == 'b' and arg2 == 25 and arg3 == false then return 199 
-  else return 25 * 4 end
+  if arg1 == 'b' and arg2 == 25 and arg3 == false then return tonumber(199) 
+  else return tonumber(tonumber(30) * tonumber(4)) end
 end
