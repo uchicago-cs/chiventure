c6e0f4228b8630e521ace239b0f724878531e8fd
Lukas Heinrich Grunzke <lukasg@linux5.cs.uchicago.edu>
2022-05-30 17:28:20 -0500

    commented out supposedly failing line

--- tests/openworld/test_autogenerate.c
+++ tests/openworld/test_autogenerate.c
@@ -485,9 +485,10 @@ Test(room, room_generate_success_zero){
     int **edges=edges_new(matrix, 3, 3);
     specgraph_t *specgraph = specgraph_new(3, roomspecs, edges);
     random_first_room(game);
-    /*
+    
 //Test fails due to this line
     room_t *room=random_room_from_game(game);
+    /*
     cr_assert_eq(0, 1, "failed to generate room\n");
     int rc;
     char *direction_to_curr;
