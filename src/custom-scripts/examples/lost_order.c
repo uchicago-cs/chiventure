942140a2680c81311fc820b96da988d854587b0c
Ramarkob <Ramarkob@gmail.com>
2022-05-29 22:57:58 -0500

    updated lost_order

--- src/custom-scripts/examples/lost_order.c
+++ src/custom-scripts/examples/lost_order.c
@@ -19,6 +19,7 @@ chiventure_ctx_t *create_sample_ctx()
 {
     game_t *game = game_new("Welcome to Chiventure!");
 
+
     /* Create two rooms (room1 and room2). room1 is the initial room */
     room_t *room0 = room_new("room0", "This is room 0", "Here, our journey begins.");
     room_t *room1 = room_new("room1", "This is room 1", "Verily, this is the first room.");
@@ -32,83 +33,61 @@ chiventure_ctx_t *create_sample_ctx()
     add_room_to_game(game, room5);
     game->curr_room = room0;
     create_connection(game, "room0", "room1", "NORTH");
-    create_connection(game, "room1", "room0", "SOUTH");
     create_connection(game, "room0", "room2", "EAST");
-    create_connection(game, "room2", "room0", "WEST");
     create_connection(game, "room0", "room3", "SOUTH");
-    create_connection(game, "room3", "room0", "NORTH");
-    create_connection(game, "room0", "room5", "WEST");
-    create_connection(game, "room5", "room0", "EAST");
+    create_connection(game, "room0", "room4", "WEST");
     create_connection(game, "room3", "room5", "SOUTH");
-    create_connection(game, "room5", "room3", "NORTH");
+
 
 
     /* Create a rock in room1 */
-    item_t *Lost_McDonalds_Order = item_new("McDonalds?","It seems to be an order of McDonalds.",
+    item_t *Lost_McDonalds_Order_item = item_new("McDonalds?","It seems to be an order of McDonalds.",
                    "It seems like someone didn't get their order... Hopefully it can get back to them!");
-    add_item_to_room(room1, Lost_McDonalds_Order);
+    add_item_to_room(room1, Lost_McDonalds_Order_item);
 
-    item_t *Lost_Wingstop_Order = item_new("Wingstop?","It seems to be an order of Wingstop.",
+    item_t *Lost_Wingstop_Order_item = item_new("Wingstop?","It seems to be an order of Wingstop.",
                    "It seems like someone didn't get their order... Hopefully it can get back to them!");
-    add_item_to_room(room2, Lost_Wingstop_Order);
+    add_item_to_room(room2, Lost_Wingstop_Order_item);
 
-    item_t *Lost_Subway_Order = item_new("Subway?","It seems to be an order of Subway.",
+    item_t *Lost_Subway_Order_item = item_new("Subway?","It seems to be an order of Subway.",
                    "It seems like someone didn't get their order... Hopefully it can get back to them!");
-    add_item_to_room(room3, Lost_Subway_Order);
+    add_item_to_room(room3, Lost_Subway_Order_item);
 
     /* Where custom_type comes into play, create a dynamic string (hold different values) depending
        on what the user enters at the start of the game */
-    data_t d1init, d2init, d3init;
-    data_t d1, d2, d3, mcd, wng, sub;
-    data_t rv2, rv3;
-
-    d1.s = (char*)malloc(sizeof(char) * 500);
-    d2.s = (char*)malloc(sizeof(char) * 500);
-    d3.s = (char*)malloc(sizeof(char) * 500);
-
-    mcd.s = "McDonalds";
-    wng.s = "Wingstop";
-    sub.s = "Subway";
-
     char* name;
-    name = (char*)malloc(sizeof(char) * 500);
     printf("Please enter your name: ");
-    scanf("%s", name);  
+    scanf("%s", &name);  
+    
+    object_t *ot1 = obj_t_str("", "../../../../src/custom-scripts/sandbox/lost_order.lua");
+    ot1 = obj_add_arg_str(obj_add_arg_str(ot1, name), "McDonalds");
+    char* custom_string1 = (char*)malloc(300);
+    custom_string1 = str_t_get(ot1);
 
-    strcpy(d1.s, name);
-    strcpy(d2.s, name);
-    strcpy(d3.s, name);
+    object_t *ot2 = obj_t_str("", "../../../../src/custom-scripts/sandbox/lost_order.lua");
+    ot2 = obj_add_arg_str(obj_add_arg_str(ot2, name), "Wingstop");
+    char* custom_string2 = (char*)malloc(300);
+    custom_string2 = str_t_get(ot2);
 
-    object_t *ot1 = obj_t_init(d1init, STR_TYPE, "../../../../src/custom-scripts/examples/lost_order.lua");
-    ot1 = obj_add_arg(obj_add_arg(ot1, d1, STR_TYPE), mcd, STR_TYPE);
-    char* custom_string1 = (char*)malloc(sizeof(char*) * 500);
-    data_t rv1 = arg_t_get(ot1);
-    custom_string1 = rv1.s;
+    object_t *ot3 = obj_t_str("", "../../../../src/custom-scripts/sandbox/lost_order.lua");
+    ot3 = obj_add_arg_str(obj_add_arg_str(ot3, name), "Subway");
+    char* custom_string3 = (char*)malloc(300);
+    custom_string3 = str_t_get(ot3);
 
-    object_t *ot2 = obj_t_init(d2init, STR_TYPE, "../../../../src/custom-scripts/examples/lost_order2.lua");
-    ot2 = obj_add_arg(obj_add_arg(ot2, d2, STR_TYPE), wng, STR_TYPE);
-    char* custom_string2 = (char*)malloc(sizeof(char*) * 500);
-    rv2 = arg_t_get(ot2);
-    custom_string2 = rv2.s;
-
-    object_t *ot3 = obj_t_init(d3init, STR_TYPE, "../../../../src/custom-scripts/examples/lost_order3.lua");
-    ot3 = obj_add_arg(obj_add_arg(ot3, d3, STR_TYPE), sub, STR_TYPE);
-    char* custom_string3 = (char*)malloc(sizeof(char*) * 500);
-    rv3 = arg_t_get(ot3);
-    custom_string3 = rv3.s;
 
     /* Associate action "INSPECT" with each order.
      * It has no conditions, so it should succeed unconditionally. */
-    add_action(Lost_McDonalds_Order, "INSPECT", custom_string1, "It smells greasy!");
+    agent_t Lost_McDonalds_Order = (agent_t){.item = Lost_McDonalds_Order_item, .npc = NULL};
+    add_action(&Lost_McDonalds_Order, "INSPECT", custom_string1, "It smells greasy!");
 
-    add_action(Lost_Wingstop_Order, "INSPECT", custom_string2, "It smells saucy!");
+    agent_t Lost_Wingstop_Order = (agent_t){.item = Lost_Wingstop_Order_item, .npc = NULL};
+    add_action(&Lost_Wingstop_Order, "INSPECT", custom_string2, "It smells saucy!");
 
-    add_action(Lost_Subway_Order, "INSPECT", custom_string3, "It smells... fresh?");
+    agent_t Lost_Subway_Order = (agent_t){.item = Lost_Subway_Order_item, .npc = NULL};
+    add_action(&Lost_Subway_Order, "INSPECT", custom_string3, "It smells... fresh?");
 
     /* Create context */
     chiventure_ctx_t *ctx = chiventure_ctx_new(game);
-
-    free(name);
 
     return ctx;
 }
