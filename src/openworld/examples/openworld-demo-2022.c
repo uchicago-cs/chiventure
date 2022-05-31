a26e4903dcfbfd45c8c424e36b9ea49d46e7bedc
Rohan Reddy Deme <rohandeme@linux2.cs.uchicago.edu>
2022-05-31 09:39:05 -0500

    Added comments

--- src/openworld/examples/openworld-demo-2022.c
+++ src/openworld/examples/openworld-demo-2022.c
@@ -2,7 +2,8 @@
  * This example program runs a full instance of chiventure with an in-memory game.
  * The CLI is monkey-patched to accept functions that serve to showcase room generation.
  * 
- *  Purpose of demo: Display versatility and efficiency of new specgraph_t struct
+ *  Purpose of demo: Display versatility and efficiency of new specgraph_t struct, especially
+ *  with respects to its edges attribute
  */
 
 #include <stdio.h>
@@ -24,12 +25,17 @@ roomspec_t *rspec_lvl1 = NULL;
 roomspec_t *rspec_lvl2 = NULL;
 roomspec_t *rspec_lvl3 = NULL;
 
+//Declaring the array that will contain all of the roomspecs
 roomspec_t** roomspecs = NULL;
 
+//EDGES: This will contain all of the edges for the specgraph
 int** edges;
 
+//SPECGRAPH: This will be the main specgraph struct that is used throughout the demo
 specgraph_t* specgraph;
 
+/* Initializes the sample, "in-memory", gencontext and other component structs. 
+   Uses placeholder variables declared above. */
 void initialize_sample_structs() {
     rspec_lvl0 = roomspec_new("library", "JCL", "John Crerar Library", NULL, 1);
     rspec_lvl1 = roomspec_new("dungeon", "Ryerson 251", "Where CS students switch majors to bizcon", NULL, 2);
@@ -53,16 +59,12 @@ void initialize_sample_structs() {
     specgraph = specgraph_new(4, roomspecs, edges);
 }
 
+/* Creates a sample in-memory game */
 chiventure_ctx_t *create_sample_ctx()
 {
     game_t *game = game_new("Welcome to Chiventure!");
 
     load_normal_mode(game);
-
-    /* Create room1, the initial room */
-    room_t *room1 = room_new("room1", "This is room 1", "This is the first room.");
-    add_room_to_game(game, room1);
-    game->curr_room = room1;
 
     /* Create context */
     chiventure_ctx_t *ctx = chiventure_ctx_new(game);
@@ -73,6 +75,8 @@ chiventure_ctx_t *create_sample_ctx()
 
 const char *banner = "THIS IS AN OPENWORLD EXAMPLE PROGRAM";
 
+/* Defines a new CLI operation that: 
+   Outputs current number of roomspecs within a specgraph. */
 char *count_rooms(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
     if (tokens[1] != NULL) {
         return "Please speak english! So uncivilized...";
@@ -84,7 +88,8 @@ char *count_rooms(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
     snprintf(buffer, OUTPUT_BUFFER_SIZE, "total number of rooms: %d", count);
     return buffer;
 }
-
+/* Defines a new CLI operation that: 
+   Outputs the list of the names of the rooms in roomspecs. */
 char *list_roomspecs(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
 {
     if (tokens[1] != NULL) {
@@ -98,6 +103,8 @@ char *list_roomspecs(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
     return buffer;
 }
 
+/* Defines a new CLI operation that: 
+   Outputs information about a specified room */
 char *room_info(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
     if (tokens[2] != NULL) {
         return "Please speak english! So uncivilized...";
@@ -119,6 +126,8 @@ char *room_info(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
     return buffer;
 }
 
+/* Defines a new CLI operation that: 
+   Outputs how likely two rooms are to generate next to each other. */
 char *relation(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
     if (tokens[3] != NULL) {
         return "Please speak english! So uncivilized...";
