59b6bcd959fa364c1ac81db7a371212e65ecc0a8
Harrison S Dong <harrisond@linux1.cs.uchicago.edu>
2022-05-31 01:32:03 -0500

    Demo Updated

--- src/action_management/examples/spring2022-demo.c
+++ src/action_management/examples/spring2022-demo.c
@@ -97,11 +97,11 @@ chiventure_ctx_t *create_sample_ctx()
     //Adding skills to skilltree and adding skilltree to class
     player->player_class = class_new("Vampire", "Vampire", "Feeds on Students",
                               NULL, NULL, NULL); //Add Stats Htbl to 2nd NULL?
-    skill_t *skill1 = skill_new(1002, ACTIVE, "Teach", "Educate the blossoming new generation", 10, 100, NULL, NULL);
-    skill_t *skill2 = skill_new(1002, ACTIVE, "Suck Blood", "No one knows how or when this happens", 10, 100, NULL, NULL);
-    skill_t *skill3 = skill_new(1002, ACTIVE, "Code", "Develop a new Heroku App", 10, 100, NULL, NULL);
-    skill_t *skill4 = skill_new(1002, ACTIVE, "Hire TA", "Spawn new senior software developers", 10, 100, NULL, NULL);
-    skill_t *skill5 = skill_new(1002, ACTIVE, "Tweet", "Call on alums to complain about software developemnt", 10, 100, NULL, NULL);
+    skill_t *skill1 = skill_new(1002, ACTIVE, "teach", "Educate the blossoming new generation", 10, 100, NULL, NULL);
+    skill_t *skill2 = skill_new(1002, ACTIVE, "suck blood", "No one knows how or when this happens", 10, 100, NULL, NULL);
+    skill_t *skill3 = skill_new(1002, ACTIVE, "code", "Develop a new Heroku App", 10, 100, NULL, NULL);
+    skill_t *skill4 = skill_new(1002, ACTIVE, "hire ta", "Spawn new senior software developers", 10, 100, NULL, NULL);
+    skill_t *skill5 = skill_new(1002, ACTIVE, "tweet", "Call on alums to complain about software developemnt", 10, 100, NULL, NULL);
     char **classes = malloc(sizeof(char*));
     classes[0] = "Borja";
     skill_node_t *skill_node1 = skill_node_new(skill1, 0, 0, classes, 0, 0);
@@ -109,22 +109,30 @@ chiventure_ctx_t *create_sample_ctx()
     skill_node_t *skill_node3 = skill_node_new(skill3, 0, 0, classes, 0, 0);
     skill_node_t *skill_node4 = skill_node_new(skill4, 0, 0, classes, 0, 0);
     skill_node_t *skill_node5 = skill_node_new(skill5, 0, 0, classes, 0, 0);
-    skill_tree_t *skill_tree = skill_tree_new(1001, "", 5);
-    skill_tree_node_add(skill_tree, skill_node1);
-    skill_tree_node_add(skill_tree, skill_node2);
-    skill_tree_node_add(skill_tree, skill_node3);
-    skill_tree_node_add(skill_tree, skill_node4);
-    skill_tree_node_add(skill_tree, skill_node5);
+    player->player_class->skilltree = skill_tree_new(1001, "Borja's Skills", 5);
+    skill_tree_node_add(player->player_class->skilltree, skill_node1);
+    skill_tree_node_add(player->player_class->skilltree, skill_node2);
+    skill_tree_node_add(player->player_class->skilltree, skill_node3);
+    skill_tree_node_add(player->player_class->skilltree, skill_node4);
+    skill_tree_node_add(player->player_class->skilltree, skill_node5);
 
     // add quests to player
-    quest_t *quest1 = quest_new("Give your first lecture", NULL, NULL);
-    quest_t *quest2 = quest_new("Perform vampire shenanigans", NULL, NULL);
-    quest_t *quest3 = quest_new("Hold Wednesday Office Hours", NULL, NULL);
-    task_t *task1 = task_new("Polish your teeth", NULL, NULL, NULL);
-    task_t *task2 = task_new("Suck blood", NULL, NULL, NULL);
-    task_t *task3 = task_new("Code", NULL, NULL, NULL);
-    mission_t *mission2 = mission_new("Polish only your sharp teeth for that shine", COLLECT_ITEM);
-    task1->mission = mission2;
+    player_quest_t *pquest1 = player_quest_new("wake up in the morning feeling like p diddy", 0);
+    player_quest_t *pquest2 = player_quest_new("perform vampire shenanigans", 0);
+    player_quest_t *pquest3 = player_quest_new("achieve retirement (finally)", 0);
+    player_task_t *ptask1 = player_task_new("polish your teeth", true);
+    quest_t *quest1 = quest_new("wake up in the morning feeling like p diddy", NULL, NULL);
+    quest_t *quest2 = quest_new("perform vampire shenanigans", NULL, NULL);
+    quest_t *quest3 = quest_new("achieve retirement (finally)", NULL, NULL);
+    task_t *task1 = task_new("polish your teeth", NULL, NULL, NULL);
+    task_t *task2 = task_new("give your first lecture", NULL, NULL, NULL);
+    task_t *task3 = task_new("hold wednesday office hours", NULL, NULL, NULL);
+    mission_t *mission1 = mission_new("polish only your sharp teeth for that shine", COLLECT_ITEM);
+    mission_t *mission2 = mission_new("enlighten students on the joys of continuous integration", COLLECT_ITEM);
+    mission_t *mission3 = mission_new("sit in office and hope no one comes in", COLLECT_ITEM);
+    task1->mission = mission1;
+    task2->mission = mission2;
+    task3->mission = mission3;
     task_tree_t *task_tree1 = malloc(sizeof(task_tree_t));
     task_tree_t *task_tree2 = malloc(sizeof(task_tree_t));
     task_tree_t *task_tree3 = malloc(sizeof(task_tree_t));
@@ -141,85 +149,86 @@ chiventure_ctx_t *create_sample_ctx()
     task_tree3->lmostchild = NULL;
     quest2->task_tree = task_tree1;
 
+    HASH_ADD_KEYPTR(hh, player->player_quests, pquest1->quest_id, strlen(pquest1->quest_id), pquest1);
+    HASH_ADD_KEYPTR(hh, player->player_quests, pquest2->quest_id, strlen(pquest2->quest_id), pquest2);
+    HASH_ADD_KEYPTR(hh, player->player_quests, pquest3->quest_id, strlen(pquest3->quest_id), pquest3);
     HASH_ADD_KEYPTR(hh, game->all_quests, quest1->quest_id, strlen(quest1->quest_id), quest1);
     HASH_ADD_KEYPTR(hh, game->all_quests, quest2->quest_id, strlen(quest2->quest_id), quest2);
     HASH_ADD_KEYPTR(hh, game->all_quests, quest3->quest_id, strlen(quest3->quest_id), quest3);
     
 
     // add items to inventory
-    item_t *sword = item_new("A sword", "A sword", "A sword");
+    item_t *sword = item_new("sword", "A sword", "A sword");
     add_item_to_player(player, sword, game);
 
-    item_t *laptop = item_new("Laptop ", "Laptop ", "Laptop ");
+    item_t *laptop = item_new("laptop", "Laptop ", "Laptop ");
     add_item_to_player(player, laptop, game);
 
-    item_t *glasses = item_new("Glasses", "Glasses",
+    item_t *glasses = item_new("glasses", "Glasses",
                       "Jinkies Borja can't see without these");
     add_item_to_player(player, glasses, game);
 
-    item_t *ring = item_new("Ring   ", "Ring   ", "Ring   ");
+    item_t *ring = item_new("ring", "Ring   ", "Ring   ");
     add_item_to_player(player, ring, game);
 
-    item_t *tape = item_new("Tape   ", "Tape   ", "Tape   ");
+    item_t *tape = item_new("tape", "Tape   ", "Tape   ");
     add_item_to_player(player, tape, game);
 
-    item_t *clock = item_new("Clock  ", "Clock  ", "Clock  ");
+    item_t *clock = item_new("clock", "Clock  ", "Clock  ");
     add_item_to_player(player, clock, game);
 
-    item_t *compass = item_new("Compass", "Compass", "Compass");
+    item_t *compass = item_new("compass", "Compass", "Compass");
     add_item_to_player(player, compass, game);
 
-    item_t *gum = item_new("Gum    ", "Gum    ", "Gum    ");
+    item_t *gum = item_new("gum", "Gum    ", "Gum    ");
     add_item_to_player(player, gum, game);
     
-    item_t *gas = item_new("Gas    ", "Gas    ", "Gas    ");
+    item_t *gas = item_new("gas", "Gas    ", "Gas    ");
     add_item_to_player(player, gas, game);
 
-    item_t *milk = item_new("Milk   ", "Milk   ", "Milk   ");
+    item_t *milk = item_new("milk", "Milk   ", "Milk   ");
     add_item_to_player(player, milk, game);
 
-    item_t *banana = item_new("Banana ", "Banana ", "Banana ");
+    item_t *banana = item_new("banana", "Banana ", "Banana ");
     add_item_to_player(player, banana, game);
 
-    item_t *pp_up = item_new("PP Up  ", "PP Up  ",
+    item_t *pp_up = item_new("pp up", "PP Up  ",
                     "It raises the PP of a selected move by 1/5 "
                     "of the move's base PP");
     add_item_to_player(player, pp_up, game);
 
-    item_t *pp_max = item_new("PP Max ", "PP Max ", "PP Max ");
+    item_t *pp_max = item_new("pp max", "PP Max ", "PP Max ");
     add_item_to_player(player, pp_max, game);
 
-    item_t *bands = item_new("Bands  ", "Bands  ", "Bands  ");
+    item_t *bands = item_new("bands", "Bands  ", "Bands  ");
     add_item_to_player(player, bands, game);
 
-    item_t *candy = item_new("Candy  ", "Candy  ", "Candy  ");
+    item_t *candy = item_new("candy", "Candy  ", "Candy  ");
     add_item_to_player(player, candy, game);
     
-    item_t *iron = item_new("Iron   ", "Iron   ", "Iron   ");
+    item_t *iron = item_new("iron", "Iron   ", "Iron   ");
     add_item_to_player(player, iron, game);
 
     // add items to room
-    item_t *soap = item_new("Soap", "Soap   ", "Soap   ");
+    item_t *soap = item_new("soap", "Soap   ", "Soap   ");
     add_item_to_room(room4, soap);
     agent_t *s = (agent_t*)(malloc(sizeof(agent_t)));
     s->item = soap; 
     add_action(s, "take", "You take the soap. It has cleansed you.", "You do not take the soap.");
 
-    item_t *rolex = item_new("Rolex", "Rolex  ", "Rolex  ");
+    item_t *rolex = item_new("rolex", "Rolex  ", "Rolex  ");
     add_item_to_room(room2, rolex); 
     agent_t *r = (agent_t*)(malloc(sizeof(agent_t)));
     r->item = rolex; 
     add_action(r, "take", "You take Borja's watch. It looks better on you.", 
                               "You're not clean enough to touch Borja's watch. Is there soap around here?");
 
-    item_t *end_item = item_new("Trophy", "Trophy ", "Trophy ");
+    item_t *end_item = item_new("trophy", "Trophy ", "Trophy ");
     add_item_to_room(room5, end_item); 
     agent_t *ei = (agent_t*)(malloc(sizeof(agent_t)));
     ei->item = end_item; 
     add_action(ei, "take", "You take the trophy. You win the demo!", 
                               "The trophy rebukes your touch. 'Too... broke...', it hisses.");
-    
-    player->player_class->skilltree = skill_tree;
     
     add_player_to_game(game, player);
     game->curr_player = player;
