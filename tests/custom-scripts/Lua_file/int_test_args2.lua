767dd5aa6ea879a8113af21c7b9ba1126783df88
Togay Brillant Atmaca <tatmaca@linux2.cs.uchicago.edu>
2022-05-31 16:14:58 -0500

    cleaning up tests folders and resolving merge conflicts

--- tests/custom-scripts/Lua_file/int_test_args2.lua
+++ tests/custom-scripts/Lua_file/int_test_args2.lua
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function foo(arg1, arg2, arg3)
     -- Arguments of an object don't have to be of the same type!
     if arg1 == 'b' and arg2 == 25 then return 100 end
 
     result = arg2
     return result
-=======
-function foo(arg1, arg2, arg3)
-  -- Arguments of an object don't have to be of the same type!
-  if arg1 == 'b' and arg2 == 25 then return 100 end
-
-  result = arg2
-  return result
->>>>>>> custom-scripts/new-tests-for-demo
 end
