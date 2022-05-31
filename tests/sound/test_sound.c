aa4d31ad0005ca1e1c28fb31754e62517c48db5d
Victor Jiao <40229970+jiaoboxuan@users.noreply.github.com>
2022-05-31 12:39:39 -0500

    Update test_sound.c
    Updated the path

--- tests/sound/test_sound.c
+++ tests/sound/test_sound.c
@@ -9,7 +9,7 @@
 /* Testing if sound_new works with background music */
 Test(sound, new_background)
 {
-    char song_name[] = "c'est_la_vie_khaled.mp3";
+    char song_name[] = "../../src/sound/sandbox/c'est_la_vie_khaled.mp3";
     sound_t *sound = sound_new(BACKGROUND, song_name);
 
      cr_assert_not_null(sound, "sound_new failed");
@@ -18,7 +18,7 @@ Test(sound, new_background)
 /* Testing if sound_new works with sound effects */
 Test(sound, new_sound_effect)
 {
-    char song_name[] = "wav_example.wav";
+    char song_name[] = "../../src/sound/sandbox/wav_example.wav";
     sound_t *sound = sound_new(SOUND_EFFECT, song_name);
     cr_assert_not_null(sound, "sound_new failed");
 }
