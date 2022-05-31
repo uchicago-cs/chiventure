fb2df0b4a80c91b17b879c8d1aeb0d16ef5dd647
Rohan Reddy Deme <rohandeme@linux2.cs.uchicago.edu>
2022-05-31 02:21:43 -0500

    This demo goin crazy

--- src/openworld/examples/openworld-demo-2022.c
+++ src/openworld/examples/openworld-demo-2022.c
@@ -14,7 +14,7 @@
 #include "game-state/path.h"
 
 
-#define OUTPUT_BUFFER_SIZE 100
+#define OUTPUT_BUFFER_SIZE 200
 
 /* ROOMSPECS
    Pre-declaring roomspecs like this is important as we need to preserve 
@@ -41,6 +41,16 @@ void initialize_sample_structs() {
     roomspecs[1] = rspec_lvl1;
     roomspecs[2] = rspec_lvl2;
     roomspecs[3] = rspec_lvl3;
+
+    int* array=(int*)malloc(16*sizeof(int));
+    for (unsigned int i = 0; i < 16; i++)
+    {
+        array[i] = 1;
+    }
+
+    edges = edges_new(array, 4, 4);
+
+    specgraph = specgraph_new(4, roomspecs, edges);
 }
 
 chiventure_ctx_t *create_sample_ctx()
@@ -63,14 +73,73 @@ chiventure_ctx_t *create_sample_ctx()
 
 const char *banner = "THIS IS AN OPENWORLD EXAMPLE PROGRAM";
 
+char *count_rooms(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
+    if (tokens[1] != NULL) {
+        return "Please speak english! So uncivilized...";
+    }
+
+    int count = specgraph->num_roomspecs;
+
+    static char buffer[OUTPUT_BUFFER_SIZE];
+    snprintf(buffer, OUTPUT_BUFFER_SIZE, "total number of rooms: %d", count);
+    return buffer;
+}
+
+char *list_roomspecs(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx)
+{
+    if (tokens[1] != NULL) {
+        return "Please speak english! So uncivilized...";
+    }
+    static char buffer[OUTPUT_BUFFER_SIZE];
+
+    snprintf(buffer, OUTPUT_BUFFER_SIZE, "1. %s\n2. %s\n3. %s\n4. %s", roomspecs[0]->room_name, roomspecs[1]->room_name,
+        roomspecs[2]->room_name, roomspecs[3]->room_name);
+
+    return buffer;
+}
+
+char *room_info(char *tokens[TOKEN_LIST_SIZE], chiventure_ctx_t *ctx) {
+    if (tokens[2] != NULL) {
+        return "Please speak english! So uncivilized...";
+    }
+    if (tokens[1] == NULL) {
+        return "Please pick a room to learn more about";
+    }
+
+    int index = atoi(tokens[1]) - 1;
+
+    if (index >= specgraph->num_roomspecs)
+    {
+        return "There aren't that many rooms";
+    }
+
+    static char buffer[OUTPUT_BUFFER_SIZE];
+    snprintf(buffer, OUTPUT_BUFFER_SIZE, "Room Name: %s || Short Description: %s || Long Description: %s", 
+        roomspecs[index]->room_name, roomspecs[index]->short_desc, roomspecs[index]->long_desc);
+    return buffer;
+}
+
 int main(int argc, char **argv)
 {   
+
+    initialize_sample_structs();
+
     chiventure_ctx_t *ctx = create_sample_ctx();
+
+    add_entry("COUNT", count_rooms, NULL, ctx->cli_ctx->table);
+    add_entry("ROOMS", list_roomspecs, NULL, ctx->cli_ctx->table);
+    add_entry("ROOMINFO", room_info, NULL, ctx->cli_ctx->table);
     
     /* Start chiventure */
     start_ui(ctx, banner);
 
     game_free(ctx->game);
+
+    if (rspec_lvl0 == NULL) {
+        printf("Failed\n");
+    }
+
+    printf("%s\n", roomspecs[1]->short_desc);
 
     return 0;
 }
